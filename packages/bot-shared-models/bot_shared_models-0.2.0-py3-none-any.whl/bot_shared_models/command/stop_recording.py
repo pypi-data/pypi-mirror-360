daf2dc6fd6c75a733655dbaccf9998f76777f56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:59 +0000

    Add package bot-shared-models/0.2.0/bot_shared_models-0.2.0-py3-none-any.whl

---
+++ packages/bot-shared-models/bot_shared_models-0.2.0-py3-none-any.whl/bot_shared_models/command/stop_recording.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+from pydantic import Field, BeforeValidator
+from .base import CommandType, BaseCommand
+
+
+class StopRecording(BaseCommand):
+    type: Annotated[
+        CommandType, BeforeValidator(lambda _: CommandType.STOP_RECORDING)
+    ] = Field(init=False, default=CommandType.STOP_RECORDING, frozen=True)
