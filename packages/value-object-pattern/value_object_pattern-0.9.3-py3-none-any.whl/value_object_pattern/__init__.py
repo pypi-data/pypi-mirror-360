e6aadc2abbe03c4e32b75d34ba7ed8f8367c06e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:46 +0000

    Add package value-object-pattern/0.9.3/value_object_pattern-0.9.3-py3-none-any.whl

---
+++ packages/value-object-pattern/value_object_pattern-0.9.3-py3-none-any.whl/value_object_pattern/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.9.3'
+
+from .decorators import process, validation
+from .models import BaseModel, EnumerationValueObject, ValueObject
+
+__all__ = (
+    'BaseModel',
+    'EnumerationValueObject',
+    'ValueObject',
+    'process',
+    'validation',
+)
---
+++ packages/value-object-pattern/value_object_pattern-0.9.3-py3-none-any.whl/value_object_pattern/usables/identifiers/__init__.py
@@ -0,0 +1,7 @@
+from .string_uuid_value_object import StringUuidValueObject
+from .uuid_value_object import UuidValueObject
+
+__all__ = (
+    'StringUuidValueObject',
+    'UuidValueObject',
+)
