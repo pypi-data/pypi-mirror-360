2d5dc519d22d362979dec5c4d582ffcd3a512cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:17 +0000

    Add package django-bulk-hooks/0.1.68/django_bulk_hooks-0.1.68-py3-none-any.whl

---
+++ packages/django-bulk-hooks/django_bulk_hooks-0.1.68-py3-none-any.whl/django_bulk_hooks/__init__.py
@@ -0,0 +1,4 @@
+from django_bulk_hooks.handler import HookHandler
+from django_bulk_hooks.manager import BulkHookManager
+
+__all__ = ["BulkHookManager", "HookHandler"]
---
+++ packages/django-bulk-hooks/django_bulk_hooks-0.1.68-py3-none-any.whl/django_bulk_hooks/constants.py
@@ -0,0 +1,11 @@
+BEFORE_CREATE = "before_create"
+AFTER_CREATE = "after_create"
+BEFORE_UPDATE = "before_update"
+AFTER_UPDATE = "after_update"
+BEFORE_DELETE = "before_delete"
+AFTER_DELETE = "after_delete"
+
+# Validation constants - run during clean() for admin/form validation
+VALIDATE_CREATE = "validate_create"
+VALIDATE_UPDATE = "validate_update"
+VALIDATE_DELETE = "validate_delete"
