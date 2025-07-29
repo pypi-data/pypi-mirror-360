93afc062972d6b69f08416407138c22431e9bb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:21 +0000

    Add package gr-libs/0.2.7/gr_libs-0.2.7.tar.gz

---
+++ packages/gr-libs/gr_libs-0.2.7.tar.gz/gr_libs-0.2.7/tests/test_graml.py
@@ -0,0 +1,20 @@
+from gr_libs.tutorials.graml_minigrid_tutorial import run_graml_minigrid_tutorial
+from gr_libs.tutorials.graml_panda_tutorial import run_graml_panda_tutorial
+from gr_libs.tutorials.graml_parking_tutorial import run_graml_parking_tutorial
+from gr_libs.tutorials.graml_point_maze_tutorial import run_graml_point_maze_tutorial
+
+
+def test_graml_minigrid_tutorial():
+    run_graml_minigrid_tutorial()
+
+
+def test_graml_panda_tutorial():
+    run_graml_panda_tutorial()
+
+
+def test_graml_parking_tutorial():
+    run_graml_parking_tutorial()
+
+
+def test_graml_point_maze_tutorial():
+    run_graml_point_maze_tutorial()
---
+++ packages/gr-libs/gr_libs-0.2.7.tar.gz/gr_libs-0.2.7/tests/test_graql.py
@@ -0,0 +1,5 @@
+from gr_libs.tutorials.graql_minigrid_tutorial import run_graql_minigrid_tutorial
+
+
+def test_graql_minigrid_tutorial():
+    run_graql_minigrid_tutorial()
