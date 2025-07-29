ae241152e66c57f3b08313b459921c9cb3095cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:20 +0000

    Add package factly-standard-names/0.2.1/factly_standard_names-0.2.1-py3-none-any.whl

---
+++ packages/factly-standard-names/factly_standard_names-0.2.1-py3-none-any.whl/factly/standard_names/settings.py
@@ -0,0 +1,4 @@
+SHEET_ID: str = "1RGmfn9_ujELGK03uPeA6hf-JdeIXvn5Ev3aC19abFcw"
+STANDARD_NAMES_SHEET_NAME: str = "standard_names"
+ABBREVIATIONS_SHEET_NAME: str = "standard_abbrevations"
+COUNTRY_SHEET_NAME: str = "country"
