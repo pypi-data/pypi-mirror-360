ca7d4f3091e8381bae142ce1f0215fa0ba1e3230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:57 +0000

    Add package pyrsm/1.6.1/pyrsm-1.6.1.tar.gz

---
+++ packages/pyrsm/pyrsm-1.6.1.tar.gz/pyrsm-1.6.1/README.md
@@ -0,0 +1,18 @@
+# PYRSM
+
+Python functions and classes for Customer Analytics at the Rady School of Management (RSM), University of California, San Diego (UCSD)
+
+## Install instructions
+
+If you have python and UV installed you can use the below to install the latest version of `pyrsm` in a project folder:
+
+```bash
+mkdir ~/project
+cd ~/project
+uv init .
+uv venv --python 3.12
+source .venv/bin/activate
+uv add pyrsm
+```
+
+&copy; Vincent Nijs (2025) <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank"><img alt="Creative Commons License" style="border-width: 0" src="images/by-nc-sa.png"/></a>
---
+++ packages/pyrsm/pyrsm-1.6.1.tar.gz/pyrsm-1.6.1/tests/test_basics.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import polars as pl
+
+# from pyrsm.basics.compare_means import compare_means
+from pyrsm.basics.correlation import correlation
+from pyrsm.basics.cross_tabs import cross_tabs
+
+df = pd.DataFrame({"x": [0, 1, 1, 1, 0, 0, 0], "y": [2, 1, 1, 1, 2, 2, 10]})
+dfp = pl.DataFrame(df.copy())
+
+# def test_compare_means():
+#     cm = compare_means({"df": df}, var1="x", var2="y")
+#     assert cm.
+#     # assert c.cr[1, 0].round(3) == -0.493, "Correlations incorrect"
+# df_nan = df.copy()
+# df_nan.loc[4, "x"] = np.nan
+# c = correlation(df_nan)
+# assert c.cr[1, 0].round(3) == -0.567, "Correlations with np.nan incorrect"
+
+
+def test_correlation_pandas():
+    c = correlation(df)
+    assert c.cr[1, 0].round(3) == -0.493, "Correlations incorrect"
+    df_nan = df.copy()
+    df_nan.loc[4, "x"] = np.nan
+    c = correlation(df_nan)
+    assert c.cr[1, 0].round(3) == -0.567, "Correlations with np.nan incorrect"
+
+
+def test_correlation_polars():
+    c = correlation(dfp)
+    assert c.cr[1, 0].round(3) == -0.493, "Correlations incorrect"
+    ## looks like an issue converting null to nan in pandas
+    # df_nan = dfp
+    # df_nan[4, "x"] = None
+    # c = correlation(df_nan)
+    # assert c.cr[1, 0].round(3) == -0.567, "Correlations with np.nan incorrect"
+
+
+def test_crosstab():
+    ct = cross_tabs(df, "x", "y")
+    assert all(
+        ct.expected.iloc[0, :].round(6) == [1.714286, 1.714286, 0.571429, 4.0000]
+    ), "Cross tab expected values incorrect"
+
+
+if __name__ == "__main__":
+    test_correlation_pandas()
+    test_correlation_polars()
+    test_crosstab()
