203cc8670544888697a3b0b79a0d0e61fcbe30e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:26 +0000

    Add package omlish/0.0.0.dev349/omlish-0.0.0.dev349-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev349-py3-none-any.whl/omlish/lang/datetimes.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+##
+
+
+def utcnow() -> datetime.datetime:
+    return datetime.datetime.now(tz=datetime.UTC)
+
+
+def utcfromtimestamp(ts: float) -> datetime.datetime:
+    return datetime.datetime.fromtimestamp(ts, tz=datetime.UTC)
