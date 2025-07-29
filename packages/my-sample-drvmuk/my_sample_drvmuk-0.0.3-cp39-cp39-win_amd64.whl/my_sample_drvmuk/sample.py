bf2c26228d07ca5bfcd40249c26ddca7b52a7598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:15 +0000

    Add package my-sample-drvmuk/0.0.3/my_sample_drvmuk-0.0.3-cp39-cp39-win_amd64.whl

---
+++ packages/my-sample-drvmuk/my_sample_drvmuk-0.0.3-cp39-cp39-win_amd64.whl/my_sample_drvmuk/sample.py
@@ -0,0 +1,8 @@
+
+class sample:
+    def __init__(self):
+        self.name = "sample"
+        self.version = "0.0.2"
+
+    def greet(self):
+        return f"Hello from {self.name} version {self.version}!"
