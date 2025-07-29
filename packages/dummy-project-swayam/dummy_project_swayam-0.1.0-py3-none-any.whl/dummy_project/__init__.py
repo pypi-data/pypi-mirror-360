a2edcc00f71d715f687c5b697bde9afee561d5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:31 +0000

    Add package dummy-project-swayam/0.1.0/dummy_project_swayam-0.1.0-py3-none-any.whl

---
+++ packages/dummy-project-swayam/dummy_project_swayam-0.1.0-py3-none-any.whl/dummy_project/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+
+def welcome():
+    """Print welcome message."""
+    print("Welcome to dummy project!")
+
+# Print welcome message when package is imported
+print("Welcome to dummy project!")
---
+++ packages/dummy-project-swayam/dummy_project_swayam-0.1.0-py3-none-any.whl/dummy_project/core.py
@@ -0,0 +1,9 @@
+def get_welcome_message():
+    """Return the welcome message as a string."""
+    return "Welcome to dummy project!"
+
+def print_info():
+    """Print package information."""
+    print("Dummy Project v0.1.0")
+    print("Created by SwayamInSync")
+    print("Repository: https://github.com/SwayamInSync/Dummy_Project")
