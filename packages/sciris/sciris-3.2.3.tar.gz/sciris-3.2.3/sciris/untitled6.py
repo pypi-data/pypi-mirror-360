906019d0089e8c62478b0c68c8ddff472503e8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:22 +0000

    Add package sciris/3.2.3/sciris-3.2.3.tar.gz

---
+++ packages/sciris/sciris-3.2.3.tar.gz/sciris-3.2.3/sciris/untitled6.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import sciris as sc
+import matplotlib.pyplot as plt
+import starsim as ss
+np; pd; sc; plt; ss
+
+def foo():
+    return 1+1
+
+prof = sc.profile(foo)
