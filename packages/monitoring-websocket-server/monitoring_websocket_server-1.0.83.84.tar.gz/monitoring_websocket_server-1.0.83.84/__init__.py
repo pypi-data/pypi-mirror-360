f5013e4f30c496eb3b2137efadc66413043495a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:24 +0000

    Add package monitoring-websocket-server/1.0.83.84/monitoring_websocket_server-1.0.83.84.tar.gz

---
+++ packages/monitoring-websocket-server/monitoring_websocket_server-1.0.83.84.tar.gz/monitoring_websocket_server-1.0.83.84/__init__.py
@@ -0,0 +1,8 @@
+"""Monitoring WebSocket Server Package.
+
+This package provides real-time system monitoring capabilities
+with WebSocket broadcasting support.
+"""
+
+__version__ = "1.0.83.83"
+__author__ = "NMicInvest"
