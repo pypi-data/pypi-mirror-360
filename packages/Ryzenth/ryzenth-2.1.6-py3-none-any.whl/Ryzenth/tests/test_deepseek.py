8257308e3fc20f7c5a3040f80a571c6c2258b51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:43 +0000

    Add package Ryzenth/2.1.6/ryzenth-2.1.6-py3-none-any.whl

---
+++ packages/Ryzenth/ryzenth-2.1.6-py3-none-any.whl/Ryzenth/tests/test_deepseek.py
@@ -0,0 +1,14 @@
+from Ryzenth import ApiKeyFrom
+
+from ..types import QueryParameter
+
+
+def test_deepseek():
+    ryz = ApiKeyFrom(..., True)
+    result = ryz._sync.what.think(
+        params=QueryParameter(
+            query="ok test"
+        ),
+        timeout=10
+    )
+    assert result is not None
