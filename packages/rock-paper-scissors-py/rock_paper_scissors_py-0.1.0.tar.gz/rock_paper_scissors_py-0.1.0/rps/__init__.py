1ed885b8444320b0313b3c54f6a36bbe1148e48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:24 +0000

    Add package rock-paper-scissors-py/0.1.0/rock_paper_scissors_py-0.1.0.tar.gz

---
+++ packages/rock-paper-scissors-py/rock_paper_scissors_py-0.1.0.tar.gz/rock_paper_scissors_py-0.1.0/rps/__init__.py
@@ -0,0 +1,13 @@
+from .game import Game
+from .player import Player, FixedActionPlayer, RandomActionPlayer
+
+__all__ = [
+    "Game",
+    "Player",
+    "FixedActionPlayer",
+    "RandomActionPlayer",
+]
+
+__version__ = "0.1.0"
+__authors__ = ["jwc20"]
+__source__ = "https://github.com/jwc20/rock-paper-scissors-py"
