f67543d0b75412e22125377dab7bccacd2068a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:23 +0000

    Add package strapi-client/3.7.4/strapi_client-3.7.4-py3-none-any.whl

---
+++ packages/strapi-client/strapi_client-3.7.4-py3-none-any.whl/strapi_client/__init__.py
@@ -0,0 +1,19 @@
+from .strapi_client_async import StrapiClientAsync
+from .strapi_client import StrapiClient
+from .types import DocumentResponse, DocumentsResponse, BaseDocument, MediaImageDocument
+from .smart_document import SmartDocument
+from .single_smart_document import SingleSmartDocument
+from .active_document import ActiveDocument, DocumentField
+
+__all__ = [
+    'StrapiClientAsync',
+    'StrapiClient',
+    'DocumentResponse',
+    'DocumentsResponse',
+    'BaseDocument',
+    'MediaImageDocument',
+    'SmartDocument',
+    'SingleSmartDocument',
+    'ActiveDocument',
+    'DocumentField'
+]
