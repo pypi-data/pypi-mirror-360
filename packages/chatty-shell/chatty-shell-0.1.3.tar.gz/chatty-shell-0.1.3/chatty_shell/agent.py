9a1dae915585365179c2f8ed786bb2d8fa5cb887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:28 +0000

    Add package chatty-shell/0.1.3/chatty-shell-0.1.3.tar.gz

---
+++ packages/chatty-shell/chatty-shell-0.1.3.tar.gz/chatty-shell-0.1.3/chatty_shell/agent.py
@@ -0,0 +1,48 @@
+import os
+import getpass
+
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from langchain_core.runnables import Runnable
+from langchain_core.tools import BaseTool
+from langgraph.checkpoint.memory import MemorySaver
+
+from chatty_shell.exceptions import MissingPromptException
+
+API_KEY = "OPENAI_API_KEY"
+
+
+def _get_llm(model: str | None = None) -> ChatOpenAI:
+    # default model is gpt-3.5-turbo
+    if model is None:
+        model = "gpt-3.5-turbo"
+    if not os.environ.get(API_KEY):
+        os.environ[API_KEY] = getpass.getpass("Enter your OpenAI-API key: ")
+
+    llm = ChatOpenAI(model=model)
+    return llm
+
+
+def _get_memory():
+    return MemorySaver()
+
+
+def get_agent_executor(
+    tools: list[BaseTool],
+    llm: ChatOpenAI | None = None,
+    memory: MemorySaver | None = None,
+    system_prompt: str = "",
+) -> Runnable:
+    if llm is None:
+        # get default llm
+        llm = _get_llm()
+    if memory is None:
+        # get new chat history
+        memory = _get_memory()
+    if system_prompt == "":
+        raise MissingPromptException
+
+    agent_executor = create_react_agent(
+        model=llm, tools=tools, prompt=system_prompt, checkpointer=memory
+    )
+    return agent_executor
---
+++ packages/chatty-shell/chatty-shell-0.1.3.tar.gz/chatty-shell-0.1.3/chatty_shell/main.py
@@ -0,0 +1,72 @@
+from langchain_core.messages import HumanMessage
+from pydantic import BaseModel
+
+from chatty_shell.agent import get_agent_executor
+from chatty_shell.messages import sort_tools_calls
+from chatty_shell.tools import shell
+from chatty_shell.system_prompt import system_prompt
+from chatty_shell.out import (
+    print_banner,
+    clear_last_line,
+    print_ai_bubble,
+    print_user_bubble,
+    print_tool_bubble,
+)
+
+# Define model and tools
+tools = [shell]
+
+# Create the React agent
+agent_executor = get_agent_executor(tools=tools, system_prompt=system_prompt)
+
+
+class ChatInput(BaseModel):
+    message: str
+
+
+def main():
+    print_banner()
+
+    while True:
+        try:
+            user_input = input("> ")
+            if user_input.lower() in {"exit", "quit"}:
+                break
+
+            clear_last_line()  # Hide the input line after pressing enter
+
+            chat_input = ChatInput(message=user_input)
+
+            print_user_bubble(chat_input.message)
+
+            result = agent_executor.invoke(
+                {"messages": [HumanMessage(content=chat_input.message)]},
+                config={
+                    "configurable": {"thread_id": "abc123"},
+                    "recursion_limit": 100,
+                },
+            )
+            messages = result.get("messages", [])
+
+            new_messages = []
+            for i in range(len(messages)):
+                if isinstance(messages[::-1][i], HumanMessage):
+                    new_messages = messages[-i:]
+                    break
+
+            sorted_tool_calls = sort_tools_calls(new_messages)
+
+            if sorted_tool_calls:
+                for command in sorted_tool_calls:
+                    print_tool_bubble(command, sorted_tool_calls[command])
+            # pprint.pp(sorted_tool_calls)
+            # print()
+            print_ai_bubble(messages[-1].content)
+
+        except KeyboardInterrupt:
+            print("\n👋 Goodbye!")
+            break
+
+
+if __name__ == "__main__":
+    main()
