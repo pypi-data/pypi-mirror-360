2152e7d5ec65cdb41f2a9a759594ca149bab97fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:29 +0000

    Add package AutoCAD/0.1.6/autocad-0.1.6.tar.gz

---
+++ packages/AutoCAD/autocad-0.1.6.tar.gz/autocad-0.1.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import sys
+
+requirements = ['psutil']
+if sys.platform == 'win32':
+    requirements.append('pywin32')
+
+setup(
+    name="AutoCAD",
+    version="0.1.6",
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'AutoCAD = AutoCAD.__main__:main',
+        ],
+    },
+    keywords=["autocad", "automation", "activex", "comtypes", "AutoCAD", "AutoCADlib"],
+    author="Jones Peter",
+    author_email="jonespetersoftware@gmail.com",
+    url="https://github.com/Jones-peter",
+    description="A professional AutoCAD automation package with many functions.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,
+)
