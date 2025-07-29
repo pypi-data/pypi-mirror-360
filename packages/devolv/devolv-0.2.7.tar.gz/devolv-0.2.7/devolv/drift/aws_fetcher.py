2ade53de55f152682e912b93f860e1dfdb6139d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:48 +0000

    Add package devolv/0.2.7/devolv-0.2.7.tar.gz

---
+++ packages/devolv/devolv-0.2.7.tar.gz/devolv-0.2.7/devolv/drift/aws_fetcher.py
@@ -0,0 +1,25 @@
+import boto3
+
+def get_aws_policy_document(policy_arn: str) -> dict:
+    """
+    Fetch the JSON document of the default version of a managed IAM policy.
+    """
+    iam = boto3.client("iam")
+    policy = iam.get_policy(PolicyArn=policy_arn)['Policy']
+    default_version = policy['DefaultVersionId']
+    version = iam.get_policy_version(PolicyArn=policy_arn, VersionId=default_version)
+    return version['PolicyVersion']['Document']
+
+def merge_policy_documents(local_doc: dict, aws_doc: dict) -> dict:
+    """
+    Merge statements by appending any local-only statements to the AWS document.
+    This is an "append-only" merge (we do not delete existing AWS statements).
+    """
+    aws_stmts = aws_doc.get("Statement", [])
+    local_stmts = local_doc.get("Statement", [])
+    merged = list(aws_stmts)  # copy existing AWS statements
+    for stmt in local_stmts:
+        if stmt not in aws_stmts:
+            merged.append(stmt)
+    aws_doc["Statement"] = merged
+    return aws_doc
