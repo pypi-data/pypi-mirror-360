6aa0303654ae26c7329cf071ddd313b8178d1976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:17 +0000

    Add package Vectoryx/0.1.0/Vectoryx-0.1.0.tar.gz

---
+++ packages/Vectoryx/Vectoryx-0.1.0.tar.gz/Vectoryx-0.1.0/Vectoryx/Vectoryx.py
@@ -0,0 +1,28 @@
+def sum_vectors(v1, v2):
+    return [v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]]
+
+def vector_dif(v1, v2):
+    return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]]
+
+def dot_product(v1, v2):
+    return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+
+def multiply_vector_to_C(v1, C):
+    return [v1[0]*C, v1[1]*C, v1[2]*C]
+
+def vector_length(v):
+    from math import sqrt
+    return sqrt(v[0]**2 + v[1]**2 + v[2]**2)
+
+def normalize_vector(v):
+    length = vector_length(v)
+    if length == 0:
+        raise ValueError("Cannot normalize zero vector")
+    return [v[0]/length, v[1]/length, v[2]/length]
+
+def cross_product(v1, v2):
+    return [
+        v1[1]*v2[2] - v1[2]*v2[1],
+        v1[2]*v2[0] - v1[0]*v2[2],
+        v1[0]*v2[1] - v1[1]*v2[0]
+    ]
---
+++ packages/Vectoryx/Vectoryx-0.1.0.tar.gz/Vectoryx-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Vectoryx',
+    version='0.1.0',
+    author='Artem Kryachko',
+    author_email='artemkryachko2007@email.com',
+    description='A simple vector operations library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ArtemK2007/Vectoryx',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
