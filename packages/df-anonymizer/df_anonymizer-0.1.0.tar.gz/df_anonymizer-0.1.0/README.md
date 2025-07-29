46915abe5f9e84e7d3d9e5591ba0e5229d7d1ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:06 +0000

    Add package df-anonymizer/0.1.0/df_anonymizer-0.1.0.tar.gz

---
+++ packages/df-anonymizer/df_anonymizer-0.1.0.tar.gz/df_anonymizer-0.1.0/README.md
@@ -0,0 +1,3 @@
+### Data Anonymization Tool
+
+A simple data anonymization library with masking, data perturbation, data permutation, data suppression, and pseudonymization functions.
---
+++ packages/df-anonymizer/df_anonymizer-0.1.0.tar.gz/df_anonymizer-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data_anonymizer",
+    version="0.1.0",
+    description="A simple data anonymization library with masking, data perturbation, data permutation, data suppression, and pseudonymization functions.",
+    autho="Tan Shih Jen",
+    packages=find_packages(),
+    install_requires=[],
+)
