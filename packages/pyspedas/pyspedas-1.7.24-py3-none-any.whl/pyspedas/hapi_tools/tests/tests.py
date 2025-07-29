d47561c01d1c1dfdaac5b43d93d0ed34a5d39e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:25 +0000

    Add package pyspedas/1.7.24/pyspedas-1.7.24-py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-1.7.24-py3-none-any.whl/pyspedas/hapi_tools/tests/tests.py
@@ -0,0 +1,57 @@
+import unittest
+from pyspedas import hapi
+from pytplot import data_exists, del_data
+
+
+class HAPITests(unittest.TestCase):
+    def test_print_servers(self):
+        with self.assertLogs(level='ERROR') as log: 
+            hapi(trange=['2003-10-20', '2003-11-30'])
+            self.assertIn("No server specified; example servers include:", log.output[0])
+
+    def test_return_catalog(self):
+        id_list = hapi(server='https://cdaweb.gsfc.nasa.gov/hapi', catalog=True, quiet=True)
+        self.assertTrue('MMS1_EDI_BRST_L2_EFIELD' in id_list)
+
+    def test_dataset_not_specified(self):
+        # dataset not specified
+        with self.assertLogs(level='ERROR') as log:
+            h_vars = hapi(trange=['2003-10-20', '2003-11-30'],
+                        server='https://cdaweb.gsfc.nasa.gov/hapi')
+            self.assertIn("Error, no dataset specified; please see the catalog for a list of available data sets.", log.output[0])
+
+    def test_trange_not_specified(self):
+        # trange not specified
+        with self.assertLogs(level='ERROR') as log:
+            h_vars = hapi(dataset='OMNI_HRO2_1MIN',
+                        server='https://cdaweb.gsfc.nasa.gov/hapi')
+            self.assertIn("Error, no trange specified", log.output[0])
+
+    def test_cdaweb_mms_spec(self):
+        h_vars = hapi(trange=['2019-10-16', '2019-10-17'],
+                      server='https://cdaweb.gsfc.nasa.gov/hapi',
+                      dataset='MMS4_EDP_SRVY_L2_HFESP')
+
+    def test_cdaweb_omni(self):
+        del_data()
+        h_vars = hapi(trange=['2003-10-20', '2003-11-30'],
+                      server='https://cdaweb.gsfc.nasa.gov/hapi',
+                      dataset='OMNI_HRO2_1MIN')
+        self.assertTrue(data_exists('BX_GSE'))
+        self.assertTrue(data_exists('BY_GSE'))
+        self.assertTrue(data_exists('BZ_GSE'))
+
+    #@unittest.skip("Skipping due to possible server-side change, unable to parse JSON response")
+    def test_string_time(self):
+        del_data()
+        server = "https://supermag.jhuapl.edu/hapi"
+        dataset = "TTB/PT1M/xyz"
+        start = "2020-05-10T00:00Z"
+        stop = "2020-05-14T00:00Z"
+        parameters = ''
+        param_list = hapi(trange=[start,stop], server=server, dataset=dataset, parameters=parameters)
+        self.assertTrue('mlt' in param_list)
+        self.assertTrue(data_exists('mlt'))
+
+if __name__ == '__main__':
+    unittest.main()
