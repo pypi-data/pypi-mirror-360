cd57c1e3dabb57e868379e110200227ff2df3bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:57 +0000

    Add package normino/10.10/normino-10.10.tar.gz

---
+++ packages/normino/normino-10.10.tar.gz/normino-10.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="normino",
+    version="10.10",
+    url="https://github.com/SLDDL/Normino",
+    py_modules=["normino"],
+    install_requires=[
+        "colorama",
+        "beautifulsoup4",
+        "requests",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "normino=normino:main",
+        ],
+    },
+    project_urls={
+        "Documentation": "https://github.com/SLDDL/Normino/blob/main/README.md",
+        "Source": "https://github.com/SLDDL/Normino",
+        "Tracker": "https://github.com/SLDDL/Normino/issues",
+        "Icon": "https://raw.githubusercontent.com/SLDDL/Normino/main/icon.png",
+    },
+)
