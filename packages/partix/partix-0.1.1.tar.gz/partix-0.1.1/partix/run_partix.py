cf1a8720c415f8b6d56281c68eb46c6e36171742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:09 +0000

    Add package partix/0.1.1/partix-0.1.1.tar.gz

---
+++ packages/partix/partix-0.1.1.tar.gz/partix-0.1.1/partix/run_partix.py
@@ -0,0 +1,31 @@
+from antlr4 import *
+from PartixLexer import PartixLexer
+from PartixParser import PartixParser
+from antlr4.tree.Tree import ParseTreeWalker
+from PartixEvalListener import PartixEvalListener
+from preprocessor import preprocess
+
+def run(program):
+    input_stream = InputStream(program)
+
+    lexer = PartixLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+
+    parser = PartixParser(stream)
+    tree = parser.block()
+
+    listener = PartixEvalListener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+
+    return listener.get_jsons()
+
+def run_ptx_file(ptx_file):
+    with open(ptx_file, "r", encoding="utf-8") as f:
+        program = f.read()
+    blocks = preprocess(program)
+    i = 1
+    for block in blocks:
+        print(f"----------Block{i}----------")
+        run(block)
+        i += 1
