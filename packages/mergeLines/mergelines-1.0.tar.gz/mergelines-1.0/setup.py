2c5d8ada518e05dfceb16778b815c4febc50737f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:22 +0000

    Add package mergeLines/1.0/mergelines-1.0.tar.gz

---
+++ packages/mergeLines/mergelines-1.0.tar.gz/mergelines-1.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+
+from setuptools import setup
+
+setup(
+    name='mergeLines',
+    packages=['mergeLine'],
+    description="merge lines after houghlinesp",
+    version='1.0'
+)
