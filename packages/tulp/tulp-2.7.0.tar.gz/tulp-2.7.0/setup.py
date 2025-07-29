28a9aa3fb71fe8b6177277a2db035e4408358de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:31 +0000

    Add package tulp/2.7.0/tulp-2.7.0.tar.gz

---
+++ packages/tulp/tulp-2.7.0.tar.gz/tulp-2.7.0/setup.py
@@ -0,0 +1,7 @@
+# setup.py
+# This file is needed for compatibility with some build tools and editable installs.
+# Most configuration is now in setup.cfg.
+from setuptools import setup
+
+setup()
+
