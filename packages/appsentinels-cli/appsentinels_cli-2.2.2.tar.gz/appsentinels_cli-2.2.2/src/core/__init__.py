921a79cf2bd328b65e2537ca327a401e6f1c3063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:06 +0000

    Add package appsentinels-cli/2.2.2/appsentinels_cli-2.2.2.tar.gz

---
+++ packages/appsentinels-cli/appsentinels_cli-2.2.2.tar.gz/appsentinels_cli-2.2.2/src/core/__init__.py
@@ -0,0 +1,20 @@
+"""
+Core module for AppSentinels CLI
+
+This package contains the core infrastructure for the CLI application.
+"""
+
+from .base_handler import BaseCommandHandler, SubcommandMixin, AsyncCommandHandler, CLIOnlyMixin
+from .cli_processor import CLIProcessor, CommandRegistry, register_command
+from .auth_context import AuthContext
+
+__all__ = [
+    'BaseCommandHandler',
+    'SubcommandMixin', 
+    'AsyncCommandHandler',
+    'CLIOnlyMixin',
+    'CLIProcessor',
+    'CommandRegistry',
+    'register_command',
+    'AuthContext'
+]
