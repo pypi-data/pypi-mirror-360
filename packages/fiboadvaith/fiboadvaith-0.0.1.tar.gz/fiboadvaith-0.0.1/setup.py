e89e20ed6848f3642b0665a8afe3a4fc34872e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:13 +0000

    Add package fiboadvaith/0.0.1/fiboadvaith-0.0.1.tar.gz

---
+++ packages/fiboadvaith/fiboadvaith-0.0.1.tar.gz/fiboadvaith-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+import os
+
+setup(
+    name='fiboadvaith',
+    version='0.0.1',
+    packages=find_packages(),
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    # For example: MIT
+    license='MIT',
+
+    # Short description of your library
+    description='thanish shetty is gay for mehul r EKBOTE',
+
+    # Your name
+    author='Advaith S', 
+
+    author_email='4dvaith.s@gmail.com',     
+
+
+)
