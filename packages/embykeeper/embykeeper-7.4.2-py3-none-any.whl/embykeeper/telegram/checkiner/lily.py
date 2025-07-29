32c918872e81a44487231d8cc571bc2e4f96591d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:00 +0000

    Add package embykeeper/7.4.2/embykeeper-7.4.2-py3-none-any.whl

---
+++ packages/embykeeper/embykeeper-7.4.2-py3-none-any.whl/embykeeper/telegram/checkiner/lily.py
@@ -0,0 +1,7 @@
+from ._templ_a import TemplateACheckin
+
+
+class LilyCheckin(TemplateACheckin):
+    name = "Lily"
+    bot_username = "lilyembybot"
+    bot_use_captcha = False
---
+++ packages/embykeeper/embykeeper-7.4.2-py3-none-any.whl/embykeeper/telegram/checkiner/mjj.py
@@ -0,0 +1,6 @@
+from ._templ_a import TemplateACheckin
+
+
+class MJJCheckin(TemplateACheckin):
+    name = "MJJ"
+    bot_username = "mjjemby_uesr_bot"
