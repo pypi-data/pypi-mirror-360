60a7df60cbd8734b0ac41fe7417bb8615be348e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:33 +0000

    Add package thinkwide-finance/1.0/thinkwide_finance-1.0.tar.gz

---
+++ packages/thinkwide-finance/thinkwide_finance-1.0.tar.gz/thinkwide_finance-1.0/setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='thinkwide-finance',
+    version='1.0',
+    packages=find_packages(),  # 自动查找包和子包
+    install_requires=[],  # 依赖项列表（如果有的话）
+)
