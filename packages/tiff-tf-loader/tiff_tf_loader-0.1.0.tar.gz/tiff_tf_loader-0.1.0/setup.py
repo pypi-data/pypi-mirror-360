f51d8f24bfe541efa9874b2853f68565b731fce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:27 +0000

    Add package tiff-tf-loader/0.1.0/tiff_tf_loader-0.1.0.tar.gz

---
+++ packages/tiff-tf-loader/tiff_tf_loader-0.1.0.tar.gz/tiff_tf_loader-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tiff-tf-loader',
+    version='0.1.0',
+    description='Load TIFF images into TensorFlow datasets using rasterio',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Hari Babu KVN',
+    author_email='haribabuatwork@gmail.com',
+    url='https://github.com/haribabugitwork/tiff-tf-loader',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow>=2.8',
+        'rasterio',
+        'scikit-learn',
+        'numpy'
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
