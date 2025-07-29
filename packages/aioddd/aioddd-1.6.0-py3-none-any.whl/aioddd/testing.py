84dd57fd5b240cf3d0064d437c587d6f4896ade8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:25 +0000

    Add package aioddd/1.6.0/aioddd-1.6.0-py3-none-any.whl

---
+++ packages/aioddd/aioddd-1.6.0-py3-none-any.whl/aioddd/testing.py
@@ -0,0 +1,54 @@
+from typing import Any, Dict, List, Optional, Type, Union
+from unittest.mock import AsyncMock, MagicMock, Mock, patch
+
+SanitizeObject = Union[Dict[Any, Any], List[Any]]
+
+
+def sanitize_objects(source: SanitizeObject, affected: SanitizeObject) -> SanitizeObject:  # pragma: no cover
+    for key, value in list(affected.items()) if isinstance(affected, dict) else enumerate(affected):
+        if not isinstance(affected, list) and key not in source:
+            affected.pop(key)
+        elif isinstance(value, (dict, list)):
+            affected[key] = sanitize_objects(source[key], value)
+    return affected
+
+
+def mock(
+    target: Union[str, object],
+    attributes: Optional[List[str]] = None,
+    target_mock_type: Type[Mock] = AsyncMock,
+    attribute_mock_type: Type[Mock] = AsyncMock,
+) -> Mock:
+    target_async_mock = target_mock_type()
+    if not attributes:
+        patch(
+            target=f'{target.__module__}.{target.__name__}' if isinstance(target, object) else target,  # type: ignore
+            side_effect=target_async_mock,
+        )
+        return target_async_mock
+    for attribute in attributes:
+        attribute_async_mock = attribute_mock_type()
+        patch.object(
+            target=target,
+            attribute=attribute,
+            side_effect=attribute_async_mock,
+        )
+        target_async_mock[attribute] = attribute_async_mock
+    return target_async_mock
+
+
+async_mock = mock
+
+
+def sync_mock(
+    target: Union[str, object],
+    attributes: Optional[List[str]] = None,
+    target_mock_type: Type[Mock] = MagicMock,
+    attribute_mock_type: Type[Mock] = MagicMock,
+) -> Mock:
+    return mock(
+        target=target,
+        attributes=attributes,
+        target_mock_type=target_mock_type,
+        attribute_mock_type=attribute_mock_type,
+    )
---
+++ packages/aioddd/aioddd-1.6.0-py3-none-any.whl/aioddd/utils.py
@@ -0,0 +1,86 @@
+from logging import NOTSET, Formatter, Logger, StreamHandler, getLogger
+from os import getenv
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union, cast
+
+
+def get_env(key: str, default: Optional[str] = None, cast_default_to_str: bool = True) -> Optional[str]:
+    """Get an environment variable, return default if it is empty or doesn't exist."""
+    value = getenv(key, default)
+    if value is None or len(value) == 0:
+        value = str(default) if cast_default_to_str else default
+    return value
+
+
+def get_str_env(key: str, default: str = '') -> str:
+    return cast(str, get_env(key=key, default=default, cast_default_to_str=True))
+
+
+_boolean_positive_values: List[str] = ['True', 'true', 'yes', 'Y', 'y', '1']
+
+
+def get_bool_env(key: str, default: Union[bool, int] = False) -> bool:
+    return get_env(key=key, default=str(int(default)), cast_default_to_str=False) in _boolean_positive_values
+
+
+def get_int_env(key: str, default: Union[bool, int] = 0) -> int:
+    val = cast(str, get_env(key=key, default=str(int(default)), cast_default_to_str=False))
+    return int(val) if val.isdigit() else default
+
+
+def get_float_env(key: str, default: Union[bool, int, float] = 0) -> float:
+    val = cast(str, get_env(key=key, default=str(float(default)), cast_default_to_str=False))
+    return float(val) if val.isdigit() or val.replace('.', '').isdigit() else default
+
+
+def get_list_str_env(
+    key: str,
+    default: Optional[List[str]] = None,
+    *,
+    delimiter: str = ',',
+    allow_empty: bool = True,
+) -> List[str]:
+    val = cast(str, get_env(key=key, default='' if allow_empty else delimiter.join(default or [])))
+    return [] if allow_empty and (val is None or len(val) == 0) else val.split(delimiter)
+
+
+def get_simple_logger(
+    name: Optional[str] = None,
+    level: Union[str, int] = NOTSET,
+    fmt: str = '[%(asctime)s] - %(name)s - %(levelname)s - %(message)s',
+) -> Logger:  # pragma: no cover
+    logger = getLogger(name)
+    logger.setLevel(level)
+    handler = StreamHandler()
+    handler.setLevel(level)
+    formatter = Formatter(fmt)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+_T = TypeVar('_T')
+_ENV: Optional[Dict[str, Any]] = None
+
+
+def env(key: Optional[str] = None, typ: Optional[Type[_T]] = None) -> _T:
+    """Get full environment variables resolved if no argument given or env var matching key and optional type given."""
+    global _ENV
+    if _ENV is None:
+        _ENV = env.resolver()  # type: ignore
+    if key is None:
+        return _ENV  # type: ignore
+    if not isinstance(_ENV, dict):
+        raise ValueError('"_ENV" variable must be a dict. Check env.resolver method.')
+    if key not in _ENV:
+        raise KeyError(
+            '<{0}{1}> does not exist as environment variable'.format(key, ': {0}'.format(typ.__name__) if typ else '')
+        )
+    val = _ENV[key]
+    if typ and not isinstance(val, (typ,)):
+        raise TypeError(
+            '<{0}{1}> does not exist as environment variable'.format(key, ': {0}'.format(typ.__name__) if typ else '')
+        )
+    return val  # type: ignore
+
+
+env.resolver = lambda: {}  # type: ignore
