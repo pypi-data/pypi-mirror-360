06fcec4403cba49da48295c72e16e8b3cd412540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:33 +0000

    Add package cardinal-cli/0.0.1/cardinal_cli-0.0.1.tar.gz

---
+++ packages/cardinal-cli/cardinal_cli-0.0.1.tar.gz/cardinal_cli-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Reserved Package
+
+This is a placeholder package to reserve the name `cardinal-cli` on PyPI.
+
+Project coming soon.
---
+++ packages/cardinal-cli/cardinal_cli-0.0.1.tar.gz/cardinal_cli-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cardinal-cli",
+    version="0.0.1",
+    description="Reserved name placeholder package",
+    packages=find_packages(),
+    install_requires=[],
+)
