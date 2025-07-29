874b6a6c2812f30fcc5e99c514549821dde5cfaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:19 +0000

    Add package my-aws-helpers/3.1.0.dev4/my_aws_helpers-3.1.0.dev4.tar.gz

---
+++ packages/my-aws-helpers/my_aws_helpers-3.1.0.dev4.tar.gz/my_aws_helpers-3.1.0.dev4/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import find_namespace_packages, setup
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+
+version = "3.1.0.dev4"
+
+setup(
+    name="my_aws_helpers",
+    version=version,
+    author="Jarrod McCarthy",
+    description="AWS Helpers",
+    url="https://github.com/JarrodMccarthy/aws_helpers.git",
+    platforms="any",
+    packages=[p for p in find_namespace_packages(where=base_path) if p.startswith("my_aws_helpers")],
+    classifiers=[
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    zip_safe = True,
+    install_requires = [
+        "boto3==1.34.36"
+    ],
+    include_package_data=True,
+)
