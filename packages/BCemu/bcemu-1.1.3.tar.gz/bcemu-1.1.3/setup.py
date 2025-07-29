4ecf962768b500114c469ebdabb489dfa8814d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:43 +0000

    Add package BCemu/1.1.3/bcemu-1.1.3.tar.gz

---
+++ packages/BCemu/bcemu-1.1.3.tar.gz/bcemu-1.1.3/setup.py
@@ -0,0 +1,30 @@
+'''
+Created on 1 June 2021
+@author: Sambit Giri
+Setup script
+'''
+
+from setuptools import setup, find_packages
+#from distutils.core import setup
+
+BCemu_link = 'https://github.com/sambit-giri/BCemu.git'
+
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='BCemu',
+      version='1.1.3',
+      description='Using emulators to implement baryonic effects.',
+      url=BCemu_link,
+      author='Sambit Giri',
+      author_email='sambit.giri@gmail.com',
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      package_data={'BCemu': ['input_data/*']},
+      install_requires=requirements,
+      # zip_safe=False,
+      include_package_data=True,
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      )
