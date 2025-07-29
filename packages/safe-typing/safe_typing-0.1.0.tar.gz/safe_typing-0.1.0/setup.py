182d0ade6d562e054dc28e84acd229fa38ef109b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:52 +0000

    Add package safe-typing/0.1.0/safe_typing-0.1.0.tar.gz

---
+++ packages/safe-typing/safe_typing-0.1.0.tar.gz/safe_typing-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='safe_typing',
+    version='0.1.0',
+    packages=find_packages(),
+    author='fswair',
+    description='safe_typing is a Python package.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
