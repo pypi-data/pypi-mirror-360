aacbfc6a93d1888ab4b99a84d4ce26be57c2ecd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:32 +0000

    Add package cheez-ppg-pico/1.0.0/cheez_ppg_pico-1.0.0.tar.gz

---
+++ packages/cheez-ppg-pico/cheez_ppg_pico-1.0.0.tar.gz/cheez_ppg_pico-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(  
+    name='cheez_ppg_pico',
+    version='1.0.0',
+    description='SDK for CheezPico(PPG) USB/BLE Devices',
+    long_description=open('README.md', encoding='utf-8').read(),  
+    long_description_content_type='text/markdown',
+    author='vecang', 
+    packages=find_packages(),
+    install_requires=[
+        'pyserial',
+        'aioserial' 
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+)
