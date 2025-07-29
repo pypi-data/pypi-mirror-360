941685c20119c4923c7cd6c6b53fa81ab28a6754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:07 +0000

    Add package tfq0seo/2.1.2/tfq0seo-2.1.2-py3-none-any.whl

---
+++ packages/tfq0seo/tfq0seo-2.1.2-py3-none-any.whl/tfq0seo/__init__.py
@@ -0,0 +1,21 @@
+"""
+tfq0seo - Professional SEO Analysis Toolkit
+Open source alternative to Screaming Frog SEO Spider
+"""
+
+__version__ = "2.1.2"
+__author__ = "tfq0"
+
+from .core.app import SEOAnalyzerApp
+from .core.crawler import WebCrawler
+from .analyzers.seo import SEOAnalyzer
+from .exporters.base import ExportManager
+
+__all__ = [
+    "SEOAnalyzerApp",
+    "WebCrawler", 
+    "SEOAnalyzer",
+    "ExportManager"
+] 
+
+
