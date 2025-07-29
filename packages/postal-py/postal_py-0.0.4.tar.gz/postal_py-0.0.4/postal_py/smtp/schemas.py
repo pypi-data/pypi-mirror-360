719667fc6d50b781a75ecfbcdd2bd11d8f47b570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:05 +0000

    Add package postal-py/0.0.4/postal_py-0.0.4.tar.gz

---
+++ packages/postal-py/postal_py-0.0.4.tar.gz/postal_py-0.0.4/postal_py/smtp/schemas.py
@@ -0,0 +1,37 @@
+import base64
+
+from pydantic import (BaseModel as PydanticBaseModel,
+                      Field,
+                      field_validator)
+
+
+class BaseModel(PydanticBaseModel):
+    model_config = {
+        'populate_by_name': True
+    }
+
+
+class SMTPAttachmentSchema(BaseModel):
+    name: str
+    content_type: str = 'application/octet-stream'
+    data: str | bytes
+
+    @field_validator('data', mode='before')
+    @classmethod
+    def to_base64(cls, value: str | bytes) -> bytes:
+        if isinstance(value, str):
+            value = base64.b64decode(value)
+        return value
+
+
+class SMTPMessageSchema(BaseModel):
+    to: list[str] = Field(default_factory=list)
+    cc: list[str] = Field(default_factory=list)
+    bcc: list[str] = Field(default_factory=list)
+    from_: str = Field(None, alias='from')
+    subject: str | None = None
+    reply_to: str | None = None
+    plain_body: str | None = None
+    html_body: str | None = None
+    attachments: list[SMTPAttachmentSchema] | None = None
+    headers: dict[str, str] | None = None
