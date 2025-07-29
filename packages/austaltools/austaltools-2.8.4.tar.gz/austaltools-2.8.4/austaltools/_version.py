6c59f6de473c58257decdb9bfa4223e4fc0d3e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:45 +0000

    Add package austaltools/2.8.4/austaltools-2.8.4.tar.gz

---
+++ packages/austaltools/austaltools-2.8.4.tar.gz/austaltools-2.8.4/austaltools/_version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__title__ = 'austaltools'
+__description__ = ('Tools for use with the Lagrangian '
+                   'atmospheric pollutant dispersion model'
+                   'AUSTAL (AUSbreitungsmodell nach TA-Luft).')
+__url__ = ''
+__version__ =  '2.8.4'
+__author__ = u'Clemens Drüe'
+__author_email__ = 'druee@uni-trier.de'
+__license__ = 'EUPL-1.2'
+__copyright__ = '(C) 2022-2025 Clemens Drüe'
