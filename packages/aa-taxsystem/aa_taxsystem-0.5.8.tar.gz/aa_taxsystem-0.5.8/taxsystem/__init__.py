6b1adbb2bdcb3e21d97efa7dc907a5d0e724e245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:15 +0000

    Add package aa-taxsystem/0.5.8/aa_taxsystem-0.5.8.tar.gz

---
+++ packages/aa-taxsystem/aa_taxsystem-0.5.8.tar.gz/aa_taxsystem-0.5.8/taxsystem/__init__.py
@@ -0,0 +1,9 @@
+"""Initialize the app"""
+
+# AA TaxSystem
+from taxsystem.app_settings import TAXSYSTEM_APP_NAME
+
+__version__ = "0.5.8"
+__title__ = "Tax System"
+
+USER_AGENT_TEXT = f"{TAXSYSTEM_APP_NAME} v{__version__}"
