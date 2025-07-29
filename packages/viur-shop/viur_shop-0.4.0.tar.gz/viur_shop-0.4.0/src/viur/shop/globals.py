a494e645154a3d437c6f760efb147d497d924fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:03 +0000

    Add package viur-shop/0.4.0/viur_shop-0.4.0.tar.gz

---
+++ packages/viur-shop/viur_shop-0.4.0.tar.gz/viur_shop-0.4.0/src/viur/shop/globals.py
@@ -0,0 +1,25 @@
+import logging
+import typing as t
+from contextvars import ContextVar
+
+if t.TYPE_CHECKING:
+    from .shop import Shop
+
+SHOP_LOGGER: logging.Logger = logging.getLogger("viur.shop")
+
+SHOP_INSTANCE: ContextVar["Shop"] = ContextVar("ShopInstance")
+SHOP_INSTANCE_VI: ContextVar["Shop"] = ContextVar("ShopInstanceVi")
+
+
+class Sentinel:
+    def __repr__(self) -> str:
+        return "<SENTINEL>"
+
+    def __bool__(self) -> bool:
+        return False
+
+
+SENTINEL: t.Final[Sentinel] = Sentinel()
+
+DEBUG_DISCOUNTS = ContextVar("DEBUG_DISCOUNTS", default=False)
+"""Print detailed discount evaluation for debugging"""
