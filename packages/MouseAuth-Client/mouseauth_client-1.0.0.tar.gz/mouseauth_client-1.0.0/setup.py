b1c84fdbdc2b292ffb3be0930e571a6b0a8f2e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:19 +0000

    Add package MouseAuth-Client/1.0.0/mouseauth_client-1.0.0.tar.gz

---
+++ packages/MouseAuth-Client/mouseauth_client-1.0.0.tar.gz/mouseauth_client-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MouseAuth-Client',
+    version='1.0.0',
+    author='LacyCat',
+    packages=find_packages(),
+    description='MouseAuth Client for Python',
+    install_requires=[
+        'requests>=2.28.0',
+        'numpy>=1.21.0',
+        'typing-extensions>=4.0.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.10',
+)
