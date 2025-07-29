1b835ce67a50217e7f82b7de2702537fc5640cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:55 +0000

    Add package allsafe-auth/1.1.8/allsafe_auth-1.1.8.tar.gz

---
+++ packages/allsafe-auth/allsafe_auth-1.1.8.tar.gz/allsafe_auth-1.1.8/allsafe_auth/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.0.0"  # Project Version
+__author__ = "Daniel Destaw Yimam"  # Project Author
+
+from .authentication import *  # Importing Authentication Methods
+from .mfa import *  # Importing MFA Management
+from .user_management import *  # Importing User Management
+from .security import *  # Importing Security and Compliance
+from .utils import *  # Importing Utility Functions
