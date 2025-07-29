bcee2f63b98f1dbfe73f37594e658e283ae7e3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:24 +0000

    Add package aioddd/1.6.0/aioddd-1.6.0.tar.gz

---
+++ packages/aioddd/aioddd-1.6.0.tar.gz/aioddd-1.6.0/aioddd/__init__.py
@@ -0,0 +1,119 @@
+# type: ignore
+# pylint: skip-file
+from .aggregates import Aggregate, AggregateRoot
+from .cqrs import (
+    Command,
+    CommandBus,
+    CommandHandler,
+    OptionalResponse,
+    Query,
+    QueryBus,
+    QueryHandler,
+    Response,
+    SimpleCommandBus,
+    SimpleQueryBus,
+)
+from .errors import (
+    BadRequestError,
+    BaseError,
+    CommandNotRegisteredError,
+    ConflictError,
+    DateTimeInvalidError,
+    EventMapperNotFoundError,
+    EventNotPublishedError,
+    ForbiddenError,
+    IdInvalidError,
+    NotFoundError,
+    QueryNotRegisteredError,
+    TimestampInvalidError,
+    UnauthorizedError,
+    UnknownError,
+)
+from .events import (
+    ConfigEventMappers,
+    Event,
+    EventBus,
+    EventHandler,
+    EventMapper,
+    EventMapperNotFoundError,
+    EventPublisher,
+    EventPublishers,
+    InternalEventPublisher,
+    SimpleEventBus,
+    find_event_mapper_by_name,
+    find_event_mapper_by_type,
+)
+from .subprocess import SubprocessResult, run_subprocess  # nosec
+from .utils import (
+    env,
+    get_bool_env,
+    get_env,
+    get_float_env,
+    get_int_env,
+    get_list_str_env,
+    get_simple_logger,
+    get_str_env,
+)
+from .value_objects import Id, StrDateTime, Timestamp
+
+__version__ = '1.6.0'
+
+__all__ = (
+    # aggregates
+    'Aggregate',
+    'AggregateRoot',
+    # cqrs
+    'Command',
+    'CommandHandler',
+    'CommandBus',
+    'SimpleCommandBus',
+    'Query',
+    'Response',
+    'OptionalResponse',
+    'QueryHandler',
+    'QueryBus',
+    'SimpleQueryBus',
+    # errors
+    'BaseError',
+    'NotFoundError',
+    'ConflictError',
+    'BadRequestError',
+    'UnauthorizedError',
+    'ForbiddenError',
+    'UnknownError',
+    'IdInvalidError',
+    'TimestampInvalidError',
+    'DateTimeInvalidError',
+    'EventNotPublishedError',
+    'CommandNotRegisteredError',
+    'QueryNotRegisteredError',
+    # events
+    'Event',
+    'EventMapper',
+    'EventPublisher',
+    'EventHandler',
+    'EventBus',
+    'SimpleEventBus',
+    'find_event_mapper_by_name',
+    'find_event_mapper_by_type',
+    'EventPublishers',
+    'ConfigEventMappers',
+    'EventMapperNotFoundError',
+    'InternalEventPublisher',
+    # utils
+    'get_env',
+    'get_str_env',
+    'get_bool_env',
+    'get_int_env',
+    'get_float_env',
+    'get_list_str_env',
+    'get_simple_logger',
+    'env',
+    # value_objects
+    'Id',
+    'Timestamp',
+    'StrDateTime',
+    # subprocess,
+    'SubprocessResult',
+    'run_subprocess',
+)
---
+++ packages/aioddd/aioddd-1.6.0.tar.gz/aioddd-1.6.0/aioddd/helpers.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+
+datetime_fromisoformat = getattr(datetime, 'fromisoformat')
