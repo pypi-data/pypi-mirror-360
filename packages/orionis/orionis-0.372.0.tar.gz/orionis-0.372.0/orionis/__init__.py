f1aa46599b7419dc6c62f435757c3afb733459b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:21 +0000

    Add package orionis/0.372.0/orionis-0.372.0.tar.gz

---
+++ packages/orionis/orionis-0.372.0.tar.gz/orionis-0.372.0/orionis/__init__.py
@@ -0,0 +1,7 @@
+from orionis.foundation.application import Application as Orionis
+from orionis.foundation.contracts.application import IApplication as IOrionis
+
+__all__ = [
+    "Orionis",
+    "IOrionis"
+]
