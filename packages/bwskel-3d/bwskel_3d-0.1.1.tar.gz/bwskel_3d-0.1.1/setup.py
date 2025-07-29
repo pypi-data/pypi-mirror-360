b43ae2b4169a8f05394184351d43d3d98892784c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:31 +0000

    Add package bwskel-3d/0.1.1/bwskel_3d-0.1.1.tar.gz

---
+++ packages/bwskel-3d/bwskel_3d-0.1.1.tar.gz/bwskel_3d-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bwskel_3d',
+    version='0.1.1',
+    description='3D binary skeletonization and pruning, Python replication of MATLAB bwskel',
+    author='Yunze Du',
+    url='https://github.com/dujay971226/bwskel',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-image'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
