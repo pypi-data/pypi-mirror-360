1b14c24512da7ece9f105f65a9a17f2321e062a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:57 +0000

    Add package allsafe-auth/1.1.5/allsafe_auth-1.1.5-py3-none-any.whl

---
+++ packages/allsafe-auth/allsafe_auth-1.1.5-py3-none-any.whl/allsafe_auth/user_management/resolvers/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py for resolvers package
+
+from .ldap_resolver import *  # LDAP Resolver
+from .mysql_resolver import *  # MySQL Resolver
+from .postgresql_resolver import *  # PostgreSQL Resolver
+from .mongodb_resolver import *  # MongoDB Resolver
+from .resolver_manager import *  # Resolver Management
