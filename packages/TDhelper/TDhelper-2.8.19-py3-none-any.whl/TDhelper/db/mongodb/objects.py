7169dd79bdef1598306b0190da80a4b02c036aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:13:20 +0000

    Add package TDhelper/2.8.19/TDhelper-2.8.19-py3-none-any.whl

---
+++ packages/TDhelper/TDhelper-2.8.19-py3-none-any.whl/TDhelper/db/mongodb/objects.py
@@ -0,0 +1,22 @@
+from TDhelper.db.mongodb.dbhelper import dbhelper
+class Objects(dbhelper): 
+    @classmethod
+    def filter(cls,**kwargs):
+        result= cls.find(**kwargs)
+        return result
+    
+    @classmethod    
+    def getById(cls,oId):
+        return cls.findOne(**{"oId":oId})
+    
+    @classmethod
+    def delete(cls,**kwargs):
+        cls.remove(**kwargs)
+        
+    @classmethod
+    def update(cls,condition:dict,**kwargs):
+        return cls.update(condition,**kwargs)
+    
+    @classmethod
+    def insert(cls,flag="one",**kwargs):
+        return cls.insert(flag,**kwargs)
