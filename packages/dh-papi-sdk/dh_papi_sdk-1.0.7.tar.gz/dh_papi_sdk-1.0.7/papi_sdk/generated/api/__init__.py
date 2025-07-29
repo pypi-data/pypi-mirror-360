4696e1158c368dd98861e79f077580a7428d1f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:56 +0000

    Add package dh-papi-sdk/1.0.7/dh_papi_sdk-1.0.7.tar.gz

---
+++ packages/dh-papi-sdk/dh_papi_sdk-1.0.7.tar.gz/dh_papi_sdk-1.0.7/papi_sdk/generated/api/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+# import apis into api package
+from ..api.auth_api import AuthApi
+from ..api.ccs_api import CcsApi
+from ..api.citi_api import CitiApi
+from ..api.delivery_areas_api import DeliveryAreasApi
+from ..api.dowjones_api import DowjonesApi
+from ..api.markups_api import MarkupsApi
+from ..api.ocus_api import OcusApi
+from ..api.sales_cockpit_api import SalesCockpitApi
+from ..api.sccs_api import SccsApi
+from ..api.shopify_api import ShopifyApi
+from ..api.vrm_api import VrmApi
+from ..api.vrm_bulk_api import VrmBulkApi
+from ..api.vrm_std_api import VrmStdApi
+
