4da2cb5853c3a1a4ea81b05092822edb06aaf069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:18 +0000

    Add package aivoice-python/0.1.3/aivoice_python-0.1.3.tar.gz

---
+++ packages/aivoice-python/aivoice_python-0.1.3.tar.gz/aivoice_python-0.1.3/aivoice_python/__init__.py
@@ -0,0 +1,27 @@
+"""
+A.I.VOICE Editor API Python Wrapper
+
+A Python library for controlling A.I.VOICE Editor through its API.
+"""
+
+from .aivoice_control import (
+    AIVoiceTTsControl, 
+    HostStatus, 
+    TextEditMode,
+    VoicePreset,
+    Style,
+    MergedVoice
+)
+
+__version__ = "0.1.3"
+__author__ = "yupix"
+__email__ = "yupi0982@outlook.jp"
+
+__all__ = [
+    "AIVoiceTTsControl",
+    "HostStatus", 
+    "TextEditMode",
+    "VoicePreset",
+    "Style",
+    "MergedVoice"
+]
