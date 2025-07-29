ec44846b9f72379e0435428adf423ef2dea0eec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:46 +0000

    Add package error-narrator/0.2.2/error_narrator-0.2.2-py3-none-any.whl

---
+++ packages/error-narrator/error_narrator-0.2.2-py3-none-any.whl/error_narrator/__init__.py
@@ -0,0 +1,9 @@
+"""
+Error Narrator
+A Python library to get AI-powered explanations for exceptions using Hugging Face models.
+"""
+
+from .narrator import ErrorNarrator
+
+__version__ = "0.2.2"
+__all__ = ['ErrorNarrator'] 
