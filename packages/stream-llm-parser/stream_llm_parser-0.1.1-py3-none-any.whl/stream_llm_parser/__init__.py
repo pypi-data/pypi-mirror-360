8179396d1616a5ec21db0a963c2f5cfc43d1cf87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:08:16 +0000

    Add package stream-llm-parser/0.1.1/stream_llm_parser-0.1.1-py3-none-any.whl

---
+++ packages/stream-llm-parser/stream_llm_parser-0.1.1-py3-none-any.whl/stream_llm_parser/__init__.py
@@ -0,0 +1,16 @@
+
+from .base_parser import SpecialTokenConfig, StreamSpecialTokenParser
+
+__version__ = "0.1.1"
+__author__ = "Ye Ting"
+__author_email__ = "yeting2938@163.com"
+__license__ = "MIT"
+__url__ = "https://github.com/TING2938/stream_llm_parser"
+
+PYPI_SIMPLE_ENDPOINT: str = "https://pypi.org/project/stream_llm_parser"
+
+__all__ = [
+    "SpecialTokenConfig",
+    "StreamSpecialTokenParser",
+    "PYPI_SIMPLE_ENDPOINT",
+]
