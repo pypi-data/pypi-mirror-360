7e679a80a4cf1419a5aeaec73aa3befabcac5445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:59 +0000

    Add package malachi/1.0.3/malachi-1.0.3.tar.gz

---
+++ packages/malachi/malachi-1.0.3.tar.gz/malachi-1.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="malachi",
+    version="1.0.3",
+    author="Tarmica Chiwara",
+    author_email="tarimicac@gmail.com",
+    description="A Python module for Windows 10 toast notifications",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lordskyzw/malachi",
+    packages=find_packages(),
+    install_requires=[
+        "pywin32"
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.6",
+)
