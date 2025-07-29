df134258d64e3d13ccfb09adffc7547553eff9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:10:10 +0000

    Add package yacv-server/0.9.6/yacv_server-0.9.6-cp312-cp312-manylinux_2_39_x86_64.whl

---
+++ packages/yacv-server/yacv_server-0.9.6-cp312-cp312-manylinux_2_39_x86_64.whl/yacv_server/frontend/___vite-browser-external_commonjs-proxy-BbkeXf_d.js
@@ -0,0 +1,2 @@
+import{bR as e}from"./index-CcRvB69z.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
+//# sourceMappingURL=___vite-browser-external_commonjs-proxy-BbkeXf_d.js.map
