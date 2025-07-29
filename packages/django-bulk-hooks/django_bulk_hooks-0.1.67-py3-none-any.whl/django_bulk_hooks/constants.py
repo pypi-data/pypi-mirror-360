93de927e6ccf93650451d8c120c2f3a1c63421db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:40 +0000

    Add package django-bulk-hooks/0.1.67/django_bulk_hooks-0.1.67-py3-none-any.whl

---
+++ packages/django-bulk-hooks/django_bulk_hooks-0.1.67-py3-none-any.whl/django_bulk_hooks/constants.py
@@ -0,0 +1,6 @@
+BEFORE_CREATE = "before_create"
+AFTER_CREATE = "after_create"
+BEFORE_UPDATE = "before_update"
+AFTER_UPDATE = "after_update"
+BEFORE_DELETE = "before_delete"
+AFTER_DELETE = "after_delete"
