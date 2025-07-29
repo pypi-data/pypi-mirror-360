8483e2d626ba9c25f6b1d06117e3c6eb50ee583d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:16 +0000

    Add package pywebwizard/1.3.15/pywebwizard-1.3.15-py3-none-any.whl

---
+++ packages/pywebwizard/pywebwizard-1.3.15-py3-none-any.whl/pywebwizard/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'pywebwizard'
+copyright = '2024, ConnorLilHomer'
+author = 'ConnorLilHomer'
+release = '1.2.54'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # Para soportar docstrings estilo Google o NumPy
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
