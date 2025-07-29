875c5c68e0484bdf3368b962463ece6a748c9d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:18 +0000

    Add package withpi/1.50.8/withpi-1.50.8-py3-none-any.whl

---
+++ packages/withpi/withpi-1.50.8-py3-none-any.whl/withpi/types/shared_params/__init__.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .example import Example as Example
+from .question import Question as Question
+from .scoring_spec import ScoringSpec as ScoringSpec
+from .exploration_mode import ExplorationMode as ExplorationMode
+from .scoring_dimension import ScoringDimension as ScoringDimension
+from .query_fanout_example import QueryFanoutExample as QueryFanoutExample
+from .scoring_sub_dimension import ScoringSubDimension as ScoringSubDimension
