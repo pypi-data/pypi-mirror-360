252da0d3d2dcfdc73d56242b3cef6beaa339d095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:21 +0000

    Add package gr-libs/0.2.7/gr_libs-0.2.7-py3-none-any.whl

---
+++ packages/gr-libs/gr_libs-0.2.7-py3-none-any.whl/tests/test_gcaura.py
@@ -0,0 +1,15 @@
+from gr_libs.tutorials.gcaura_panda_tutorial import run_gcaura_panda_tutorial
+from gr_libs.tutorials.gcaura_parking_tutorial import run_gcaura_parking_tutorial
+from gr_libs.tutorials.gcaura_point_maze_tutorial import run_gcaura_point_maze_tutorial
+
+
+def test_gcaura_panda_tutorial():
+    run_gcaura_panda_tutorial()
+
+
+def test_gcaura_parking_tutorial():
+    run_gcaura_parking_tutorial()
+
+
+def test_gcaura_point_maze_tutorial():
+    run_gcaura_point_maze_tutorial()
