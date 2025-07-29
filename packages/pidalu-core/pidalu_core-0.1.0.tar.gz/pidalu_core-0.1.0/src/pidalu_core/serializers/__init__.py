e2889a6924a49f79b4f129dca2bd99a92751e55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:00 +0000

    Add package pidalu-core/0.1.0/pidalu_core-0.1.0.tar.gz

---
+++ packages/pidalu-core/pidalu_core-0.1.0.tar.gz/pidalu_core-0.1.0/src/pidalu_core/serializers/__init__.py
@@ -0,0 +1,11 @@
+from .dynamic_serializer import (
+    DynamicFieldsModelSerializer,
+    DynamicSerializer,
+    DummySerializer,
+)
+
+__all__ = [
+    "DynamicFieldsModelSerializer",
+    "DynamicSerializer",
+    "DummySerializer",
+]
