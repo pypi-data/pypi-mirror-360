613af8df9a8da67c786e0d20353b5a8a44354572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:00 +0000

    Add package pyteda/0.1.2/pyteda-0.1.2.tar.gz

---
+++ packages/pyteda/pyteda-0.1.2.tar.gz/pyteda-0.1.2/teda/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 00:43:05 2020
+
+@author: ElíasN
+"""
+
+# Expose main submodules as namespaces
+from . import simulation
+from . import analysis
+from . import background
+from . import models
+from . import observation
+
