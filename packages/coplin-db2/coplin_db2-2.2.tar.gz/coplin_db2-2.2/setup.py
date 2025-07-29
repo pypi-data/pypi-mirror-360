2dea7e4698e9fb904f7c613717657f4eeb989114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:07 +0000

    Add package coplin-db2/2.2/coplin_db2-2.2.tar.gz

---
+++ packages/coplin-db2/coplin_db2-2.2.tar.gz/coplin_db2-2.2/setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from pathlib import Path
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+
+about = dict()
+with open(os.path.join(this_directory, 'db2', '__version__.py'), 'r', encoding='utf-8') as read_file:
+    exec(read_file.read(), about)
+
+with open(os.path.join(this_directory, 'README.md'), 'r', encoding='utf-8') as read_file:
+    long_description = read_file.read()
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    url=about['__url__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    description=about['__description__'],
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    license=about['__license__'],
+    packages=['db2', 'db2.utils'],
+    py_modules=['db2'],
+    install_requires=['numpy', 'pandas'] + (
+        ['ibm_db==3.1.4'] if sys.platform.startswith('win') else ['ibm_db']
+    ),
+    python_requires='>=3.8,<3.12'
+)
