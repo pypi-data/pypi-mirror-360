f8f4b28881290c83b4b4167538f49f142870b98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:10 +0000

    Add package py-pd-stepper/0.1.0/py_pd_stepper-0.1.0-py3-none-any.whl

---
+++ packages/py-pd-stepper/py_pd_stepper-0.1.0-py3-none-any.whl/pd_stepper/controller.py
@@ -0,0 +1,37 @@
+from .serial_port import SerialPort
+
+class Controller:
+    def __init__(self, port: SerialPort):
+        self.__serial_port = port
+
+    def get_status(self):
+        return self.__serial_port.communicate('status')
+
+    def turn_light_on(self):
+        self.__serial_port.communicate('setLight:0')
+
+    def turn_light_off(self):
+        self.__serial_port.communicate('setLight:1')
+
+    def set_target_position(self, position: int):
+        self.__serial_port.communicate(f'setTarget:{position}')
+
+    def set_acceleration(self, accel: int):
+        self.__serial_port.communicate(f'setAcceleration:{accel}')
+
+    def run_motor(self, speed: float):
+        """
+        run motor permanently on given speed
+        """
+        self.__serial_port.communicate(f'runMotor:{speed}')
+
+    def stop_motor(self):
+        self.__serial_port.communicate('stopMotor')
+
+    def set_voltage(self, voltage: int):
+        if voltage not in [5,9,12,15,20]:
+            raise ValueError("Voltage must be 5, 9, 12, 15 or 20")
+        self.__serial_port.communicate(f'setVoltage:{voltage}')
+
+    def set_speed(self, speed: float):
+        self.__serial_port.communicate(f'setSpeed:{speed}')
