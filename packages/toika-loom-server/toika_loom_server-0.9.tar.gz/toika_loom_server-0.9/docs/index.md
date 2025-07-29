3b4b66ccb07682bce8153807d3c40eeeb2e6eb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:27 +0000

    Add package toika-loom-server/0.9/toika_loom_server-0.9.tar.gz

---
+++ packages/toika-loom-server/toika_loom_server-0.9.tar.gz/toika_loom_server-0.9/docs/index.md
@@ -0,0 +1,23 @@
+# Toika Web Server
+
+Software that allows you to control a Toika dobby loom from any modern web browser, e.g. on a phone or tablet.
+
+For more information, read the [main documentation](https://r-owen.github.io/base_loom_server/).
+
+You must decide whether the control direction (weave/unweave) using the REVERSE button on the dobby head or the direction button in the browser display.
+Read more about this under [Weave Direction](https://r-owen.github.io/base_loom_server/weaving/#weave-direction).
+Make your choice in the Settings panel.
+
+## Links
+
+toika_loom_server depends on [base_loom_server](https://pypi.org/project/base-loom-server/), which does most of the work and has the [main documentation](https://r-owen.github.io/base_loom_server/).
+
+toka_loom_server is served at [PyPI](https://pypi.org/project/toika-loom-server/).
+The source code is at [github](https://r-owen.github.io/toika_loom_server/).
+I suggest filing bug reports using [base_loom_server's issue tracker](https://github.com/r-owen/toika_loom_server/issues).
+
+## Acknowledgements
+
+Thanks to Jukka Yrjölä and Jarkko Yrjölä from Toika for providing the API and helpful clarification.
+
+Thanks to WillowGoose ([willowlovestoweave](https://www.instagram.com/willowlovestoweave/) on Instagram) for patiently testing this software and proving valuable feedback.
---
+++ packages/toika-loom-server/toika_loom_server-0.9.tar.gz/toika_loom_server-0.9/src/toika_loom_server/mock_loom.py
@@ -0,0 +1,81 @@
+__all__ = ["MockLoom"]
+
+from base_loom_server.base_mock_loom import BaseMockLoom
+
+from toika_loom_server.utils import shaft_word_from_bytes
+
+
+class MockLoom(BaseMockLoom):
+    """Simulate a Toika ES dobby loom.
+
+    Parameters
+    ----------
+    verbose : bool
+        If True, log diagnostic information.
+
+    The user controls this loom by:
+
+    * Call command_reader.create_writer() to create a command writer.
+    * Call reply_writer.create_reader() to create a reply reader.
+    * Read replies from the reply reader.
+    * Write commands to the command writer.
+    """
+
+    terminator = b""
+
+    def __post_init__(self) -> None:
+        if self.num_shafts % 8 != 0:
+            raise ValueError(f"num_shafts={self.num_shafts} must be a multiple of 8")
+
+    async def basic_read(self) -> bytes:
+        """Read one command to the loom.
+
+        Perform no error checking, except that self.reader exists.
+        """
+        assert self.reader is not None  # make mypy happy
+        return await self.reader.readexactly(self.num_shafts // 8)
+
+    async def handle_read_bytes(self, read_bytes: bytes) -> None:
+        """Handle one command from the web server."""
+
+        if len(read_bytes) != self.num_shafts // 8:
+            self.log.warning(
+                f"MockLoom: ignoring command {read_bytes!r}; length != {self.num_shafts // 8}"
+            )
+            return
+
+        if not self.pick_wanted:  # type: ignore
+            self.log.warning(
+                f"MockLoom: ignoring command: {read_bytes!r}; pick not wanted"
+            )
+            return
+
+        self.shaft_word = shaft_word_from_bytes(
+            read_bytes, num_bytes=self.num_shafts // 8
+        )
+        self.pick_wanted = False
+        await self.report_pick_wanted()
+        await self.report_shafts()
+
+    async def move(self, shaft_word: int) -> None:
+        """This loom does not report shafts moving, so skip the usual code."""
+        self.moving = False  # paranoia; it should never be set
+        self.shaft_word = shaft_word
+        await self.report_shafts()
+
+    async def report_direction(self) -> None:
+        if self.verbose:
+            self.log.info(f"{self}.direction_forward={self.direction_forward}")
+
+    async def report_motion_state(self) -> None:
+        if self.verbose:
+            self.log.info(f"{self}.moving={self.moving}")
+
+    async def report_pick_wanted(self) -> None:
+        if self.pick_wanted:
+            reply = b"\x01" if self.direction_forward else b"\x02"
+            await self.write(reply)
+
+    async def report_shafts(self) -> None:
+        if self.verbose:
+            self.log.info(f"{self}.shaft_word={self.shaft_word}")
