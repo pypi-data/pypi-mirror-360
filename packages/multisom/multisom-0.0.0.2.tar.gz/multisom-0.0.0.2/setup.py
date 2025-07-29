9705e883b0004898aa035648fb2b79c3e04d70fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:50 +0000

    Add package multisom/0.0.0.2/multisom-0.0.0.2.tar.gz

---
+++ packages/multisom/multisom-0.0.0.2.tar.gz/multisom-0.0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MultiSOM',
+    version='0.0.0.2',
+    author='E.Bringa-F.Aquistapacce-SiMaF',
+    author_email='tatoaquistapacce@gmail.com',
+    license='MIT',
+    description='Defect analysis and vacancy calculation for materials science',
+    url='https://github.com/SIMAF-MDZ/MultiSOM',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=['pandas','numpy'],
+    include_package_data=True,
+)
