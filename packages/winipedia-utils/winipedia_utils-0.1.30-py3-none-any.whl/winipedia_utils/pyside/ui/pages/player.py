d2896fe8bc3a7f46b95514bf2efe4a0238ba8cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:14 +0000

    Add package winipedia-utils/0.1.30/winipedia_utils-0.1.30-py3-none-any.whl

---
+++ packages/winipedia-utils/winipedia_utils-0.1.30-py3-none-any.whl/winipedia_utils/pyside/ui/pages/player.py
@@ -0,0 +1,26 @@
+"""Player page module.
+
+This module contains the player page class for the VideoVault application.
+"""
+
+from typing import final
+
+from winipedia_utils.pyside.ui.pages.base.base import Base as BasePage
+from winipedia_utils.pyside.ui.widgets.media_player import MediaPlayer
+
+
+class Player(BasePage):
+    """Player page for the VideoVault application."""
+
+    @final
+    def setup(self) -> None:
+        """Setup the UI."""
+        self.media_player = MediaPlayer(self.v_layout)
+
+    @final
+    def play_data(self, data: bytes, name: str) -> None:
+        """Play the video."""
+        # set current page to player
+        self.set_page()
+        # Stop current playback and clean up resources
+        self.media_player.play_data(data, name)
