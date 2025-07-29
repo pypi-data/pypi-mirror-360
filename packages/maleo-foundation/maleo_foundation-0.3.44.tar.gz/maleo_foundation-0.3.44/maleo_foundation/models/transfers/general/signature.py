1b38fa2401a23934d8e8b1ec01c0dd135c7426be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:46 +0000

    Add package maleo-foundation/0.3.44/maleo_foundation-0.3.44.tar.gz

---
+++ packages/maleo-foundation/maleo_foundation-0.3.44.tar.gz/maleo_foundation-0.3.44/maleo_foundation/models/transfers/general/signature.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from maleo_foundation.models.schemas.signature import MaleoFoundationSignatureSchemas
+
+class BaseSignatureGeneralTransfers:
+    class SignaturePackage(
+        MaleoFoundationSignatureSchemas.Message,
+        MaleoFoundationSignatureSchemas.Signature
+    ): pass
