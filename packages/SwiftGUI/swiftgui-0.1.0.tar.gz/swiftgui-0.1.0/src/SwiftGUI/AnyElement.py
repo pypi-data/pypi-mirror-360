3ae15686ff577c99ba33bd6155275e54fb7c9d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:52 +0000

    Add package SwiftGUI/0.1.0/swiftgui-0.1.0.tar.gz

---
+++ packages/SwiftGUI/swiftgui-0.1.0.tar.gz/swiftgui-0.1.0/src/SwiftGUI/AnyElement.py
@@ -0,0 +1,7 @@
+from SwiftGUI import BaseElement,BaseWidget,BaseWidgetContainer,Button,Input,Form,Frame
+
+# Typehint for any element returned
+AnyElement = BaseElement | BaseWidget | BaseWidgetContainer | Button | Input | Form | Frame
+
+
+
---
+++ packages/SwiftGUI/swiftgui-0.1.0.tar.gz/swiftgui-0.1.0/src/SwiftGUI/WidgetsAdvanced.py
@@ -0,0 +1,56 @@
+from collections.abc import Iterable
+from SwiftGUI import BaseElement, Frame, Text, Input
+
+
+# Advanced / Combined elements
+
+class Form(BaseElement):
+    """
+    Grid-Layout-Form with text-Input-combinations
+
+    Still very WIP (of course), just a proof of concept
+    """
+
+    def __init__(
+            self,
+            texts:Iterable[str],
+            key:any = "",
+            seperate_keys:bool=False,   # Key for every input
+    ):
+        self.key = key
+        self.texts = texts
+
+        self.layout = [
+            [
+                Text(line),
+                Input(key=key + line if seperate_keys else None),
+            ] for line in texts
+        ]
+
+        self._sg_widget = Frame(self.layout)
+
+    def _personal_init(self):
+        self._sg_widget._init(self,self.window)
+
+    def _get_value(self) -> any:
+        return {
+            line:elem[1].value for line,elem in zip(self.texts,self.layout)
+        }
+
+    def set_value(self,val:dict[str:str]):
+        """
+        Update only passed keys with their value
+        :param val:
+        :return:
+        """
+        for i,text in enumerate(self.texts):
+            if text in val.keys():
+                self.layout[i][1].value = val[text]
+
+    def clear_all_values(self):
+        """
+        Does what it says
+        :return:
+        """
+        for i,_ in enumerate(self.texts):
+            self.layout[i][1].value = ""
