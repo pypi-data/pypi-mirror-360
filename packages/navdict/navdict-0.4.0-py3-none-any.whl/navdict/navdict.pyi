181176cafe9d8618e2fc303ffad791732fc98534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:45 +0000

    Add package navdict/0.4.0/navdict-0.4.0-py3-none-any.whl

---
+++ packages/navdict/navdict-0.4.0-py3-none-any.whl/navdict/navdict.pyi
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import TypeAlias
+
+def load_yaml(resource_name: str, parent: NavigableDict | None = ...) -> NavDict: ...
+def load_csv(resource_name: str, *args, parent: navdict | None = None, **kwargs): ...
+
+class NavigableDict(dict):
+    def __init__(
+        self,
+        head: dict | None = ...,
+        label: str | None = ...,
+        _filename: str | Path | None = ...,
+    ): ...
+    @staticmethod
+    def from_yaml_file(filename: str | Path | None = ...) -> NavigableDict: ...
+
+navdict: TypeAlias = NavigableDict
+NavDict: TypeAlias = NavigableDict
