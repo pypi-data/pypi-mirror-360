e81df62edff27c1bd8f118b04192a3a581ce1a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:09 +0000

    Add package passagemath-coin-or-cbc/10.4.1/passagemath_coin_or_cbc-10.4.1.tar.gz

---
+++ packages/passagemath-coin-or-cbc/passagemath_coin_or_cbc-10.4.1.tar.gz/passagemath_coin_or_cbc-10.4.1/README.md
@@ -0,0 +1,70 @@
+# COIN-OR mixed integer linear programming backend for SageMath (sage-numerical-backends-coin fork)
+
+[![PyPI](https://img.shields.io/pypi/v/passagemath-coin-or-cbc)](https://pypi.org/project/passagemath-coin-or-cbc/ "PyPI: passagemath-coin-or-cbc")
+[![GitHub Workflow Status](https://github.com/passagemath/passagemath-coin-or-cbc/workflows/Build%20and%20test%20Python%20package/badge.svg)](https://github.com/passagemath/passagemath-coin-or-cbc/actions "GitHub Actions: passagemath-coin-or-cbc")
+
+`CoinBackend` has previously been available as part of the [SageMath](http://www.sagemath.org/) source tree,
+from which it is built as an "optional extension" when the `cbc` Sage package is installed.
+However, it has not been available in binary distributions.
+
+The standalone Python package `sage-numerical-backends-coin` was created from the SageMath sources, version 9.0.beta10; the in-tree version of `CoinBackend` has been removed in Sage ticket https://trac.sagemath.org/ticket/28175.  SageMath 9.1 and later makes the package available as an optional Sage package (SPKG).
+
+The current version of this package is the compatible fork `passagemath-coin-or-cbc`.
+
+It can also be installed on top of various Sage installations using pip.
+(Your installation of Sage must be based on Python 3; if your SageMath is version 9.2 or newer, it is.)
+
+## Installation
+
+CBC can either be installed using its Sage package using
+
+    $ sage -i cbc
+
+or any of the methods explained at https://github.com/coin-or/Cbc .
+
+This package finds the CBC installation by means of ``pkgconfig``.
+
+Install this package from PyPI using
+
+    $ sage -pip install passagemath-coin-or-cbc
+
+or from GitHub using
+
+    $ sage -pip install git+https://github.com/passagemath/passagemath-coin-or-cbc
+
+(See [`.github/workflows/build.yml`](.github/workflows/build.yml) for details about package prerequisites on various systems.)
+
+## Using this package
+
+After a successful installation, Sage will automatically make this new backend
+the default MIP solver.
+
+To select the `'Coin'` solver explicitly as the default MIP backend, additionally use the following command.
+
+    sage: default_mip_solver('Coin')
+
+To make these settings permanent, add this command to your `~/.sage/init.sage` file.
+Note that this setting will not affect doctesting (`sage -t`) because this file is ignored in doctesting mode.
+
+## Running doctests
+
+To run the (limited) testsuite of this package, use:
+
+    $ sage setup.py test
+
+To run the Sage testsuite with the default MIP solver set to the backend provided by this package, use:
+
+    $ sage setup.py check_sage_testsuite
+
+## Running tests with tox
+
+The doctests can also be invoked using `tox`:
+
+    $ tox -e local
+    $ tox -e local-sage_testsuite
+
+If you have `docker` installed, more tests can be run:
+
+    $ tox -e docker-sage_binary-cbc_coinbrew
+
+See `tox.ini` for the available options.
---
+++ packages/passagemath-coin-or-cbc/passagemath_coin_or_cbc-10.4.1.tar.gz/passagemath_coin_or_cbc-10.4.1/setup.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python
+## -*- encoding: utf-8 -*-
+
+import os
+import sys
+from setuptools import setup
+from setuptools import Extension
+from setuptools.command.test import test as TestCommand # for tests
+from Cython.Build import cythonize
+from Cython.Compiler.Errors import CompileError
+from codecs import open # To open the README file with proper encoding
+from sage.env import sage_include_directories
+
+# For the tests
+class SageTest(TestCommand):
+    def run_tests(self):
+        # Passing optional=sage avoids using sage.misc.package.list_packages,
+        # which gives an error on Debian unstable as of 2019-12-27:
+        # FileNotFoundError: [Errno 2] No such file or directory: '/usr/share/sagemath/build/pkgs'
+        errno = os.system("PYTHONPATH=`pwd` sage -t --force-lib --optional=sage sage_numerical_backends_coin")
+        if errno != 0:
+            sys.exit(1)
+
+class SageTestSage(SageTest):
+    def run_tests(self):
+        errno = os.system("PYTHONPATH=`pwd` sage -c 'load(\"check_sage_testsuite.py\")'")
+        if errno != 0:
+            sys.exit(1)
+
+# Get information from separate files (README, VERSION)
+def readfile(filename):
+    with open(filename, encoding='utf-8') as f:
+        return f.read()
+
+ # Cython modules
+import pkgconfig
+
+try:
+    cbc_pc = pkgconfig.parse('cbc')
+except pkgconfig.PackageNotFoundError:    # exception handling from sage trac #28883 for pkgconfig version 1.5.1
+    from collections import defaultdict
+    cbc_pc = defaultdict(list, {'libraries': ['Cbc']})
+
+if cbc_pc:
+    print("Using pkgconfig: {}".format(sorted(cbc_pc.items())), file=sys.stderr)
+cbc_libs = cbc_pc['libraries']
+cbc_library_dirs = cbc_pc['library_dirs']
+cbc_include_dirs = cbc_pc['include_dirs']
+
+
+ext_modules = [Extension('sage_numerical_backends_coin.coin_backend',
+                         sources=[os.path.join('sage_numerical_backends_coin',
+                                    'coin_backend.pyx')],
+                         libraries=cbc_libs,
+                         include_dirs=sage_include_directories() + cbc_include_dirs,
+                         library_dirs=cbc_library_dirs,
+                         extra_compile_args=['-std=c++11'])
+    ]
+
+compile_time_env = {'HAVE_SAGE_CPYTHON_STRING': True,
+                    'HAVE_ADD_COL_UNTYPED_ARGS': True}
+print("Using compile_time_env: {}".format(compile_time_env), file=sys.stderr)
+
+if any(x in sys.argv
+       for x in ['build', 'build_ext', 'bdist_wheel', 'install']):
+    ext_modules = cythonize(ext_modules, include_path=sys.path,
+                            compile_time_env=compile_time_env)
+
+setup(
+    name="passagemath-coin-or-cbc",
+    version=readfile("VERSION").strip(),
+    description="COIN-OR Cbc backend for Sage MixedIntegerLinearProgram",
+    long_description = readfile("README.md"), # get the long description from the README
+    long_description_content_type='text/markdown', # https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+    url="https://github.com/passagemath/passagemath-coin-or-cbc",
+    # Author list obtained by running the following command on sage 9.0.beta9:
+    # for f in coin_backend.p*; do git blame -w -M -C -C --line-porcelain "$f" | grep -I '^author '; done | sort -f | uniq -ic | sort -n
+    # cut off at < 10 lines of attribution.
+    author='Nathann Cohen, Yuan Zhou, John Perry, Zeyi Wang, Martin Albrecht, Jori Mäntysalo, Matthias Koeppe, Erik M. Bray, Jeroen Demeyer, Nils Bruin, Julien Puydt, Dima Pasechnik, and others',
+    author_email='mkoeppe@math.ucdavis.edu',
+    license='GPLv2+', # This should be consistent with the LICENCE file
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 "Intended Audience :: Science/Research",
+                 'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+                 "Programming Language :: Python",
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10',
+                 'Programming Language :: Python :: 3.11',
+                 'Programming Language :: Python :: 3.12',
+                 'Programming Language :: Python :: 3.13',
+                 ],
+    ext_modules=ext_modules,
+    cmdclass = {'test': SageTest, 'check_sage_testsuite': SageTestSage}, # adding a special setup command for tests
+    keywords=['milp', 'linear-programming', 'optimization'],
+    packages=['sage_numerical_backends_coin'],
+    package_dir={'sage_numerical_backends_coin': 'sage_numerical_backends_coin'},
+    package_data={'sage_numerical_backends_coin': ['*.pxd']},
+    install_requires = ['passagemath-categories'],
+    extras_require = {'doc': ['sphinx']},
+)
