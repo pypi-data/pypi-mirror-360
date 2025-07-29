332ec537f61ca9ab3647a90f996a1cd643d85832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:26 +0000

    Add package raj-operands/0.1/raj_operands-0.1-py3-none-any.whl

---
+++ packages/raj-operands/raj_operands-0.1-py3-none-any.whl/raj_operands/__init__.py
@@ -0,0 +1,7 @@
+def operands(operand1,operand2):
+    if  operand1==operand2:
+        print("{}and {} operands are same".format(operand1,operand2))
+    else:
+        print("{}and{} operands are not same".format(operand1,operand2))
+operands(10,10)        
+
