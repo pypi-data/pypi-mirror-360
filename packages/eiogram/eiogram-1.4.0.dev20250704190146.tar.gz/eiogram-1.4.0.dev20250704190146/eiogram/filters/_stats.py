fd6d1ae56fe2597343c3247f03de0a7ae880b6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:17 +0000

    Add package eiogram/1.4.0.dev20250704190146/eiogram-1.4.0.dev20250704190146.tar.gz

---
+++ packages/eiogram/eiogram-1.4.0.dev20250704190146.tar.gz/eiogram-1.4.0.dev20250704190146/eiogram/filters/_stats.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from ._base import Filter
+from ..stats import State
+
+
+class StatsFilter(Filter):
+    def __init__(self, state: Optional[State]):
+        super().__init__(lambda stats: (stats is not None and stats == state.name))
+
+
+class IgnoreStatsFilter(Filter):
+    def __init__(self):
+        super().__init__(lambda x: True)
