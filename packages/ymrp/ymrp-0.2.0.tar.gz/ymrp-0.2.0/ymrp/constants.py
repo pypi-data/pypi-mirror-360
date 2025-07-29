2c76596f0bcc298ca25dfd88e1e6d2afb22de69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:20 +0000

    Add package ymrp/0.2.0/ymrp-0.2.0.tar.gz

---
+++ packages/ymrp/ymrp-0.2.0.tar.gz/ymrp-0.2.0/ymrp/constants.py
@@ -0,0 +1,25 @@
+# Date constants
+MONTHS = {
+    'января': '01',
+    'февраля': '02',
+    'марта': '03',
+    'апреля': '04',
+    'мая': '05',
+    'июня': '06',
+    'июля': '07',
+    'августа': '08',
+    'сентября': '09',
+    'октября': '10',
+    'ноября': '11',
+    'декабря': '12',
+}
+
+# Time constants
+SMALL_TIMEOUT = 5_000  # 5 seconds
+MEDIUM_TIMEOUT = 10_000  # 10 seconds
+BIG_TIMEOUT = 15_000  # 15 seconds
+
+# Selector constants
+REVIEWS_CONTAINER = '.business-reviews-card-view__reviews-container'
+REVIEW = '.business-reviews-card-view__review'
+REVIEW_VIEW_EXPAND = '.business-review-view__expand[aria-hidden="false"]'
