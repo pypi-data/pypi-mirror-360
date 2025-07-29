104160d4725a3aa25f2f4185d6b922d7708aac9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:21 +0000

    Add package Vectoryx/0.2.1/vectoryx-0.2.1-py3-none-any.whl

---
+++ packages/Vectoryx/vectoryx-0.2.1-py3-none-any.whl/Vectoryx/__init__.py
@@ -0,0 +1,13 @@
+from .Vectoryx import (
+    sum_vectors,
+    vector_dif,
+    dot_product,
+    multiply_vector_to_C,
+    vector_length,
+    normalize_vector,
+    cross_product,
+    is_colinear,
+    is_ortogonal,
+    cosinus_similarity,
+    v_angle
+)
