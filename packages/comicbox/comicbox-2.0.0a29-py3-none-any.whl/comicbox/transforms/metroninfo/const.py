ce0dbc6c6eec268e97668b333b8e6458a28d3b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:22 +0000

    Add package comicbox/2.0.0a29/comicbox-2.0.0a29-py3-none-any.whl

---
+++ packages/comicbox/comicbox-2.0.0a29-py3-none-any.whl/comicbox/transforms/metroninfo/const.py
@@ -0,0 +1,5 @@
+"""Metron Transform constants."""
+
+from comicbox.identifiers import IdSources
+
+DEFAULT_ID_SOURCE = IdSources.METRON.value
---
+++ packages/comicbox/comicbox-2.0.0a29-py3-none-any.whl/comicbox/transforms/metroninfo/identifier_attribute.py
@@ -0,0 +1,45 @@
+"""Metron ID attributes to comicbox identifiers transform."""
+
+from collections.abc import Mapping
+
+from loguru import logger
+
+from comicbox.identifiers.identifiers import create_identifier
+from comicbox.schemas.comicbox import ID_KEY_KEY, IDENTIFIERS_KEY
+from comicbox.transforms.metroninfo.const import DEFAULT_ID_SOURCE
+
+ID_ATTRIBUTE = "@id"
+
+
+def metron_id_attribute_to_cb(
+    id_type: str, metron_obj: Mapping | str, comicbox_obj: dict, id_source: str
+) -> None:
+    """Create a metron tag identifier from a metron identifier attribute."""
+    try:
+        if not (
+            isinstance(metron_obj, Mapping) and (id_key := metron_obj.get(ID_ATTRIBUTE))
+        ):
+            return
+        comicbox_identifier = create_identifier(
+            id_source, id_key, id_type=id_type, default_id_source=DEFAULT_ID_SOURCE
+        )
+        comicbox_obj[IDENTIFIERS_KEY] = {id_source: comicbox_identifier}
+    except Exception as exc:
+        logger.warning(f"Parsing metron tag identifier {id_type}:{metron_obj} - {exc}")
+
+
+def metron_id_attribute_from_cb(
+    metron_obj: dict, comicbox_obj: Mapping, primary_id_source: str
+) -> None:
+    """Crete a metron id attribute from comicbox identifier."""
+    comicbox_identifiers = comicbox_obj.get(IDENTIFIERS_KEY)
+    if not comicbox_identifiers:
+        return
+    for id_source, identifier in comicbox_identifiers.items():
+        if (
+            primary_id_source
+            and id_source == primary_id_source
+            and (id_key := identifier.get(ID_KEY_KEY))
+        ):
+            metron_obj[ID_ATTRIBUTE] = id_key
+            break
