ba5e408633bd4c9f42b0f9fe4a8c3f9ecade00e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:30 +0000

    Add package sphinxcontrib-osint/0.1.0/sphinxcontrib_osint-0.1.0-py3-none-any.whl

---
+++ packages/sphinxcontrib-osint/sphinxcontrib_osint-0.1.0-py3-none-any.whl/sphinxcontrib/osint/scripts/__init__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+"""
+The osint scripts
+------------------
+
+
+"""
+from __future__ import annotations
+
+__author__ = 'bibi21000 aka Sébastien GALLET'
+__email__ = 'bibi21000@gmail.com'
+
