24735f3975ffebe8561bdd55aab89129ee28d873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:34 +0000

    Add package diagnosticism/0.9.1/diagnosticism-0.9.1.tar.gz

---
+++ packages/diagnosticism/diagnosticism-0.9.1.tar.gz/diagnosticism-0.9.1/diagnosticism/__init__.py
@@ -0,0 +1,33 @@
+
+__author__      =   'Matt Wilson'
+__copyright__   =   'Copyright 2019-2025, Synesis Information Systems, Copyright 2019, Synesis Software'
+__credits__     =   [
+        'Garth Lancaster',
+        'Matt Wilson',
+ ]
+__email__       =   'matthew@synesis.com.au'
+__license__     =   'BSD-3-Clause'
+__maintainer__  =   'Matt Wilson'
+__status__      =   'Beta'
+__version__     =   '0.9.1'
+
+from .contingent_reporting import (
+    abort,
+    report,
+)
+from .logging import (
+    enable_logging,
+    is_logging_enabled,
+    is_severity_logged,
+    log,
+    set_log_filter,
+)
+from .program_name import *
+from .severity import *
+from .tracing import (
+    enable_tracing,
+    is_tracing_enabled,
+    trace,
+)
+from .warning import warn
+
