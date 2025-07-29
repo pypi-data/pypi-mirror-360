17338980cb99b06101b6ecab4d8f4514cca9b048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:27 +0000

    Add package flexmetric/0.3.0/flexmetric-0.3.0.tar.gz

---
+++ packages/flexmetric/flexmetric-0.3.0.tar.gz/flexmetric-0.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flexmetric",
+    version="0.3.0",
+    author="Nikhil Lingadhal",
+    description="A secure flexible Prometheus exporter for commands, databases, functions, and scripts.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nikhillingadhal1999/custom_prometheus_agent",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "prometheus_client",
+        "PyYAML",
+        "psutil",
+        "setuptools",
+        "wheel",
+        "twine",
+        "flask"
+    ],
+    entry_points={
+        "console_scripts": [
+            "flexmetric = flexmetric.metric_process.prometheus_agent:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
