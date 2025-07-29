d535c623ff0266e1ba3e8339c4cf629257ceb26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:16 +0000

    Add package simpleconfigfinder/0.2.1/simpleconfigfinder-0.2.1-py3-none-any.whl

---
+++ packages/simpleconfigfinder/simpleconfigfinder-0.2.1-py3-none-any.whl/simpleconfigfinder/__init__.py
@@ -0,0 +1,8 @@
+"""configfinder finds the next configuration file upstream"""
+
+from simpleconfigfinder.configfinder import (
+    ConfigNotFound,
+    config_finder,
+    config_walker,
+    find_file,
+)
