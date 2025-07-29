8e2256a7025eba2ec938cdc24d79982fc57b0e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:36 +0000

    Add package articuno/0.3.10/articuno-0.3.10.tar.gz

---
+++ packages/articuno/articuno-0.3.10.tar.gz/articuno-0.3.10/articuno/__init__.py
@@ -0,0 +1,6 @@
+from .convert import df_to_pydantic, infer_pydantic_model
+from .codegen import generate_pydantic_class_code
+
+
+__all__ = ["df_to_pydantic", "infer_pydantic_model", "generate_pydantic_class_code"]
+__version__ = "0.3.10"
