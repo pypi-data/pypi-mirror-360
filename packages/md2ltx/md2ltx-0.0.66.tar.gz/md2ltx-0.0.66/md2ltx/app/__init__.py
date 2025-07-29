ef414678b519c2a645085b46723ec8e42aa0d15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:19 +0000

    Add package md2ltx/0.0.66/md2ltx-0.0.66.tar.gz

---
+++ packages/md2ltx/md2ltx-0.0.66.tar.gz/md2ltx-0.0.66/md2ltx/app/__init__.py
@@ -0,0 +1,4 @@
+from .main import preprocess_markdown_file, compile_markdown_to_pdf
+from .constants import templates  # Import templates from md2ltx.app.constants
+
+__all__ = ['preprocess_markdown_file', 'compile_markdown_to_pdf', 'templates']
