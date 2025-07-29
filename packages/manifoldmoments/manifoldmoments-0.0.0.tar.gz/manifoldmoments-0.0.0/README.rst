aa73056ce0caed5ea215b8ecc4855a3bbdf9c6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:44 +0000

    Add package manifoldmoments/0.0.0/manifoldmoments-0.0.0.tar.gz

---
+++ packages/manifoldmoments/manifoldmoments-0.0.0.tar.gz/manifoldmoments-0.0.0/README.rst
@@ -0,0 +1,7 @@
+.. Copyright © 2025 Liu Zhang
+
+===============
+ManifoldMoments
+===============
+ 
+Version 0.0.0 — Initial release.
---
+++ packages/manifoldmoments/manifoldmoments-0.0.0.tar.gz/manifoldmoments-0.0.0/src/manifoldmoments/__init__.py
@@ -0,0 +1,6 @@
+"""
+Version 0.0.0 — Initial release
+"""
+raise ImportError(
+    "Implementation is not yet published in Version 0.0.0 — Initial release."
+)
