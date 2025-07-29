3b8c0bb2fa34e593c4eb0d455fbeeb8f2f7fdce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:56 +0000

    Add package performance-catcher/2.0.3/performance_catcher-2.0.3-py3-none-any.whl

---
+++ packages/performance-catcher/performance_catcher-2.0.3-py3-none-any.whl/perfcat/__init__.py
@@ -0,0 +1,36 @@
+import asyncio
+from . import pages, logger  # noqa
+import secrets
+from nicegui import Client, app, ui, App
+from importlib import resources
+from perfcat.services import AndroidProfielerService
+
+with resources.path("perfcat", "static") as path:
+    app.add_static_files("/static", path.as_posix())
+
+with resources.path("perfcat", "media") as path:
+    app.add_static_files(
+        "/media",
+        path.as_posix(),
+    )
+
+
+@app.on_shutdown
+async def teardown(app: App):
+    await AndroidProfielerService.stop_adb_server()
+
+
+def run(debug: bool = False):
+    app.native.window_args["resizable"] = True
+    app.native.window_args["text_select"] = True
+    app.native.window_args["easy_drag"] = True
+    app.native.start_args["debug"] = debug
+
+    ui.run(
+        native=True,
+        reload=False,
+        window_size=(1280, 800),
+        title="",
+        frameless=False,
+        storage_secret=secrets.token_hex(16),
+    )
