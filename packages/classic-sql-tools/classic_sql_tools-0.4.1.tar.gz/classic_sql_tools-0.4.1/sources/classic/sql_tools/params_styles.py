82b1e41ea50b038599cc90f27301bc426717477f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:55 +0000

    Add package classic-sql-tools/0.4.1/classic_sql_tools-0.4.1.tar.gz

---
+++ packages/classic-sql-tools/classic_sql_tools-0.4.1.tar.gz/classic_sql_tools-0.4.1/sources/classic/sql_tools/params_styles.py
@@ -0,0 +1,25 @@
+import sys
+
+
+class ParamStyleRecognizer:
+    known_styles: dict[type, str]
+
+    def __init__(self):
+        self.known_styles = {}
+
+    def get(self, conn: object) -> str:
+        try:
+            return self.known_styles[conn.__class__]
+        except KeyError:
+            modname = conn.__module__
+            while modname:
+                try:
+                    style = sys.modules[modname].paramstyle  # type: ignore
+                    self.known_styles[conn.__class__] = style
+                    return style
+                except AttributeError:
+                    if "." in modname:
+                        modname = modname.rsplit(".", 1)[0]
+                    else:
+                        break
+        raise TypeError(f"Can't find paramstyle for connection {conn!r}")
