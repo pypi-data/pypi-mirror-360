7910f6fa36d90587d00f12b8bd2176a9569c3e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:52 +0000

    Add package Vectoryx/0.1.2/vectoryx-0.1.2.tar.gz

---
+++ packages/Vectoryx/vectoryx-0.1.2.tar.gz/vectoryx-0.1.2/tests/tests.py
@@ -0,0 +1,17 @@
+from Vectoryx import sum_vectors, vector_dif, dot_product, multiply_vector_to_C, cross_product, normalize_vector, is_colinear, is_ortogonal
+
+print(sum_vectors([1,2,3], [5,6,7]))
+
+print(vector_dif([1,2,3], [5,6,7]))
+
+print(dot_product([1,2,3], [5,6,7]))
+
+print(multiply_vector_to_C([1,2,3], 5))
+
+print(cross_product([1,2,3], [5,6,7]))
+
+print(normalize_vector([1,2,3]))
+
+print(is_colinear([1,2,3], [5,6,7]))
+
+print(is_ortogonal([1,2,3], [5,6,7]))
