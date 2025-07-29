4cbbc7ae61406de0171332525ddacd435f0f2831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:28 +0000

    Add package chatty-shell/0.1.3/chatty_shell-0.1.3-py3-none-any.whl

---
+++ packages/chatty-shell/chatty_shell-0.1.3-py3-none-any.whl/chatty_shell/messages.py
@@ -0,0 +1,22 @@
+from langchain_core.messages import ToolMessage, AIMessage
+
+
+def sort_tools_calls(messages: list[ToolMessage | AIMessage]) -> dict[str, str]:
+    tool_calls = {}
+    for message in messages:
+        if not isinstance(message, AIMessage):
+            continue
+        if not message.tool_calls:
+            continue
+        for tool_call in message.tool_calls:
+            command = tool_call["args"]["command"]
+            id = tool_call["id"]
+            tool_calls[command] = id
+
+    for message in messages:
+        if not isinstance(message, ToolMessage):
+            continue
+        for command in tool_calls:
+            if tool_calls[command] == message.tool_call_id:
+                tool_calls[command] = message.content
+    return tool_calls
