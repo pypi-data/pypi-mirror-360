22ff152e4057d47aa8cc3835f4b802b03c0a4d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:08:07 +0000

    Add package hh-applicant-tool/0.5.8/hh_applicant_tool-0.5.8-py3-none-any.whl

---
+++ packages/hh-applicant-tool/hh_applicant_tool-0.5.8-py3-none-any.whl/hh_applicant_tool/operations/config.py
@@ -0,0 +1,36 @@
+import argparse
+import logging
+import os
+import subprocess
+
+from ..main import BaseOperation
+from ..main import Namespace as BaseNamespace
+
+logger = logging.getLogger(__package__)
+
+EDITOR = os.getenv("EDITOR", "nano")
+
+
+class Namespace(BaseNamespace):
+    print: bool
+
+
+class Operation(BaseOperation):
+    """Редактировать конфигурационный файл или показать путь до него"""
+
+    def setup_parser(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument(
+            "-p",
+            "--print",
+            type=bool,
+            default=False,
+            action=argparse.BooleanOptionalAction,
+            help="Напечатать путь и выйти",
+        )
+
+    def run(self, _, args: Namespace) -> None:
+        config_path = str(args.config._config_path)
+        if args.print:
+            print(config_path)
+        else:
+            subprocess.call([EDITOR, config_path])
