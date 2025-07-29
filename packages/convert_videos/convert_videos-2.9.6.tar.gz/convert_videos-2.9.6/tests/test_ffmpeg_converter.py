c63edbbd3f1bb5da414a0742e17b11f015476b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:22 +0000

    Add package convert_videos/2.9.6/convert_videos-2.9.6.tar.gz

---
+++ packages/convert_videos/convert_videos-2.9.6.tar.gz/convert_videos-2.9.6/tests/test_ffmpeg_converter.py
@@ -0,0 +1,158 @@
+import pytest
+from mock import mock_open, patch
+from video_utils import Codec
+
+from convert_videos import ffmpeg_converter
+from convert_videos.ffmpeg_converter import FFmpegConverter
+from convert_videos.settings import AudioSettings, VideoSettings
+
+
+@pytest.fixture
+@patch("os.path.exists", return_value=True)
+@patch("os.path.isfile", return_value=True)
+@patch("os.access", return_value=True)
+@patch("os.path.isdir")
+@patch("os.remove")
+def target(mock_remove, mock_isdir, mock_access, mock_isfile, mock_exists):
+    audio_settings = AudioSettings(codec=Codec("AAC"), channels=2, bitrate=120)
+
+    video_settings = VideoSettings(
+        codec=Codec("HEVC"), quality=25, preset="slow", encoder="software"
+    )
+
+    return FFmpegConverter(
+        source_file_path="/asdf/foo/bar.mkv",
+        video_settings=video_settings,
+        audio_settings=audio_settings,
+        destination_file_path="/asdf/temp/path.mkv",
+        extra_ffmpeg_input_args="",
+        extra_ffmpeg_output_args="",
+        dry_run=False,
+    )
+
+
+@patch.object(FFmpegConverter, "_generate_ffmpeg_settings", return_value="12345")
+@patch.object(ffmpeg_converter, "ffmpy")
+def test_process(mock_ffmpy, mock_settings, target):
+    target.process()
+    mock_ffmpy.FFmpeg.assert_called_with(
+        inputs={"/asdf/foo/bar.mkv": "12345"}, outputs={"/asdf/temp/path.mkv": "12345"}
+    )
+    mock_ffmpy.FFmpeg().run.assert_called()
+
+
+@patch.object(FFmpegConverter, "_generate_ffmpeg_settings", return_value="12345")
+@patch.object(ffmpeg_converter, "ffmpy")
+def test_process_dryrun(mock_ffmpy, mock_settings, target):
+    target.dry_run = True
+    target.process()
+    mock_ffmpy.FFmpeg.assert_called_with(
+        inputs={"/asdf/foo/bar.mkv": "12345"}, outputs={"/asdf/temp/path.mkv": "12345"}
+    )
+    mock_ffmpy.FFmpeg().run.assert_not_called()
+
+
+def test_generate_ffmpeg_output_settings(target):
+    result = target._generate_ffmpeg_settings("output")
+    assert (
+        result
+        == "-y -threads 0 -map 0:v -map 0:s? -c:s copy -vcodec libx265 -preset slow -crf 25 -strict -2 -map 0:a -acodec aac -ab 120k -ac 2 "
+    )
+
+
+def test_extra_output_args(target):
+    target.extra_ffmpeg_output_args = "-foo"
+    result = target._generate_ffmpeg_settings("output")
+    assert (
+        result
+        == "-y -threads 0 -map 0:v -map 0:s? -c:s copy -vcodec libx265 -preset slow -crf 25"
+        + " -strict -2 -map 0:a -acodec aac -ab 120k -ac 2 -foo"
+    )
+
+
+def test_generate_ffmpeg_input_settings(target):
+    result = target._generate_ffmpeg_settings("input")
+    assert result == " "
+
+
+def test_generate_ffmpeg_input_settings_nvidia_hw(target):
+    target.video_settings.encoder = "nvidia"
+    result = target._generate_ffmpeg_settings("input")
+    assert result == " -hwaccel cuda"
+
+
+def test_generate_ffmpeg_input_settings_intel_hw(target):
+    target.video_settings.encoder = "intel"
+    result = target._generate_ffmpeg_settings("input")
+    assert result == " -hwaccel qsv -hwaccel_output_format qsv"
+
+
+def test_extra_input_args(target):
+    target.extra_ffmpeg_input_args = "-foo"
+    result = target._generate_ffmpeg_settings("input")
+    assert result == " -foo"
+
+
+@patch("os.path.exists", return_value=True)
+@patch("os.path.isfile", return_value=True)
+@patch("os.access", return_value=True)
+@patch("os.path.isdir")
+@patch("os.remove")
+def test_validate_destination_happy_path_exists(
+    mock_remove, mock_isdir, mock_access, mock_isfile, mock_exists, target
+):
+    result = target._validate_destination()
+    assert result is True
+
+
+@patch("os.path.exists", return_value=True)
+@patch("os.path.isfile", return_value=True)
+@patch("os.access", return_value=False)
+@patch("os.path.isdir")
+@patch("os.remove")
+def test_validate_destination_unwritable(
+    mock_remove, mock_isdir, mock_access, mock_isfile, mock_exists, target
+):
+    with pytest.raises(IOError):
+        target._validate_destination()
+
+
+@patch("os.path.exists", return_value=True)
+@patch("os.path.isfile", return_value=False)
+@patch("os.access")
+@patch("os.path.isdir", return_falue=True)
+@patch("os.remove")
+def test_validate_destination_is_dir(
+    mock_remove, mock_isdir, mock_access, mock_isfile, mock_exists, target
+):
+    with pytest.raises(IOError):
+        target._validate_destination()
+
+
+@patch("os.path.exists", return_value=False)
+@patch("os.path.isfile")
+@patch("os.access")
+@patch("os.path.isdir")
+@patch("os.remove")
+def test_validate_destination_doesnt_exist(
+    mock_remove, mock_isdir, mock_access, mock_isfile, mock_exists, target
+):
+    m = mock_open()
+    with patch("builtins.open", m):
+        result = target._validate_destination()
+        assert result is True
+
+
+@patch("os.path.exists", return_value=False)
+@patch("os.path.isfile")
+@patch("os.access")
+@patch("os.path.isdir")
+@patch("os.remove")
+def test_validate_destination_invalid_path(
+    mock_remove, mock_isdir, mock_access, mock_isfile, mock_exists, target
+):
+    m = mock_open()
+    m.side_effect = FileNotFoundError
+    with patch("builtins.open", m):
+        with pytest.raises(FileNotFoundError):
+            target._validate_destination()
---
+++ packages/convert_videos/convert_videos-2.9.6.tar.gz/convert_videos-2.9.6/tests/test_video_processor.py
@@ -0,0 +1,220 @@
+import pytest
+from mock import Mock, patch
+from video_utils import Codec, Video
+
+from convert_videos import video_processor
+from convert_videos.settings import AudioSettings, VideoSettings
+from convert_videos.video_processor import Status, VideoProcessor
+
+
+def mock_temp_file(name):
+    mock_file = Mock()
+    mock_file.name = name
+    mock_file.__enter__ = Mock(return_value=mock_file)
+    mock_file.__exit__ = Mock(return_value=None)
+    return mock_file
+
+
+@pytest.fixture
+def target():
+    audio_settings = AudioSettings(codec=Codec("AAC"), channels=2, bitrate=120)
+
+    video_settings = VideoSettings(
+        codec=Codec("HEVC"), quality=25, preset="slow", encoder="software"
+    )
+
+    return VideoProcessor(
+        video=Video("bar.mkv", "/asdf/foo"),
+        video_settings=video_settings,
+        audio_settings=audio_settings,
+        container="asdf",
+        in_place=False,
+        dry_run=False,
+    )
+
+
+@patch("tempfile.NamedTemporaryFile", autospec=True)
+def test_create_temp_file_default(mock_NamedTemporaryFile, target):
+    target._create_temp_file()
+    mock_NamedTemporaryFile.assert_called_with(dir=None, suffix=".asdf")
+
+
+@patch("tempfile.NamedTemporaryFile", autospec=True)
+def test_create_temp_file_new_dir(mock_NamedTemporaryFile, target):
+    target.temp_directory = "/foo/bar"
+    target._create_temp_file()
+    mock_NamedTemporaryFile.assert_called_with(dir="/foo/bar", suffix=".asdf")
+
+
+@patch.object(video_processor, "FFmpegConverter")
+@patch.object(VideoProcessor, "_move_output_video")
+@patch.object(VideoProcessor, "_create_temp_file")
+def test_process(
+    mock_create_temp_file, mock_move_output_video, mock_ffmpeg_converter, target
+):
+    mock_create_temp_file.return_value = mock_temp_file("/foo")
+    target.process()
+    mock_ffmpeg_converter.assert_called_with(
+        audio_settings=target.audio_settings,
+        video_settings=target.video_settings,
+        extra_ffmpeg_input_args="",
+        extra_ffmpeg_output_args="",
+        destination_file_path="/foo",
+        source_file_path="/asdf/foo/bar.mkv",
+        dry_run=False,
+    )
+    mock_ffmpeg_converter().process.assert_called()
+    mock_move_output_video.assert_called()
+
+
+@patch.object(video_processor, "FFmpegConverter")
+@patch.object(VideoProcessor, "_move_output_video")
+@patch.object(VideoProcessor, "_create_temp_file")
+def test_process_dry_run(
+    mock_create_temp_file, mock_move_output_video, mock_ffmpeg_converter, target
+):
+    mock_create_temp_file.return_value = mock_temp_file("/foo")
+    target.dry_run = True
+    target.process()
+    mock_ffmpeg_converter.assert_called_with(
+        audio_settings=target.audio_settings,
+        video_settings=target.video_settings,
+        extra_ffmpeg_input_args="",
+        extra_ffmpeg_output_args="",
+        destination_file_path="/foo",
+        source_file_path="/asdf/foo/bar.mkv",
+        dry_run=True,
+    )
+    mock_ffmpeg_converter().process.assert_called()
+    mock_move_output_video.assert_called()
+
+
+@patch.object(VideoProcessor, "renamed_path")
+@patch("os.path.exists", return_value=True)
+def test_already_processed_renamed_file_exists(mock_exists, mock_renamed_path, target):
+    result = target.already_processed()
+    assert result is True
+
+
+@patch.object(VideoProcessor, "renamed_path")
+@patch("os.path.exists", return_value=False)
+def test_already_processed_container_matches(mock_exists, mock_renamed_path, target):
+    target.video.name = "bar - HEVC.mkv"
+    result = target.already_processed()
+    assert result is True
+
+
+@patch.object(VideoProcessor, "renamed_path")
+@patch("os.path.exists", return_value=False)
+def test_already_processed_false(mock_exists, mock_renamed_path, target):
+    result = target.already_processed()
+    assert result is False
+
+
+def test_renamed_path(target):
+    result = target.renamed_path()
+    assert result == "/asdf/foo/bar - HEVC.asdf"
+
+
+def test_in_place_file_path(target):
+    result = target.in_place_file_path()
+    assert result == "/asdf/foo/bar.asdf"
+
+
+@patch("shutil.move")
+@patch("os.remove")
+def test_move_output_video(mock_remove, mock_move, target):
+    target.temp_file = mock_temp_file("/foo/bar/baz")
+    target._move_output_video()
+    mock_move.assert_called_with("/foo/bar/baz", "/asdf/foo/bar - HEVC.asdf")
+    mock_remove.assert_not_called()
+
+
+@patch("shutil.move")
+@patch("os.remove")
+def test_move_output_video_dry_run(mock_remove, mock_move, target):
+    target.dry_run = True
+    target.temp_file = mock_temp_file("/foo/bar/baz")
+    target._move_output_video()
+    mock_move.assert_not_called()
+    mock_remove.assert_not_called()
+
+
+@patch("shutil.move")
+@patch("os.remove")
+def test_move_output_video_in_place(mock_remove, mock_move, target):
+    target.temp_file = mock_temp_file("/foo/bar/baz")
+    target.in_place = True
+    target._move_output_video()
+    mock_move.assert_any_call("/foo/bar/baz", "/asdf/foo/bar - HEVC.asdf")
+    mock_remove.assert_called_with("/asdf/foo/bar.mkv")
+    mock_move.assert_any_call("/asdf/foo/bar - HEVC.asdf", "/asdf/foo/bar.asdf")
+
+
+@patch("shutil.move")
+@patch("os.remove")
+def test_move_output_video_in_place_dry_run(mock_remove, mock_move, target):
+    target.dry_run = True
+    target.temp_file = mock_temp_file("/foo/bar/baz")
+    target.in_place = True
+    target._move_output_video()
+    mock_move.assert_not_called()
+    mock_remove.assert_not_called()
+
+
+def test_in_desired_format(target):
+    target.video.codec = Codec("HEVC")
+    target.video_settings.codec = Codec("HEVC")
+    response = target.process()
+    assert response == Status.IN_DESIRED_FORMAT
+
+
+@patch.object(VideoProcessor, "already_processed", return_value=True)
+def test_already_processed(mock_already_processed, target):
+    response = target.process()
+    assert response == Status.ALREADY_PROCESSED
+
+
+@patch.object(VideoProcessor, "already_processed", return_value=False)
+@patch.object(VideoProcessor, "_create_temp_file")
+@patch("convert_videos.video_processor.FFmpegConverter")
+@patch.object(VideoProcessor, "_move_output_video")
+def test_converted(m1, m2, m3, m4, target):
+    response = target.process()
+    assert response == Status.CONVERTED
+
+
+@patch.object(VideoProcessor, "already_processed", return_value=False)
+@patch.object(VideoProcessor, "_create_temp_file")
+@patch("convert_videos.video_processor.FFmpegConverter", side_effect=Exception)
+@patch.object(VideoProcessor, "_move_output_video")
+def test_failed(m1, m2, m3, m4, target):
+    response = target.process()
+    assert response == Status.FAILED
+
+
+@patch.object(VideoProcessor, "_is_below_minimum_size", return_value=True)
+def test_below_minimum_size(mock_is_below_minimum_size, target):
+    response = target.process()
+    assert response == Status.BELOW_MINIMUM_SIZE
+
+
+def test_is_below_minimum_size_true(target):
+    target.minimum_size_b = 200 * 1024 * 1024
+    target.video.size_b = 100 * 1024 * 1024
+    result = target._is_below_minimum_size()
+    assert result is True
+
+
+@patch("os.path.getsize", return_value=300 * 1024 * 1024)
+def test_is_below_minimum_size_false(mock_getsize, target):
+    target.minimum_size = 200
+    result = target._is_below_minimum_size()
+    assert result is False
+
+
+@patch("os.path.getsize", return_value=300 * 1024 * 1024)
+def test_is_below_minimum_size_zero(mock_getsize, target):
+    target.minimum_size = 0
+    result = target._is_below_minimum_size()
+    assert result is False
