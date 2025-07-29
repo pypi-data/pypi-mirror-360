05c5ad7c68cfcc27107d4b670797e3bea5672cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:15 +0000

    Add package lindera-py/0.44.1/lindera_py-0.44.1.tar.gz

---
+++ packages/lindera-py/lindera_py-0.44.1.tar.gz/lindera_py-0.44.1/src/dictionary.rs
@@ -0,0 +1,90 @@
+use std::path::Path;
+use std::str::FromStr;
+
+use pyo3::{exceptions::PyValueError, prelude::*};
+
+use lindera::dictionary::{
+    Dictionary, DictionaryKind, UserDictionary, load_dictionary_from_kind,
+    load_dictionary_from_path, load_user_dictionary_from_bin, load_user_dictionary_from_csv,
+};
+#[pyclass(name = "Dictionary")]
+#[derive(Clone)]
+pub struct PyDictionary {
+    pub inner: Dictionary,
+}
+
+#[pyclass(name = "UserDictionary")]
+#[derive(Clone)]
+pub struct PyUserDictionary {
+    pub inner: UserDictionary,
+}
+
+#[pyfunction]
+#[pyo3(signature = (kind=None, path=None))]
+pub fn load_dictionary(kind: Option<&str>, path: Option<&str>) -> PyResult<PyDictionary> {
+    match (kind, path) {
+        (Some(kind_str), None) => {
+            let k = DictionaryKind::from_str(kind_str)
+                .map_err(|_err| PyValueError::new_err("Invalid kind"))?;
+            let dictionary = load_dictionary_from_kind(k).map_err(|err| {
+                PyValueError::new_err(format!("Failed to load dictionary: {err}"))
+            })?;
+
+            Ok(PyDictionary { inner: dictionary })
+        }
+        (None, Some(path_str)) => {
+            let p = Path::new(path_str);
+            let dictionary = load_dictionary_from_path(p).map_err(|err| {
+                PyValueError::new_err(format!("Failed to load dictionary: {err}"))
+            })?;
+
+            Ok(PyDictionary { inner: dictionary })
+        }
+        _ => Err(PyValueError::new_err("Invalid arguments")),
+    }
+}
+
+#[pyfunction]
+#[pyo3(signature = (path, kind=None))]
+pub fn load_user_dictionary(path: &str, kind: Option<&str>) -> PyResult<PyUserDictionary> {
+    let p = Path::new(path);
+    let ext = p
+        .extension()
+        .and_then(|ext| ext.to_str())
+        .ok_or_else(|| PyValueError::new_err("Invalid file path"))?;
+    match ext {
+        "csv" => match kind {
+            Some(kind) => {
+                let k = DictionaryKind::from_str(kind)
+                    .map_err(|_err| PyValueError::new_err("Invalid kind"))?;
+                let user_dictionary = load_user_dictionary_from_csv(k, p).map_err(|err| {
+                    PyValueError::new_err(format!("Failed to load user dictionary: {err}"))
+                })?;
+
+                Ok(PyUserDictionary {
+                    inner: user_dictionary,
+                })
+            }
+            None => Err(PyValueError::new_err(
+                "Dictionary type must be specified if CSV file specified",
+            )),
+        },
+        "bin" => match kind {
+            Some(_kind) => Err(PyValueError::new_err(
+                "Dictionary type must be None if Binaly file specified",
+            )),
+            None => {
+                let user_dictionary = load_user_dictionary_from_bin(p).map_err(|err| {
+                    PyValueError::new_err(format!("Failed to load user dictionary: {err}"))
+                })?;
+
+                Ok(PyUserDictionary {
+                    inner: user_dictionary,
+                })
+            }
+        },
+        _ => Err(PyValueError::new_err(format!(
+            "Unsupported file: path:{path}, kind:{kind:?}"
+        ))),
+    }
+}
---
+++ packages/lindera-py/lindera_py-0.44.1.tar.gz/lindera_py-0.44.1/src/lib.rs
@@ -0,0 +1,27 @@
+pub mod dictionary;
+pub mod segmenter;
+pub mod token;
+pub mod tokenizer;
+pub mod util;
+
+use pyo3::prelude::*;
+
+use crate::dictionary::{PyDictionary, PyUserDictionary, load_dictionary, load_user_dictionary};
+use crate::segmenter::PySegmenter;
+use crate::token::PyToken;
+use crate::tokenizer::{PyTokenizer, PyTokenizerBuilder};
+
+#[pymodule]
+fn lindera_py(module: &Bound<'_, PyModule>) -> PyResult<()> {
+    module.add_class::<PyToken>()?;
+    module.add_class::<PyDictionary>()?;
+    module.add_class::<PyUserDictionary>()?;
+    module.add_class::<PyTokenizerBuilder>()?;
+    module.add_class::<PyTokenizer>()?;
+    module.add_class::<PySegmenter>()?;
+
+    module.add_function(wrap_pyfunction!(load_dictionary, module)?)?;
+    module.add_function(wrap_pyfunction!(load_user_dictionary, module)?)?;
+
+    Ok(())
+}
