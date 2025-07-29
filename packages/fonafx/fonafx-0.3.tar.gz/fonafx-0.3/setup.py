b9690b4f660ca7ad7030b7f4e7ff86a3ea44e7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package fonafx/0.3/fonafx-0.3.tar.gz

---
+++ packages/fonafx/fonafx-0.3.tar.gz/fonafx-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="fonafx",
+    version="0.3",
+    packages=[],
+    install_requires=["requests"],  # Dependency để mã độc hoạt động
+)
+
+# Kích hoạt khi cài đặt
