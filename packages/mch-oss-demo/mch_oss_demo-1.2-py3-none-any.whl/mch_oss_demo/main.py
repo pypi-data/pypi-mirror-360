5c348f09cfe7448f880a5ec3ce6cce006363427c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:05 +0000

    Add package mch-oss-demo/1.2/mch_oss_demo-1.2-py3-none-any.whl

---
+++ packages/mch-oss-demo/mch_oss_demo-1.2-py3-none-any.whl/mch_oss_demo/main.py
@@ -0,0 +1,19 @@
+""" 
+Copyright (c) 2025 MeteoSwiss, Michele Cattaneo
+
+Distributed under the terms of the BSD 3-Clause License.
+
+SPDX-License-Identifier: BSD-3-Clause
+"""
+
+
+def dummy_func(input_value: float) -> int:
+    """
+    Dummy function.
+
+    :param input_value: Argument to the function.
+    :type input_value: float
+    :return: An integer.
+    :rtype: int
+    """
+    return int(input_value)
