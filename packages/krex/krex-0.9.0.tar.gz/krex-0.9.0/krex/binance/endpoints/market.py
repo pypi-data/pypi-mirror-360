ddd9b38002ff53b84d56f64700a2eb509b7782e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:21 +0000

    Add package krex/0.9.0/krex-0.9.0.tar.gz

---
+++ packages/krex/krex-0.9.0.tar.gz/krex-0.9.0/krex/binance/endpoints/market.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class SpotMarket(str, Enum):
+    EXCHANGE_INFO = "/api/v3/exchangeInfo"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class FuturesMarket(str, Enum):
+    EXCHANGE_INFO = "/fapi/v1/exchangeInfo"
+    BOOK_TICKER = "/fapi/v1/ticker/bookTicker"
+    KLINE = "/fapi/v1/klines"
+    PREMIUM_INDEX = "/fapi/v1/premiumIndex"
+    FUNDING_RATE_HISTORY = "/fapi/v1/fundingRate"
+
+    def __str__(self) -> str:
+        return self.value
