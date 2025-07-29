4a931d88075c65bc45b7d44d450439cc29cd5adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:21 +0000

    Add package honeybee-display/0.5.5/honeybee_display-0.5.5.tar.gz

---
+++ packages/honeybee-display/honeybee_display-0.5.5.tar.gz/honeybee_display-0.5.5/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('extras-requirements.txt') as f:
+    extras_requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="honeybee-display",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Adds methods to translate honeybee objects to VisualizationSets.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/honeybee-display",
+    packages=setuptools.find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={'full': extras_requirements},
+    entry_points={
+        "console_scripts": ["honeybee-display = honeybee_display.cli:display"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent"
+    ],
+    license="AGPL-3.0"
+)
