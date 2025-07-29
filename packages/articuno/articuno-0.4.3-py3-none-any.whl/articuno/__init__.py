3f617c23049ef0d1c06340869723f7fc76ed55b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:49 +0000

    Add package articuno/0.4.3/articuno-0.4.3-py3-none-any.whl

---
+++ packages/articuno/articuno-0.4.3-py3-none-any.whl/articuno/__init__.py
@@ -0,0 +1,16 @@
+from .models import df_to_pydantic, df_to_patito, infer_pydantic_model, infer_patito_model
+from .codegen import generate_class_code
+from .cli import app as cli_app
+from .bootstrap import bootstrap_app
+
+__all__ = [
+    "df_to_pydantic",
+    "df_to_patito",
+    "infer_pydantic_model",
+    "infer_patito_model",
+    "generate_class_code",
+    "cli_app",
+    "bootstrap_app",
+]
+
+__version__ = "0.4.3"
