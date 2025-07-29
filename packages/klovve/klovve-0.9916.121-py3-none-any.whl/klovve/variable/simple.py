071b5f360d8639de6f83619284ead70d68fc1b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:20 +0000

    Add package klovve/0.9916.121/klovve-0.9916.121-py3-none-any.whl

---
+++ packages/klovve/klovve-0.9916.121-py3-none-any.whl/klovve/variable/simple.py
@@ -0,0 +1,26 @@
+#  SPDX-FileCopyrightText: © 2025 Josef Hahn
+#  SPDX-License-Identifier: AGPL-3.0-only
+import typing as t
+
+import klovve.debug
+from klovve.variable.base import VariableBaseImpl
+
+
+class SimpleVariable[T](VariableBaseImpl[T]):
+
+    def __init__(self, *, initial_value: t.Any = None, is_externally_settable: bool = True):
+        super().__init__(is_externally_settable=is_externally_settable)
+        self.__value = initial_value
+
+    def _value(self):
+        klovve.debug.log.debug("get simple variable value of %s (= <%s>)", self, self.__value)
+        return self.__value
+
+    def set_value(self, value, *, internally=False):
+        if not (internally or self.is_externally_settable()):
+            raise RuntimeError("this variable is not externally settable")
+
+        if self.__value != value:
+            klovve.debug.log.debug("set simple variable value of %s to <%s>", self, value)
+            self.__value = value
+            self._changed(value)
