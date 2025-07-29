5c893111fa131e64555391b6be4993a6afca5f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:34 +0000

    Add package layrz-sdk/3.1.30/layrz_sdk-3.1.30-py3-none-any.whl

---
+++ packages/layrz-sdk/layrz_sdk-3.1.30-py3-none-any.whl/layrz_sdk/entities/asset_constants.py
@@ -0,0 +1,33 @@
+"""Asset constants"""
+
+from datetime import timedelta
+
+from pydantic import BaseModel, Field
+
+
+class AssetConstants(BaseModel):
+  """Asset constants"""
+
+  model_config = {
+    'json_encoders': {
+      timedelta: lambda v: v.total_seconds(),
+    },
+  }
+
+  distance_traveled: float = Field(
+    default=0.0,
+    description='Total distance traveled by the asset in meters',
+    alias='distanceTraveled',
+  )
+
+  elapsed_time: timedelta = Field(
+    default=timedelta(seconds=0),
+    description='Total elapsed time for the asset in seconds',
+    alias='elapsedTime',
+  )
+
+  primary_device: str = Field(
+    default='N/a',
+    description='Primary device associated with the asset',
+    alias='primaryDevice',
+  )
