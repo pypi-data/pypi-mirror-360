6e52154e87e0ad8d17e06b18fa4adb2c82b0a109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:08:06 +0000

    Add package pygameBasics/0.1.0/pygamebasics-0.1.0.tar.gz

---
+++ packages/pygameBasics/pygamebasics-0.1.0.tar.gz/pygamebasics-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pygameBasics",
+    version="0.1.0",
+    description="Auto-uploaded package",
+    packages=find_packages(),
+    python_requires=">=3.6"
+)
