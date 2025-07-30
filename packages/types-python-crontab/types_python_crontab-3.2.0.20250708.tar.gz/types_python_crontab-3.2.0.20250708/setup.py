a376de2b4d46c1b72d20f929cbf0c321f3568a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:13 +0000

    Add package types-python-crontab/3.2.0.20250708/types_python_crontab-3.2.0.20250708.tar.gz

---
+++ packages/types-python-crontab/types_python_crontab-3.2.0.20250708.tar.gz/types_python_crontab-3.2.0.20250708/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'cronlog-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed'], 'crontab-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed'], 'crontabs-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed']})
