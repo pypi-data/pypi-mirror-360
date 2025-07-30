a6c787def7aad201930f9a15a9c6971b7f34fcad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:16 +0000

    Add package autopptx/1.0.0/autopptx-1.0.0.tar.gz

---
+++ packages/autopptx/autopptx-1.0.0.tar.gz/autopptx-1.0.0/autopptx/Image/__init__.py
@@ -0,0 +1,15 @@
+from .image import replace_image
+from .images import replace_images
+from .style import (
+    set_image_style,
+    extract_image_style,
+    transfer_image_style,
+)
+
+__all__ = [
+    "replace_image",
+    "replace_images",
+    "set_image_style",
+    "extract_image_style",
+    "transfer_image_style",
+]
