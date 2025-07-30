ab71a3bf6c14a1f894b4502f2efcaf12534d06ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:24 +0000

    Add package openweatherwrap/1.0.0/openweatherwrap-1.0.0.tar.gz

---
+++ packages/openweatherwrap/openweatherwrap-1.0.0.tar.gz/openweatherwrap-1.0.0/docs/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+
+project = 'OpenWeatherWrap'
+copyright = '2025, lythox'
+author = 'lythox'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_copybutton',
+    'sphinx_autodoc_typehints',
+    'myst_parser',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'requests': ('https://requests.readthedocs.io/en/latest/', None),
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
