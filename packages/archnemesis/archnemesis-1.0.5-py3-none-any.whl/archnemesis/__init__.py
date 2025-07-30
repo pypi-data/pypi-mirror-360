6b598f9be5f01884b86c50395baaf9d8344613e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:45 +0000

    Add package archnemesis/1.0.5/archnemesis-1.0.5-py3-none-any.whl

---
+++ packages/archnemesis/archnemesis-1.0.5-py3-none-any.whl/archnemesis/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+from .cfg import *
+
+from .Spectroscopy_0 import *
+from .Scatter_0 import *
+from .Surface_0 import *
+from .Atmosphere_0 import *
+from .Stellar_0 import *
+from .Layer_0 import *
+from .CIA_0 import *
+from .Measurement_0 import *
+from .AtmCalc_0 import *
+from .Path_0 import *
+from .Variables_0 import *
+from .ForwardModel_0 import *
+from .OptimalEstimation_0 import *
+from .NestedSampling_0 import *
+from .Telluric_0 import *
+#from .LineData_0 import *
+
+from .Models import Models
+from .Data import *
+from .Files import *
+from .Retrievals import *
