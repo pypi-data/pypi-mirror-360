7f1fb52bfa746d05fe7fc481b9325b9fe2d337f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:48 +0000

    Add package brats/0.0.21/brats-0.0.21.tar.gz

---
+++ packages/brats/brats-0.0.21.tar.gz/brats-0.0.21/brats/utils/exceptions.py
@@ -0,0 +1,19 @@
+from dacite import DaciteError
+
+
+class AlgorithmNotCPUCompatibleException(Exception):
+    """Exception raised when an CPU-incompatible algorithm tries to run on CPU."""
+
+    pass
+
+
+class BraTSContainerException(Exception):
+    """Exception raised when an algorithm container fails"""
+
+    pass
+
+
+class AlgorithmConfigException(DaciteError):
+    """Exception raised when the algorithm config file has issues."""
+
+    pass
