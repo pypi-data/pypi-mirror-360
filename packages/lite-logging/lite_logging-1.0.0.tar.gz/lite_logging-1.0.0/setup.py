da9ab761c8b243ee5653de97a0c297b21d612ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:41 +0000

    Add package lite-logging/1.0.0/lite_logging-1.0.0.tar.gz

---
+++ packages/lite-logging/lite_logging-1.0.0.tar.gz/lite_logging-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lite_logging',  # Replace with your package’s name
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Missclick',  
+    author_email='gabrielgarronedev@gmail.com',
+    description='A library for terminal logging with color support and debug information',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+
+)
