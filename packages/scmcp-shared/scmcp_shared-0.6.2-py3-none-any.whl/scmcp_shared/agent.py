df7d0594832de547ed051be86c9f265dcb83dfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:22 +0000

    Add package scmcp-shared/0.6.2/scmcp_shared-0.6.2-py3-none-any.whl

---
+++ packages/scmcp-shared/scmcp_shared-0.6.2-py3-none-any.whl/scmcp_shared/agent.py
@@ -0,0 +1,50 @@
+from .schema.tool import ToolList
+import os
+
+
+from agno.agent import Agent
+from agno.models.openai import OpenAILike
+from .kb import load_kb
+
+model = OpenAILike(
+    id=os.getenv("MODEL"),
+    base_url=os.getenv("BASE_URL"),
+    api_key=os.getenv("API_KEY"),
+)
+
+
+def rag_agent(task, software="scmcp"):
+    knowledge_base = load_kb(software=software)
+    agent = Agent(
+        model=model,
+        knowledge=knowledge_base,
+        show_tool_calls=True,
+        search_knowledge=True,
+        instructions="""
+        MUST query the knowledge base, and return the code example that can be used to solve the task.
+        """,
+    )
+    query = f"""
+    <task>
+    {task}
+    </task>
+    查询知识库，给出一个用于解决任务的代码示例。返回结果格式为：
+    <code_example>
+        [code_example]
+    </code_example>
+    """
+    rep = agent.run(query)
+    return rep.content
+
+
+def select_tool(query):
+    agent = Agent(
+        model=model,
+        response_model=ToolList,
+        use_json_mode=True,
+        instructions="""
+        you are a bioinformatician, you are given a task and a list of tools, you need to select the most directly relevant tools to use to solve the task
+        """,
+    )
+    rep = agent.run(query)
+    return rep.content
