21f76697732aa270ea97c0b2059ef8aa53223d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:46 +0000

    Add package aiogram-renderer/1.3.7b4/aiogram_renderer-1.3.7b4.tar.gz

---
+++ packages/aiogram-renderer/aiogram_renderer-1.3.7b4.tar.gz/aiogram_renderer-1.3.7b4/aiogram_renderer/middlewares.py
@@ -0,0 +1,27 @@
+from aiogram import types
+from aiogram import BaseMiddleware
+from typing import Any, Callable
+from aiogram.fsm.context import FSMContext
+from .bot_mode import BotModes, BotMode
+from .renderer import Renderer
+from .window import Window
+
+
+class RendererMiddleware(BaseMiddleware):
+    def __init__(self, windows: list[Window] = None, modes: list[BotMode] = None):
+        self.windows = windows
+        self.modes = modes
+
+    async def __call__(self, handler: Callable, event: types.Message, data: dict[str, Any]) -> Any:
+        # Если есть FSMContext, то передаем его в renderer и bot_modes
+        for key, value in data.items():
+            if isinstance(value, FSMContext):
+                bot_modes = BotModes(*self.modes, fsm=value) if (self.modes is not None) else None
+                renderer = Renderer(event=event, fsm=value, windows=self.windows, bot_modes=bot_modes)
+                data["renderer"] = renderer
+                result = await handler(event, data)
+                del renderer
+                return result
+
+        result = await handler(event, data)
+        return result
