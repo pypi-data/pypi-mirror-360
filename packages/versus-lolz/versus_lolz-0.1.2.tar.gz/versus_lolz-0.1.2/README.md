cf174e4052c06a01d67b7b9e76ff428fd9c86bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:05 +0000

    Add package versus-lolz/0.1.2/versus_lolz-0.1.2.tar.gz

---
+++ packages/versus-lolz/versus_lolz-0.1.2.tar.gz/versus_lolz-0.1.2/README.md
@@ -0,0 +1,43 @@
+# Versus.lolz
+
+Unofficial Python client for Lolzteam API
+
+## Installation
+```bash
+pip install pylolzteamapi
+```
+
+## Usage
+
+```python
+import lolz.eh
+
+from lolz.sync import Client
+import lolz
+
+
+lolz.eh.hook()
+
+client = Client("token (you can omit `Bearer ` from the beginning)")
+
+steam_accs = client.search(lolz.CAT_STEAM,
+                           trade_limit="nomatter",
+                           page=2, pmin=30,
+                           currency="rub"
+)
+
+for acc in steam_accs[:3]:
+    print(acc.title)
+    print("Price:", acc.price)
+    print("Seller:", acc.seller.username)
+    print()
+```
+
+## Issues and pull requests
+
+Please, feel free to open issues and pull requests.
+
+Pull requests must be made on [Твой Гит](https://tvoygit.ru/vi_is_lonely/versus.lolz).
+Issues can be made both on Твой Гит and GitHub.
+
+**LICENSE** (AGPL-v3.0-only)
---
+++ packages/versus-lolz/versus_lolz-0.1.2.tar.gz/versus_lolz-0.1.2/src/lolz/__init__.py
@@ -0,0 +1,81 @@
+GAME_TEAM_FORTRESS_2 = GAME_TF2 = 440
+GAME_DOTA2 = GAME_D2 = 570
+GAME_CS2 = GAME_CS = 730
+GAME_STEAM = 753
+GAME_KILLING_FLOOR_2 = GAME_KF2 = 232090
+GAME_RUST = 252490
+GAME_UNTURNED = 304930
+GAME_DONT_STARVE_TOGETHER = GAME_DST = 322330
+GAME_PUBG = 578080
+
+CAT_ALL = "/"
+CAT_STEAM = "/steam"
+CAT_FORTNITE = "/fortnite"
+CAT_RIOT = "/riot"
+CAT_TELEGRAM = CAT_TG = "/telegram"
+CAT_SUPERCELL = CAT_SUCE = "/supercell"
+CAT_ORIGIN = "/origin"
+CAT_GIFTS = "/gifts"
+CAT_EPIC_GAMES = CAT_EG = "/epicgames"
+CAT_ESCAPE_FROM_TARKOV = CAT_EFT = "/escape-from-tarkov"
+CAT_SOCIAL_CLUB = CAT_SOCL = "/social-club"
+CAT_UPLAY = "/uplay"
+CAT_WAR_THUNDER = CAT_WT = "/war-thunder"
+CAT_DISCORD = CAT_DC = "/discord"
+CAT_TIKTOK = CAT_TT = "/tiktok"
+CAT_INSTAGRAM = CAT_IG = "/instagram"
+CAT_BATTLENET = CAT_BN = "/battlenet"
+CAT_VPN = "/vpn"
+CAT_ROBLOX = "/roblox"
+CAT_WARFACE = CAT_WF = "/warface"
+CAT_MINECRAFT = CAT_MC = "/minecraft"
+CAT_CHAT_GPT = CAT_GPT = "/chatgpt"
+CAT_MIHOYO = CAT_MHY = "/mihoyo"
+CAT_WORLD_OF_TANKS = CAT_WOT = "/world-of-tanks"
+CAT_WOT_BLITZ = CAT_WOTB = "/wot-blitz"
+
+DAY = "day"
+MONTH = "month"
+YEAR = "year"
+
+YES = "yes"
+NO = "no"
+NOMATTER = "nomatter"
+
+PRICE_UP = "price_to_up"
+PRICE_DOWN = "price_to_down"
+PRICE_UP_UPLOAD = "pdate_to_up_upload"
+PRICE_DOWN_UPLOAD = "pdate_to_down_upload"
+PDATE_UP = "pdate_to_up"
+PDATE_DOWN = "pdate_to_down"
+PDATE_UP_UPLOAD = "pdate_to_up_upload"
+PDATE_DOWN_UPLOAD = "pdate_to_down_upload"
+EDATE_UP = "edate_to_up"
+EDATE_DOWN = "edate_to_down"
+DDATE_UP = "ddate_to_up"
+DDATE_DOWN = "ddate_to_down"
+
+ACTIVE = "active"
+PAID = "paid"
+DELETED = "deleted"
+AWAITING = "awaiting"
+CLOSED = "closed"
+DISCOUNT_REQUEST = "discount_request"
+STICKIED = "stickied"
+PRE_ACTIVE = "pre_active"
+
+RUB = "rub"
+UAH = "uah"
+KZT = "kzt"
+BYN = "byn"
+USD = "usd"
+EUR = "eur"
+GBP = "gbp"
+CNY = "cny"
+TRY = "try"
+JPY = "jpy"
+BRL = "brl"
+
+AUTOREG = "autoreg"
+NATIVE = "native"
+NOMARKET = "no_market"
