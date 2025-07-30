a7f342a46731a8b3708f14694e0519a40bef122a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:18 +0000

    Add package bfg-llmutil/0.5.1/bfg_llmutil-0.5.1.tar.gz

---
+++ packages/bfg-llmutil/bfg_llmutil-0.5.1.tar.gz/bfg_llmutil-0.5.1/examples/example_function_call.py
@@ -0,0 +1,36 @@
+from llmutil import Result, Tooling, new_response
+
+
+def add(a, b):
+    return a + b
+
+
+class UseAdd(Tooling):
+    def on_function_call(self, name, args):
+        if name == "add":
+            return Result(add(args["a"], args["b"]))
+
+    def get_tools(self):
+        return {
+            "add": {
+                "a": "number",
+                "b": "number",
+            }
+        }
+
+
+messages = [
+    {
+        "role": "system",
+        "content": "you cannot do math. you must use the add() function to add numbers.",
+    },
+    {
+        "role": "user",
+        "content": "alice has 10 apples, bob has 20 apples, how many apples do they have in total?",
+    },
+]
+
+output = new_response(messages, model="gpt-4.1-mini", tooling=UseAdd())
+
+# Alice and Bob have 30 apples in total.
+print(output)
