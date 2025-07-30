893a9eb40f58a3ce2d5d7adf7b268a5aa1761066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:41 +0000

    Add package bluer-ugv/6.192.1/bluer_ugv-6.192.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.192.1.tar.gz/bluer_ugv-6.192.1/bluer_ugv/swallow/README.py
@@ -0,0 +1,29 @@
+assets2 = "https://github.com/kamangir/assets2/blob/main/bluer-swallow"
+
+dict_of_images = {
+    "https://github.com/kamangir/assets/blob/main/bluer-ugv/bluer-light.png?raw=true": "",
+    "../../diagrams/bluer-swallow/3d-design.png": "../../diagrams/bluer-swallow/3d-design.stl",
+    f"{assets2}/20250605_180136.jpg?raw=true": "",
+    "../../diagrams/bluer-swallow/analog.png": "../../diagrams/bluer-swallow/analog.svg",
+    f"{assets2}/20250608_144453.jpg?raw=true": "",
+    f"{assets2}/20250609_164433.jpg?raw=true": "",
+    f"{assets2}/20250611_100917.jpg?raw=true": "",
+    "../../diagrams/bluer-swallow/digital.png": "../../diagrams/bluer-swallow/digital.svg",
+    f"{assets2}/20250614_114954.jpg?raw=true": "",
+    f"{assets2}/20250615_192339.jpg?raw=true": "",
+    f"{assets2}/20250616_134654.jpg?raw=true": "",
+    f"{assets2}/20250616_145049.jpg?raw=true": "",
+    f"{assets2}/20250618_122604.jpg?raw=true": "",
+    f"{assets2}/20250618_102816~2_1.gif?raw=true": "",
+    "../../diagrams/bluer-swallow/cover.png": "../../diagrams/bluer-swallow/cover.stl",
+    f"{assets2}/20250629_123616.jpg?raw=true": "",
+    f"{assets2}/20250630_214923.jpg?raw=true": "",
+    f"{assets2}/20250703_153834.jpg?raw=true": "",
+    "../../diagrams/bluer-swallow/steering-over-current.png": "../../diagrams/bluer-swallow/steering-over-current.svg",
+    f"{assets2}/20250707_122000.jpg?raw=true": "",
+}
+
+items = [
+    "[![image]({})]({})".format(image, url if url else image)
+    for image, url in dict_of_images.items()
+]
---
+++ packages/bluer-ugv/bluer_ugv-6.192.1.tar.gz/bluer_ugv-6.192.1/bluer_ugv/swallow/session/__main__.py
@@ -0,0 +1,25 @@
+import argparse
+
+from blueness import module
+from blueness.argparse.generic import sys_exit
+
+from bluer_ugv import NAME
+from bluer_ugv.logger import logger
+from bluer_ugv.swallow.session.functions import start_session
+
+NAME = module.name(__file__, NAME)
+
+parser = argparse.ArgumentParser(NAME)
+parser.add_argument(
+    "task",
+    type=str,
+    help="start_session",
+)
+args = parser.parse_args()
+
+success = False
+if args.task == "start_session":
+    success = start_session()
+else:
+    success = None
+sys_exit(logger, NAME, args.task, success)
