38eb5e2eac3f9ea6377b88cfdfac5018e9cb2a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:14 +0000

    Add package CTkFileDialog/0.1.4/ctkfiledialog-0.1.4.tar.gz

---
+++ packages/CTkFileDialog/ctkfiledialog-0.1.4.tar.gz/ctkfiledialog-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CTkFileDialog',              
+    version='0.1.4',                    
+    author='Tu Nombre o Alias',
+    author_email='tuemail@ejemplo.com',
+    description='Selector de archivos personalizado usando CustomTkinter',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/FlickGMD/CTkFileDialog',  
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',      
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "opencv-python",
+        "customtkinter",
+        "Pillow",
+        "CTkMessagebox",
+        "CTkToolTip",
+    ],
+    include_package_data=True,
+)
