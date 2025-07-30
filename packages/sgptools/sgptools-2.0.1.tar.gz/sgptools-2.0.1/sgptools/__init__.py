32927ec990d9901d71de2ea6adad1d64656fe611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:11 +0000

    Add package sgptools/2.0.1/sgptools-2.0.1.tar.gz

---
+++ packages/sgptools/sgptools-2.0.1.tar.gz/sgptools-2.0.1/sgptools/__init__.py
@@ -0,0 +1,19 @@
+"""
+SGP-Tools: SGP-based Optimization Tools
+
+Software Suite for Sensor Placement and Informative Path Planning.
+
+The library includes python code for the following:
+- Greedy algorithm-based approaches
+- Bayesian optimization-based approaches
+- Genetic algorithm-based approaches
+- Sparse Gaussian process (SGP)-based approaches
+
+"""
+
+__version__ = "2.0.1"
+__author__ = 'Kalvik'
+
+from .core import *
+from .kernels import *
+from .utils import *
