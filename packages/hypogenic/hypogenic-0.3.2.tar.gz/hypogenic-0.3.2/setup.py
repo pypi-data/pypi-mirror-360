4d59737e93610b3f9eda86de3ea6f7c98585c600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:02 +0000

    Add package hypogenic/0.3.2/hypogenic-0.3.2.tar.gz

---
+++ packages/hypogenic/hypogenic-0.3.2.tar.gz/hypogenic-0.3.2/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = [
+        line.strip()
+        for line in f
+        if line.strip() and not line.startswith("#") and not line.startswith("//")
+    ]
+
+setuptools.setup(
+    name="hypogenic",
+    version="0.3.2",
+    author="Haokun Liu, Mingxuan Li, Chenfei Yuan, Yangqiaoyu Zhou, Tejes Srivastava",
+    author_email="haokunliu@uchicago.edu",
+    description="A package for generating and evaluating hypotheses.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "hypogenic_generation=hypogenic_cmd.generation:main",
+            "hypogenic_inference=hypogenic_cmd.inference:main",
+        ],
+    },
+    # Note: doc2json must be installed separately via:
+    # pip install git+https://github.com/allenai/s2orc-doc2json@71c022ed4bed3ffc71d22c2ac5cdbc133ad04e3c
+)
