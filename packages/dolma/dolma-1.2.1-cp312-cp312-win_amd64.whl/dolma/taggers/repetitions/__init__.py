ebbda0f161c092654b76eb80a1c72c327486f927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:47 +0000

    Add package dolma/1.2.1/dolma-1.2.1-cp312-cp312-win_amd64.whl

---
+++ packages/dolma/dolma-1.2.1-cp312-cp312-win_amd64.whl/dolma/taggers/repetitions/__init__.py
@@ -0,0 +1,13 @@
+from .repetitions_taggers import (
+    ParagraphRepetitionsTagger,
+    RepetitionsTagger,
+    TokenizerRepetitionsSkipEmptyTagger,
+    TokenizerRepetitionsTagger,
+)
+
+__all__ = [
+    "RepetitionsTagger",
+    "ParagraphRepetitionsTagger",
+    "TokenizerRepetitionsTagger",
+    "TokenizerRepetitionsSkipEmptyTagger",
+]
