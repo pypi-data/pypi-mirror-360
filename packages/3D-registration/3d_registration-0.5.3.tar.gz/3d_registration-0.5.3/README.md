9c4d049a28f06eb215f522752ce5b5c29ebdff85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:24 +0000

    Add package 3D-registration/0.5.3/3d_registration-0.5.3.tar.gz

---
+++ packages/3D-registration/3d_registration-0.5.3.tar.gz/3d_registration-0.5.3/README.md
@@ -0,0 +1,92 @@
+# Registration tools
+
+## Purpose and "history"
+This repository is about two scripts to do spatial and temporal registration of 3D microscopy images.
+It was initially developed to help friends with their ever moving embryos living under a microscope.
+I found that actually quite a few people were interested so I made a  version of it that is somewhat easier to use.
+
+In theory, the main difficulty to make the whole thing work is to install the different libraries.
+
+## Credits
+The whole thing is just a wrapping of the amazing blockmatching algorithm developed by [S. Ourselin et al.] and currently maintained Grégoire Malandin et al.@[Team Morpheme - inria] (if I am not mistaking).
+
+## Installation
+
+[conda] and [pip] are required to install `registration-tools`
+
+We recommand to install the registration tools in a specific environement (like [conda]). For example the following way:
+
+    conda create -n registration python=3.10
+You can then activate the environement the following way:
+
+    conda activate registration
+
+For here onward we assume that you are running the commands from the `registration` [conda] environement.
+
+Then, to install the whole thing, it is necessary to first install blockmatching. To do so you can run the following command:
+    
+    conda install vt -c morpheme
+
+Then, you can install the 3D-registration library either directly via pip:
+
+    pip install 3D-registration
+
+Or, if you want the latest version, by specifying the git repository:
+
+    pip install git+https://github.com/GuignardLab/registration-tools.git
+
+### Troubleshooting
+- Windows:
+
+    If you are trying to run the script on Windows you might need to install `pthreadvse2.dll`. 
+
+    It can be found there: https://www.pconlife.com/viewfileinfo/pthreadvse2-dll/ . Make sure to download the version that matches your operating system (32 or 64 bits, most likely 64).
+
+## Usage
+
+Most of the description on how to use the two scripts is described in the [manual] (Note that the installation part is quite outdated, the remaining is ok).
+
+That being said, once installed, one can run either of the scripts from anywhere in a terminal by typing:
+
+    time-registration
+
+or
+
+    spatial-registration
+
+The location of the json files or folder containing the json files will be prompted and when provided the registration will start.
+
+It is also possible to run the registration from a script/notebook the following way:
+```python
+from registrationtools import TimeRegistration
+tr = TimeRegistration('path/to/param.json')
+tr.run_trsf()
+```
+
+or
+
+```python
+from registrationtools import TimeRegistration
+tr = TimeRegistration('path/to/folder/with/jsonfiles/')
+tr.run_trsf()
+```
+
+or
+
+```python
+from registrationtools import TimeRegistration
+tr = TimeRegistration()
+tr.run_trsf()
+```
+
+and a path will be asked to be inputed.
+
+### Example json files
+
+Few example json files are provided to help the potential users. You can find informations about what they do in the [manual].
+
+[S. Ourselin et al.]: http://www-sop.inria.fr/asclepios/Publications/Gregoire.Malandain/ourselin-miccai-2000.pdf
+[Team Morpheme - inria]: https://team.inria.fr/morpheme/
+[conda]: https://conda.io/projects/conda/en/latest/user-guide/install/index.html
+[pip]: https://pypi.org/project/pip/
+[manual]: https://github.com/GuignardLab/registration-tools/blob/master/User-manual/usage/user-manual.pdf
---
+++ packages/3D-registration/3d_registration-0.5.3.tar.gz/3d_registration-0.5.3/src/IO/tif.py
@@ -0,0 +1,165 @@
+# -*- python -*-
+#
+#       adapted from C. Pradal
+#
+#       Copyright 2011 INRIA - CIRAD - INRA
+#
+#       File author(s): Christophe Pradal <christophe.pradal@cirad.fr>
+#                       Daniel Barbeau    <daniel.barbeau@cirad.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+################################################################################
+"""
+This module reads 3D tiff format
+"""
+
+
+__license__ = "Cecill-C"
+__revision__ = " $Id$ "
+
+import numpy as np
+from .spatial_image import SpatialImage
+
+
+__all__ = []
+
+import decimal
+
+from tifffile import TiffFile, imwrite
+
+__all__ += ["read_tif", "write_tif", "mantissa"]
+
+
+def read_tif(filename, channel=0):
+    """Read a tif image
+
+    :Parameters:
+    - `filename` (str) - name of the file to read
+    """
+
+    def format_digit(v):
+        try:
+            v = eval(v)
+        except Exception:
+            pass
+        return v
+
+    with TiffFile(filename) as tif:
+        if "ImageDescription" in tif.pages[0].tags:
+            description = (
+                tif.pages[0].tags["ImageDescription"].value.split("\n")
+            )
+            separator = set.intersection(*[set(k) for k in description]).pop()
+            info_dict = {
+                v.split(separator)[0]: format_digit(v.split(separator)[1])
+                for v in description
+            }
+        else:
+            info_dict = {}
+
+        if "XResolution" in tif.pages[0].tags:
+            vx = tif.pages[0].tags["XResolution"].value
+            if vx[0] != 0:
+                vx = vx[1] / vx[0]
+                if isinstance(vx, list):
+                    vx = vx[1] / vx[0]
+            else:
+                vx = 1.0
+        else:
+            vx = 1.0
+
+        if "YResolution" in tif.pages[0].tags:
+            vy = tif.pages[0].tags["YResolution"].value
+            if vy[0] != 0:
+                vy = vy[1] / vy[0]
+                if isinstance(vy, list):
+                    vy = vy[1] / vy[0]
+            else:
+                vy = 1.0
+        else:
+            vy = 1.0
+
+        if "ZResolution" in tif.pages[0].tags:
+            vz = tif.pages[0].tags["ZResolution"].value
+            if vz[0] != 0:
+                if isinstance(vz, list):
+                    vz = vz[1] / vz[0]
+            else:
+                vz = 1.0
+        elif "spacing" in info_dict:
+            vz = info_dict["spacing"]
+        else:
+            vz = 1.0
+
+        im = tif.asarray()
+        if len(im.shape) == 3:
+            im = np.transpose(im, (2, 1, 0))
+        elif len(im.shape) == 2:
+            im = np.transpose(im, (1, 0))
+        if 3 <= len(im.shape):
+            im = SpatialImage(im, (vx, vy, vz))
+        else:
+            print(im.shape, vx, vy)
+            im = SpatialImage(im, (vx, vy))
+        im.resolution = (vx, vy, vz)
+    return im
+
+
+def mantissa(value):
+    """Convert value to [number, divisor] where divisor is power of 10"""
+    # -- surely not the nicest thing around --
+    d = decimal.Decimal(str(value))  # -- lovely...
+    sign, digits, exp = d.as_tuple()
+    n_digits = len(digits)
+    dividend = int(
+        sum(v * (10 ** (n_digits - 1 - i)) for i, v in enumerate(digits))
+        * (1 if sign == 0 else -1)
+    )
+    divisor = int(10**-exp)
+    return dividend, divisor
+
+
+def write_tif(filename, obj):
+    if len(obj.shape) > 3:
+        raise IOError(
+            "Vectorial images are currently unsupported by tif writer"
+        )
+    is3D = len(obj.shape) == 3
+
+    if hasattr(obj, "resolution"):
+        res = obj.resolution
+    elif hasattr(obj, "voxelsize"):
+        res = obj.resolution
+    else:
+        res = (1, 1, 1) if is3D else (1, 1)
+
+    vx = 1.0 / res[0]
+    vy = 1.0 / res[1]
+    if is3D:
+        spacing = res[2]
+        metadata = {"spacing": spacing, "axes": "ZYX"}
+    else:
+        metadata = {}
+    extra_info = {
+        "XResolution": res[0],
+        "YResolution": res[1],
+        "spacing": (
+            spacing if is3D else None
+        ),  # : no way to save the spacing (no specific tag)
+    }
+    print(extra_info)
+
+    if obj.dtype.char in "BHhf":
+        return imwrite(
+            filename,
+            obj.T,
+            imagej=True,
+            resolution=(vx, vy),
+            metadata=metadata,
+        )
+    else:
+        return imwrite(filename, obj.T)
