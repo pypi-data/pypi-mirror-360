9c199ed14729021185937da19e53ee7553137808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:50 +0000

    Add package uagent-a2a-adapter/0.1.3/uagent_a2a_adapter-0.1.3-py3-none-any.whl

---
+++ packages/uagent-a2a-adapter/uagent_a2a_adapter-0.1.3-py3-none-any.whl/uagent_a2a_adapter/__init__.py
@@ -0,0 +1,6 @@
+"""uAgent A2A Adapter Package for A2A Integration."""
+
+from .adapter import A2AAdapter, A2ARegisterTool
+
+__version__ = "0.1.3"
+__all__ = ["A2AAdapter", "A2ARegisterTool","A2AAgentConfig"]
