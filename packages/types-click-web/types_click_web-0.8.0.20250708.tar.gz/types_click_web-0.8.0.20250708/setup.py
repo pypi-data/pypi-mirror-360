4aced72023f23fab311050c865595d30fe8507a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:13 +0000

    Add package types-click-web/0.8.0.20250708/types_click_web-0.8.0.20250708.tar.gz

---
+++ packages/types-click-web/types_click_web-0.8.0.20250708.tar.gz/types_click_web-0.8.0.20250708/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'click_web-stubs': ['__init__.pyi', 'exceptions.pyi', 'resources/__init__.pyi', 'resources/cmd_exec.pyi', 'resources/cmd_form.pyi', 'resources/index.pyi', 'resources/input_fields.pyi', 'web_click_types.pyi', 'METADATA.toml', 'py.typed']})
