784eb6abbd9e6255569f6c86dd68590ee061578d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:58 +0000

    Add package portal-env/0.2.0/portal_env-0.2.0.tar.gz

---
+++ packages/portal-env/portal_env-0.2.0.tar.gz/portal_env-0.2.0/portal_env/envs/retro/env_main.py
@@ -0,0 +1,35 @@
+from portal_env import EnvSidePortal
+import gymnasium
+import retro
+
+
+class GymnasiumWrapper(gymnasium.Env):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.retro_env = retro.make(*args, **kwargs)
+
+    @property
+    def action_space(self):
+        return self.retro_env.action_space
+    
+    @property
+    def observation_space(self):
+        return self.retro_env.observation_space
+
+    def step(self, action):
+        obs, reward, done, info = self.retro_env.step(action)
+        terminated = done
+        truncated = False
+        return obs, reward, terminated, truncated, info
+    
+    def reset(self, *, seed = None, options = None):
+        return self.retro_env.reset(), {}
+
+
+def main():
+    portal = EnvSidePortal(env_factory=GymnasiumWrapper)
+    portal.start()
+
+
+if __name__ == '__main__':
+    main()
