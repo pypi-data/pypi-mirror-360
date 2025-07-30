1907424fb7127279ac295f860466a0a6758effce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:21 +0000

    Add package BayesInference/0.0.4/bayesinference-0.0.4.tar.gz

---
+++ packages/BayesInference/bayesinference-0.0.4.tar.gz/bayesinference-0.0.4/setup.py
@@ -0,0 +1,38 @@
+import setuptools 
+import platform
+VERSION = '0.0.4' 
+
+system = platform.system()
+if system == "Linux":
+    jax_dep = "jax[cuda12]"
+elif system == "Darwin":
+    jax_dep = "jax"
+else:
+    jax_dep = "jax"
+
+# Setting up
+setuptools.setup(
+        name="BayesInference", 
+        version=VERSION,
+        author="Sebastian Sosa",
+        author_email="<s.sosa@live.fr>",
+        description="GNU GENERAL PUBLIC LICENSE",
+        long_description="Bayesian Inference (BI) software is available in both Python and R. It aims to unify the modeling experience by integrating an intuitive model-building syntax with the flexibility of low-level abstraction coding available but also pre-build function for high-level of abstraction and including hardware-accelerated computation for improved scalability.",
+        packages=setuptools.find_packages(),
+        install_requires=[jax_dep, 'numpyro', 'pandas', 'seaborn', 'tensorflow_probability', 'arviz'],
+        python_requires=">=3.9",
+        keywords=['python', 'Bayesian inferences'],
+        include_package_data=True,
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows"
+        ],
+        project_urls={
+            "Homepage": "https://github.com/BGN-for-ASNA/BI",
+            "Bug Tracker": "https://github.com/BGN-for-ASNA/BI/issues"
+        }
+)
