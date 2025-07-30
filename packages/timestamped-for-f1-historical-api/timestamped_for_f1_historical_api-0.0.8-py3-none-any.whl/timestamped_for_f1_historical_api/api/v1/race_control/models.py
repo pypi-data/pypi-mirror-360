6f12c076b1c7f24210b2a53b277f2d22107e69d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:15 +0000

    Add package timestamped-for-f1-historical-api/0.0.8/timestamped_for_f1_historical_api-0.0.8-py3-none-any.whl

---
+++ packages/timestamped-for-f1-historical-api/timestamped_for_f1_historical_api-0.0.8-py3-none-any.whl/timestamped_for_f1_historical_api/api/v1/race_control/models.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from timestamped_for_f1_historical_api.core.db import Base, get_base_metadata
+from timestamped_for_f1_historical_api.core.models import ResponseModel
+# Prevent circular imports for SQLAlchemy models since we are using type annotation
+if TYPE_CHECKING:
+    from timestamped_for_f1_historical_api.api.v1.event.models import Event
+
+
+class RaceControl(Base):
+    __tablename__ = "race_control"
+    metadata = get_base_metadata()
+
+    id: Mapped[int] = mapped_column(unique=True) # For internal use only
+    date: Mapped[DateTime]
+    message: Mapped[str]
+    
+    # One-to-one weak rel with event as owner
+    event_id: Mapped[int] = mapped_column(ForeignKey(column="event.id"), primary_key=True)
+    event: Mapped["Event"] = relationship(back_populates="race_control", cascade="all, delete-orphan", single_parent=True)
+    
+    def __repr__(self) -> str:
+        return f"RaceControl(id={self.id!r}, date={self.date!r}, message={self.message!r}, event_id={self.event_id!r}"
+
+
+class RaceControlResponse(ResponseModel):
+    date: datetime
+    message: str
