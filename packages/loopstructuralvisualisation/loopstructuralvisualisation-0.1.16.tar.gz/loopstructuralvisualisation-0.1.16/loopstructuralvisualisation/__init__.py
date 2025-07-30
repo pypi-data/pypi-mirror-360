d532b310dc3718162c4c2a611febd034c4a6620f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:27 +0000

    Add package loopstructuralvisualisation/0.1.16/loopstructuralvisualisation-0.1.16.tar.gz

---
+++ packages/loopstructuralvisualisation/loopstructuralvisualisation-0.1.16.tar.gz/loopstructuralvisualisation-0.1.16/loopstructuralvisualisation/__init__.py
@@ -0,0 +1,10 @@
+from ._3d_viewer import Loop3DView
+from ._colours import random_colour
+from ._rotation_angle import RotationAnglePlotter
+from ._2d_viewer import Loop2DView
+from ._stratigraphic_column import StratigraphicColumnView
+
+try:
+    from ._register_loop_ui import *  # this replaces default pyvista trame ui
+except ImportError:
+    print("Could not import trame ui")
