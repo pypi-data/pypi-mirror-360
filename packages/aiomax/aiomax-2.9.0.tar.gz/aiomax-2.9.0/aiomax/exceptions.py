282d9917c43446ea8ef46740a61a6ca469b2e98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:08 +0000

    Add package aiomax/2.9.0/aiomax-2.9.0.tar.gz

---
+++ packages/aiomax/aiomax-2.9.0.tar.gz/aiomax-2.9.0/aiomax/exceptions.py
@@ -0,0 +1,16 @@
+class InvalidToken(Exception):
+    """
+    Invalid token Exception
+    """
+
+
+class AttachmentNotReady(Exception):
+    """
+    Attachment not ready Exception
+    """
+
+
+class ChatNotFound(Exception):
+    """
+    Chat not found Exception
+    """
