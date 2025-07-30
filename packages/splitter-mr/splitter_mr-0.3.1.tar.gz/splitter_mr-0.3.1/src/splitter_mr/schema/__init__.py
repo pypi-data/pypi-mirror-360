40e3789584f092c8f9f78c97585f7df2dfb1fa75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:45 +0000

    Add package splitter-mr/0.3.1/splitter_mr-0.3.1.tar.gz

---
+++ packages/splitter-mr/splitter_mr-0.3.1.tar.gz/splitter_mr-0.3.1/src/splitter_mr/schema/__init__.py
@@ -0,0 +1,17 @@
+from .schemas import (
+    DEFAULT_EXTRACTION_PROMPT,
+    DEFAULT_IMAGE_CAPTION_PROMPT,
+    DOCLING_SUPPORTED_EXTENSIONS,
+    LANGUAGES,
+    ReaderOutput,
+    SplitterOutput,
+)
+
+__all__ = [
+    "ReaderOutput",
+    "SplitterOutput",
+    "LANGUAGES",
+    "DEFAULT_EXTRACTION_PROMPT",
+    "DEFAULT_IMAGE_CAPTION_PROMPT",
+    "DOCLING_SUPPORTED_EXTENSIONS",
+]
