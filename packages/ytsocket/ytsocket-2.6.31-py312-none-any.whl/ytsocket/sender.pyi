b872d52bbcec169be5981dad39324c9fe6e1d763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:35 +0000

    Add package ytsocket/2.6.31/ytsocket-2.6.31-py312-none-any.whl

---
+++ packages/ytsocket/ytsocket-2.6.31-py312-none-any.whl/ytsocket/sender.pyi
@@ -0,0 +1,14 @@
+# Stub file for type hints
+
+import queue
+from typing import Callable, Any
+from johnsonthy.thread.handler import Runnable
+from johnsonthy.logging import LOGGER
+from socket import socket
+
+timeout: int: ...
+separator: str: ...
+class Sender:
+    def __init__(self, *args, **kwargs): ...
+    def run(self, *args, **kwargs): ...
+
