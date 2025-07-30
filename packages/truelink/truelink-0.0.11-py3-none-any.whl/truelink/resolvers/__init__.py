cff351ef083de6164d58bbfc5695d04b0e959f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:21 +0000

    Add package truelink/0.0.11/truelink-0.0.11-py3-none-any.whl

---
+++ packages/truelink/truelink-0.0.11-py3-none-any.whl/truelink/resolvers/__init__.py
@@ -0,0 +1,21 @@
+from .base import BaseResolver
+# from .yandex import YandexDiskResolver
+from .buzzheavier import BuzzHeavierResolver
+# from .devuploads import DevUploadsResolver
+from .lulacloud import LulaCloudResolver
+# from .uploadhaven import UploadHavenResolver
+from .fuckingfast import FuckingFastResolver
+# from .mediafile import MediaFileResolver
+# from .mediafire import MediaFireResolver
+
+__all__ = [
+    'BaseResolver',
+    # 'YandexDiskResolver',
+    'BuzzHeavierResolver',
+    # 'DevUploadsResolver',
+    'LulaCloudResolver',
+    # 'UploadHavenResolver',
+    'FuckingFastResolver',
+    # 'MediaFileResolver',
+    # 'MediaFireResolver',
+]
