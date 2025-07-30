3b76367184aa8f39a9992d5897b5416e0bb5d755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:51 +0000

    Add package blackfire/1.20.31/blackfire-1.20.31-cp39-cp39-macosx_15_0_x86_64.whl

---
+++ packages/blackfire/blackfire-1.20.31-cp39-cp39-macosx_15_0_x86_64.whl/blackfire/hooks/odoo/middleware.py
@@ -0,0 +1,27 @@
+from blackfire.utils import get_logger
+from blackfire.hooks.wsgi import BlackfireWSGIMiddleware
+
+log = get_logger(__name__)
+
+
+class OdooMiddleware(BlackfireWSGIMiddleware):
+
+    FRAMEWORK = 'odoo'
+
+    def build_blackfire_yml_response(
+        self, blackfireyml_content, agent_response, environ, start_response
+    ):
+        from werkzeug.wrappers import Response
+        # send response if signature is validated
+        if agent_response:
+            return Response(
+                response=blackfireyml_content or '', headers=[agent_response]
+            )(environ, start_response)
+
+        return Response()(environ, start_response)
+
+    def get_view_name(self, environ):
+        return None
+
+    def __getattr__(self, name):
+        return getattr(self.app, name)
