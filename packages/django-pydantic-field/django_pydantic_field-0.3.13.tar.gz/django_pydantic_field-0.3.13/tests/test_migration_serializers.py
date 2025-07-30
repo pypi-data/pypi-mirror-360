5ad1ca5da879bf18214eb20169f26f2e7a6504ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:56 +0000

    Add package django-pydantic-field/0.3.13/django_pydantic_field-0.3.13.tar.gz

---
+++ packages/django-pydantic-field/django_pydantic_field-0.3.13.tar.gz/django_pydantic_field-0.3.13/tests/test_migration_serializers.py
@@ -0,0 +1,47 @@
+import sys
+import typing as t
+import typing_extensions as te
+
+from django.db.migrations.writer import MigrationWriter
+import pytest
+
+import django_pydantic_field
+try:
+    from django_pydantic_field.compat.django import GenericContainer
+except ImportError:
+    from django_pydantic_field._migration_serializers import GenericContainer  # noqa
+
+if sys.version_info < (3, 9):
+    test_types = [
+        str,
+        list,
+        t.List[str],
+        t.Union[te.Literal["foo"], t.List[str]],
+        t.List[t.Union[int, bool]],
+        t.Tuple[t.List[te.Literal[1]], t.Union[str, te.Literal["foo"]]],
+        t.ForwardRef("str"),
+    ]
+else:
+    test_types = [
+        str,
+        list,
+        list[str],
+        t.Union[t.Literal["foo"], list[str]],
+        list[t.Union[int, bool]],
+        tuple[list[t.Literal[1]], t.Union[str, t.Literal["foo"]]],
+        t.ForwardRef("str"),
+    ]
+
+
+@pytest.mark.parametrize("raw_type", test_types)
+def test_wrap_unwrap_idempotent(raw_type):
+    wrapped_type = GenericContainer.wrap(raw_type)
+    assert raw_type == GenericContainer.unwrap(wrapped_type)
+
+
+@pytest.mark.parametrize("raw_type", test_types)
+def test_serialize_eval_idempotent(raw_type):
+    raw_type = GenericContainer.wrap(raw_type)
+    expression, _ = MigrationWriter.serialize(GenericContainer.wrap(raw_type))
+    imports = dict(typing=t, typing_extensions=te, django_pydantic_field=django_pydantic_field)
+    assert eval(expression, imports) == raw_type
