d0c5877ed75e9983d0960bc75dcb5f4d74feeacd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:34 +0000

    Add package gsheeter/0.1.9/gsheeter-0.1.9.tar.gz

---
+++ packages/gsheeter/gsheeter-0.1.9.tar.gz/gsheeter-0.1.9/gsheeter/__init__.py
@@ -0,0 +1,43 @@
+""" Google spreadsheet Python API Abstractor integrated with pandas"""
+
+__version__ = '0.1.3'
+__author__ = 'Yunjong Guk'
+
+from .auth.auth import (
+	service_account
+)
+from .cache.cache import (
+	cache,
+	set_cache_usage
+)
+from .drive.drive import Drive
+from .environ.environ import (
+	set_table_buffer,
+	set_table_filler,
+	set_float_format,
+	TABLE_BUFFER,
+	TABLE_FILLER,
+	FLOAT_FORMAT
+)
+from .spreadsheet.sheet_objects import SheetSquared, Table
+from .spreadsheet.spreadsheet import Spreadsheet
+from .spreadsheet.sheet import Sheet
+from .spreadsheet.chart import Chart
+
+__all__ = (
+	'service_account',
+	'set_table_buffer',
+	'set_table_filler',
+	'Drive',
+	'Spreadsheet',
+	'Sheet',
+	'Table',
+	'Chart',
+	'SheetSquared',
+	'cache',
+	'set_cache_usage',
+	'set_float_format',
+	'TABLE_BUFFER',
+	'TABLE_FILLER',
+	'FLOAT_FORMAT',
+)
---
+++ packages/gsheeter/gsheeter-0.1.9.tar.gz/gsheeter-0.1.9/gsheeter/environ/environ.py
@@ -0,0 +1,14 @@
+TABLE_FILLER = '-'
+TABLE_BUFFER = '/'
+
+
+def set_table_filler(val: str) -> None:
+	TABLE_FILLER = val
+
+def set_table_buffer(val: str) -> None:
+	TABLE_BUFFER = val
+
+FLOAT_FORMAT = '{:.2f}'
+
+def set_float_format(val: str) -> None:
+	FLOAT_FORMAT = val
