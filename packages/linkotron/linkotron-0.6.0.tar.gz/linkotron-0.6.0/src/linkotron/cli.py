9a3e5fa2af6f7ac1277f134a517094c2f47a6215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:28 +0000

    Add package linkotron/0.6.0/linkotron-0.6.0.tar.gz

---
+++ packages/linkotron/linkotron-0.6.0.tar.gz/linkotron-0.6.0/src/linkotron/cli.py
@@ -0,0 +1,65 @@
+"""
+linkotron: CLI to format GitHub links in a shorter format.
+"""
+
+from __future__ import annotations
+
+import argparse
+
+from . import __version__, shorten
+
+try:
+    import pyperclip as copier  # type: ignore[import-untyped]
+except ImportError:
+    try:
+        import xerox as copier  # type: ignore[import-not-found]
+    except ImportError:
+        copier = None
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.color = True  # type: ignore[attr-defined]
+    parser.add_argument(
+        "-V", "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+    parser.add_argument("input", help="text containing GitHub links to shorten")
+    parser.add_argument(
+        "--no-copy", action="store_true", help="do not copy output to clipboard"
+    )
+
+    format_group = parser.add_argument_group("formatters")
+    format_group = format_group.add_mutually_exclusive_group()
+    for name, help_text in (
+        ("md", "Markdown"),
+        ("rst", "reStructuredText"),
+        ("term", "terminal"),
+    ):
+        format_group.add_argument(
+            f"-{name[0]}",
+            f"--{name}",
+            f"--{help_text.lower()}",
+            action="store_const",
+            const=name,
+            dest="formatter",
+            help=(
+                "output in OSC 8 for terminal"
+                if help_text.startswith("term")
+                else f"output in {help_text}"
+            ),
+        )
+
+    args = parser.parse_args()
+
+    output = shorten(line=args.input, formatter=args.formatter)
+    if copier and not args.no_copy and output != args.input:
+        copier.copy(output)
+        print(f"Copied! {output}")
+    else:
+        print(f"{output}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/linkotron/linkotron-0.6.0.tar.gz/linkotron-0.6.0/tests/test_linkotron.py
@@ -0,0 +1,72 @@
+"""
+Unit tests
+"""
+
+from __future__ import annotations
+
+import pytest
+
+import linkotron
+
+
+@pytest.mark.parametrize(
+    "link, expected",
+    [
+        ("https://github.com/python/peps", "python/peps"),
+        ("https://github.com/python/peps/issues/2", "python/peps#2"),
+        ("https://github.com/python/peps/pull/2399", "python/peps#2399"),
+        ("https://github.com/Textualize/rich/discussions/2258", "Textualize/rich#2258"),
+        (
+            "https://github.com/"
+            "python/peps/commit/ceb81fd7b71f74aaa2295594f86501acbe620cda",
+            "python/peps#ceb81fd",
+        ),
+        (
+            "https://github.com/python/peps/pull/2399#issuecomment-1063409480",
+            "python/peps#2399 (comment)",
+        ),
+        (
+            "https://github.com/python/peps/pull/2399#discussion_r823103351",
+            "python/peps#2399 (comment)",
+        ),
+        (
+            "https://github.com/Textualize/rich/discussions/2258#discussioncomment-3543128",
+            "Textualize/rich#2258 (comment)",
+        ),
+    ],
+)
+def test_shorten(link: str, expected: str) -> None:
+    # Act / Assert
+    assert linkotron.shorten(link) == expected
+    assert linkotron.shorten(link + "/") == expected
+
+
+@pytest.mark.parametrize("link, expected", [("some text", "some text")])
+def test_shorten_no_link(link: str, expected: str) -> None:
+    # Act / Assert
+    assert linkotron.shorten(link) == expected
+
+
+@pytest.mark.parametrize(
+    "link, formatter, expected",
+    [
+        (
+            "https://github.com/python/peps/pull/2399",
+            "md",
+            "[python/peps#2399](https://github.com/python/peps/pull/2399)",
+        ),
+        (
+            "https://github.com/python/peps/pull/2399",
+            "rst",
+            "`python/peps#2399 <https://github.com/python/peps/pull/2399>`__",
+        ),
+        (
+            "https://github.com/python/peps/pull/2399",
+            "terminal",
+            "\033]8;;https://github.com/python/peps/pull/2399\033\\python/peps#2399\033]8;;\033\\",
+        ),
+    ],
+)
+def test_shorten_into_format(link: str, formatter: str, expected: str) -> None:
+    # Act / Assert
+    assert linkotron.shorten(link, formatter=formatter) == expected
