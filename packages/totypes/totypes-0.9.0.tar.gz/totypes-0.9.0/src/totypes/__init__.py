60fc09139df0309bdecdcf494922938fbc768fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:02 +0000

    Add package totypes/0.9.0/totypes-0.9.0.tar.gz

---
+++ packages/totypes/totypes-0.9.0.tar.gz/totypes-0.9.0/src/totypes/__init__.py
@@ -0,0 +1,14 @@
+"""totypes - Custom datatypes useful in a topology optimization context.
+
+Copyright (c) 2023 The INVRS-IO authors.
+"""
+
+__version__ = "v0.9.0"
+__author__ = "Martin F. Schubert <mfschubert@gmail.com>"
+
+__all__ = ["json_utils", "symmetry", "types"]
+
+from totypes import json_utils as json_utils
+from totypes import partition_utils as partition_utils
+from totypes import symmetry as symmetry
+from totypes import types as types
