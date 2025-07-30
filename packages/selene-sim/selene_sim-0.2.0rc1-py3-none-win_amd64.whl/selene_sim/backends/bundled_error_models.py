0c20baccbda2aac6ced6a066d06700caa8ebe8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:24 +0000

    Add package selene-sim/0.2.0rc1/selene_sim-0.2.0rc1-py3-none-win_amd64.whl

---
+++ packages/selene-sim/selene_sim-0.2.0rc1-py3-none-win_amd64.whl/selene_sim/backends/bundled_error_models.py
@@ -0,0 +1,9 @@
+from selene_depolarizing_error_model_plugin import (
+    DepolarizingPlugin as DepolarizingErrorModel,
+)
+from selene_ideal_error_model_plugin import IdealPlugin as IdealErrorModel
+
+__all__ = [
+    "DepolarizingErrorModel",
+    "IdealErrorModel",
+]
