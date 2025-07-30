f19a38c211ddf2ad4d143399c2a6464928b142fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:35 +0000

    Add package WhiskiWrap/1.2.3/whiskiwrap-1.2.3.tar.gz

---
+++ packages/WhiskiWrap/whiskiwrap-1.2.3.tar.gz/whiskiwrap-1.2.3/setup.py
@@ -0,0 +1,40 @@
+
+from setuptools import setup
+
+setup(
+   name='WhiskiWrap',
+   version='1.2.3',
+   author='Chris Rodgers',
+   author_email='',
+   maintainer=', '.join(['cxrodgers','aiporre','vncntprvst']),
+   packages=['WhiskiWrap','wwutils'],
+   # scripts=['bin/script1','bin/script2'],
+   url='http://pypi.python.org/pypi/WhiskiWrap/',
+   license='LICENSE.txt',
+   description='Whisk package wrapper created by cxrodgers',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   install_requires=[
+      'tables>=3.5.1',
+      'pandas',
+      'MediaInfo',
+      'future',
+      'tifffile',
+      'imagecodecs',
+      'statsmodels',
+      'ffmpeg-python>=0.2.0',
+      'whisk-janelia[ffmpeg]',
+      'zarr',
+      'pyarrow',
+      'matplotlib',
+      'easygui',
+      'opencv-python',  # Required for whiskerpad module
+      'h5py',  # Required for combine_sides module
+      'seaborn',  # Optional, for plotting
+      'plotly',  # Optional, for interactive plotting
+      'cmcrameri',  # Optional, for colormaps
+      'psutil',  # Optional, for system resource monitoring
+      ],
+   python_requires='>=3.10',
+   include_package_data=True,
+)
