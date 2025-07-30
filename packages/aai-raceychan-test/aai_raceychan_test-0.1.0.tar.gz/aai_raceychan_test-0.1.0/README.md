230ee24e2471b1697dd22411103c6a925cf8fef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:28 +0000

    Add package aai-raceychan-test/0.1.0/aai_raceychan_test-0.1.0.tar.gz

---
+++ packages/aai-raceychan-test/aai_raceychan_test-0.1.0.tar.gz/aai_raceychan_test-0.1.0/README.md
@@ -0,0 +1,18 @@
+# AAI
+
+A dummy package for testing PyPI publishing.
+
+## Installation
+
+```bash
+pip install aai
+```
+
+## Usage
+
+```python
+from aai import hello_world, add_numbers
+
+print(hello_world())  # Hello from AAI package!
+print(add_numbers(2, 3))  # 5
+```
---
+++ packages/aai-raceychan-test/aai_raceychan_test-0.1.0.tar.gz/aai_raceychan_test-0.1.0/src/aai/__init__.py
@@ -0,0 +1,13 @@
+"""
+AAI - A dummy package for testing PyPI publishing.
+"""
+
+__version__ = "0.1.0"
+
+def hello_world():
+    """Return a simple greeting message."""
+    return "Hello from AAI package!"
+
+def add_numbers(a: int, b: int) -> int:
+    """Add two numbers together."""
+    return a + b
