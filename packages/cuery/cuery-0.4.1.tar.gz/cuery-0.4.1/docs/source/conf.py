39c3e5afcac680fca76612af4fbc0b6862ee8da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:03 +0000

    Add package cuery/0.4.1/cuery-0.4.1.tar.gz

---
+++ packages/cuery/cuery-0.4.1.tar.gz/cuery-0.4.1/docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "cuery"
+copyright = "2025, Thomas Buhrmann"
+author = "Thomas Buhrmann"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["autoapi.extension", "sphinx.ext.autodoc", "sphinxcontrib.autodoc_pydantic"]
+
+autoapi_dirs = ["../../src"]
+autodoc_typehints = "description"
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
