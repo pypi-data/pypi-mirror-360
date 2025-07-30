7db49ebee280501e31d7e8089882d6cb5f4a18ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:02 +0000

    Add package invrs-gym/1.6.0/invrs_gym-1.6.0.tar.gz

---
+++ packages/invrs-gym/invrs_gym-1.6.0.tar.gz/invrs_gym-1.6.0/src/invrs_gym/__init__.py
@@ -0,0 +1,10 @@
+"""invrs_gym - A collection of inverse design challenges.
+
+Copyright (c) 2023 The INVRS-IO authors.
+"""
+
+__version__ = "v1.6.0"
+__author__ = "Martin F. Schubert <mfschubert@gmail.com>"
+
+from invrs_gym import challenges as challenges
+from invrs_gym import utils as utils
