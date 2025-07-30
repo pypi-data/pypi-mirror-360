710b1c5a4c7ed00ebd1af6c7d90beff3192acafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:29 +0000

    Add package panther/5.0.0b11/panther-5.0.0b11.tar.gz

---
+++ packages/panther/panther-5.0.0b11.tar.gz/panther-5.0.0b11/panther/file_handler.py
@@ -0,0 +1,48 @@
+from functools import cached_property
+
+from pydantic import BaseModel, field_validator, model_serializer
+
+from panther import status
+from panther.exceptions import APIError
+
+
+class File(BaseModel):
+    file_name: str
+    content_type: str
+    file: bytes
+
+    @cached_property
+    def size(self):
+        return len(self.file)
+
+    def save(self) -> str:
+        if hasattr(self, '_file_name'):
+            return self._file_name
+
+        self._file_name = self.file_name
+        # TODO: check for duplication
+        with open(self._file_name, 'wb') as file:
+            file.write(self.file)
+
+        return self.file_name
+
+    @model_serializer(mode='wrap')
+    def _serialize(self, handler):
+        result = handler(self)
+        result['path'] = self.save()
+        return result
+
+    def __repr__(self) -> str:
+        return f'{self.__repr_name__()}(file_name={self.file_name}, content_type={self.content_type})'
+
+    __str__ = __repr__
+
+
+class Image(File):
+    @field_validator('content_type')
+    @classmethod
+    def validate_content_type(cls, content_type: str) -> str:
+        if not content_type.startswith('image/'):
+            msg = f"{content_type} is not a valid image 'content_type'"
+            raise APIError(detail=msg, status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        return content_type
