d3a26f0bbcec8efe518758db088c9c8baf5d7fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:24 +0000

    Add package pyeasyal/0.1.1/pyeasyal-0.1.1.tar.gz

---
+++ packages/pyeasyal/pyeasyal-0.1.1.tar.gz/pyeasyal-0.1.1/pyeasyal/core.py
@@ -0,0 +1,6 @@
+
+def say_hello(name):
+    print(f"👋 Hello, {name}!")
+
+def ask(question):
+    return input(f"❓ {question} ")
---
+++ packages/pyeasyal/pyeasyal-0.1.1.tar.gz/pyeasyal-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyeasyal",
+    version="0.1.1",
+    packages=find_packages(),
+    description="Make Python easier and friendlier!",
+    author="Alhawari Code",
+    author_email="alhawari.officail@gmail.com",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
