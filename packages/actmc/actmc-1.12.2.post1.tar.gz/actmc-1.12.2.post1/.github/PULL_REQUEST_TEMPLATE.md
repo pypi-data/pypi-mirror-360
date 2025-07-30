3c01324a80e5be281a9e7ffc79f1bf765ffabd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:38 +0000

    Add package actmc/1.12.2.post1/actmc-1.12.2.post1.tar.gz

---
+++ packages/actmc/actmc-1.12.2.post1.tar.gz/actmc-1.12.2.post1/.github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,15 @@
+## Pull Request
+
+### Description
+Please provide a brief description of the changes in this pull request.
+
+### Checklist
+Please check the following before submitting your pull request:
+
+- [ ] I have read and followed the project's contributing guidelines.
+- [ ] I have tested my changes locally and they work as expected.
+- [ ] I have added necessary documentation or updated existing documentation (if applicable).
+- [ ] I have written unit tests for the changes I've made (if applicable).
+
+### Additional Information
+Add any additional information or context about the pull request here.
---
+++ packages/actmc/actmc-1.12.2.post1.tar.gz/actmc-1.12.2.post1/actmc/__init__.py
@@ -0,0 +1,30 @@
+"""
+actmc
+
+Real-time Minecraft Event Handling and Protocol Integration in Python
+
+:copyright: (c) 2025-present Snifo
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'actmc'
+__version__ = '1.12.2.post1'
+__license__ = 'MIT License'
+__author__ = 'Snifo'
+__email__ = 'Snifo@mail.com'
+__github__ = 'https://github.com/mrsnifo/actmc'
+
+from .chunk import *
+from .client import *
+from .errors import *
+from .gateway import *
+from .math import *
+from .protocol import *
+from .state import *
+from .tcp import *
+from .user import *
+from . import (
+    utils as utils,
+    entities as entities,
+    ui as ui,
+)
