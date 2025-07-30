57b1160fbbc8050b787fd9d10410e12a14a1abe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:57 +0000

    Add package wyrmx-cli/1.0.17/wyrmx_cli-1.0.17.tar.gz

---
+++ packages/wyrmx-cli/wyrmx_cli-1.0.17.tar.gz/wyrmx_cli-1.0.17/wyrmx/main.py
@@ -0,0 +1,28 @@
+import typer
+from typing import Optional
+from wyrmx.commands import build, new, run, version as ver
+from wyrmx.commands.file_generators import generate_controller, generate_service
+
+app = typer.Typer()
+
+
+app.command()(build)
+app.command()(new)
+app.command()(run)
+
+app.command("generate:controller")(generate_controller)
+app.command("gc")(generate_controller)
+
+
+
+app.command("generate:service")(generate_service)
+app.command("gs")(generate_service)
+
+@app.callback(invoke_without_command=True)
+def main( version: bool = typer.Option( None, "--version", is_eager=True, help="Show the application version and exit.")): 
+    if version: ver()
+
+
+if __name__ == "__main__":
+    app()
+
