79826a8bee01be96387880650053bdf9c25306f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:01 +0000

    Add package patchmatch-cython/0.1.0/patchmatch_cython-0.1.0-cp310-cp310-win32.whl

---
+++ packages/patchmatch-cython/patchmatch_cython-0.1.0-cp310-cp310-win32.whl/patchmatch_cython/__init__.py
@@ -0,0 +1,19 @@
+import typing
+
+__version__: str = "0.1.0"
+
+# Core algorithm imports (always available)
+from .python_solver import PythonSolver
+from .inpaint import inpaint_pyramid, inpaint_single
+
+# Try to import Cython-accelerated version
+try:
+    from .cython_solver import CythonSolver
+    __all__: typing.List[str] = [
+        "PythonSolver", "inpaint_pyramid", "inpaint_single",
+        "CythonSolver"
+    ]
+except ImportError:
+    __all__ = [
+        "PythonSolver", "inpaint_pyramid", "inpaint_single"
+    ] 
