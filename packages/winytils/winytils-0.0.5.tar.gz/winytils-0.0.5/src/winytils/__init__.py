5456a4c921d57a2f903c6f5a435d2370182ae267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:00 +0000

    Add package winytils/0.0.5/winytils-0.0.5.tar.gz

---
+++ packages/winytils/winytils-0.0.5.tar.gz/winytils-0.0.5/src/winytils/__init__.py
@@ -0,0 +1,12 @@
+"""
+To execute every module directly - remove relative imports "."
+"""
+
+from . import workstation_events
+from . import privileges
+from . import windows
+from . import workstation
+from . import win11toast
+
+
+__version__ = "0.0.5"
---
+++ packages/winytils/winytils-0.0.5.tar.gz/winytils-0.0.5/tests/test.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+import time
+
+sys.path.append(str(Path(__file__).parent.parent.absolute()))
+
+from src.winytils.windows import Window, Windows
+from src.winytils.workstation import Workstation
+from src.winytils.win11toast import toast
+
+
+def test_windows():
+    windows = Windows.filter(by_exe="explorer.exe")
+    [print(w) for w in windows]
+
+
+def test2():
+    time.sleep(3)
+    window = Windows.foregrond()
+
+    if window:
+        if window.is_uwp():
+            child = window.uwp_child
+            if child:
+                window = child
+        print(window)
+        window.icon.save("icon.png")
+
+
+def test3():
+    workstation = Workstation()
+    workstation.lock()
+    time.sleep(5)
+    print(workstation.is_locked())
+
+
+def test4():
+    toast("Hello Python🐍")
+
+
+if __name__ == "__main__":
+    test4()
