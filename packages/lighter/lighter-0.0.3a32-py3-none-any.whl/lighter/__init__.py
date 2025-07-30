b2f66011d96653941e8bc793f72f2cf9352c5d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:23 +0000

    Add package lighter/0.0.3a32/lighter-0.0.3a32-py3-none-any.whl

---
+++ packages/lighter/lighter-0.0.3a32-py3-none-any.whl/lighter/__init__.py
@@ -0,0 +1,13 @@
+"""
+Lighter is a framework for streamlining deep learning experiments with configuration files.
+"""
+
+__version__ = "0.0.3a32"
+
+from .utils.logging import _setup_logging
+
+_setup_logging()
+
+from .engine.config import Config
+from .engine.runner import Runner
+from .system import System
---
+++ packages/lighter/lighter-0.0.3a32-py3-none-any.whl/lighter/callbacks/__init__.py
@@ -0,0 +1,3 @@
+from .freezer import Freezer
+from .writer.file import FileWriter
+from .writer.table import TableWriter
