2ec56f899538d037a406851074ad504d56a8052f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:35 +0000

    Add package subcli/0.1.3/subcli-0.1.3.tar.gz

---
+++ packages/subcli/subcli-0.1.3.tar.gz/subcli-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name="subcli",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=["click", "requests", "beautifulsoup4", "InquirerPy"],
+    entry_points={
+        "console_scripts": [
+            "subcli=subcli.subcli:subcli",
+        ],
+    },
+    author="yannickRafael",
+    description="A CLI tool to download subtitles",
+    long_description_content_type='text/markdown',
+    long_description=description,
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires=">=3.6",
+)
