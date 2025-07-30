a1f311e36027ddc9a87be4418ae99a232c0eb1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:50 +0000

    Add package antonior-tabplot-pkg/0.1.8/antonior_tabplot_pkg-0.1.8-py3-none-any.whl

---
+++ packages/antonior-tabplot-pkg/antonior_tabplot_pkg-0.1.8-py3-none-any.whl/meu_tabplot_lib/__init__.py
@@ -0,0 +1,7 @@
+# meu_tabplot_lib/__init__.py
+
+# Exponha as funções principais do seu módulo 'binning'
+from .binning import simulate_tabplot_binning, plot_tabplot_interactive
+
+# Você também pode definir uma versão para o seu pacote aqui
+__version__ = '0.1.8'
