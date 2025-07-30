8baf1643d56d1bccd0ae860c3f0e86bca5c74ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:36 +0000

    Add package blc-py/0.1.1/blc_py-0.1.1-py3-none-any.whl

---
+++ packages/blc-py/blc_py-0.1.1-py3-none-any.whl/blc/__init__.py
@@ -0,0 +1,14 @@
+from .blc import Blc
+from .barbell import Barbell
+from .plates import Plates
+
+__all__ = [
+    "Blc",
+    "Barbell",
+    "Plates",
+]
+
+
+__version__ = "0.1.1"
+__authors__ = ["jwc20"]
+__source__ = "https://github.com/jwc20/blc-py"
