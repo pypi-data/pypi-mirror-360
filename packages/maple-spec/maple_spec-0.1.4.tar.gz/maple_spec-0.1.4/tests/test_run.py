8f8a56464db1b3325bc21d982b8e489360b4db07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:10 +0000

    Add package maple-spec/0.1.4/maple_spec-0.1.4.tar.gz

---
+++ packages/maple-spec/maple_spec-0.1.4.tar.gz/maple_spec-0.1.4/tests/test_run.py
@@ -0,0 +1,47 @@
+import maple as mp
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def test_success_run():
+    project_id = "24fa0ed1c3"
+    mp.init_model(project_id=project_id, model_id="2696b4a381")
+    mp.run(spec)
+    assert mp._project_id == project_id
+    test_case = mp.get_current_test_case()
+
+    assert test_case is not None
+    assert len(test_case.assertions) == 1
+    assert test_case.assertions[0].passed()
+    assert not test_case.assertions[0].failed()
+
+    return
+
+
+def test_error_run():
+    some = "hello"
+    other = {"name": "i'm a function"}
+    mp.run(spec, some, other)  # type: ignore
+
+
+def test_multiple_streams():
+    stream_id = "24fa0ed1c3"
+    mp.init_model(project_id=stream_id, model_id="2696b4a381")
+    mp.run(spec)
+
+    # We set the stream id to another, it doesn't
+    # matter that is not valid since we will not use it to query
+    mp.init_model("other", "rehto")
+
+    # Setting the stream should reset the current object
+    assert mp.get_current_obj() is None
+
+
+def spec():
+    min_height = 900
+    mp.it(f"checks window height is greater than {min_height} mm")
+
+    mp.get("category", "Windows").where(
+        "speckle_type", "Objects.Other.Instance:Objects.Other.Revit.RevitInstance"
+    ).its("Height").should("be.greater", min_height)
