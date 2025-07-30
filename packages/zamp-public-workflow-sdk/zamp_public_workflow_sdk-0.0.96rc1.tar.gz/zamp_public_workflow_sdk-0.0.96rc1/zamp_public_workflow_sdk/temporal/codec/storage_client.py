6f048c526c6eac5a397b99ca836d9694dc025b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:30 +0000

    Add package zamp-public-workflow-sdk/0.0.96rc1/zamp_public_workflow_sdk-0.0.96rc1.tar.gz

---
+++ packages/zamp-public-workflow-sdk/zamp_public_workflow_sdk-0.0.96rc1.tar.gz/zamp_public_workflow_sdk-0.0.96rc1/zamp_public_workflow_sdk/temporal/codec/storage_client.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from google.cloud import storage
+
+class StorageClient(ABC):
+    @abstractmethod
+    async def upload_file(self, object_name: str, data: bytes | str) -> None:
+        pass
+
+    @abstractmethod
+    async def get_file(self, object_name: str) -> bytes:
+        pass
+
+class GCSClient(StorageClient):
+    def __init__(self, project_id: str, bucket_name: str):
+        self.client = storage.Client(project=project_id)
+        self.bucket = self.client.bucket(bucket_name)
+
+    async def upload_file(self, object_name: str, data: bytes | str) -> None:
+        self.bucket.blob(object_name).upload_from_string(data)
+
+    async def get_file(self, object_name: str) -> bytes:
+        return self.bucket.blob(object_name).download_as_bytes()
