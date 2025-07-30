bb2882a5e2a0fd6b3d498b2f00ea8ac85da0d448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:30 +0000

    Add package ErisPulse-SystemStatus/1.0.3/erispulse_systemstatus-1.0.3-py3-none-any.whl

---
+++ packages/ErisPulse-SystemStatus/erispulse_systemstatus-1.0.3-py3-none-any.whl/SystemStatus/__init__.py
@@ -0,0 +1,20 @@
+moduleInfo = {
+    "meta": {
+        "name": "SystemStatus",
+        "version": "1.0.2",
+        "description": "获取系统内存、CPU、硬盘占用",
+        "author": "ShanFish",
+        "homepage": "https://github.com/shanfishapp/ErisPulse-SystemStatus",
+        "license": "MIT",
+    },
+    "dependencies": {
+        "requires": [],
+        "optional": [],
+        "pip": ["psutil"]
+    }
+}
+
+from .Core import Main
+# build_hash="a6fa91df10f81e518984202560c681421d10238aa697cec1b161742d75e9898a"
+
+# build_hash="134bde1628bf9a1c24628dd8cf4f29e942c1f94c1150084fdc26abd45886f26e"
