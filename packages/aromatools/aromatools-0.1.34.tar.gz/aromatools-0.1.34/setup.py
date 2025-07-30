01aeabf454952ec0fd0adfa700180088f6c23962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:28 +0000

    Add package aromatools/0.1.34/aromatools-0.1.34.tar.gz

---
+++ packages/aromatools/aromatools-0.1.34.tar.gz/aromatools-0.1.34/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aromatools",
+    version="0.1.34",
+    packages=find_packages(),
+    description="Herramientas para el análisis de aromaticidad",
+    author="Fernando Martínez Villarino",
+    author_email="fernandomv5897@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "arogeometric = aromatools.arogeometric.__main__:main"
+        ]
+    },
+    python_requires=">=3.10",
+    install_requires=[
+        "numpy",
+        "networkx",
+        "matplotlib",
+        "art",
+        "ase",
+        "pandas",
+        "plotly"
+    ],
+)
+
