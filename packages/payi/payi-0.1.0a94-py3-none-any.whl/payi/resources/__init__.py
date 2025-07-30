036c16e05ef2232067d494b5e426973fa4b77252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:27 +0000

    Add package payi/0.1.0a94/payi-0.1.0a94-py3-none-any.whl

---
+++ packages/payi/payi-0.1.0a94-py3-none-any.whl/payi/resources/__init__.py
@@ -0,0 +1,89 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .ingest import (
+    IngestResource,
+    AsyncIngestResource,
+    IngestResourceWithRawResponse,
+    AsyncIngestResourceWithRawResponse,
+    IngestResourceWithStreamingResponse,
+    AsyncIngestResourceWithStreamingResponse,
+)
+from .limits import (
+    LimitsResource,
+    AsyncLimitsResource,
+    LimitsResourceWithRawResponse,
+    AsyncLimitsResourceWithRawResponse,
+    LimitsResourceWithStreamingResponse,
+    AsyncLimitsResourceWithStreamingResponse,
+)
+from .requests import (
+    RequestsResource,
+    AsyncRequestsResource,
+    RequestsResourceWithRawResponse,
+    AsyncRequestsResourceWithRawResponse,
+    RequestsResourceWithStreamingResponse,
+    AsyncRequestsResourceWithStreamingResponse,
+)
+from .use_cases import (
+    UseCasesResource,
+    AsyncUseCasesResource,
+    UseCasesResourceWithRawResponse,
+    AsyncUseCasesResourceWithRawResponse,
+    UseCasesResourceWithStreamingResponse,
+    AsyncUseCasesResourceWithStreamingResponse,
+)
+from .categories import (
+    CategoriesResource,
+    AsyncCategoriesResource,
+    CategoriesResourceWithRawResponse,
+    AsyncCategoriesResourceWithRawResponse,
+    CategoriesResourceWithStreamingResponse,
+    AsyncCategoriesResourceWithStreamingResponse,
+)
+from .experiences import (
+    ExperiencesResource,
+    AsyncExperiencesResource,
+    ExperiencesResourceWithRawResponse,
+    AsyncExperiencesResourceWithRawResponse,
+    ExperiencesResourceWithStreamingResponse,
+    AsyncExperiencesResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "LimitsResource",
+    "AsyncLimitsResource",
+    "LimitsResourceWithRawResponse",
+    "AsyncLimitsResourceWithRawResponse",
+    "LimitsResourceWithStreamingResponse",
+    "AsyncLimitsResourceWithStreamingResponse",
+    "IngestResource",
+    "AsyncIngestResource",
+    "IngestResourceWithRawResponse",
+    "AsyncIngestResourceWithRawResponse",
+    "IngestResourceWithStreamingResponse",
+    "AsyncIngestResourceWithStreamingResponse",
+    "CategoriesResource",
+    "AsyncCategoriesResource",
+    "CategoriesResourceWithRawResponse",
+    "AsyncCategoriesResourceWithRawResponse",
+    "CategoriesResourceWithStreamingResponse",
+    "AsyncCategoriesResourceWithStreamingResponse",
+    "ExperiencesResource",
+    "AsyncExperiencesResource",
+    "ExperiencesResourceWithRawResponse",
+    "AsyncExperiencesResourceWithRawResponse",
+    "ExperiencesResourceWithStreamingResponse",
+    "AsyncExperiencesResourceWithStreamingResponse",
+    "UseCasesResource",
+    "AsyncUseCasesResource",
+    "UseCasesResourceWithRawResponse",
+    "AsyncUseCasesResourceWithRawResponse",
+    "UseCasesResourceWithStreamingResponse",
+    "AsyncUseCasesResourceWithStreamingResponse",
+    "RequestsResource",
+    "AsyncRequestsResource",
+    "RequestsResourceWithRawResponse",
+    "AsyncRequestsResourceWithRawResponse",
+    "RequestsResourceWithStreamingResponse",
+    "AsyncRequestsResourceWithStreamingResponse",
+]
---
+++ packages/payi/payi-0.1.0a94-py3-none-any.whl/payi/resources/experiences/types/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .types import (
+    TypesResource,
+    AsyncTypesResource,
+    TypesResourceWithRawResponse,
+    AsyncTypesResourceWithRawResponse,
+    TypesResourceWithStreamingResponse,
+    AsyncTypesResourceWithStreamingResponse,
+)
+from .limit_config import (
+    LimitConfigResource,
+    AsyncLimitConfigResource,
+    LimitConfigResourceWithRawResponse,
+    AsyncLimitConfigResourceWithRawResponse,
+    LimitConfigResourceWithStreamingResponse,
+    AsyncLimitConfigResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "LimitConfigResource",
+    "AsyncLimitConfigResource",
+    "LimitConfigResourceWithRawResponse",
+    "AsyncLimitConfigResourceWithRawResponse",
+    "LimitConfigResourceWithStreamingResponse",
+    "AsyncLimitConfigResourceWithStreamingResponse",
+    "TypesResource",
+    "AsyncTypesResource",
+    "TypesResourceWithRawResponse",
+    "AsyncTypesResourceWithRawResponse",
+    "TypesResourceWithStreamingResponse",
+    "AsyncTypesResourceWithStreamingResponse",
+]
