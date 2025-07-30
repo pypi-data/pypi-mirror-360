0c64b256e09fefaae2dc00f2caed719a2cebaebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:35 +0000

    Add package structifyai/1.127.0/structifyai-1.127.0.tar.gz

---
+++ packages/structifyai/structifyai-1.127.0.tar.gz/structifyai-1.127.0/src/structify/types/structure_enhance_property_params.py
@@ -0,0 +1,45 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["StructureEnhancePropertyParams", "RunMetadata", "StopConfig"]
+
+
+class StructureEnhancePropertyParams(TypedDict, total=False):
+    entity_id: Required[str]
+
+    property_name: Required[str]
+
+    allow_extra_entities: bool
+
+    banned_domains: List[str]
+
+    run_metadata: Optional[RunMetadata]
+
+    special_job_type: Optional[Literal["HumanLLM"]]
+
+    starting_searches: List[str]
+
+    starting_urls: List[str]
+
+    stop_config: Optional[StopConfig]
+    """Configuration parameters for the StopChecker"""
+
+
+class RunMetadata(TypedDict, total=False):
+    node_id: Required[str]
+
+    session_id: Required[str]
+
+
+class StopConfig(TypedDict, total=False):
+    max_steps_without_save: Required[int]
+
+    max_errors: Optional[int]
+
+    max_execution_time_secs: Optional[int]
+
+    max_total_steps: Optional[int]
