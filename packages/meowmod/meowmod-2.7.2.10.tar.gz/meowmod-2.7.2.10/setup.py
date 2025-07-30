bc03a5cdd7bd8bd1dcd3e33c36ea37c49222a432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:42 +0000

    Add package meowmod/2.7.2.10/meowmod-2.7.2.10.tar.gz

---
+++ packages/meowmod/meowmod-2.7.2.10.tar.gz/meowmod-2.7.2.10/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="meowmod",
+    version="2.7.2.10",
+    author="小孫孫",
+    author_email="sun1000526@gmail.com",
+    description="Battle Cats Save File Editor - 修改器套件",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "meowmod": ["files/*", "files/locale/*"],
+    },
+    install_requires=[
+        "colored==1.4.4",
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        "console_scripts": [
+            "meowmod = meowmod.__main__:main",
+        ],
+    },
+)
