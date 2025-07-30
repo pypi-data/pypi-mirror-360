47d563639b42785a5323fc55064be855420a4c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:07 +0000

    Add package rtfparse/0.9.5/rtfparse-0.9.5-py3-none-any.whl

---
+++ packages/rtfparse/rtfparse-0.9.5-py3-none-any.whl/rtfparse/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+# Towncrier needs version
+# from rtfparse.__about__ import __version__
+__all__ = ["rtfparse.__about__.__version__"]
+
+if __name__ == "__main__":
+    from rtfparse.cli import main
+
+    main()
