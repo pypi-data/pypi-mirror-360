6a41dde19aa14016cda37d2ad3957a6bfe3948f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:54 +0000

    Add package apple-newton/0.0.3/apple_newton-0.0.3-py3-none-any.whl

---
+++ packages/apple-newton/apple_newton-0.0.3-py3-none-any.whl/apple_newton/__init__.py
@@ -0,0 +1,11 @@
+from ._version import version, version_tuple
+from .main import add, minus
+
+__copyright__ = "Copyright (C) 2020 Hiroshi Fujiwara"
+__version__ = version
+__version_tuple__ = version_tuple
+__license__ = "MIT"
+__author__ = "Hiroshi Fujiwara"
+__author_email__ = "hiroshi.829f@gmail.com"
+__url__ = "http://github.com/account/repository"
+__all__ = ["add", "minus"]
