b967bf8a15bec5ea32406b6a129d583d74eb6695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:25 +0000

    Add package fm-nbn/0.2/fm_nbn-0.2.tar.gz

---
+++ packages/fm-nbn/fm_nbn-0.2.tar.gz/fm_nbn-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fm_nbn",
+    version="0.2",
+    packages=find_packages(),
+    description="A simple folder manipulation utility library",
+    author="NB Ngọc",
+    author_email="nbngoc.udu.ptit@email.com",
+    python_requires='>=3.6',
+)
