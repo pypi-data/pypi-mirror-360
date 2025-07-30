c09c44fb39af8620e50a98d21a92c554e9db6151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:46 +0000

    Add package iboxstacksops/1.0.3/iboxstacksops-1.0.3.tar.gz

---
+++ packages/iboxstacksops/iboxstacksops-1.0.3.tar.gz/iboxstacksops-1.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    packages=[
+        "iboxstacksops",
+    ],
+    package_data={},
+    install_requires=[
+        "boto3",
+        "prettytable",
+        "PyYAML>=5,==5.*",
+    ],
+    python_requires=">=3.7",
+    scripts=[
+        "scripts/ibox_stacksops.py",
+    ],
+    include_package_data=True,
+)
