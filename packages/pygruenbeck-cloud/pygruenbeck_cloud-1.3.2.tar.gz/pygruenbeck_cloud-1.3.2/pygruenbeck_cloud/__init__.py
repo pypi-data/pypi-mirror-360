28f15bbbf53cbb6e20cc1f6c9804ca3d624eb366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:17 +0000

    Add package pygruenbeck-cloud/1.3.2/pygruenbeck_cloud-1.3.2.tar.gz

---
+++ packages/pygruenbeck-cloud/pygruenbeck_cloud-1.3.2.tar.gz/pygruenbeck_cloud-1.3.2/pygruenbeck_cloud/__init__.py
@@ -0,0 +1,10 @@
+"""pygruenbeck_cloud is a Python library to communicate with the Gruenbeck cloud."""
+
+# flake8: noqa
+from .pygruenbeck_cloud import PyGruenbeckCloud
+
+__version__ = "1.3.2"  # pragma: no cover
+
+__all__ = [
+    "PyGruenbeckCloud",
+]
