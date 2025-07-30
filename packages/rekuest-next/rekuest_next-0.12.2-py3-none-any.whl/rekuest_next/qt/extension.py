1af15e796f64e6d5f78eb722019b4da9c5eb5b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:19 +0000

    Add package rekuest-next/0.12.2/rekuest_next-0.12.2-py3-none-any.whl

---
+++ packages/rekuest-next/rekuest_next-0.12.2-py3-none-any.whl/rekuest_next/qt/extension.py
@@ -0,0 +1,26 @@
+
+
+
+from rekuest_next.agents.extension import AgentExtension
+from qtpy.QtWidgets import QApplication,
+from qtpy import QtCore
+
+class QtExtension(QtCore.QObject):
+    """
+    Base class for Qt extensions.
+    """
+
+    def __init__(self, qt_window, app):
+        self.app = qt_window
+
+    def run(self):
+        """
+        Run the extension.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    def stop(self):
+        """
+        Stop the extension.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
