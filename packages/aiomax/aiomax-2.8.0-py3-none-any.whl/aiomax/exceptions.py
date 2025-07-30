2e7d589b617d0f59793d0983b3897ab4f0976682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:19 +0000

    Add package aiomax/2.8.0/aiomax-2.8.0-py3-none-any.whl

---
+++ packages/aiomax/aiomax-2.8.0-py3-none-any.whl/aiomax/exceptions.py
@@ -0,0 +1,14 @@
+class InvalidToken(Exception):
+    """
+    Invalid token Exception
+    """
+
+class AttachmentNotReady(Exception):
+    """
+    Attachment not ready Exception
+    """
+
+class ChatNotFound(Exception):
+    """
+    Chat not found Exception
+    """
