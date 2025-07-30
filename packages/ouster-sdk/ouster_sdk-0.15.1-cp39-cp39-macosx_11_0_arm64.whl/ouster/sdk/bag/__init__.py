5383e079fd9d37d1e320828b03aeee11d14888fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:35 +0000

    Add package ouster-sdk/0.15.1/ouster_sdk-0.15.1-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/ouster-sdk/ouster_sdk-0.15.1-cp39-cp39-macosx_11_0_arm64.whl/ouster/sdk/bag/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright (c) 2024, Ouster, Inc.
+All rights reserved.
+
+Ouster Bag file support
+"""
+# flake8: noqa: F401 (unused imports)
+
+from .bag_packet_source import BagPacketSource
+from .bag_scan_source import BagScanSource
