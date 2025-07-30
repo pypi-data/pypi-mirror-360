7e072fc11e01043818e4c033d9e807ebaf0ca6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:36 +0000

    Add package lamindb/1.7a1/lamindb-1.7a1-py3-none-any.whl

---
+++ packages/lamindb/lamindb-1.7a1-py3-none-any.whl/lamindb/setup/__init__.py
@@ -0,0 +1,14 @@
+import lamindb_setup as _lamindb_setup
+from lamindb_setup import *  # noqa: F403
+from lamindb_setup import (
+    connect,
+    delete,
+    init,
+    settings,
+)
+
+from . import core, errors, types
+
+del connect  # we have this at the root level, hence, we don't want it here
+__doc__ = _lamindb_setup.__doc__.replace("lamindb_setup", "lamindb.setup")
+settings.__doc__ = settings.__doc__.replace("lamindb_setup", "lamindb.setup")
