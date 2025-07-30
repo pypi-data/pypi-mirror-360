d18ecbf3fa7c80eea1b0939f9d94f4aace7c4a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:22 +0000

    Add package bonesis/0.6.8.1/bonesis-0.6.8.1-py3-none-any.whl

---
+++ packages/bonesis/bonesis-0.6.8.1-py3-none-any.whl/bonesis/reprogramming.py
@@ -0,0 +1,195 @@
+# Copyright or © or Copr. Loïc Paulevé (2023)
+#
+# loic.pauleve@cnrs.fr
+#
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+#
+
+
+from colomoto import minibn
+
+import networkx as nx
+from functools import reduce
+
+import bonesis
+from bonesis0.asp_encoding import symbol_of_py
+
+def prune_domain_for_marker(f, M):
+    def get_doi(g):
+        return reduce(set.union, (nx.ancestors(g, m) for m in M), set(M))
+    if isinstance(f, minibn.BooleanNetwork):
+        keep = get_doi(f.influence_graph())
+        return f.__class__({i: f[i] for i in keep})
+    else:
+        return f.subgraph(get_doi(f))
+
+def marker_reprogramming_fixpoints(f, M, k, at_least_one=True, **some_opts):
+    bo = bonesis.BoNesis(f)
+    control = bo.Some(max_size=k, **some_opts)
+    with bo.mutant(control):
+        if at_least_one:
+            bo.fixed(~bo.obs(M))
+        bo.all_fixpoints(bo.obs(M))
+    return control.assignments()
+
+def source_marker_reprogramming_fixpoints(f, z, M, k, at_least_one=True,
+        **some_opts):
+    bo = bonesis.BoNesis(f)
+    control = bo.Some(max_size=k, **some_opts)
+    with bo.mutant(control):
+        if at_least_one:
+            ~bo.obs(z) >= bo.fixed(~bo.obs(M))
+        ~bo.obs(z) >> "fixpoints" ^ {bo.obs(M)}
+    return control.assignments()
+
+def trapspace_reprogramming(f, M, k, algorithm="cegar", **some_opts):
+    """
+    Marker/Trapspace reprogramming of BN domain `f`.
+
+    Returns mutations which ensure that all the minimal trap spaces match with
+    the given marker `M`.
+    """
+    f = prune_domain_for_marker(f, M)
+    if algorithm == "cegar":
+        meth = _trapspace_reprogramming_cegar
+    else:
+        meth = _trapspace_reprogramming_complementary
+    return meth(f, M, k, **some_opts)
+
+marker_reprogramming = trapspace_reprogramming
+
+
+def _trapspace_reprogramming_complementary(f, M, k, **some_opts):
+    bo = bonesis.BoNesis(f)
+    bad_control = bo.Some(max_size=k, **some_opts)
+    with bo.mutant(bad_control):
+        x = bo.cfg()
+        bo.in_attractor(x) != bo.obs(M)
+    return bad_control.complementary_assignments()
+
+def source_marker_reprogramming(f, z, M, k, **some_opts):
+    f = prune_domain_for_marker(f, M)
+    bo = bonesis.BoNesis(f)
+    bad_control = bo.Some(max_size=k, **some_opts)
+    with bo.mutant(bad_control):
+        x = bo.cfg()
+        ~bo.obs(z) >= bo.in_attractor(x) != bo.obs(M)
+    return bad_control.complementary_assignments()
+
+
+###
+### CEGAR-based implementations
+###
+
+from bonesis.views import SomeView
+
+def _trapspace_reprogramming_cegar(dom, M, k, **some_opts):
+    data = {"M": M}
+
+    bo = bonesis.BoNesis(dom, data)
+    P = bo.Some(max_size=k, **some_opts)
+    P_id = symbol_of_py(P.name)
+    with bo.mutant(P):
+        # there exists at least one minimal trap spaces matching M
+        bo.fixed(bo.obs("M"))
+
+    class BoCegar(SomeView):
+        single_shot = False
+        project = False
+        ice = 0
+        def __iter__(self):
+            self.configure()
+            return self
+
+        def __next__(self):
+            while True:
+                with self.control.solve(yield_=True) as candidates:
+                    found_P = False
+                    for candidate in candidates:
+                        found_P = True
+                        P = self.parse_model(candidate)
+                        p = candidate.symbols(shown=True)
+
+                        ## check candidate
+                        boc = bonesis.BoNesis(dom, data)
+                        with boc.mutant(P):
+                            x = boc.cfg()
+                            boc.in_attractor(x)
+                            x != boc.obs("M")
+                        view = x.assignments(limit=1)
+                        view.configure()
+                        if (ce := next(iter(view.control.solve(yield_=True)), None)) is not None:
+                            self.ice += 1
+                            x = [a for a in ce.symbols(shown=True)
+                                    if a.name == "cfg" and a.arguments[0].string == "__cfg0"]
+                        break
+
+                    if not found_P:
+                        raise StopIteration
+
+                if ce is None:
+                    inject = f":- {','.join(map(str, p))}."
+                    self.control.add("skip", [], inject)
+                    self.control.ground([("skip", [])])
+                    return P
+                ns = f"_ce{self.ice}_"
+                cets = f"{ns}ts"
+                fixts = f"{ns}fix"
+                inject = [f"mcfg({cets},{a.arguments[1]},{a.arguments[2]})." for a in x]
+                if not isinstance(dom, minibn.BooleanNetwork):
+                    conds = list(map(str,p)) + [f"#count {{ N : some_freeze({P_id},N,_) }} {len(p)}"]
+                    inject.append(f":- {','.join(conds)}.")
+                inject += [
+                        # compute trap space of counter example
+                        f"mcfg({cets},N,V) :- {ns}eval({cets},N,V).",
+                        f"clamped({cets},N,V) :- mutant(_,N,V).",
+                        # choose sub-space in it
+                        f"1 {{mcfg({fixts},N,V):mcfg({cets},N,V)}} :- mcfg({cets},N,_).",
+                        # saturate it
+                        f"mcfg({fixts},N,V) :- {ns}eval({fixts},N,V).",
+                        f"clamped({fixts},N,V) :- clamped({cets},N,V).",
+
+                        f"{ns}eval(X,N,C,-1) :- clause(N,C,L,-V), mcfg(X,L,V), not clamped(X,N,_).",
+                        f"{ns}eval(X,N,C,1) :- mcfg(X,L,V): clause(N,C,L,V); clause(N,C,_,_), mcfg(X,_,_), not clamped(X,N,_).",
+                        f"{ns}eval(X,N,1) :- {ns}eval(X,N,C,1), clause(N,C,_,_).",
+                        f"{ns}eval(X,N,-1) :- {ns}eval(X,N,C,-1): clause(N,C,_,_); clause(N,_,_,_), mcfg(X,_,_).",
+                        f"{ns}eval(X,N,V) :- clamped(X,N,V).",
+                        f"{ns}eval(X,N,V) :- constant(N,V), mcfg(X,_,_), not clamped(X,N,_).",
+
+                        f"{ns}eval(X,N,V) :- {ns}evalbdd(X,N,V), node(N), not clamped(X,N,_).",
+                        f"{ns}evalbdd(X,V,V) :- mcfg(X,_,_), V=(-1;1).",
+                        f"{ns}evalbdd(X,B,V) :- bdd(B,N,_,HI), mcfg(X,N,1), {ns}evalbdd(X,HI,V).",
+                        f"{ns}evalbdd(X,B,V) :- bdd(B,N,LO,_), mcfg(X,N,-1), {ns}evalbdd(X,LO,V).",
+                        f"{ns}evalbdd(X,B,V) :- mcfg(X,_,_), bdd(B,V).",
+                    ]
+                # TS(y) must match with M
+                inject.append(f":- obs(\"M\",N,V), mcfg({fixts},N,-V).")
+                inject = "\n".join(inject)
+                self.control.add(f"cegar{ns}", [], inject)
+                self.control.ground([(f"cegar{ns}", [])])
+
+    return BoCegar(P, bo, solutions="subset-minimal")
---
+++ packages/bonesis/bonesis-0.6.8.1-py3-none-any.whl/bonesis0/asp_encoding.py
@@ -0,0 +1,196 @@
+# Copyright or © or Copr. Loïc Paulevé (2023)
+#
+# loic.pauleve@cnrs.fr
+#
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+#
+
+import os
+
+import clingo as asp
+clingo_Tuple = asp.Tuple_ if hasattr(asp, "Tuple_") else asp.Tuple
+
+from scipy.special import binom
+
+from functools import reduce
+
+from mpbn import MPBooleanNetwork
+
+def py_of_symbol(symb):
+    if symb.type is asp.SymbolType.String:
+        return symb.string
+    if symb.type is asp.SymbolType.Number:
+        return symb.number
+    if symb.type is asp.SymbolType.Function:
+        return tuple(map(py_of_symbol, symb.arguments))
+    raise ValueError
+
+def symbol_of_py(obj):
+    if isinstance(obj, str):
+        return asp.String(obj)
+    elif isinstance(obj, int):
+        return asp.Number(obj)
+    elif isinstance(obj, tuple):
+        return clingo_Tuple([symbol_of_py(o) for o in obj])
+    return obj
+
+def symbols(*objs):
+    return [symbol_of_py(obj) for obj in objs]
+
+def portfolio_path(name):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)),
+            f"{name}.cfg")
+
+def parse_nb_threads(opt):
+    if opt is None:
+        return 0
+    if isinstance(opt, str):
+        opt = int(opt.split(",")[0])
+    return opt
+
+def string_of_facts(facts):
+    if not facts:
+        return ""
+    return "{}.\n".format(".\n".join(map(str,facts)))
+
+def print_facts(facts):
+    if facts:
+        print(string_of_facts(facts))
+
+def nb_clauses(d):
+    return int(binom(d, d//2))
+
+def pkn_to_facts(pkn, maxclause=None, allow_skipping_nodes=False):
+    facts = []
+    if not allow_skipping_nodes:
+        facts.append(asp.Function("nbnode", symbols(len(pkn.nodes()))))
+        for n in pkn.nodes():
+            facts.append(asp.Function("node", symbols(n)))
+    else:
+        facts.append("nbnode(NB) :- NB = #count{N: node(N)}")
+        for n in pkn.nodes():
+            facts.append("{{{}}}".format(asp.Function("node", symbols(n))))
+    for (orig, dest, data) in pkn.edges(data=True):
+        if data["sign"] in ["ukn","?","0",0]:
+            args = symbols(orig, dest)
+            f = "in({},{},(-1;1))".format(*args)
+            facts.append(f)
+        else:
+            ds = data["sign"]
+            if ds in ["-","+"]:
+                ds += "1"
+            s = int(ds)
+            facts.append(asp.Function("in", symbols(orig, dest, s)))
+    def bounded_nb_clauses(d):
+        nbc = nb_clauses(d)
+        if maxclause:
+            nbc = min(maxclause, nbc)
+        return nbc
+    for n, i in pkn.in_degree(pkn.nodes()):
+        facts.append(asp.Function("maxC", symbols(n, bounded_nb_clauses(i))))
+    facts += pkn.rules
+    return facts
+
+def obs_to_facts(pstate, obsid):
+    return [asp.Function("obs", [obsid, n, 2*v-1]) for (n,v) in pstate.items()]
+
+def sanitize_identifier(nodeid):
+    if isinstance(nodeid, str):
+        nodeid = nodeid.replace("-","_")
+    else:
+        nodeid = f"x{nodeid}"
+    return nodeid
+
+def dnfs_of_facts(fs, ns=""):
+    bn = {}
+    clause_func = f"{ns}clause"
+    constant_func = f"{ns}constant"
+    for d in fs:
+        if d.name == clause_func:
+            (i,cid,lit,sign) = list(map(py_of_symbol, d.arguments))
+            i = sanitize_identifier(i)
+            lit = sanitize_identifier(lit)
+            if i not in bn:
+                bn[i] = []
+            if cid > len(bn[i]):
+                bn[i] += [set() for j in range(cid-len(bn[i]))]
+            bn[i][cid-1].add((sign,lit))
+        elif d.name == constant_func and len(d.arguments) == 2:
+            (i,v) = list(map(py_of_symbol, d.arguments))
+            i = sanitize_identifier(i)
+            bn[i] = v == 1
+    return bn
+
+def minibn_of_facts(fs, ns=""):
+    dnfs = dnfs_of_facts(fs, ns=ns)
+    bn = MPBooleanNetwork()
+    def make_lit(l):
+        s,v=l
+        v = bn.v(v)
+        if s < 0:
+            v = ~v
+        return v
+    def make_clause(ls):
+        ls = list(map(make_lit, ls))
+        if len(ls) == 1:
+            return ls[0]
+        return bn.ba.AND(*ls)
+    def make_dnf(cs):
+        if isinstance(cs, bool):
+            return cs
+        cs = filter(len, cs)
+        cs = list(map(make_clause, cs))
+        if len(cs) == 1:
+            return cs[0]
+        return bn.ba.OR(*cs)
+    for (node, cs) in sorted(dnfs.items()):
+        bn[node] = make_dnf(cs)
+    return bn
+
+def configurations_of_facts(fs, pred="cfg", keys="auto"):
+    cfgs = {}
+    auto_keys = keys == "auto"
+    select = [] if auto_keys else (None if keys == "all" else keys)
+    for a in fs:
+        if a.name != pred:
+            continue
+        arity = len(a.arguments)
+        if arity == 1 and auto_keys:
+            select.append(py_of_symbol(a.arguments[0]))
+            continue
+        if arity != 3:
+            continue
+        cid, n, v = a.arguments
+        n = py_of_symbol(n)
+        v = v.number
+        cid = py_of_symbol(cid)
+        if cid not in cfgs:
+            cfgs[cid] = {}
+        cfgs[cid][n] = max(v, 0)
+    if select is not None:
+        return {k: cfgs[k] for k in select}
+    return cfgs
