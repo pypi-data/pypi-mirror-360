d6accb9199fcb43155cc1597e99ae511583a2d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:04 +0000

    Add package patchkit/0.2.0/patchkit-0.2.0-py3-none-any.whl

---
+++ packages/patchkit/patchkit-0.2.0-py3-none-any.whl/patchkit/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Main entry point for patchkit module.
+This allows running the package as: python -m patchkit
+"""
+
+from .cli import main
+
+if __name__ == "__main__":
+    main() 
