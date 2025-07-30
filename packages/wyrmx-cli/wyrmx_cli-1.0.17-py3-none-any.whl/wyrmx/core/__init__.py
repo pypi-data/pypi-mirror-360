05534a7538abd76f4324d67d77723e6661352711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:57 +0000

    Add package wyrmx-cli/1.0.17/wyrmx_cli-1.0.17-py3-none-any.whl

---
+++ packages/wyrmx-cli/wyrmx_cli-1.0.17-py3-none-any.whl/wyrmx/core/__init__.py
@@ -0,0 +1,4 @@
+from wyrmx.core.app import WyrmxAPP
+from wyrmx.core.decorators import controller, service, model, singleton
+from wyrmx.core.scope import Scope
+from wyrmx.core.container import container
