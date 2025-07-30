25539abcf9ea1113b7063b5b66fc7030cd8e639e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:18 +0000

    Add package aiogram-samples/0.2.1/aiogram_samples-0.2.1.tar.gz

---
+++ packages/aiogram-samples/aiogram_samples-0.2.1.tar.gz/aiogram_samples-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiogram_samples",
+    version="0.2.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "aiogram_samples = aiogram_samples.cli:main"
+        ],
+    },
+    author="Lump",
+    author_email="igropritegatel11@gmail.com",
+    description="Генератор шаблонов для aiogram обработчиков и начала проекта",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lumpButton/aiogram_samples",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
+
+
+# happy birthday
