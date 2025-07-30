6298030933d81c32545ba24b9ec95ded70b2e007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:23 +0000

    Add package self-ai-operating-computer/2.0.4/self-ai-operating-computer-2.0.4.tar.gz

---
+++ packages/self-ai-operating-computer/self-ai-operating-computer-2.0.4.tar.gz/self-ai-operating-computer-2.0.4/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+# Read the contents of your requirements.txt file
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
+    required = f.read().splitlines()
+
+# Read the contents of your README.md file for the project description
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="self-ai-operating-computer",
+    version="2.0.4",
+    packages=find_packages(),
+    install_requires=required,  # Add dependencies here
+    entry_points={
+        "console_scripts": [
+            "operate=operate.main:main_entry",
+        ],
+    },
+    package_data={
+        # Include the file in the operate.models.weights package
+        "operate.models.weights": ["best.pt"],
+    },
+    long_description=long_description,  # Add project description here
+    long_description_content_type="text/markdown",  # Specify Markdown format
+    # include any other necessary setup options here
+)
