c4d7ff7337095d7d5461d1dc0d1f3fd7ca40fa48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package easybuild-easyblocks/5.1.1/easybuild_easyblocks-5.1.1.tar.gz

---
+++ packages/easybuild-easyblocks/easybuild_easyblocks-5.1.1.tar.gz/easybuild_easyblocks-5.1.1/easybuild/easyblocks/generic/buildenv.py
@@ -0,0 +1,88 @@
+##
+# Copyright 2015-2025 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/easybuilders/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for creating a module that loads the build
+environment flags for the current toolchain
+
+@author: Alan O'Cais (Juelich Supercomputing Centre)
+"""
+import os
+import stat
+
+from easybuild.easyblocks.generic.bundle import Bundle
+from easybuild.tools.filetools import adjust_permissions, copy_dir
+from easybuild.tools.toolchain.toolchain import RPATH_WRAPPERS_SUBDIR
+
+
+class BuildEnv(Bundle):
+    """
+    Build environment of toolchain: only generate module file
+    """
+
+    def prepare_step(self, *args, **kwargs):
+        """
+        Custom prepare step for buildenv: export rpath wrappers if they are being used
+        """
+        # passed to toolchain.prepare to specify location for RPATH wrapper scripts (if RPATH linking is enabled)
+        self.rpath_wrappers_dir = self.builddir
+
+        super().prepare_step(*args, **kwargs)
+
+    def install_step(self, *args, **kwargs):
+        """
+        Custom install step for buildenv: copy RPATH wrapper scripts to install dir, if desired
+        """
+        super().install_step(*args, **kwargs)
+
+        # copy RPATH wrapper scripts to install directory (if they exist)
+        wrappers_dir = os.path.join(self.rpath_wrappers_dir, RPATH_WRAPPERS_SUBDIR)
+        if os.path.exists(wrappers_dir):
+            self.rpath_wrappers_dir = os.path.join(self.installdir, 'bin')
+            copy_dir(wrappers_dir, os.path.join(self.rpath_wrappers_dir, RPATH_WRAPPERS_SUBDIR))
+            # Make sure wrappers are readable/executable by everyone
+            adjust_permissions(
+                self.rpath_wrappers_dir,
+                stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+                )
+
+    def make_module_extra(self):
+        """Add all the build environment variables."""
+        txt = super().make_module_extra()
+
+        # include environment variables defined for (non-system) toolchain
+        if not self.toolchain.is_system_toolchain():
+            for key, val in sorted(self.toolchain.vars.items()):
+                txt += self.module_generator.set_environment(key, val)
+
+        self.log.debug(f"make_module_extra added this: {txt}")
+        return txt
+
+    def make_module_step(self, fake=False):
+        """Specify correct bin directories for buildenv installation."""
+        wrappers_dir = os.path.join(self.rpath_wrappers_dir, RPATH_WRAPPERS_SUBDIR)
+        if os.path.exists(wrappers_dir):
+            self.module_load_environment.PATH = [os.path.join(wrappers_dir, d) for d in os.listdir(wrappers_dir)]
+
+        return super().make_module_step(fake=fake)
---
+++ packages/easybuild-easyblocks/easybuild_easyblocks-5.1.1.tar.gz/easybuild_easyblocks-5.1.1/easybuild/easyblocks/h/hpl.py
@@ -0,0 +1,132 @@
+##
+# Copyright 2009-2025 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/easybuilders/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for building and installing HPL, implemented as an easyblock
+
+@author: Stijn De Weirdt (Ghent University)
+@author: Dries Verdegem (Ghent University)
+@author: Kenneth Hoste (Ghent University)
+@author: Pieter De Baets (Ghent University)
+@author: Jens Timmerman (Ghent University)
+"""
+
+import os
+
+from easybuild.easyblocks.generic.configuremake import ConfigureMake
+from easybuild.tools.build_log import EasyBuildError
+from easybuild.tools.filetools import change_dir, copy_file, mkdir, remove_file, symlink
+from easybuild.tools.run import run_shell_cmd
+
+
+class EB_HPL(ConfigureMake):
+    """
+    Support for building HPL (High Performance Linpack)
+    - create Make.UNKNOWN
+    - build with make and install
+    """
+
+    def configure_step(self, subdir=None):
+        """
+        Create Make.UNKNOWN file to build from
+        - provide subdir argument so this can be reused in HPCC easyblock
+        """
+
+        basedir = self.cfg['start_dir']
+        if subdir:
+            makeincfile = os.path.join(basedir, subdir, 'Make.UNKNOWN')
+            setupdir = os.path.join(basedir, subdir, 'setup')
+        else:
+            makeincfile = os.path.join(basedir, 'Make.UNKNOWN')
+            setupdir = os.path.join(basedir, 'setup')
+
+        change_dir(setupdir)
+
+        cmd = "/bin/bash make_generic"
+
+        run_shell_cmd(cmd)
+
+        remove_file(makeincfile)
+        symlink(os.path.join(setupdir, 'Make.UNKNOWN'), makeincfile)
+
+        # go back
+        change_dir(self.cfg['start_dir'])
+
+    def build_step(self, topdir=None):
+        """
+        Build with make and correct make options
+        - provide topdir argument so this can be reused in HPCC easyblock
+        """
+
+        for envvar in ['MPICC', 'LIBLAPACK_MT', 'CPPFLAGS', 'LDFLAGS', 'CFLAGS']:
+            # environment variable may be defined but empty
+            if os.getenv(envvar, None) is None:
+                raise EasyBuildError("Required environment variable %s not found (no toolchain used?).", envvar)
+
+        # build dir
+        if not topdir:
+            topdir = self.cfg['start_dir']
+        extra_makeopts = 'TOPdir="%s" ' % topdir
+
+        # compilers
+        extra_makeopts += 'CC="%(mpicc)s" MPICC="%(mpicc)s" LINKER="%(mpicc)s" ' % {'mpicc': os.getenv('MPICC')}
+
+        # libraries: LAPACK and FFTW
+        extra_makeopts += 'LAlib="%s" ' % os.getenv('LIBLAPACK_MT')
+
+        # HPL options
+        extra_makeopts += 'HPL_OPTS="%s " ' % os.getenv('CPPFLAGS')
+
+        # linker flags
+        extra_makeopts += 'LINKFLAGS="%s %s %s" ' % (os.getenv('CFLAGS'), os.getenv('LDFLAGS'), os.getenv('LIBS', ''))
+
+        # C compilers flags
+        extra_makeopts += "CCFLAGS='$(HPL_DEFS) %s' " % os.getenv('CFLAGS')
+
+        # set options and build
+        self.cfg.update('buildopts', extra_makeopts)
+        super().build_step()
+
+    def install_step(self):
+        """
+        Install by copying files to install dir
+        """
+        srcdir = os.path.join(self.cfg['start_dir'], 'bin', 'UNKNOWN')
+        destdir = os.path.join(self.installdir, 'bin')
+        mkdir(destdir)
+        for filename in ["xhpl", "HPL.dat"]:
+            srcfile = os.path.join(srcdir, filename)
+            copy_file(srcfile, destdir)
+
+    def sanity_check_step(self):
+        """
+        Custom sanity check for HPL
+        """
+
+        custom_paths = {
+            'files': ["bin/xhpl"],
+            'dirs': []
+        }
+
+        super().sanity_check_step(custom_paths)
