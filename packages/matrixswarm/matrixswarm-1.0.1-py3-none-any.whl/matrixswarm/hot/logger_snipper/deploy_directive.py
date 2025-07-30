8cbe686e2c7af0735b83370acbc408ca4c2546f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:15 +0000

    Add package matrixswarm/1.0.1/matrixswarm-1.0.1-py3-none-any.whl

---
+++ packages/matrixswarm/matrixswarm-1.0.1-py3-none-any.whl/matrixswarm/hot/logger_snipper/deploy_directive.py
@@ -0,0 +1,12 @@
+directive = {
+    "name": "logger_snipper",
+    "app": "matrix-core",
+    "config": {
+        "target": "logger-1",
+        "match": "access_token",
+        "mode": "redact"
+    },
+    "filesystem": {},
+    "directives": {},
+    "hotswap": True
+}
