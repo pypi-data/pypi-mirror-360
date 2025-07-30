8192145c3b2818813a68947086a020b39f27e321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:13 +0000

    Add package flet-geolocator/0.2.0.dev35/flet_geolocator-0.2.0.dev35-py3-none-any.whl

---
+++ packages/flet-geolocator/flet_geolocator-0.2.0.dev35-py3-none-any.whl/flet_geolocator/__init__.py
@@ -0,0 +1,13 @@
+from .geolocator import Geolocator
+from .types import (
+    ForegroundNotificationConfiguration,
+    GeolocatorAndroidConfiguration,
+    GeolocatorConfiguration,
+    GeolocatorIosActivityType,
+    GeolocatorIosConfiguration,
+    GeolocatorPermissionStatus,
+    GeolocatorPosition,
+    GeolocatorPositionAccuracy,
+    GeolocatorPositionChangeEvent,
+    GeolocatorWebConfiguration,
+)
