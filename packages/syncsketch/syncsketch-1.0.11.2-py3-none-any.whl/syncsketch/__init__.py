8d2d9ee6f4012b1ee3ec61a0b5ecea653408fe6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:20 +0000

    Add package syncsketch/1.0.11.2/syncsketch-1.0.11.2-py3-none-any.whl

---
+++ packages/syncsketch/syncsketch-1.0.11.2-py3-none-any.whl/syncsketch/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: yafes
+# @Date:   2018-11-20 17:39:54
+# @Last Modified by:   Brady Endres
+# @Last Modified time: 2025-07-07
+
+from __future__ import absolute_import
+
+from .syncsketch import SyncSketchAPI
+
+__version__ = "1.0.11.2"
+__author__ = "SyncSketch Dev Team"
+__credits__ = "Philip Floetotto, Yafes Sahin, Brady Endres, Eric Palakovich Carr"
