86af96be7975aaa2f52c1316839941685b550d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:07 +0000

    Add package xync-bot/0.3.24.dev3/xync_bot-0.3.24.dev3.tar.gz

---
+++ packages/xync-bot/xync_bot-0.3.24.dev3.tar.gz/xync_bot-0.3.24.dev3/xync_bot/routers/pay/cd.py
@@ -0,0 +1,50 @@
+from aiogram.filters.callback_data import CallbackData
+
+from xync_bot.routers.pay.dep import PayStep, ActionType
+
+
+class TargetType(CallbackData, prefix="target"):
+    is_fiat: int  # bool
+    is_target: int  # bool
+
+
+class Cur(CallbackData, prefix="cur"):
+    id: int
+    is_target: int  # bool
+
+
+class Coin(CallbackData, prefix="coin"):
+    id: int
+    is_target: int  # bool
+
+
+class Cred(CallbackData, prefix="cred"):
+    id: int
+
+
+class Ex(CallbackData, prefix="ex"):
+    id: int
+    name: str
+    is_target: int  # bool
+
+
+class Pm(CallbackData, prefix="pm"):
+    pmcur_id: int
+    name: str
+    is_target: bool
+
+
+class Ppo(CallbackData, prefix="ppo"):
+    num: int
+
+
+class PayNav(CallbackData, prefix="pay_nav"):
+    to: PayStep
+
+
+class Time(CallbackData, prefix="time"):
+    minutes: int  # время в минутах
+
+
+class Action(CallbackData, prefix="action"):
+    act: ActionType  # "received" или "not_received"
