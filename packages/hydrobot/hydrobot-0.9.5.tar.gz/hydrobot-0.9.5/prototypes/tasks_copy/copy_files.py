b8dc10d3a45f3bab7658ac9a7c6262a805c53aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:18 +0000

    Add package hydrobot/0.9.5/hydrobot-0.9.5.tar.gz

---
+++ packages/hydrobot/hydrobot-0.9.5.tar.gz/hydrobot-0.9.5/prototypes/tasks_copy/copy_files.py
@@ -0,0 +1,13 @@
+"""Copy tasks prototype."""
+
+import hydrobot.tasks as tasks
+
+destination_path = r"C:\Users\SIrvine\PycharmProjects\hydro-processing-tools\prototypes\tasks_copy\output_dump"
+
+rainfall_config = tasks.csv_to_batch_dicts(
+    r"C:\Users\SIrvine\PycharmProjects\hydro-processing-tools\prototypes\tasks_copy\RainfallReprocessing.csv"
+)
+
+tasks.create_mass_hydrobot_batches(
+    destination_path + r"\test_home", destination_path, rainfall_config
+)
