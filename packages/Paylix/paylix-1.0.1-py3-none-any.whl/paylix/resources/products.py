9126c1ab2ba7219d8d89330d8791e3c3144243d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:35 +0000

    Add package Paylix/1.0.1/paylix-1.0.1-py3-none-any.whl

---
+++ packages/Paylix/paylix-1.0.1-py3-none-any.whl/paylix/resources/products.py
@@ -0,0 +1,42 @@
+def get_products(self):
+    return self.handle_response(
+        self.request("/products"),
+        "products"
+    )
+
+
+def create_product(self, **kwargs):
+    return self.handle_response(
+        self.request("/products", "POST", kwargs),
+        "uniqid"
+    )
+
+
+def get_product(self, uniqid):
+    return self.handle_response(
+        self.request(f"/products/{uniqid}"),
+        "product"
+    )
+
+
+def update_product(self, uniqid, **kwargs):
+    return self.handle_response(
+        self.request(f"/products/{uniqid}", "PUT", kwargs)
+    )
+
+
+def delete_product(self, uniqid):
+    return self.handle_response(
+        self.request(f"/products/{uniqid}", "DELETE")
+    )
+
+def licensing_check(self, **kwargs):
+    return self.handle_response(
+        self.request(f"/products/licensing/check", "POST", kwargs),
+        "license"
+    )
+
+def licensing_update_hardware_id(self, **kwargs):
+    return self.handle_response(
+        self.request(f"/products/licensing/hardware_id", "PUT", kwargs)
+    )
