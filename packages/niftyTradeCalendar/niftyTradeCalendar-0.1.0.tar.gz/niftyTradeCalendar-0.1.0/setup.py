759d638c78d4213d5b67cfca538e3ead5807cb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:46 +0000

    Add package niftyTradeCalendar/0.1.0/niftyTradeCalendar-0.1.0.tar.gz

---
+++ packages/niftyTradeCalendar/niftyTradeCalendar-0.1.0.tar.gz/niftyTradeCalendar-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="niftyTradeCalendar",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Sonu Kumar",
+    author_email="skrdeveloper1@gmail.com",
+    description="Holiday and Trading Day Calendar for NIFTY",
+    url="https://github.com/skrdeveloper1/niftyTradeCalendar",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    project_urls={
+        "LinkedIn": "https://in.linkedin.com/in/skrdeveloper1",
+        "GitHub": "https://github.com/skrdeveloper1/niftyTradeCalendar",
+    },
+    python_requires=">=3.6",
+)
