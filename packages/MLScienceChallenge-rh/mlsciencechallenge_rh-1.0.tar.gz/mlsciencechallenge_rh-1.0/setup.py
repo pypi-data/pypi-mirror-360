367a7d154d8b504cabd4fa5d26bf61c03dbb0d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:13 +0000

    Add package MLScienceChallenge-rh/1.0/mlsciencechallenge_rh-1.0.tar.gz

---
+++ packages/MLScienceChallenge-rh/mlsciencechallenge_rh-1.0.tar.gz/mlsciencechallenge_rh-1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "MLScienceChallenge_rh",
+    version = "1.0",
+    author = "Raymond Hawkins",
+    author_email = "raymond.hawkins@mail.utoronto.ca",
+    description = "ML Science Coding Challenge for CZI Biohub application.",
+    url = "https://github.com/rayhawkins/MLScienceChallenge",
+    project_urls = {
+        "Bug Tracker": "https://github.com/rayhawkins/MLScienceChallenge/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = "==3.10"
+)
