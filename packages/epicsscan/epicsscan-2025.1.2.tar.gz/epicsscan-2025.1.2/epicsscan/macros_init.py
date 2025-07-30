c433d8391e9ee9aad51aef85c45e779e136d4cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:08 +0000

    Add package epicsscan/2025.1.2/epicsscan-2025.1.2.tar.gz

---
+++ packages/epicsscan/epicsscan-2025.1.2.tar.gz/epicsscan-2025.1.2/epicsscan/macros_init.py
@@ -0,0 +1,335 @@
+#!/usr/bin/env python
+"""
+Macro symbols and builtin function for EpicsScan MacroKernel
+
+"""
+
+import numpy as np
+import scipy.constants as consts
+
+from epics import caget, caput, get_pv, PV
+from time import monotonic as clock, sleep
+from xraydb import (atomic_density, atomic_mass, atomic_name, atomic_number,
+          atomic_symbol, chemparse, core_width, darwin_width, f0, f1_chantler,
+          f2_chantler, fluor_yield, get_material, guess_edge,
+          ionchamber_fluxes, ionization_potential, material_mu,
+          material_mu_components, mirror_reflectivity, mu_chantler, mu_elam,
+          xray_delta_beta, xray_edge, xray_edges, xray_line, xray_lines)
+
+INITSYMS = ['clock', 'sleep', 'caget', 'caput', 'get_pv', 'PV', 'consts',
+            'etok', 'ktoe', 'AMU', 'ATOM_NAMES', 'ATOM_SYMS', 'AVOGADRO',
+            'DEG2RAD', 'E_MASS', 'PI', 'PLANCK_HBARC', 'PLANCK_HC', 'RAD2DEG',
+            'RYDBERG', 'R_ELECTRON_ANG', 'R_ELECTRON_CM', 'SI_PREFIXES',
+            'TAU', 'XAFS_KTOE', 'atomic_density', 'atomic_mass',
+            'atomic_name', 'atomic_number', 'atomic_symbol', 'chemparse',
+            'core_width', 'darwin_width', 'f0', 'f1_chantler', 'f2_chantler',
+            'fluor_yield', 'get_material', 'guess_edge', 'index_nearest',
+            'index_of', 'ionchamber_fluxes', 'ionization_potential',
+            'material_mu', 'material_mu_components', 'mirror_reflectivity',
+            'mu_elam', 'xray_delta_beta', 'xray_edge', 'xray_edges',
+            'xray_line', 'xray_lines', 'get_dbinfo', 'set_dbinfo',
+            'check_scan_abort', 'scan_from_db', 'do_scan', 'do_slewscan',
+            'move_instrument', 'move_samplestage']
+
+physical_constants = consts.physical_constants
+
+PI = np.pi
+TAU = 2*np.pi
+
+DEG2RAD = TAU/360
+RAD2DEG = 1.0/DEG2RAD
+
+# atoms/mol =  6.0221413e23  atoms/mol
+AVOGADRO = consts.Avogadro
+
+# ATOMIC MASS in grams
+AMU = consts.atomic_mass * 1000.0
+
+# electron rest mass in eV
+E_MASS = consts.electron_mass * consts.c**2 / consts.e
+
+# Planck's Constant
+#   h*c    ~= 12398.42 eV*Ang
+#   hbar*c ~=  1973.27 eV*Ang
+PLANCK_HC    = 1.e10 * consts.Planck * consts.c / consts.e
+PLANCK_HBARC = PLANCK_HC / TAU
+
+# Rydberg constant in eV (~13.6 eV)
+RYDBERG = consts.Rydberg * consts.Planck * consts.c/ consts.e
+
+# classical electron radius in cm and Ang
+R_ELECTRON_CM  = 100.0 * consts.physical_constants['classical electron radius'][0]
+R_ELECTRON_ANG = 1.e8 * R_ELECTRON_CM
+
+# XAFS K to E # 3.8099819442818976
+XAFS_KTOE = 1.e20*consts.hbar**2 / (2*consts.m_e * consts.e)
+
+# will be able to import these from xraydb when v 4.5.1 is required
+ATOM_SYMS = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg',
+           'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr',
+           'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br',
+           'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd',
+           'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La',
+           'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er',
+           'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au',
+           'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th',
+           'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md',
+           'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn',
+           'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+
+ATOM_NAMES = ['hydrogen', 'helium', 'lithium', 'beryllium', 'boron', 'carbon',
+            'nitrogen', 'oxygen', 'fluorine', 'neon', 'sodium', 'magnesium',
+            'aluminum', 'silicon', 'phosphorus', 'sulfur', 'chlorine', 'argon',
+            'potassium', 'calcium', 'scandium', 'titanium', 'vanadium',
+            'chromium', 'manganese', 'iron', 'cobalt', 'nickel', 'copper',
+            'zinc', 'gallium', 'germanium', 'arsenic', 'selenium', 'bromine',
+            'krypton', 'rubidium', 'strontium', 'yttrium', 'zirconium',
+            'niobium', 'molybdenum', 'technetium', 'ruthenium', 'rhodium',
+            'palladium', 'silver', 'cadmium', 'indium', 'tin', 'antimony',
+            'tellurium', 'iodine', 'xenon', 'cesium', 'barium', 'lanthanum',
+            'cerium', 'praseodymium', 'neodymium', 'promethium', 'samarium',
+            'europium', 'gadolinium', 'terbium', 'dysprosium', 'holmium',
+            'erbium', 'thulium', 'ytterbium', 'lutetium', 'hafnium',
+            'tantalum', 'tungsten', 'rhenium', 'osmium', 'iridium', 'platinum',
+            'gold', 'mercury', 'thallium', 'lead', 'bismuth', 'polonium',
+            'astatine', 'radon', 'francium', 'radium', 'actinium', 'thorium',
+            'protactinium', 'uranium', 'neptunium', 'plutonium', 'americium',
+            'curium', 'berkelium', 'californium', 'einsteinium', 'fermium',
+            'mendelevium', 'nobelium', 'lawrencium', 'rutherfordium',
+            'dubnium', 'seaborgium', 'bohrium', 'hassium', 'meitnerium',
+            'darmstadtium', 'roentgenium', 'copernicium', 'nihonium',
+            'flerovium', 'moscovium', 'livermorium', 'tennessine', 'oganesson']
+
+
+SI_PREFIXES = {'y': 1.e-24, 'yocto': 1.e-24,
+               'z': 1.e-21, 'zepto': 1.e-21,
+               'a': 1.e-18, 'atto': 1.e-18,
+               'f': 1.e-15, 'femto': 1.e-15,
+               'p': 1.e-12, 'pico': 1.e-12,
+               'n': 1.e-9, 'nano': 1.e-9,
+               '\u03bc': 1.e-6, 'u': 1.e-6, 'micro': 1.e-6,
+               'm': 1.e-3, 'milli': 1.e-3,
+               'c': 1.e-2, 'centi': 1.e-2,
+               'd': 1.e-1, 'deci': 1.e-1,
+               'da': 1.e1, 'deka': 1.e1,
+               'h': 1.e2, 'hecto': 1.e2,
+               'k': 1.e3, 'kilo': 1.e3,
+               'M': 1.e6, 'mega': 1.e6,
+               'G': 1.e9, 'giga': 1.e9,
+               'T': 1.e12, 'tera': 1.e12,
+               'P': 1.e15, 'peta': 1.e15,
+               'E': 1.e18, 'exa': 1.e18,
+               'Z': 1.e21, 'zetta': 1.e21,
+               'Y': 1.e24, 'yotta': 1.e24,
+  }
+
+
+def etok(energy):
+    """convert photo-electron energy to wavenumber"""
+    if energy < 0: return 0
+    return np.sqrt(energy/XAFS_KTOE)
+
+def ktoe(k):
+    """convert photo-electron wavenumber to energy"""
+    if k < 0: return 0
+    return XAFS_KTOE*k*k
+
+def as_ndarray(obj):
+    """
+    make sure a float, int, list of floats or ints,
+    or tuple of floats or ints, acts as a numpy array
+    """
+    if isinstance(obj, (float, int)):
+        return np.array([obj])
+    return np.asarray(obj)
+
+def index_of(array, value):
+    """
+    return index of array *at or below* value
+    returns 0 if value < min(array)
+
+    >> ix = index_of(array, value)
+
+    Arguments
+    ---------
+    array  (ndarray-like):  array to find index in
+    value  (float): value to find index of
+
+    Returns
+    -------
+    integer for index in array at or below value
+    """
+    if value < min(array):
+        return 0
+    return max(np.where(array<=value)[0])
+
+def index_nearest(array, value):
+    """
+    return index of array *nearest* to value
+
+    >>> ix = index_nearest(array, value)
+
+    Arguments
+    ---------
+    array  (ndarray-like):  array to find index in
+    value  (float): value to find index of
+
+    Returns
+    -------
+    integer for index in array nearest value
+
+    """
+    return np.abs(array-value).argmin()
+
+
+def scan_from_db(scanname, filename="scan.001"):
+    """
+    get scan definition from ScanDB by name
+    """
+    try:
+        scan = _scandb.make_scan(scanname)
+        scan.filename = filename
+        scan.mkernel = _mkernel
+    except:
+        raise ValueError(f"no scan definition '{scanname}' found")
+    return scan
+
+def do_scan(scanname, filename="scan.001", nscans=1, comments=""):
+    """do_scan(scanname, filename="scan.001", nscans=1, comments="")
+
+    execute a step scan as defined in Scan database
+
+    Arguments
+    ---------
+    scanname (string): name of scan
+    filename (string): name of output data file ['scan.001']
+    comments (string): user comments for file ['']
+    nscans (integer):  number of repeats to make. [1]
+
+    Examples
+    --------
+      do_scan("cu_xafs", "cu_sample1.001", nscans=3)
+
+    Notes
+    -----
+      1. The filename will be incremented so that each scan uses a new filename.
+    """
+    if nscans is not None:
+        _scandb.set_info("nscans", nscans)
+
+    scan = scan_from_db(scanname, filename=filename)
+    scan.comments = comments
+    if scan.scantype == "slew":
+        return scan.run(filename=filename, comments=comments)
+    else:
+        scans_completed = 0
+        nscans = int(_scandb.get_info("nscans"))
+        abort  = _scandb.get_info("request_abort", as_bool=True)
+        while (scans_completed  < nscans) and not abort:
+            scan.run()
+            scans_completed += 1
+            abort  = _scandb.get_info("request_abort", as_bool=True)
+            nscans = int(_scandb.get_info("nscans"))
+        return scan
+
+def do_slewscan(scanname, filename="scan.001", nscans=1, comments=""):
+    """do_scan(scanname, filename="scan.001", nscans=1, comments="")
+
+    execute a slew scan as defined in Scan database
+
+    Arguments
+    ---------
+    scanname (string):  name of scan
+    filename (string):  name of output data file ['scan.001']
+    comments (string): user comments for file ['']
+    nscans (integr):   number of repeats to make. [1]
+
+    Examples
+    --------
+      do_slewscan("cu_xafs", "cu_sample1.001", nscans=3)
+
+    Notes
+    -----
+      1. The filename will be incremented so that each scan uses a new filename.
+    """
+    if nscans is not None:
+        _scandb.set_info("nscans", nscans)
+
+    scan = scan_from_db(scanname, filename=filename)
+    scan.comments = comments
+    if scan.scantype != "slew":
+        return do_scan(scanname, filename=filename, nscans=nscans, comments=comments)
+    else:
+        return scan.run(filename=filename, comments=comments)
+
+def get_dbinfo(key, default=None, as_bool=False, as_int=False, full_row=False):
+    """get a value for a keyword in the scan info table,
+    where most status information is kept.
+
+    Arguments
+    ---------
+     key        name of data to look up
+     default    (default None) value to return if key is not found
+     as_bool    (default False) convert to bool
+     as_int     (default False) convert to int
+     full_row   (default False) return full row, not just value
+
+    Notes
+    -----
+     1.  if this key doesn"t exist, it will be added with the default
+         value and the default value will be returned.
+     2.  the full row will include notes, create_time, modify_time
+
+    """
+    return _scandb.get_info(key, default=default, full_row=full_row,
+                            as_int=as_int, as_bool=as_bool)
+
+def set_dbinfo(key, value, notes=None, **kws):
+    """set a value for a keyword in the scan info table."""
+    return _scandb.set_info(key, value, notes=notes)
+
+def check_scan_abort():
+    """returns whether Abort has been requested"""
+    return get_dbinfo('request_abort', as_bool=True)
+
+def move_instrument(inst_name, position_name, wait=True, infoname=None, timeout=60.0):
+    """move an Epics Instrument to a named position
+
+    Arguments
+    ---------
+    inst_name (string):     name of Epics Instrument
+    position_name (string): name of position for the Instrument
+    wait (True or False):   whether to wait for move to complete [True]
+    timeout (float):        time in seconds to give up waiting [60]
+
+    Examples
+    -------
+       move_instrument('Double H Mirror Stripes', 'platinum', wait=True)
+
+    """
+    _instdb.restore_position(inst_name, position_name, wait=wait, timeout=timeout)
+    if infoname is not None:
+        _scandb.set_info(infoname, position_name)
+
+
+def move_samplestage(position_name, wait=True, timeout=60.0):
+    """move Instrument for Samplestage to a named position
+
+    Arguments
+    ---------
+    position_name (string): name of sample position
+    wait (True or False):   whether to wait for move to complete [True]
+    timeout (float):        time in seconds to give up waiting [60]
+
+    Notes
+    -----
+     the instrument for the sample will be fetched from
+             _scandb.get_info('samplestage_instrument')
+
+    Examples
+    --------
+        move_sample('Map1 positionA', wait=True)
+
+    """
+    inst_name = _scandb.get_info('samplestage_instrument')
+    _instdb.restore_position(inst_name, position_name, wait=wait, timeout=timeout)
---
+++ packages/epicsscan/epicsscan-2025.1.2.tar.gz/epicsscan-2025.1.2/epicsscan/server.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python
+"""
+scan server
+"""
+import traceback
+from time import time, sleep
+
+from collections import deque
+from datetime import datetime
+import epics
+from pyshortcuts import isotime
+
+from .scandb import ScanDB
+from .utils import strip_quotes, plain_ascii, is_complete
+
+from .macro_kernel import MacroKernel
+
+from .epics_scandb import EpicsScanDB
+# from .abort_slewscan import abort_slewscan
+
+DEBUG_TIMER = False
+
+class ScanServer():
+    """Scan Server"""
+    def __init__(self, dbname=None,  **kws):
+        self.epicsdb = None
+        self.scandb = None
+        self.abort = False
+        self.command_in_progress = False
+        self.req_shutdown = False
+        self.req_abort = False
+        self.req_pause = False
+        self.req_shutdown = False
+        self.fileroot = None
+        self.connect(dbname, **kws)
+
+    def connect(self, dbname, **kws):
+        """connect to Scan Database"""
+        self.scandb = ScanDB(dbname=dbname, **kws)
+
+        self.set_scan_message('Server Initializing')
+        self.scandb.set_hostpid()
+        self.scandb.set_info('request_abort',    0)
+        self.scandb.set_info('request_pause',    0)
+        self.scandb.set_info('request_shutdown', 0)
+        self.set_workdir()
+
+        self.mkernel = MacroKernel(self.scandb, load_macros=True)
+        sleep(0.05)
+        self.set_scan_message('Server Connected.')
+        if 'startup' in self.mkernel.get_macros():
+            self.scandb.add_command("startup()")
+
+        eprefix = self.scandb.get_info('epics_status_prefix')
+        basedir = self.scandb.get_info('server_fileroot')
+        workdir = self.scandb.get_info('user_folder')
+
+        if eprefix is not None:
+            self.epicsdb = EpicsScanDB(prefix=eprefix)
+            sleep(0.1)
+            self.epicsdb.Shutdown = 0
+            self.epicsdb.Abort = 0
+            self.epicsdb.basedir = plain_ascii(basedir)
+            self.epicsdb.workdir = plain_ascii(workdir)
+
+    def set_scan_message(self, msg):
+        self.scandb.set_info('scan_message', msg)
+        if self.epicsdb is not None:
+            self.epicsdb.message = msg
+        print(msg)
+
+    def sleep(self, t=0.05):
+        try:
+            sleep(t)
+        except KeyboardInterrupt:
+            self.abort = True
+
+    def finish(self):
+        "shut down scan server"
+        self.set_scan_message('Server Shutting Down')
+        self.scandb.set_info('request_pause',    0)
+        self.scandb.set_info('request_abort',    0)
+        self.scandb.set_info('request_shutdown', 0)
+        sleep(0.025)
+
+    def set_status(self, status):
+        "set status"
+        self.scandb.set_info('scan_status', status)
+        if self.epicsdb is not None:
+            self.epicsdb.status = status.title()
+
+    def set_workdir(self, verbose=False):
+        "ser working folder"
+        self.scandb.set_workdir(verbose=verbose)
+        self.fileroot = self.scandb.get_info('server_fileroot')
+
+    def do_command(self, cmd_row):
+        """execute a single command: a row from the commands table"""
+        self.set_workdir(verbose=False)
+        cmdid = cmd_row.id
+        command = plain_ascii(cmd_row.command)
+        # print(f"#Server.do_command: <{command}>")
+        cmd_stat = self.scandb.get_command_status(cmdid).lower()
+        if str(cmd_stat) not in ('requested', 'starting', 'running', 'aborting'):
+            msg = f"Warning: skipping command <{command}s> status={cmd_stat}"
+            self.set_scan_message(msg)
+            self.scandb.set_command_status('canceled', cmdid=cmdid)
+            return
+
+        if len(command) < 1 or command in (None, 'None', ''):
+            self.scandb.set_command_status('canceled', cmdid=cmdid)
+            return
+
+        workdir = plain_ascii(self.scandb.get_info('user_folder'))
+        if self.epicsdb is not None:
+            self.epicsdb.workdir = workdir
+
+        if not is_complete(command):
+            self.set_scan_message("Error: command <command> is incomplete")
+            self.scandb.set_command_status('canceled', cmdid=cmdid)
+            return
+
+        self.command_in_progress = True
+        self.set_status('starting')
+        self.set_scan_message(f"Executing: <{command}>")
+        self.scandb.set_command_status('starting', cmdid=cmdid)
+
+        args    = strip_quotes(plain_ascii(cmd_row.arguments)).strip()
+        notes   = strip_quotes(plain_ascii(cmd_row.notes)).strip()
+        nrepeat = int(cmd_row.nrepeat)
+
+        filename = cmd_row.output_file
+        if filename is None:
+            filename = ''
+        filename = strip_quotes(plain_ascii(filename))
+
+        if command.lower() in ('scan', 'slewscan'):
+            scanname = args
+            words = [f"'{scanname}'"]
+            if nrepeat > 1 and command != 'slewscan':
+                words.append(f"nscans={nrepeat:d}")
+                self.scandb.set_info('nscans', nrepeat)
+            if len(notes) > 0:
+                words.append(f"comments='{notes}'")
+            if len(filename) > 0:
+                words.append(f"filename='{filename}'")
+                self.scandb.set_filename(filename)
+
+            self.scandb.update('scandefs', where={'name': scanname},
+                               last_used_time=datetime.now())
+            command = f"do_{command}"
+            args = ', '.join(words)
+        elif command.lower().startswith('restart_scanserver'):
+            self.scandb.set_info('error_message',   '')
+            self.scandb.set_info('request_shutdown', 1)
+        elif command.lower().startswith('load_macro'):
+            self.scandb.set_info('error_message',   '')
+            self.scandb.set_command_status('running', cmdid=cmdid)
+            self.set_scan_message('Server reloading macros..')
+            self.mkernel.load_macros()
+            self.scandb.set_command_status('finished', cmdid=cmdid)
+        else:
+            if len(args) == 0:
+                cmd = command
+            else:
+                cmd = f"{command}({args})"
+            self.scandb.set_info('scan_progress', 'running')
+            self.scandb.set_info('error_message',   '')
+            self.scandb.set_info('current_command', cmd)
+            self.scandb.set_info('current_command_id', cmdid)
+            self.set_status('running')
+            self.scandb.set_command_status('running', cmdid=cmdid)
+            if self.epicsdb is not None:
+                self.epicsdb.cmd_id = cmdid
+                self.epicsdb.command = cmd
+
+            try:
+                # print(f"#Server.do_command  run <{cmd}> {isotime()}")
+                self.mkernel.run(cmd)
+            except:
+                pass
+            status, msg = 'finished', 'scan complete'
+            errors = self.mkernel.get_error()
+            # print(f"#Server.do_command  errors? {len(errors)}")
+            if len(errors) > 0:
+                ebuff = []
+                for err in errors:
+                    exc_type, exc_val, exc_tb = err.exc_info
+                    ebuff.append(f'#> Exception {exc_type}: {exc_val}')
+                    ebuff.extend(err.get_error())
+                    print(f'#>Exception {exc_type}: {exc_val}')
+                    print(traceback.print_tb(exc_tb))
+                emsg = '\n'.join(ebuff)
+                self.scandb.set_info('error_message', emsg)
+                msg = 'scan completed with error'
+                print('## Error ', emsg)
+            sleep(0.1)
+            self.scandb.set_info('scan_progress', msg)
+            self.scandb.set_command_status(status, cmdid=cmdid)
+        self.set_status('idle')
+        self.command_in_progress = False
+
+    def look_for_interrupts(self):
+        """look for aborts"""
+        get_info = self.scandb.get_info
+        self.req_abort = get_info('request_abort', as_bool=True)
+        self.req_pause = get_info('request_pause', as_bool=True)
+        self.req_shutdown = get_info('request_shutdown', as_bool=True)
+        if self.epicsdb is not None:
+            if self.epicsdb.Abort == 1:
+                self.req_abort = 1
+            if self.epicsdb.Shutdown == 1:
+                self.req_shutdown = 1
+
+        if not self.scandb.check_hostpid():
+            print("No Longer Host, exiting")
+            sleep(5)
+            self.req_shutdown = 1
+        return self.req_abort
+
+    def clear_interrupts(self):
+        """re-set interrupt requests:
+        abort / pause / resume
+        if scandb is being used, these are looked up from database.
+        otherwise local Macro variables are used.
+        """
+        self.req_abort = self.req_pause = False
+        self.scandb.set_info('request_abort', 0)
+        self.scandb.set_info('request_pause', 0)
+        if self.epicsdb is not None:
+            self.epicsdb.Abort = 0
+            self.epicsdb.Shutdown = 0
+
+    def set_heartbeat(self):
+        "set scan infor heart beat"
+        tmsg = isotime()
+        self.scandb.set_info('heartbeat', tmsg)
+        if self.epicsdb is not None:
+            self.epicsdb.TSTAMP = tmsg
+
+    def mainloop(self):
+        """ main serve loop"""
+        self.set_status('idle')
+        msgtime = time()
+        self.set_scan_message('Server Ready')
+        request_id = self.scandb.status_codes['requested']
+
+        # Note: this loop is really just looking for new commands
+        # or interrupts, so does not need to go super fast.
+        cmds = deque([])
+        while True:
+            epics.poll(0.05, 1.0)
+
+            now = time()
+            # update server heartbeat / message
+            if now > msgtime + 0.75:
+                msgtime = now
+                self.set_heartbeat()
+
+            self.look_for_interrupts()
+            # shutdown?
+            if self.req_shutdown:
+                break
+
+            # pause: sleep, continue loop until un-paused
+            if self.req_pause:
+                sleep(1.0)
+                continue
+
+            # abort current command?
+            if self.req_abort:
+                if len(cmds) > 0:
+                    cmd = cmds.popleft()
+                    self.scandb.set_command_status('aborted', cmdid=cmd.id)
+                self.clear_interrupts()
+                sleep(1.0)
+
+            # we are not paused or aborting:
+            # if there are more commands in the queue, do the next one
+            if len(cmds) > 0:
+                self.do_command(cmds.popleft())
+
+            # otherwise get ordered list of requested commands
+            else:
+                cmds = deque(self.scandb.get_rows('commands',
+                                             status=request_id,
+                                             order_by='run_order'))
+            self.scandb.set_info('n_command_queue', len(cmds))
+
+        self.finish()
