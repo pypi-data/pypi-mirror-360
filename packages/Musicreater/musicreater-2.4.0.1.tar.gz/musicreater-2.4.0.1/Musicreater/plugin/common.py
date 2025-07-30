a68785cc669c08de536a01175d233a41b49f5c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:01 +0000

    Add package Musicreater/2.4.0.1/musicreater-2.4.0.1.tar.gz

---
+++ packages/Musicreater/musicreater-2.4.0.1.tar.gz/musicreater-2.4.0.1/Musicreater/plugin/common.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+存放通用的普遍性的插件内容
+"""
+
+"""
+版权所有 © 2025 金羿 & 诸葛亮与八卦阵
+Copyright © 2025 Eilles & bgArray
+
+开源相关声明请见 仓库根目录下的 License.md
+Terms & Conditions: License.md in the root directory
+"""
+
+# 睿乐组织 开发交流群 861684859
+# Email TriM-Organization@hotmail.com
+# 若需转载或借鉴 许可声明请查看仓库目录下的 License.md
+
+
+import math
+
+
+def bottem_side_length_of_smallest_square_bottom_box(total: int, maxHeight: int):
+    """给定总方块数量和最大高度，返回所构成的图形外切正方形的边长
+    :param total: 总方块数量
+    :param maxHeight: 最大高度
+    :return: 外切正方形的边长 int"""
+    return math.ceil(math.sqrt(math.ceil(total / maxHeight)))
