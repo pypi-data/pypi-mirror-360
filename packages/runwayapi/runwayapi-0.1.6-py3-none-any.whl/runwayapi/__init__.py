a5fdee650693326dc6619b2d180158de27085c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:04 +0000

    Add package runwayapi/0.1.6/runwayapi-0.1.6-py3-none-any.whl

---
+++ packages/runwayapi/runwayapi-0.1.6-py3-none-any.whl/runwayapi/__init__.py
@@ -0,0 +1,30 @@
+from .main import (
+    login,
+    get_user_team_id,
+    get_min_session_id,
+    get_sessions,
+    create_session,
+    get_asset_group_id,
+    generate_image,
+    generate_video_for_gen3a,
+    upload_image,
+    is_can_generate_image,
+    is_can_generate_video
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "login",
+    "get_user_team_id",
+    "get_min_session_id", 
+    "get_sessions",
+    "create_session",
+    "get_asset_group_id",
+    "generate_image",
+    "generate_video_for_gen3a",
+    "generate_video_for_gen4",
+    "upload_image",
+    "is_can_generate_image",
+    "is_can_generate_video"
+] 
