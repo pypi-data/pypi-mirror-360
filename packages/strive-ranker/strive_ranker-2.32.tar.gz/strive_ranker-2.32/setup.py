40d37b1a8c6a954c4496e1c88bee577d6de3ec1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:28 +0000

    Add package strive-ranker/2.32/strive_ranker-2.32.tar.gz

---
+++ packages/strive-ranker/strive_ranker-2.32.tar.gz/strive_ranker-2.32/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='strive_ranker',
+    version='2.32',
+    author='Carlo Moro',
+    author_email='cnmoro@gmail.com',
+    description="Semantic Tokenized Ranking via Vectorization & Embeddings",
+    packages=find_packages(),
+    package_data={
+        "strive": ["resources/*"]
+    },
+    include_package_data=True,
+    install_requires=[
+        "numpy<2",
+        "nltk",
+        "scikit-learn",
+        "fasttext",
+        "model2vec",
+        "faiss-cpu",
+        "bm25s"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
