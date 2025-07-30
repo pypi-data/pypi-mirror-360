5e2a9573c687a767e62bc4611956e20159f1d934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:24 +0000

    Add package AOT-biomaps/2.6.6/aot_biomaps-2.6.6.tar.gz

---
+++ packages/AOT-biomaps/aot_biomaps-2.6.6.tar.gz/aot_biomaps-2.6.6/AOT_biomaps/__init__.py
@@ -0,0 +1,21 @@
+from .AOT_Acoustic import *
+from .AOT_Optic import *
+from .AOT_Reconstruction import *
+from .AOT_Experiment import *
+from .config import config
+
+__version__ = '2.6.6'
+
+if config.get_process() == 'gpu':
+    __process__ = 'gpu'
+else:
+    __process__ = 'cpu'
+
+def initialize(process='cpu'):
+    config.set_process(process)
+    if process == 'gpu':
+        config.select_best_gpu()
+        print(f"Initialized with process: {config.get_process()} using GPU: {config.bestGPU}")
+    else:
+        print(f"Initialized with process: {config.get_process()}")
+
