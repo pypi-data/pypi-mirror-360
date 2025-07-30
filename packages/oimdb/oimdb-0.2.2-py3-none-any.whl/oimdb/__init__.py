431417615dcef1a62d71d33705acf6809812f1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:59 +0000

    Add package oimdb/0.2.2/oimdb-0.2.2-py3-none-any.whl

---
+++ packages/oimdb/oimdb-0.2.2-py3-none-any.whl/oimdb/__init__.py
@@ -0,0 +1,5 @@
+from oimdb.core.all_titles import get_alternate_titles
+from oimdb.core.title import get_main_title
+from oimdb.core.raiting import get_rating
+
+__all__ = ["get_alternate_titles", "get_main_title", "get_rating"]
