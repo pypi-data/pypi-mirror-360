bad55e2cf1c142606833993917dcb2de99472668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:40 +0000

    Add package grafi/0.0.19/grafi-0.0.19-py3-none-any.whl

---
+++ packages/grafi/grafi-0.0.19-py3-none-any.whl/grafi/common/events/topic_events/output_async_event.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from pydantic import ConfigDict
+
+from grafi.common.events.event import EventType
+from grafi.common.events.topic_events.topic_event import TopicEvent
+from grafi.common.models.message import Messages
+
+
+class OutputAsyncEvent(TopicEvent):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    consumed_event_ids: List[str] = []
+    publisher_name: str
+    publisher_type: str
+    event_type: EventType = EventType.OUTPUT_TOPIC
+    data: Messages
