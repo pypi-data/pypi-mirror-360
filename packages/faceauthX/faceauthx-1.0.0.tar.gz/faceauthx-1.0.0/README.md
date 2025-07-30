be40801b1e3331a39a646deeda328b5f1fd04f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:40 +0000

    Add package faceauthX/1.0.0/faceauthx-1.0.0.tar.gz

---
+++ packages/faceauthX/faceauthx-1.0.0.tar.gz/faceauthx-1.0.0/README.md
@@ -0,0 +1,13 @@
+# FaceAuth 🔐
+
+A futuristic face login system with deep learning-based liveness detection, pattern-based matching, and real-time verification.
+
+## Features
+✅ Face registration  
+✅ Deep learning spoof detection  
+✅ Face pattern generation (SHA-256)  
+✅ Pattern-based login  
+
+## Installation
+```bash
+pip install faceauth
---
+++ packages/faceauthX/faceauthx-1.0.0.tar.gz/faceauthx-1.0.0/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='faceauthX',
+    version='1.0.0',
+    description='Next-gen real-time face authentication with anti-spoof and face pattern matching',
+    author='Sukhraj Singh',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'face_recognition',
+        'opencv-python',
+        'torch',
+        'torchvision',
+        'numpy',
+        'Pillow'
+    ],
+    python_requires='>=3.7',
+)
