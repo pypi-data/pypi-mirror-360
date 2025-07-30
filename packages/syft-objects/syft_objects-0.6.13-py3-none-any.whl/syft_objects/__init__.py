b798c832e63fa788971a83b465484750b9cd29f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:33 +0000

    Add package syft-objects/0.6.13/syft_objects-0.6.13-py3-none-any.whl

---
+++ packages/syft-objects/syft_objects-0.6.13-py3-none-any.whl/syft_objects/__init__.py
@@ -0,0 +1,36 @@
+# syft-objects - Distributed file discovery and addressing system 
+
+__version__ = "0.6.13"
+
+# Core imports
+from .models import SyftObject
+from .data_accessor import DataAccessor
+from .factory import syobj
+from .collections import ObjectsCollection
+from .utils import scan_for_syft_objects, load_syft_objects_from_directory
+from .client import check_syftbox_status, get_syft_objects_port, get_syft_objects_url
+from .auto_install import ensure_syftbox_app_installed
+
+# Create global objects collection instance
+objects = ObjectsCollection()
+
+# Export main classes and functions
+__all__ = [
+    "SyftObject", 
+    "DataAccessor",
+    "syobj", 
+    "objects", 
+    "ObjectsCollection",
+    "scan_for_syft_objects",
+    "load_syft_objects_from_directory",
+    "get_syft_objects_port",
+    "get_syft_objects_url"
+]
+
+# Check SyftBox status - only show banner if there are issues or delays
+check_syftbox_status()
+ensure_syftbox_app_installed(silent=True)
+
+# Import _print_startup_banner here to avoid circular imports
+from .client import _print_startup_banner
+_print_startup_banner(only_if_needed=True)
