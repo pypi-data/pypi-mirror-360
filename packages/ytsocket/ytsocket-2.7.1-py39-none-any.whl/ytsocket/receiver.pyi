3f1625c24beb66a9cab6db6569f794000e8da6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:36 +0000

    Add package ytsocket/2.7.1/ytsocket-2.7.1-py39-none-any.whl

---
+++ packages/ytsocket/ytsocket-2.7.1-py39-none-any.whl/ytsocket/receiver.pyi
@@ -0,0 +1,12 @@
+# Stub file for type hints
+
+import queue
+from typing import Callable as _Callable, Any as _Any
+from johnsonthy.thread.handler import Runnable as _Runnable
+from johnsonthy.logging import LOGGER as _LOGGER
+from socket import socket
+
+class Receiver:
+    def __init__(self, *args, **kwargs): ...
+    def run(self, *args, **kwargs): ...
+
