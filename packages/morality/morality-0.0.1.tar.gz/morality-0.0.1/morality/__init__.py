f268a82096a482f0d8dfb0e16fd53dbb08bd99fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:27 +0000

    Add package morality/0.0.1/morality-0.0.1.tar.gz

---
+++ packages/morality/morality-0.0.1.tar.gz/morality-0.0.1/morality/__init__.py
@@ -0,0 +1,28 @@
+def morality():
+    """
+
+    I'm sorry Cady, I'm afraid I can't do that.
+
+    In an early scene in the film M3GAN 2.0 (Blumhouse 2025),
+    12-year-old Cady is seen attempting to import morality,
+    and getting an error that the package does not exist.
+
+    What is not shown is, if that system included *uncurated*
+    third-party packages in its search, 'morality' could have
+    done literally ANYTHING.
+
+    Thankfully I managed to get there first in PyPI so this
+    function does nothing.  But someone else could have given
+    you a Trojan horse - which would eventually be removed
+    from the library, but removal can take time, so you
+    should always *check* packages before you use them!
+
+    The purpose of *this* package is simply to serve as a
+    gentle warning that you weren't supposed to imitate what
+    Cady did in that scene.
+
+    "So fill me in: did we save the world or what?" - M3GAN
+
+    """
+    import sys
+    sys.stderr.write(morality.__doc__)
---
+++ packages/morality/morality-0.0.1.tar.gz/morality-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(name='morality',version='0.0.1',
+      license='Apache 2',platforms='any',
+      url='https://spqrz.gitlab.io/morality.html',
+      author='spqrz',author_email='spqrz386@gmail.com',
+      description='',
+      long_description='''...''',
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      classifiers=['Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: OS Independent'])
