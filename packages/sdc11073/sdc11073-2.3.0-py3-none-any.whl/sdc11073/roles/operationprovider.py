6d158cfcf6160a62f61a8ad153140c09029dcb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:48 +0000

    Add package sdc11073/2.3.0/sdc11073-2.3.0-py3-none-any.whl

---
+++ packages/sdc11073/sdc11073-2.3.0-py3-none-any.whl/sdc11073/roles/operationprovider.py
@@ -0,0 +1,8 @@
+from sdc11073.roles import providerbase
+
+
+class OperationProvider(providerbase.ProviderRole):
+    """Handle operations that work on operation states.
+
+     Empty implementation, not needed/used for sdc11073 tests.
+     """
