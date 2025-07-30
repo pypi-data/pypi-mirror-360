f48477f74f2760d380c6a12897b99c2ef2225328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:49 +0000

    Add package inkhinge/0.1.6/inkhinge-0.1.6-py3-none-any.whl

---
+++ packages/inkhinge/inkhinge-0.1.6-py3-none-any.whl/inkhinge/__init__.py
@@ -0,0 +1,9 @@
+"""inkhinge - 一个专为光谱分析设计的下一代深度学习工具集"""
+
+__version__ = '0.1.6'
+__author__ = 'Fenglong Li,Siyang Li,Kehan Li,Zhiqing Guo,Kailong Fan,Hao wu'
+__email__ = '15340666394@163.com'
+__license__ = 'MIT'
+__url__ = 'https://github.com/1412universe/inkhinge'
+
+from .core import add_numbers, multiply_numbers
