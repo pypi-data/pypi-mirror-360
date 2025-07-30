5788c4a681757eeb26e99dcb360ac5b461390b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:25 +0000

    Add package clear-skies/2.0.0/clear_skies-2.0.0.tar.gz

---
+++ packages/clear-skies/clear_skies-2.0.0.tar.gz/clear_skies-2.0.0/src/clearskies/contexts/cli.py
@@ -0,0 +1,7 @@
+from clearskies.contexts.context import Context
+from clearskies.input_outputs import Cli as CliInputOutput
+
+
+class Cli(Context):
+    def __call__(self):  # type: ignore
+        return self.execute_application(CliInputOutput())
