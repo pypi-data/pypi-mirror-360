6e3d8bf7cf0c0eadf48be19fa9a1f916aa696cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:16 +0000

    Add package matterlab-hotplates/1.0.3/matterlab_hotplates-1.0.3-py3-none-any.whl

---
+++ packages/matterlab-hotplates/matterlab_hotplates-1.0.3-py3-none-any.whl/matterlab_hotplates/__init__.py
@@ -0,0 +1,5 @@
+from .ika_hotplate import IKAHotplate
+from .heidolph_hotplate import HeidolphHotplate
+from .base_hotplate import HeatStirPlate
+
+__all__ = ["IKAHotplate", "HeidolphHotplate", "HeatStirPlate"]
