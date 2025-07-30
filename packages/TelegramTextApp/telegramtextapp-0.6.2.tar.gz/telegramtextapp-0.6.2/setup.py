502e6c2cad2d5b49243087d0d2a823cacd5beff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:35 +0000

    Add package TelegramTextApp/0.6.2/telegramtextapp-0.6.2.tar.gz

---
+++ packages/TelegramTextApp/telegramtextapp-0.6.2.tar.gz/telegramtextapp-0.6.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TelegramTextApp',
+    version='0.6.2',
+    packages=find_packages(where="."),
+    include_package_data=True,
+    package_data={
+        "developer_application": ["*"],
+    },
+    install_requires=[
+        'aiogram',
+        'aiohttp',
+        'aiosqlite',
+        'asyncio',
+        'dotenv',
+        'requests'
+    ],
+    description='Библиотека для создания текстовых приложений в telegram',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='falbue',
+    author_email='cyansair05@gmail.com',
+    url='https://github.com/falbue/TelegramTextApp',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
