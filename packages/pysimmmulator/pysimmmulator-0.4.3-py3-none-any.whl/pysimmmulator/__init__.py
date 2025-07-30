1b603650b4849787b780456e05d774f08ae3829f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:05 +0000

    Add package pysimmmulator/0.4.3/pysimmmulator-0.4.3-py3-none-any.whl

---
+++ packages/pysimmmulator/pysimmmulator-0.4.3-py3-none-any.whl/pysimmmulator/__init__.py
@@ -0,0 +1,10 @@
+__title__ = "PySiMMM"
+__author__ = "RyanAugust"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025"
+__version__ = "0.4.3"
+
+import os
+
+from .simulate import simmm, multisimmm
+from .load_parameters import load_config, define_basic_params
