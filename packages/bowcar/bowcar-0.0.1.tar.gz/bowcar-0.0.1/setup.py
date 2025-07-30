4469a29ff19ce7348f0f6460034206a9acc5f18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:09:38 +0000

    Add package bowcar/0.0.1/bowcar-0.0.1.tar.gz

---
+++ packages/bowcar/bowcar-0.0.1.tar.gz/bowcar-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bowcar", ## 소문자 영단어
+    version="0.0.1", ##
+    author="ITPLE", ## ex) Sunkyeong Lee
+    author_email="itple@itpleinfo.com", ##
+    description="A package for controlling a bowcar // 바우카를 사용하기 위한 모듈", ##
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itple-books/BowCar", ##
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
