db0f4c0753bdacc311684488cbcbe02d8257f1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:30 +0000

    Add package jotsu-mcp/0.3.4/jotsu_mcp-0.3.4.tar.gz

---
+++ packages/jotsu-mcp/jotsu_mcp-0.3.4.tar.gz/jotsu_mcp-0.3.4/jotsu/mcp/common/__init__.py
@@ -0,0 +1,7 @@
+from .models import (
+    Workflow,
+    WorkflowServer,
+    WorkflowServerFull
+)
+
+__all__ = (Workflow, WorkflowServer, WorkflowServerFull)
---
+++ packages/jotsu-mcp/jotsu_mcp-0.3.4.tar.gz/jotsu_mcp-0.3.4/jotsu/mcp/server/utils.py
@@ -0,0 +1,19 @@
+import json
+import typing
+from pydantic import BaseModel
+
+from .cache import AsyncCache
+
+T = typing.TypeVar('T', bound=BaseModel)
+
+
+# Get as a pydantic type.
+async def cache_get(cache: AsyncCache, key: str, cls: typing.Type[T]) -> T | None:
+    value = await cache.get(key)
+    if value:
+        return cls(**json.loads(value))
+    return None
+
+
+async def cache_set(cache: AsyncCache, key: str, value: BaseModel, expires_in: int | None = None) -> None:
+    await cache.set(key, value=value.model_dump_json(), expires_in=expires_in)
