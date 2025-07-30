4b25ef9dcaffec37681dda423a7d7f92345d5105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:03 +0000

    Add package autoagentsai/0.1.7/autoagentsai-0.1.7.tar.gz

---
+++ packages/autoagentsai/autoagentsai-0.1.7.tar.gz/autoagentsai-0.1.7/src/autoagentsai/models.py
@@ -0,0 +1,25 @@
+# src/autoagentsai/types.py
+from typing import Optional, List, Dict
+from pydantic import BaseModel, Field
+
+
+class ImageInput(BaseModel):
+    url: str
+
+
+class ChatRequest(BaseModel):
+    agentId: str
+    chatId: Optional[str] = None
+    userChatInput: str
+    images: Optional[List[ImageInput]] = Field(default_factory=list)
+    files: Optional[List[str]] = Field(default_factory=list)
+    state: Optional[Dict[str, str]] = Field(default_factory=dict)
+    buttonKey: Optional[str] = ""
+    debug: Optional[bool] = False
+
+class ChatHistoryRequest(BaseModel):
+    agentId: Optional[str] = None
+    agentUUid: str
+    chatId: str
+    pageSize: int = 100
+    pageNumber: int = 1
