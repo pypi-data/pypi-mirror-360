0d4ff2b9e16746d71b95f94b47f83e963b88e9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:18 +0000

    Add package archigeo/2.2.3/archigeo-2.2.3-cp312-cp312-win_amd64.whl

---
+++ packages/archigeo/archigeo-2.2.3-cp312-cp312-win_amd64.whl/archigeo/basic/geometry/__init__.py
@@ -0,0 +1,15 @@
+# from .ivector import Point, Vector, Line2dExpr, Line3dExpr, PlaneExpr
+from .lineseg import LineSeg
+from .polyline import Polyline
+from .rect import Rect
+from .point import Point
+
+from .anglegrid import AngleGrid
+from .space3d import Space3d
+from .perspective import Perspective
+
+from .circle import Circle
+from .arc2d import Arc2d
+from .arc3d import Arc3d
+from .arcseries import ArcSeries, SmoothArcSeries, NCenCir
+from .ellipse import Ellipse
---
+++ packages/archigeo/archigeo-2.2.3-cp312-cp312-win_amd64.whl/archigeo/basic/geometry/ellipse.pyi
@@ -0,0 +1,8 @@
+from _typeshed import Incomplete
+
+class Ellipse:
+    a: Incomplete
+    b: Incomplete
+    dir_angle: Incomplete
+    cen: Incomplete
+    def __init__(self, a, b, cen=[0, 0], angle: int = 0) -> None: ...
