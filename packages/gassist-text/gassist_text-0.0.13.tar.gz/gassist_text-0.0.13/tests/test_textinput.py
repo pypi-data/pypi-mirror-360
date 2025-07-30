a18539da66d05b82572c1c012d1fbef84667a5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:00 +0000

    Add package gassist-text/0.0.13/gassist_text-0.0.13.tar.gz

---
+++ packages/gassist-text/gassist_text-0.0.13.tar.gz/gassist_text-0.0.13/tests/test_textinput.py
@@ -0,0 +1,17 @@
+"""Tests for TextAssistant."""
+
+import google.oauth2.credentials
+import grpc
+import pytest
+
+from gassist_text import TextAssistant
+
+
+def test_textinput() -> None:
+    """Test assist call raises if no credentials."""
+    credentials = google.oauth2.credentials.Credentials(token=None)
+    with (
+        TextAssistant(credentials) as assistant,
+        pytest.raises(grpc._channel._MultiThreadedRendezvous),
+    ):
+        assistant.assist("tell me a joke")
