a46f8187b0bc0949d6e8d87c322be1c8fa26077d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:47 +0000

    Add package abstract-parallels/0.0.403/abstract_parallels-0.0.403.tar.gz

---
+++ packages/abstract-parallels/abstract_parallels-0.0.403.tar.gz/abstract_parallels-0.0.403/setup.py
@@ -0,0 +1,49 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='abstract_parallels',
+    version='0.0.403',
+    author='putkoff',
+    author_email='partners@abstractendeavors.com',
+    description="A Python package for managing clipboard, window info, services, and APIs with GUI interfaces",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/abstractendeavors/abstract_parallels',  # Update with actual repo if available
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={
+        'abstract_parallels': [
+            '*.sh',
+            'apis/*.sh',
+            'clipit/*.sh',
+            'services_mgr/*.sh',
+            'window_mgr/*.sh',
+        ]
+    },
+        include_package_data=True,
+    install_requires=[
+        'flask',
+        'paramiko',
+        'PyQt5',
+        # 'abstract_utilities',  # Uncomment if external dependency
+    ],
+
+
+    python_requires=">=3.6",
+    setup_requires=['wheel'],
+
+)
---
+++ packages/abstract-parallels/abstract_parallels-0.0.403.tar.gz/abstract_parallels-0.0.403/src/abstract_parallels/__init__.py
@@ -0,0 +1,5 @@
+from .apis import *
+from .clipit import *
+from .window_mgr import *
+from .services_mgr import *
+from .parallels_control_center import *
