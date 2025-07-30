c96be1853b280e5eecf1490aa13d8bbd44f8d967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:15 +0000

    Add package flet-charts/0.2.0.dev18/flet_charts-0.2.0.dev18.tar.gz

---
+++ packages/flet-charts/flet_charts-0.2.0.dev18.tar.gz/flet_charts-0.2.0.dev18/src/flet_charts/bar_chart_rod_stack_item.py
@@ -0,0 +1,31 @@
+from dataclasses import field
+from typing import Optional
+
+import flet as ft
+
+__all__ = ["BarChartRodStackItem"]
+
+
+@ft.control("BarChartRodStackItem")
+class BarChartRodStackItem(ft.BaseControl):
+    from_y: Optional[ft.Number] = None
+    """
+    The starting position of this item inside a bar rod.
+    """
+
+    to_y: ft.Number = 0
+    """
+    The ending position of this item inside a bar rod.
+    """
+
+    color: Optional[ft.ColorValue] = None
+    """
+    The [color](https://flet.dev/docs/reference/colors) of this item.
+    """
+
+    border_side: ft.BorderSide = field(default_factory=lambda: ft.BorderSide.none())
+    """
+    A border around this item.
+
+    Value is of type [`BorderSide`](https://flet.dev/docs/reference/types/borderside).
+    """
