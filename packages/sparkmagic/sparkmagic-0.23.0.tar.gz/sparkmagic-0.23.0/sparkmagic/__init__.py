e4dc75e5c971d5b0f9ece7a667bd75be2c88ae0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:54 +0000

    Add package sparkmagic/0.23.0/sparkmagic-0.23.0.tar.gz

---
+++ packages/sparkmagic/sparkmagic-0.23.0.tar.gz/sparkmagic-0.23.0/sparkmagic/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.23.0"
+
+from sparkmagic.serverextension.handlers import (
+    load_jupyter_server_extension,
+)  # noqa: #501
+
+
+def _jupyter_server_extension_paths():
+    return [{"module": "sparkmagic"}]
+
+
+def _jupyter_nbextension_paths():
+    return []
