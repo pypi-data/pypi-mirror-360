d845cea0c9fb4397c50fedbdf0d89ff90bcaa843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:51 +0000

    Add package pyramid-formencode-classic/0.10.0/pyramid_formencode_classic-0.10.0.tar.gz

---
+++ packages/pyramid-formencode-classic/pyramid_formencode_classic-0.10.0.tar.gz/pyramid_formencode_classic-0.10.0/src/pyramid_formencode_classic/debugtoolbar/__init__.py
@@ -0,0 +1,19 @@
+# stdlib
+from typing import TYPE_CHECKING
+
+# local
+from .panels.formencode_classic import FormencodeClassicDebugPanel
+
+# typing
+if TYPE_CHECKING:
+    from pyramid.config import Configurator
+
+
+# ==============================================================================
+
+
+def includeme(config: "Configurator") -> None:
+    """
+    Pyramid API hook
+    """
+    config.add_debugtoolbar_panel(FormencodeClassicDebugPanel)
