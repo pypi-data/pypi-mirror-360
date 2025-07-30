c9c57574b73f44e8afcb9bdd24c5423f6a6e3bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:46 +0000

    Add package mlp-mixer-pytorch/0.3.1/mlp_mixer_pytorch-0.3.1.tar.gz

---
+++ packages/mlp-mixer-pytorch/mlp_mixer_pytorch-0.3.1.tar.gz/mlp_mixer_pytorch-0.3.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'mlp-mixer-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.3.1',
+  license='MIT',
+  description = 'MLP Mixer - Pytorch',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/mlp-mixer-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'image recognition'
+  ],
+  install_requires=[
+    'einops>=0.8',
+    'torch>=2.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
