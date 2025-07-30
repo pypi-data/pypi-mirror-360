6c2e5ce20955d3bbdabac2f815c8a279bb95a5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:44 +0000

    Add package orionis/0.375.0/orionis-0.375.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.375.0-py3-none-any.whl/tests/example/test_example.py
@@ -0,0 +1,24 @@
+from orionis.console.output.contracts.console import IConsole
+from orionis.test.cases.synchronous import SyncTestCase
+
+class TestExample(SyncTestCase):
+    """
+    Unit tests for basic example functionality.
+    """
+
+    def testUnitExample(self):
+        """
+        Test that basic equality assertions work as expected.
+
+        Asserts
+        -------
+        1 == 1 : bool
+            Ensures that the integer 1 is equal to itself.
+        2 == 2 : bool
+            Ensures that the integer 2 is equal to itself.
+        """
+        # Check if 1 equals 1
+        self.assertEqual(2, 2)
+
+        # Check if 2 equals 2
+        self.assertEqual(3, 3)
