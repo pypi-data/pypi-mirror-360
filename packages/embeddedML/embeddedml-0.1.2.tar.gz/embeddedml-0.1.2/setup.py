c0162f96d505bc9c0f45810f8083b0a7bc1350b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:45 +0000

    Add package embeddedML/0.1.2/embeddedml-0.1.2.tar.gz

---
+++ packages/embeddedML/embeddedml-0.1.2.tar.gz/embeddedml-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="EmbeddedML",
+    version="0.1.2",
+    description="Optimized Machine Learning Library for Embedded Systems",
+    author="Halil Hüseyin Çalışkan",
+    author_email="caliskanhalil815@gmail.com",
+    license="MIT",
+    packages=find_packages(),  # Burada otomatik embeddedML klasörünü bulur
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
