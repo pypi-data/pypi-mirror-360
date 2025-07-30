cebeae7606b4e49c1ddb74d396dfc7440662a546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:24 +0000

    Add package fsio/1.2.0/fsio-1.2.0.tar.gz

---
+++ packages/fsio/fsio-1.2.0.tar.gz/fsio-1.2.0/tests/cli/test_cli_program.py
@@ -0,0 +1,40 @@
+# -*- encode: utf-8 -*-
+
+import os
+from unittest import TestCase
+
+from typer.testing import CliRunner
+
+from fsio.cli.program import app
+
+runner = CliRunner()
+
+
+class TestCliProgram(TestCase):
+    def setUp(self) -> None:
+        self.test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+    def test_supported_types(self) -> None:
+        result = runner.invoke(app, ["supported-types"])
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, 'parquet\n')
+
+    def test_detect_file_type_no_file_exists(self) -> None:
+        file_path = 'fake/path'
+        result = runner.invoke(app, ["detect-file-type", file_path])
+        self.assertEqual(result.exit_code, 1)
+        self.assertTrue(f'[ERROR] File not found: {file_path}' in result.output)
+
+    def test_detect_file_type_unknown_type(self) -> None:
+        file_path = os.path.join(self.test_data_dir, 'example.ext')
+        result = runner.invoke(app, ["detect-file-type", file_path])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertTrue('File type could not be detected' in result.output)
+
+    def test_detect_file_type_parquet(self) -> None:
+        file_path = os.path.join(self.test_data_dir, 'example.parquet')
+        result = runner.invoke(app, ["detect-file-type", file_path])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertTrue('parquet' in result.output)
