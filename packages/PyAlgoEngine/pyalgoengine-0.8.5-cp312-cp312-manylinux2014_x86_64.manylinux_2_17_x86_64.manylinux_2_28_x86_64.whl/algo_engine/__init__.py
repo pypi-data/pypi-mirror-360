a2cfd249dd7ec686f0e97970074f0ce5b2da93bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:13 +0000

    Add package PyAlgoEngine/0.8.5/pyalgoengine-0.8.5-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/PyAlgoEngine/pyalgoengine-0.8.5-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/algo_engine/__init__.py
@@ -0,0 +1,52 @@
+__version__ = "0.8.5"
+
+import logging
+import os
+import traceback
+
+from . import profile
+from .base.telemetrics import LOGGER
+
+if 'ALGO_DIR' in os.environ:
+    WORKING_DIRECTORY = os.path.realpath(os.environ['ALGO_DIR'])
+else:
+    WORKING_DIRECTORY = str(os.getcwd())
+
+
+def set_logger(logger: logging.Logger):
+    from . import base
+    from . import engine
+    from . import backtest
+    from . import strategy
+    from . import apps
+
+    base.set_logger(logger=logger)
+    engine.set_logger(logger=logger.getChild('Engine'))
+    backtest.set_logger(logger=logger.getChild('BackTest'))
+    strategy.set_logger(logger=logger.getChild('Strategy'))
+    apps.set_logger(logger=logger.getChild('Apps'))
+
+
+LOGGER.info(f'AlgoEngine version {__version__}')
+
+# import addon module
+try:
+    from . import algo_addon
+
+    LOGGER.info(f'PyAlgoEngineAddons import successful, version {algo_addon.__version__}')
+except ImportError:
+    algo_addon = None
+    LOGGER.debug(f'Install PyAlgoEngineAddons to use additional trading algos module\n{traceback.format_exc()}')
+
+
+def get_include():
+    import os
+    return os.path.dirname(__file__)
+
+
+__all__ = [
+    'apps', 'backtest', 'base', 'engine', 'monitor', 'profile', 'strategy', 'utils',
+    'algo_addon',
+    'get_include',
+    'LOGGER'
+]
