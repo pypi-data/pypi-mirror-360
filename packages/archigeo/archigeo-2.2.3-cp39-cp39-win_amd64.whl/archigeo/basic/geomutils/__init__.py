8af9887bb38fd2d8f7cdd8b7b50585446cebda71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:18 +0000

    Add package archigeo/2.2.3/archigeo-2.2.3-cp39-cp39-win_amd64.whl

---
+++ packages/archigeo/archigeo-2.2.3-cp39-cp39-win_amd64.whl/archigeo/basic/geomutils/__init__.py
@@ -0,0 +1,7 @@
+from .uangle import angle_90, angle_9090, angle_180, angle_360, angle_sub, angle2xy, angle_byline
+from .uvec import pt2d, pt3d, pt_add, pt_sub, pt_rotate, pt_scale, vec_dot, vec_mag, vec3d_cross, angle2vec
+from .uline import *
+from .uplane import *
+from .upoly import *
+# from .udist import *
+from .urelation import *
---
+++ packages/archigeo/archigeo-2.2.3-cp39-cp39-win_amd64.whl/archigeo/compa/plus.pyi
@@ -0,0 +1,6 @@
+from _typeshed import Incomplete
+
+package_path: Incomplete
+ver_path: Incomplete
+
+def poly_apart_poly(poly1, poly2): ...
