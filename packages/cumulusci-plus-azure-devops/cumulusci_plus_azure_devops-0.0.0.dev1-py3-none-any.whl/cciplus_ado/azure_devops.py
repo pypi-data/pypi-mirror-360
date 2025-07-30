f25918d529e2967392af703c59a1d7bb6f8517eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:40 +0000

    Add package cumulusci-plus-azure-devops/0.0.0.dev1/cumulusci_plus_azure_devops-0.0.0.dev1-py3-none-any.whl

---
+++ packages/cumulusci-plus-azure-devops/cumulusci_plus_azure_devops-0.0.0.dev1-py3-none-any.whl/cciplus_ado/azure_devops.py
@@ -0,0 +1,35 @@
+from cumulusci.core.utils import import_global
+from cumulusci.plugins.plugin_base import PluginBase
+from cumulusci.vcs.vcs_source import VCSSource
+
+import_global("cciplus_ado.vcs.ado.service.AzureDevOpsService")
+
+VCSSource.register(
+    "azure_devops", "cciplus_ado.vcs.ado.source.azure_devops.ADOSource"
+)
+
+from cciplus_ado.vcs.ado.dependencies.ado_dependencies import VCS_ADO as dep
+from cciplus_ado.vcs.ado.dependencies.ado_resolvers import VCS_ADO as res
+
+assert (
+    dep == res
+), "VCS_ADO must match in dependencies and resolvers. (Assertion done to load the ado dependencies and resolvers correctly)"
+
+
+class AzureDevOpsPlugin(PluginBase):
+    """Plugin for Azure DevOps integration."""
+
+    plugin_config_file = "cumulusci_plugin.yml"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def initialize(self) -> None:
+        """Initialize the plugin."""
+        super().initialize()
+        # Add any additional initialization here
+
+    def teardown(self) -> None:
+        """Tear down the plugin."""
+        super().teardown()
+        # Add any additional cleanup here
