91ff7633a149ac23fd1488a6980445cb07463a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:42 +0000

    Add package NectaPy/0.10.0/NectaPy-0.10.0-py3-none-any.whl

---
+++ packages/NectaPy/NectaPy-0.10.0-py3-none-any.whl/NectaPy/__init__.py
@@ -0,0 +1,5 @@
+from .main import Necta
+st_result = Necta().st_result
+sc_result = Necta().sc_result
+st_compare = Necta().st_compare
+st_mentioned = Necta().st_mentioned
---
+++ packages/NectaPy/NectaPy-0.10.0-py3-none-any.whl/NectaPy/pre.py
@@ -0,0 +1,7 @@
+# from bs4 import BeautifulSoup
+# import requests
+# response = requests.get("")
+# Parse the HTML
+# soup = BeautifulSoup(response.content, 'html.parser')
+# table = soup.find_all('table')[0].find_all('tr')[1]
+# print(table.get_text().replace('\n',' | '))
