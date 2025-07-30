c667fe01a74815628e5741ca887e1d140c64fafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:55 +0000

    Add package filoma/1.1.0/filoma-1.1.0-cp311-cp311-win_amd64.whl

---
+++ packages/filoma/filoma-1.1.0-cp311-cp311-win_amd64.whl/filoma/__init__.py
@@ -0,0 +1,15 @@
+"""
+filoma - A modular Python tool for profiling files, analyzing directory structures, and inspecting image data.
+
+Features:
+- Directory analysis with optional Rust acceleration (5-20x faster)
+- Image analysis for .tif, .png, .npy, .zarr files
+- File profiling with system metadata
+- Modular, extensible codebase
+"""
+
+# Make main modules easily accessible
+from . import dir, fileinfo, img
+from ._version import __version__
+
+__all__ = ["__version__", "dir", "img", "fileinfo"]
