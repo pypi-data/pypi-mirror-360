785c700740d857c3680f62a3d258b6b594c5642e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:58 +0000

    Add package symbolic-regression-toolkit/1.3.1/symbolic_regression_toolkit-1.3.1.tar.gz

---
+++ packages/symbolic-regression-toolkit/symbolic_regression_toolkit-1.3.1.tar.gz/symbolic_regression_toolkit-1.3.1/SRToolkit/utils/expression_compiler.py
@@ -0,0 +1,152 @@
+"""
+This module contains functions that convert an expression in infix notation to an executable python function.
+"""
+from typing import List, Tuple, Union
+
+from SRToolkit.utils.expression_tree import Node, tokens_to_tree, is_float
+from SRToolkit.utils.symbol_library import SymbolLibrary
+
+# Generated functions are defined (through exec) here, so numpy needs to be imported
+import numpy as np
+
+def expr_to_executable_function(expr: Union[List[str], Node], symbol_library: SymbolLibrary=SymbolLibrary.default_symbols()) -> callable:
+    """
+    Converts an expression in infix notation to an executable function.
+
+    Examples:
+        >>> executable_fun = expr_to_executable_function(["X_0", "+", "1"])
+        >>> executable_fun(np.array([[1], [2], [3], [4]]), np.array([]))
+        array([2, 3, 4, 5])
+        >>> executable_fun = expr_to_executable_function(["pi"])
+        >>> executable_fun(np.array([[1], [2], [3], [4]]), np.array([1]))
+        array([3.14159265, 3.14159265, 3.14159265, 3.14159265])
+        >>> executable_fun = expr_to_executable_function(["C"])
+        >>> executable_fun(np.array([[1], [2], [3], [4]]), np.array([1]))
+        array([1, 1, 1, 1])
+        >>> tree = tokens_to_tree(["X_0", "+", "1"], SymbolLibrary.default_symbols(1))
+        >>> executable_fun = expr_to_executable_function(tree)
+        >>> executable_fun(np.array([[1], [2], [3], [4]]), np.array([]))
+        array([2, 3, 4, 5])
+
+    Args:
+        expr: The expression given as a list of tokens in the infix notation or as an instance of SRToolkit.utils.expression_tree.Node
+        symbol_library: The symbol library to use. Defaults to SymbolLibrary.default_symbols().
+
+    Raises:
+        Exception: If expression is not of the right type
+
+    Returns:
+        An executable function that takes in a 2D array of input values and a 1D array of constant values and returns the output of the expression.
+    """
+    if not (isinstance(expr, list) or isinstance(expr, Node)):
+        raise Exception("Expression must be given as either a list of tokens or a tree (an instance of the "
+                        "SRToolkit.utils.expression_tree.Node class)")
+
+    if isinstance(expr, list):
+        tree = tokens_to_tree(expr, symbol_library)
+    else:
+        tree = expr
+    code, symbol, var_counter, const_counter = tree_to_function_rec(tree, symbol_library)
+
+    fun_string = "def _executable_expression_(X, C):\n"
+    for c in code:
+        fun_string += "\t" + c + "\n"
+    fun_string += "\treturn " + symbol
+
+    exec(fun_string)
+    return locals()["_executable_expression_"]
+
+
+def expr_to_error_function(expr: Union[List[str], Node], symbol_library: SymbolLibrary=SymbolLibrary.default_symbols()) -> callable:
+    """
+    Converts an expression in infix notation to an executable function that returns the root mean squared error between
+    the output of the expression and the target values.
+
+    Examples:
+        >>> executable_fun = expr_to_error_function(["X_0", "+", "1"])
+        >>> executable_fun(np.array([[1], [2], [3], [4]]), np.array([]), np.array([2, 3, 4, 5]))
+        0.0
+        >>> tree = tokens_to_tree(["X_0", "+", "1"], SymbolLibrary.default_symbols(1))
+        >>> executable_fun = expr_to_error_function(tree)
+        >>> executable_fun(np.array([[1], [2], [3], [4]]), np.array([]), np.array([2, 3, 4, 5]))
+        0.0
+
+    Args:
+        expr: The expression given as a list of tokens in the infix notation or as an instance of SRToolkit.utils.expression_tree.Node
+        symbol_library: The symbol library to use. Defaults to SymbolLibrary.default_symbols().
+
+    Raises:
+        Exception: If expression is not of the right type
+
+    Returns:
+        An executable function that takes in a 2D array of input values `X`, a 1D array of constant values `C`, and a 1D array of target values `y`. It returns the root mean squared error between the output of the expression and the target values.
+    """
+    if not (isinstance(expr, list) or isinstance(expr, Node)):
+        raise Exception("Expression must be given as either a list of tokens or a tree (an instance of the "
+                        "SRToolkit.utils.expression_tree.Node class)")
+
+    if isinstance(expr, list):
+        tree = tokens_to_tree(expr, symbol_library)
+    else:
+        tree = expr
+    code, symbol, var_counter, const_counter = tree_to_function_rec(tree, symbol_library)
+
+    fun_string = "def _executable_expression_(X, C, y):\n"
+    for c in code:
+        fun_string += "\t" + c + "\n"
+    fun_string += f"\treturn np.sqrt(np.mean(({symbol}-y)**2))"
+
+    exec(fun_string)
+    return locals()["_executable_expression_"]
+
+
+def tree_to_function_rec(tree: Node, symbol_library: SymbolLibrary, var_counter: int=0, const_counter: int=0) -> Tuple[List[str], str, int, int]:
+    """
+    Recursively converts a parse tree into a string of Python code that can be executed to evaluate the expression
+    represented by the tree.
+
+    Args:
+        tree: The root of the parse tree to convert.
+        symbol_library: The symbol library to use when converting the tree. This library defines the properties of the symbols in the tree.
+        var_counter: The number of variables encountered so far. This is used to create a unique variable name for each variable.
+        const_counter: The number of constants encountered so far. This is used to select the correct constant value from the constant array.
+
+    Returns:
+        A list of strings, where each string contains a line of Python code to execute to evaluate the expression represented by the tree.
+        The name of the variable that represents the output of the expression.
+        The updated value of `var_counter`.
+        The updated value of `const_counter`.
+
+    Raises:
+        Exception: If the parse tree contains an invalid symbol.
+
+    Notes:
+        This function is a helper function for `expr_to_executable_function` and similar and should not be called directly
+        unless you want to customize the way the expression is defined. For examples, see the code of `expr_to_executable_function` and `expr_to_error_function` in this module.
+
+
+    """
+    if tree.left is None and tree.right is None:
+        if symbol_library.get_type(tree.symbol) in ["var", "lit"]:
+            return [], symbol_library.get_np_fn(tree.symbol), var_counter, const_counter
+        elif symbol_library.get_type(tree.symbol) == "const":
+            return [], symbol_library.get_np_fn(tree.symbol).format(const_counter), var_counter, const_counter + 1
+        else:
+            if is_float(tree.symbol):
+                return [], tree.symbol, var_counter, const_counter
+            else:
+                raise Exception(f"Encountered invalid symbol {tree.symbol} while converting to function.")
+
+    elif tree.left is not None and tree.right is None:
+        code, symbol, var_counter, const_counter = tree_to_function_rec(tree.left, symbol_library, var_counter, const_counter)
+        output_symbol = "y_{}".format(var_counter)
+        code.append(symbol_library.get_np_fn(tree.symbol).format(output_symbol, symbol))
+        return code, output_symbol, var_counter + 1, const_counter
+
+    else:
+        left_code, left_symbol, var_counter, const_counter = tree_to_function_rec(tree.left, symbol_library, var_counter, const_counter)
+        right_code, right_symbol, var_counter, const_counter = tree_to_function_rec(tree.right, symbol_library, var_counter, const_counter)
+        output_symbol = "y_{}".format(var_counter)
+        code = left_code + right_code
+        code.append(symbol_library.get_np_fn(tree.symbol).format(output_symbol, left_symbol, right_symbol))
+        return code, output_symbol, var_counter + 1, const_counter
---
+++ packages/symbolic-regression-toolkit/symbolic_regression_toolkit-1.3.1.tar.gz/symbolic_regression_toolkit-1.3.1/SRToolkit/utils/expression_simplifier.py
@@ -0,0 +1,264 @@
+from typing import Union, List
+
+import numpy as np
+from sympy import sympify, expand, Expr, Basic
+from sympy.core import Mul, Add, Pow, Symbol
+from sympy import symbols as sp_symbols
+import re
+
+from SRToolkit.utils.symbol_library import SymbolLibrary
+from SRToolkit.utils.expression_tree import Node
+from SRToolkit.utils.expression_tree import is_float
+
+def simplify(expr: Union[List[str], Node], symbol_library: SymbolLibrary=SymbolLibrary.default_symbols()) -> Union[List[str], Node]:
+    """
+    Simplifies a mathematical expression by:
+        1. making use of sympy's simplification functions
+        2. simplifying constants, e.g. C*C + C -> C
+
+    Examples:
+        >>> expr = ["C", "+", "C" "*", "C", "+", "X_0", "*", "X_1", "/", "X_0"]
+        >>> print("".join(simplify(expr)))
+        C+X_1
+    
+    Args:
+        expr: The expression given as a list of tokens in the infix notation or as an instance of SRToolkit.utils.expression_tree.Node
+        symbol_library: The symbol library to use. Defaults to SymbolLibrary.default_symbols().
+
+    Raises:
+        Exception: If problems occur during simplification or if the expression contains invalid symbols.
+
+    Returns:
+        The simplified expression
+    """
+    is_tree = False
+    if isinstance(expr, Node):
+        expr = expr.to_list(symbol_library=symbol_library, notation="infix")
+        is_tree = True
+
+    variables = symbol_library.get_symbols_of_type("var")
+
+    # We expect only one symbol for constants
+    if len(symbol_library.get_symbols_of_type("const")) > 0:
+        constant = symbol_library.get_symbols_of_type("const")[0]
+    else:
+        # In this case constants shouldn't be problematic as they are not in the SymbolLibrary
+        # Just in case and to not change other functions, I changed it to __C__.
+        constant = "__C__"
+
+    expr = _simplify_expression("".join(expr), constant, variables)
+    expr = sympify(_denumerate_constants(str(expr), constant), evaluate=False)
+    expr = _sympy_to_sr(expr)
+    if not _check_tree(expr, symbol_library):
+        raise Exception("Simplified expression contains invalid symbols. Possibly skip its simplification or add symbols to the SymbolLibrary.")
+
+    if is_tree:
+        return expr
+    else:
+        return expr.to_list(symbol_library=symbol_library, notation="infix")
+
+
+def _check_tree(expr: Node, symbol_library: SymbolLibrary) -> bool:
+    """
+    Checks if the expression tree contains only valid symbols from the symbol library.
+
+    Args:
+        expr: The expression tree to check.
+        symbol_library: The symbol library to use.
+
+    Returns:
+        True if the expression tree contains only valid symbols from the symbol library, False otherwise.
+    """
+    if expr.symbol not in symbol_library.symbols and not is_float(expr.symbol):
+        return False
+    if isinstance(expr.left, Node) and not _check_tree(expr.left, symbol_library):
+        return False
+    if isinstance(expr.right, Node) and not _check_tree(expr.right, symbol_library):
+        return False
+
+    return True
+
+
+def _sympy_to_number(expr):
+    """
+    Extracts the number contained in the Sympy node 
+    """
+    evaluated = float(expr.evalf())
+    return int(evaluated) if evaluated.is_integer() else evaluated
+
+
+def _sympy_to_sr(expr: Union[Expr, Basic]) -> Node:
+    """
+    Converts a Sympy expression into an instance of SRtoolkit.expression_tree.Node, explicitly handling left-associative division.
+
+    Args:
+        expr: The Sympy expression.
+
+    Returns:
+        An instance of SRtoolkit.expression_tree.Node that corresponds to the expression given as the input.
+    """
+    if expr.is_Number:
+        return Node(str(_sympy_to_number(expr)))
+    
+    if expr.is_Symbol:
+        return Node(str(expr))
+
+    if expr.is_Function:
+        func_name = expr.func.__name__
+        arg = _sympy_to_sr(expr.args[0])
+        return Node(func_name, left=arg)
+
+    if isinstance(expr, Add):
+        args = expr.as_ordered_terms()
+        # Detect subtraction
+        if len(args) == 2 and args[1].is_Mul and args[1].args[0] == -1:
+            return Node('-', _sympy_to_sr(-args[1]), _sympy_to_sr(args[0]))
+        # Handle regular addition
+        root = Node('+', _sympy_to_sr(args[1]), _sympy_to_sr(args[0]))
+        for term in args[2:]:
+            root = Node('+', _sympy_to_sr(term), root)
+        return root
+
+    if isinstance(expr, Mul):
+        # Process factors explicitly, ensuring left-to-right associativity
+        factors = list(expr.args)
+        root = _sympy_to_sr(factors[0])  # Start with the first factor
+        for factor in factors[1:]:
+            if factor.is_Pow and factor.args[1] == -1:  # Division
+                divisor = _sympy_to_sr(factor.args[0])
+                root = Node('/', divisor, root)  # Left-to-right division
+            else:  # Multiplication
+                multiplicand = _sympy_to_sr(factor)
+                root = Node('*', multiplicand, root)
+        return root
+
+    if isinstance(expr, Pow):
+        base, exp = expr.args
+        return Node('^', _sympy_to_sr(exp), _sympy_to_sr(base))
+
+    if expr.is_Rational and expr.q != 1:
+        return Node('/', _sympy_to_sr(expr.q), _sympy_to_sr(expr.p))
+
+    raise ValueError(f"{expr}")
+
+
+def _simplify_constants(eq, c, var):
+    """ Simplifies the constants in a Sympy expression. output[2][0][1] is the simplified expression.
+
+    Args:
+        eq: The Sympy expression.
+        c: The constant symbol.
+        var: List of symbols representing variables.
+    
+    Returns:
+        - bool: True if the expression contains a variable.
+        - bool: True if the expression contains the constant.
+        - list: List of tuples containing the original and simplified expressions
+    """
+    if len(eq.args) == 0:
+        if eq in var:
+            return True, False, [(eq, eq)]
+        elif eq in eq.free_symbols:
+            return False, True, [(eq, c)]
+        else:
+            return False, False, [(eq, eq)]
+    else:
+        has_var, has_c, subs = [], [], []
+        for a in eq.args:
+            a_rec = _simplify_constants (a, c, var)
+            has_var += [a_rec[0]]; has_c += [a_rec[1]]; subs += [a_rec[2]]
+        if sum(has_var) == 0 and True in has_c:
+            return False, True, [(eq, c)]
+        else:          
+            args = []
+            if isinstance(eq, (Add, Mul, Pow)):
+                has_free_c = False
+                if True in [has_c[i] and not has_var[i] for i in range(len(has_c))]:
+                    has_free_c = True
+                    
+                for i in range(len(has_var)):
+                    if has_var[i] or (not has_free_c and not has_c[i]):
+                        if len(subs[i]) > 0:
+                            args += [eq.args[i].subs(subs[i])]
+                        else:
+                            args += [eq.args[i]]
+                if has_free_c:
+                    args += [c]
+                
+            else:
+                for i in range(len(has_var)):
+                    if len(subs[i]) > 0:
+                        args += [eq.args[i].subs(subs[i])]
+                    else:
+                        args += [eq.args[i]]
+            return True in has_var, True in has_c, [(eq, eq.func(*args))]
+
+
+def _enumerate_constants(expr, constant):
+    """ Enumerates the constants in a Sympy expression. 
+
+    Example: C*x**2 + C*x + C -> C0*x**2 + C1*x + C2
+
+    Input:
+        expr - Sympy expression
+        constant - constant symbol
+
+    Returns:
+        Sympy expression with enumerated constants
+        list of enumerated constants"""
+        
+    char_list = np.array(list(str(expr)), dtype='<U16')
+    constind = np.where(char_list == constant)[0]
+    """ Rename all constants: c -> cn, where n is the index of the associated term"""
+    constants = [constant+str(i) for i in range(len(constind))]
+    char_list[constind] = constants
+    return sympify("".join(char_list)), tuple(constants)
+
+
+def _denumerate_constants(expr, constant):
+    """ Removes the enumeration of constants in a Sympy expression.
+
+    Args:
+        expr: Sympy expression
+        constant: constant symbol
+
+    Returns:
+        Sympy expression with denumerated constants
+    """
+    return re.sub(f'{constant}\\d', constant, expr)
+
+
+def _simplify_expression (expr_str, constant, variables):
+    """Simplifies a mathematical expression.
+
+    Args:
+        expr_str: String representing the expression.
+        constant: The character representing numerical constants.
+        variables: List of characters representing variables.
+
+    Returns:
+        expr: Sympy expression object in canonical form.
+        symbols_params: Tuple of enumerated constants.
+    """
+    x = [sp_symbols(s.strip("'")) for s in variables]
+    c = sp_symbols(constant)
+
+    expr, _ = _enumerate_constants(expr_str, constant)
+    expr = _simplify_constants(expr, c, x)[2][0][1]
+    expr, _ = _enumerate_constants(expr, constant)
+    expr = expand(expr)
+    expr = _simplify_constants(expr, c, x)[2][0][1]
+    # expr, _ = _enumerate_constants(expr, constant)
+    # expr = _simplify_constants(expr, c, x)[2][0][1]
+    return expr
+
+
+if __name__ == "__main__":
+    # Should simplify
+    expr = ["C", "+", "C" "*", "C", "+", "X_0", "*", "X_1", "/", "X_0"]
+    print(simplify(expr))
+
+    # Should raise an exception
+    expr = ["X_0", "*", "X_0", "^2"]
+    sl = SymbolLibrary.from_symbol_list(["+", "*", "-"], 1)
+    print(simplify(expr, symbol_library=sl))
