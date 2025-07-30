702049cfef7bdb4fa346ebe0fbfcb12434b69473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:49 +0000

    Add package niftyTradeCalendar/0.2.0/niftyTradeCalendar-0.2.0.tar.gz

---
+++ packages/niftyTradeCalendar/niftyTradeCalendar-0.2.0.tar.gz/niftyTradeCalendar-0.2.0/niftyTradeCalendar/__init__.py
@@ -0,0 +1,6 @@
+# niftyTradeCalendar/__init__.py
+"""
+niftyTradeCalendar - A utility to manage NSE holidays, trading days, and expiry dates.
+"""
+
+from .main import is_holiday, get_trading_days, get_weekly_expiry_dates
