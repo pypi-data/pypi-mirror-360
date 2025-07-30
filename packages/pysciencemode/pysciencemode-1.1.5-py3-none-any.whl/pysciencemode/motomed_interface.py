7b7f6b5d6b65a6092f606b9e81853591a91ba245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:27 +0000

    Add package pysciencemode/1.1.5/pysciencemode-1.1.5-py3-none-any.whl

---
+++ packages/pysciencemode/pysciencemode-1.1.5-py3-none-any.whl/pysciencemode/motomed_interface.py
@@ -0,0 +1,486 @@
+"""
+Motomed Interface class used to control and get data from Motomed while connected to the rehamove2.
+See ScienceMode2 - Description and protocol for more information.
+"""
+
+from .acks import (
+    get_motomed_mode_ack,
+    init_phase_training_ack,
+    start_phase_ack,
+    pause_phase_ack,
+    stop_phase_training_ack,
+    set_rotation_direction_ack,
+    set_speed_ack,
+    set_gear_ack,
+    start_basic_training_ack,
+    pause_basic_training_ack,
+    continue_basic_training_ack,
+    stop_basic_training_ack,
+    motomed_error_ack,
+)
+from .enums import Rehastim2Commands
+from .utils import packet_construction, signed_int
+
+from time import sleep
+import numpy as np
+
+
+class _Motomed:
+    """
+    Class to control the motomed
+    """
+
+    def __init__(self, rehastim_interface, show_log: bool = False):
+        """
+        Parameters
+        ----------
+        rehastim_interface: Stimulator
+            Stim interface object.
+        show_log: bool
+            If True, print the log of the communication.
+        """
+        self.gear = 0
+        self.body_training = 1
+        self.direction = 0
+        self.active = 0
+        self.passive_speed = 0  # tr/min
+        self.is_phase_initialize = False
+        self.training_side = 0
+        self.crank_orientation = 0
+        self.fly_wheel = 0
+        self.phase_variant = -1
+        self.spasm_detection = 0
+        self.last_phase_result = 0
+        self.is_phase_training = False
+        self.show_log = show_log
+        self.max_phase_result = 1
+        self.rehastim = rehastim_interface
+        self.is_phase_result = False
+
+    def _send_packet(self, cmd: str) -> (None, str):
+        """
+        Calls the methode that construct the packet according to the command.
+
+        Parameters
+        ----------
+        cmd: str
+            Command that will be sent.
+
+        Returns
+        -------
+            In the case of an InitAck, return the string 'InitAck'. None otherwise.
+        """
+        self.rehastim.motomed_done.wait()  # If the event is set, motomed last command is done next command can be sent
+        if cmd == "InitPhaseTraining":
+            packet = packet_construction(
+                self.rehastim.packet_count, "InitPhaseTraining", [self.body_training]
+            )
+            self.rehastim.motomed_done.clear()
+        elif cmd == "StartPhase":
+            self.is_phase_result = False
+            self.is_phase_training = True
+            data_command = [
+                self.phase_variant,
+                self.passive_speed,
+                self.gear,
+                self.direction,
+                self.fly_wheel,
+                self.spasm_detection,
+                self.training_side,
+                self.crank_orientation,
+            ]
+            packet = packet_construction(
+                self.rehastim.packet_count, "StartPhase", data_command
+            )
+            self.rehastim.motomed_done.clear()
+        elif cmd == "SetRotationDirection":
+            packet = packet_construction(
+                self.rehastim.packet_count, "SetRotationDirection", [self.direction]
+            )
+            self.rehastim.motomed_done.clear()
+        elif cmd == "SetSpeed":
+            packet = packet_construction(
+                self.rehastim.packet_count, "SetSpeed", [self.passive_speed]
+            )
+            self.rehastim.motomed_done.clear()
+        elif cmd == "SetGear":
+            packet = packet_construction(
+                self.rehastim.packet_count, "SetGear", [self.gear]
+            )
+            self.rehastim.motomed_done.clear()
+        elif cmd == "StartBasicTraining":
+            packet = packet_construction(
+                self.rehastim.packet_count, "StartBasicTraining", [self.body_training]
+            )
+            self.rehastim.motomed_done.clear()
+        else:
+            packet = packet_construction(self.rehastim.packet_count, cmd)
+        init_ack = self.rehastim.send_generic_packet(cmd, packet)
+        if init_ack:
+            return init_ack
+
+    def get_motomed_mode(self) -> str:
+        """
+        Get the mode of the motomed.
+
+        Returns
+        -------
+        The mode of the motomed in a string get_mot_mode_ack. Otherwise, raise an error.
+        """
+        self._send_packet("GetMotomedMode")
+        get_mot_mode_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if get_mot_mode_ack in [
+            "Transfer error",
+            "Busy error",
+            "Motomed busy",
+            "Motomed connection error",
+        ]:
+            raise RuntimeError("Error getting motomed mode : " + str(get_mot_mode_ack))
+        else:
+            return get_mot_mode_ack
+
+    def init_phase_training(self, arm_training: bool = True):
+        """
+        Initialize the phase training.
+
+        Parameters
+        ----------
+        arm_training: bool
+            If True, the training is for the arm. If False, the training is for the leg.
+        """
+        self.body_training = 1 if arm_training else 0
+        self._send_packet("InitPhaseTraining")
+        init_phase_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if init_phase_ack != "Phase training initialized":
+            raise RuntimeError("Error initializing phase : " + str(init_phase_ack))
+        self.is_phase_initialize = True
+
+    def start_phase(
+        self,
+        go_forward: bool = True,
+        active: bool = True,
+        symmetry_training: bool = False,
+        motomedmax_game: bool = False,
+        gear: int = 0,
+        speed: int = 0,
+        fly_wheel: int = 0,
+        spasm_detection: bool = False,
+        direction_restoration: bool = False,
+        training_side: str = "both",
+        crank_symetric: bool = False,
+    ):
+        """
+        Start the phase training.
+
+        Parameters
+        ----------
+        go_forward: bool
+            If True, the phase will be done in the forward direction.
+             If False, the phase will be done in the backward direction.
+        active: bool
+            If True, the phase will be done in active mode.
+        symmetry_training: bool
+            If True, the phase will be done in symmetry training mode.
+        motomedmax_game: bool
+            If True, the phase will be done in motomedmax game mode.
+        gear: int
+            Gear of the motomed.
+        speed: int
+            Speed of the motomed.
+        fly_wheel: int
+            Fly wheel of the motomed.
+        spasm_detection: bool
+            If True, the phase will be done with spasm detection.
+        direction_restoration: bool
+            If True, the phase will be done with direction restoration after spasm.
+        training_side: str
+            Side of the training. Can be "left", "right" or "both".
+        crank_symetric: bool
+            If True, the phase will be done with a symetric crank.
+        """
+        if active:
+            self.phase_variant = 0
+        elif not active and not symmetry_training and not motomedmax_game:
+            self.phase_variant = 1
+        elif symmetry_training:
+            self.phase_variant = 2
+        elif motomedmax_game:
+            self.phase_variant = 3
+
+        if self.phase_variant not in [0, 1, 2, 3]:
+            raise RuntimeError(
+                "Please chose one option between 'active' 'symmetry_training' and 'Motomedmax_game'."
+            )
+
+        if not self.is_phase_initialize:
+            raise RuntimeError("Phase not initialized.")
+
+        self.direction = 1 if go_forward else 0
+        if gear > 20 or gear < 0:
+            raise RuntimeError("Gear must be in [0, 20].")
+        else:
+            self.gear = gear
+
+        if speed > 90 or speed < 0:
+            raise RuntimeError("Speed must be in [0, 90].")
+        else:
+            self.passive_speed = speed
+
+        if fly_wheel > 100 or fly_wheel < 0:
+            raise RuntimeError("fly_wheel must be in [0, 100].")
+        else:
+            self.fly_wheel = fly_wheel
+        if not spasm_detection:
+            self.spasm_detection = 0
+            if direction_restoration:
+                raise RuntimeError(
+                    "You can use direction restoration only if spasm detection is active."
+                )
+        else:
+            if direction_restoration:
+                self.spasm_detection = 2
+            else:
+                self.spasm_detection = 1
+        if training_side == "both":
+            self.training_side = 0
+        elif training_side == "left":
+            self.training_side = 1
+        elif training_side == "right":
+            self.training_side = 2
+        else:
+            raise RuntimeError(
+                "Training side must be 'both', 'right' or 'left'."
+                f"You have : {training_side}."
+            )
+        self.crank_orientation = 1 if crank_symetric else 0
+        self._send_packet("StartPhase")
+        start_phase_train_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if (
+            start_phase_train_ack
+            != "Start phase training / change phase sent to MOTOmed"
+        ):
+            raise RuntimeError("Error starting phase : " + str(start_phase_train_ack))
+
+    def _pause_phase_training(self):
+        """
+        Pause the phase training.
+        """
+        self._send_packet("PausePhase")
+        ack = self._calling_ack(self.rehastim._get_last_ack())
+        if ack != "Start pause sent to MOTOmed":
+            raise RuntimeError("Error starting phase : " + str(ack))
+
+    def _stop_phase_training(self):
+        """
+        Stop the phase training.
+        """
+        self._send_packet("StopPhaseTraining")
+        stop_phase_ack = self._calling_ack(self.rehastim._get_last_ack())
+        self.is_phase_training = False
+        if stop_phase_ack == "PhaseResult":
+            print("Result of the phase available.")
+        elif stop_phase_ack != "Stop phase training sent to MOTOmed":
+            raise RuntimeError("Error starting phase : " + str(stop_phase_ack))
+
+    def _continue_phase_training(self):
+        """
+        Continue the phase training.
+        """
+        self._send_packet("ContinuePhaseTraining")
+        continue_phase_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if continue_phase_ack != "Stop phase training sent to MOTOmed":
+            raise RuntimeError("Error starting phase : " + str(continue_phase_ack))
+
+    def stop_training(self):
+        """
+        Stop the training.
+        """
+        (
+            self._stop_phase_training()
+            if self.is_phase_training
+            else self._stop_basic_training()
+        )
+
+    def pause_training(self):
+        """
+        Pause the training.
+        """
+        (
+            self._pause_phase_training()
+            if self.is_phase_training
+            else self._pause_basic_training()
+        )
+
+    def continue_training(self):
+        """
+        Continue the training.
+        """
+        (
+            self._continue_phase_training()
+            if self.is_phase_training
+            else self._continue_basic_training()
+        )
+
+    def start_basic_training(self, arm_training: bool = True):
+        """
+        Start the basic training.
+
+        Parameters
+        ----------
+        arm_training: bool
+            If True, the training will be done with the arm.
+
+        Returns
+        -------
+        None
+        """
+        self.body_training = 1 if arm_training else 0
+        self._send_packet("StartBasicTraining")
+        start_basic_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if start_basic_ack != "Sent start basic training to MOTOmed":
+            raise RuntimeError("Error starting phase : " + str(start_basic_ack))
+
+    def _stop_basic_training(self):
+        """
+        Stop the basic training.
+        """
+        self._send_packet("StopBasicTraining")
+        stop_basic_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if stop_basic_ack != "Sent stop basic training to MOTOmed":
+            raise RuntimeError("Error starting phase : " + str(stop_basic_ack))
+
+    def _pause_basic_training(self):
+        """
+        Pause the basic training.
+        """
+        self._send_packet("PauseBasicTraining")
+        pause_basic_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if pause_basic_ack != "Sent basic pause to MOTOmed":
+            raise RuntimeError("Error pause phase : " + str(pause_basic_ack))
+
+    def _continue_basic_training(self):
+        """
+        Continue the basic training.
+        """
+        self._send_packet("ContinueBasicTraining")
+        start_basic_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if start_basic_ack != "Sent continue basic training to MOTOmed":
+            raise RuntimeError("Error starting phase : " + str(start_basic_ack))
+
+    def set_direction(self, go_forward: bool = True):
+        """
+        Set the direction of the training.
+        """
+        self.direction = 1 if go_forward else 0
+        self._send_packet("SetRotationDirection")
+        rotation_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if rotation_ack != "Sent rotation direction to MOTOmed":
+            raise RuntimeError("Error starting phase : " + str(rotation_ack))
+
+    def set_speed(self, passive_speed: int):
+        """
+        Set the speed of the training.
+        """
+        self.passive_speed = passive_speed
+        self._send_packet("SetSpeed")
+        speed_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if speed_ack != "Sent speed to MOTOmed":
+            raise RuntimeError("Error sending speed : " + str(speed_ack))
+
+    def set_gear(self, gear: int):
+        """
+        Set the gear of the training.
+        """
+        self.gear = gear
+        self._send_packet("SetGear")
+        gear_ack = self._calling_ack(self.rehastim._get_last_ack())
+        if gear_ack != "Set Gear to MOTOmed":
+            raise RuntimeError("Error sending gear : " + str(gear_ack))
+
+    def _calling_ack(self, packet: bytes) -> str:
+        """
+        Check for motomed response and return it.
+
+        Parameters
+        ----------
+        packet: bytes
+            Packet which needs to be processed.
+
+        Returns
+        -------
+            A string which is the message corresponding to the processing of the packet.
+        """
+        # self.rehastim.event_ack.wait()
+
+        if packet == "InitAck" or packet[6] == 1:
+            return "InitAck"
+        elif packet[6] == Rehastim2Commands["GetMotomedModeAck"].value:
+            return get_motomed_mode_ack(packet)
+        elif packet[6] == Rehastim2Commands["InitPhaseTrainingAck"].value:
+            return init_phase_training_ack(packet)
+        elif packet[6] == Rehastim2Commands["StartPhaseAck"].value:
+            return start_phase_ack(packet)
+        elif packet[6] == Rehastim2Commands["PausePhaseAck"].value:
+            return pause_phase_ack(packet)
+        elif packet[6] == Rehastim2Commands["StopPhaseTrainingAck"].value:
+            return stop_phase_training_ack(packet)
+        elif packet[6] == Rehastim2Commands["SetRotationDirectionAck"].value:
+            return set_rotation_direction_ack(packet)
+        elif packet[6] == Rehastim2Commands["SetSpeedAck"].value:
+            return set_speed_ack(packet)
+        elif packet[6] == Rehastim2Commands["SetGearAck"].value:
+            return set_gear_ack(packet)
+        elif packet[6] == Rehastim2Commands["StartBasicTrainingAck"].value:
+            return start_basic_training_ack(packet)
+        elif packet[6] == Rehastim2Commands["PauseBasicTrainingAck"].value:
+            return pause_basic_training_ack(packet)
+        elif packet[6] == Rehastim2Commands["ContinueBasicTrainingAck"].value:
+            return continue_basic_training_ack(packet)
+        elif packet[6] == Rehastim2Commands["StopBasicTrainingAck"].value:
+            return stop_basic_training_ack(packet)
+        elif packet[6] == Rehastim2Commands["MotomedCommandDone"].value:
+            return stop_basic_training_ack(packet)
+        elif packet[6] == Rehastim2Commands["MotomedError"].value:
+            return motomed_error_ack(signed_int(packet[7:8]))
+        else:
+            raise RuntimeError("Error packet : not understood")
+
+    def get_angle(self):
+        """
+        Get the actual angle of the motomed (left side).
+
+        Returns
+        -------
+            The angle of the training.
+        """
+        return self.rehastim.get_angle()
+
+    def get_speed(self):
+        """
+        Get the actual speed of the motomed.
+
+        Returns
+        -------
+            The speed of the motomed.
+        """
+        return self.rehastim.get_speed()
+
+    def get_torque(self):
+        """
+        Get the actual torque of the motomed.
+
+        Returns
+        -------
+            The torque of the motomed.
+        """
+        return self.rehastim.get_torque()
+
+    def get_phase_result(self):
+        """
+        Get the actual torque of the motomed.
+
+        Returns
+        -------
+            The torque of the motomed.
+        """
+        return self.rehastim.get_phase_result()
---
+++ packages/pysciencemode/pysciencemode-1.1.5-py3-none-any.whl/pysciencemode/utils.py
@@ -0,0 +1,262 @@
+import crccheck
+from .enums import ErrorCode, Rehastim2Commands
+
+"""
+This code provides utility functions for working with the Rehastim device, including packet construction and data 
+validation. It also includes functions to convert and check various parameters used in Rehastim communication.
+"""
+
+
+def signed_int(packet: bytes) -> int:
+    """
+    Converts a signed int from the packet received from the Rehastim.
+
+    Parameters
+    ----------
+    packet: bytes
+        Packet received from the Rehastim.
+    Returns
+    -------
+    signed_int: int
+        Signed int converted.
+    """
+    return int.from_bytes(packet, "big", signed=True)
+
+
+def check_stimulation_interval(stimulation_interval: int = None):
+    """
+    Checks if the stimulation interval is within limits.
+
+    Parameters
+    ----------
+    stimulation_interval: int
+        Stimulation interval to check. If out of limits, raise a ValueError.
+
+    """
+    if stimulation_interval:
+        if stimulation_interval < 8 or stimulation_interval > 1025:
+            raise ValueError(
+                "Error : Stimulation interval [8,1025]. Stimulation given : %s"
+                % stimulation_interval
+            )
+
+
+def check_inter_pulse_interval(inter_pulse_interval: int = None):
+    """
+    Checks if the "inter pulse interval" is within limits.
+
+    Parameters
+    ----------
+    inter_pulse_interval: int, optional
+        Inter pulse interval to check. If out of limits, raise a ValueError.
+
+    """
+    if inter_pulse_interval:
+        if inter_pulse_interval < 2 or inter_pulse_interval > 129:
+            raise ValueError(
+                "Error : Inter pulse interval [2,129], given : %s"
+                % inter_pulse_interval
+            )
+
+
+def check_low_frequency_factor(low_frequency_factor: int = None):
+    """
+    Checks if the low frequency factor is within limits.
+
+    Parameters
+    ----------
+    low_frequency_factor: int, optional
+        Low frequency factor to check. If out of limits, raise a ValueError.
+    """
+    if low_frequency_factor:
+        if low_frequency_factor < 0 or low_frequency_factor > 7:
+            raise ValueError(
+                "Error : Low frequency factor [0,7], given : %s" % low_frequency_factor
+            )
+
+
+def check_unique_channel(list_channels: list = None) -> bool:
+    """
+    Checks if there is not 2 times the same channel in the list given.
+
+    Parameters
+    ----------
+    list_channels: list[Channel], optional
+        Contains a list of channel.
+
+    Returns
+    -------
+    True if all channels are unique, False and print a warning if not.
+    """
+    if list_channels:
+        active_channel = []
+        for i in range(len(list_channels)):
+            if list_channels[i].get_no_channel() in active_channel:
+                print(
+                    "Warning : 2 channel no%s" % list_channels[i].get_no_channel()
+                    + " in list_channels given. The first one given will be used."
+                )
+                list_channels.pop(i)
+                return False
+            else:
+                active_channel.append(list_channels[i].get_no_channel())
+        return True
+
+
+def check_list_channel_order(list_channels):
+    """
+    Checks if the channels in the list_channels given are ordered.
+
+    Parameters
+    ----------
+    list_channels: list[Channel]
+        Contains the channels. Raises a RuntimeError if not ordered.
+    """
+    number_previous_channel = 0
+    for i in range(len(list_channels)):
+        if list_channels[i].get_no_channel() < number_previous_channel:
+            raise RuntimeError(
+                "Error: channels in list_channels given are not in order."
+            )
+        number_previous_channel = list_channels[i].get_no_channel()
+
+
+def calc_electrode_number(
+    list_channels: list, enable_low_frequency: bool = False
+) -> int:
+    """
+    When enable_low_frequency = False :
+    Calculates the number corresponding to which electrode is activated.
+    During the initialisation, the computer needs to tell the Rehastim which channel needs to be activated. It is
+    done through the addition of 2 pow the number of the channel.
+    For example if the channel 1 and 4 needs to be activated, electrode_number = 2**1 + 2**4
+
+    When enable_low_frequency = True :
+    Calculates the number corresponding to which electrode has the low_frequency_factor enabled.
+
+    Parameters
+    ----------
+    list_channels: list[Channel]
+        Contains the channels that will be activated.
+    enable_low_frequency: bool
+        Choose whether the number correspond to active channel (False) or correspond to channel with low frequency
+        factor enabled (True).
+
+    Returns
+    -------
+    electrode_number: int
+        Electrode number calculated.
+    """
+    electrode_number = 0
+    for i in range(len(list_channels)):
+        if enable_low_frequency:
+            if list_channels[i].get_enable_low_frequency():
+                electrode_number += 2 ** (list_channels[i].get_no_channel() - 1)
+        if not enable_low_frequency:
+            electrode_number += 2 ** (list_channels[i].get_no_channel() - 1)
+    return electrode_number
+
+
+def packet_construction(
+    packet_count: int, packet_type: str, packet_data: list = None
+) -> bytes:
+    """
+    Constructs the packet which will be sent to the Rehastim.
+
+    Parameters
+    ----------
+    packet_count: int
+        Correspond to the number of packet sent to the Rehastim.
+    packet_type: str
+        Correspond to the command that will be sent.
+    packet_data: list
+        Contain the data of the packet.
+
+    Returns
+    -------
+    packet_construct: bytes
+        Packet constructed which will be sent.
+    """
+
+    start_byte = 0xF0
+    stop_byte = 0x0F
+    stuffing_byte = 0x81
+
+    packet = [start_byte]
+    packet_command = Rehastim2Commands[packet_type].value
+    packet_payload = [packet_count, packet_command]
+    packet_payload = _stuff_packet_byte(packet_payload)
+    if packet_data is not None:
+        packet_data = _stuff_packet_byte(packet_data, command_data=True)
+        packet_payload += packet_data
+
+    checksum = crccheck.crc.Crc8.calc(packet_payload)
+    checksum = _stuff_byte(checksum)
+    data_length = _stuff_byte(len(packet_payload))
+
+    packet = (
+        packet
+        + [stuffing_byte]
+        + [checksum]
+        + [stuffing_byte]
+        + [data_length]
+        + packet_payload
+        + [stop_byte]
+    )
+    return b"".join([byte.to_bytes(1, "little") for byte in packet])
+
+
+def _stuff_packet_byte(packet: list, command_data: bool = False) -> list:
+    """
+    Stuffs each byte if necessary of the packet.
+
+    Parameters
+    ----------
+    packet: list
+        Packet containing the bytes that will be checked and stuffed if necessary.
+    command_data: bool
+        True if the packet is a command data, False if not.
+    Returns
+    -------
+    packet: list
+        Packet returned with stuffed byte.
+    """
+    stuffed_bytes = [240, 15, 129, 85, 10]
+    stuffing_byte = 0x81
+
+    if command_data:
+        packet_tmp = []
+        for i in range(len(packet)):
+            if packet[i] in stuffed_bytes:
+                packet_tmp = packet_tmp + [stuffing_byte, _stuff_byte(packet[i])]
+            else:
+                packet_tmp.append(packet[i])
+        return packet_tmp
+    else:
+        for i in range(len(packet)):
+            if packet[i] in stuffed_bytes:
+                packet[i] = _stuff_byte(packet[i])
+        return packet
+
+
+def _stuff_byte(byte: int) -> int:
+    """
+    Stuffs the byte given.
+    Parameters
+    ----------
+    byte: int
+        Byte which needs to be stuffed.
+    Returns
+    -------
+    byte_stuffed: int
+        The byte stuffed.
+    """
+    stuffing_key = 0x55
+
+    return (byte & ~stuffing_key) | (~byte & stuffing_key)
+
+
+def generic_error_check(ack_object):
+    error_code = ErrorCode(ack_object.result)
+    if error_code.message:
+        raise ValueError(error_code.message)
