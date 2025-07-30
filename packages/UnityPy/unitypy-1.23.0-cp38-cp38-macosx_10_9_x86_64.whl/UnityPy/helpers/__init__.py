72b22c26a62369f30dd08b367f52924d521795f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:42 +0000

    Add package UnityPy/1.23.0/unitypy-1.23.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/UnityPy/unitypy-1.23.0-cp38-cp38-macosx_10_9_x86_64.whl/UnityPy/helpers/__init__.py
@@ -0,0 +1,9 @@
+from . import ArchiveStorageManager, CompressionHelper, ContainerHelper, ImportHelper, TypeTreeHelper
+
+__all__ = [
+    "ArchiveStorageManager",
+    "CompressionHelper",
+    "ImportHelper",
+    "TypeTreeHelper",
+    "ContainerHelper",
+]
