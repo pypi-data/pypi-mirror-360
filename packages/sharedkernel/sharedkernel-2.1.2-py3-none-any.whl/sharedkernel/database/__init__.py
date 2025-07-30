54edd4b346ab39c3435221132ea14006f2a1f9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:16 +0000

    Add package sharedkernel/2.1.2/sharedkernel-2.1.2-py3-none-any.whl

---
+++ packages/sharedkernel/sharedkernel-2.1.2-py3-none-any.whl/sharedkernel/database/__init__.py
@@ -0,0 +1,1 @@
+# from .mongo_repository_base import MongoRepositoryBase
---
+++ packages/sharedkernel/sharedkernel-2.1.2-py3-none-any.whl/sharedkernel/objects/__init__.py
@@ -0,0 +1,4 @@
+from .base_document import BaseDocument
+from .jwt_model import JwtModel
+from .result import Result
+from .json_string_model import JsonStringModel
