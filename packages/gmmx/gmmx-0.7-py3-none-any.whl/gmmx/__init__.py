93e7a99349afdb81773279755850afa1aeef722b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:53 +0000

    Add package gmmx/0.7/gmmx-0.7-py3-none-any.whl

---
+++ packages/gmmx/gmmx-0.7-py3-none-any.whl/gmmx/__init__.py
@@ -0,0 +1,4 @@
+from .fit import EMFitter as EMFitter
+from .fit import EMFitterResult as EMFitterResult
+from .gmm import GaussianMixtureModelJax as GaussianMixtureModelJax
+from .gmm import GaussianMixtureSKLearn as GaussianMixtureSKLearn
