6681d03352f76ada3f07eadf7ef59a2d0f185db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:05:57 +0000

    Add package pypinindia/0.1.8/pypinindia-0.1.8.tar.gz

---
+++ packages/pypinindia/pypinindia-0.1.8.tar.gz/pypinindia-0.1.8/pinin/__init__.py
@@ -0,0 +1,62 @@
+"""
+pypinindia - Indian Pincodes and related Information
+
+A modern Python library for Indian pincode lookup and geographical information.
+
+This library provides comprehensive pincode data lookup functionality for:
+- State, district, and taluk information
+- Office names and types
+- Delivery status information
+- Search and filtering capabilities
+
+Usage:
+    from pinin import get_pincode_info, get_state, PincodeData
+    
+    # Quick pincode lookup
+    info = get_pincode_info("110001")
+    state = get_state("110001")
+    
+    # Using PincodeData class
+    pincode_data = PincodeData()
+    district = pincode_data.get_district("110001")
+"""
+
+from .core import (
+    PincodeData,
+    get_pincode_info,
+    get_state,
+    get_district,
+    get_taluk,
+    get_offices,
+    search_by_state,
+    search_by_district,
+    get_states,
+    get_districts,
+)
+from .exceptions import (
+    PininError,
+    InvalidPincodeError,
+    DataNotFoundError,
+    DataLoadError,
+)
+
+__version__ = "0.1.7"
+__author__ = "Raja CSP Raman"
+__email__ = "raja.csp@gmail.com"
+
+__all__ = [
+    "PincodeData",
+    "get_pincode_info",
+    "get_state",
+    "get_district",
+    "get_taluk",
+    "get_offices",
+    "search_by_state",
+    "search_by_district",
+    "get_states",
+    "get_districts",
+    "PininError",
+    "InvalidPincodeError",
+    "DataNotFoundError",
+    "DataLoadError",
+]
