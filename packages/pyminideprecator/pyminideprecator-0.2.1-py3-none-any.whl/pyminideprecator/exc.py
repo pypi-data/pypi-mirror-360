90421d07de5375bc8ddae6f7ec0a153c352c7537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:35 +0000

    Add package pyminideprecator/0.2.1/pyminideprecator-0.2.1-py3-none-any.whl

---
+++ packages/pyminideprecator/pyminideprecator-0.2.1-py3-none-any.whl/pyminideprecator/exc.py
@@ -0,0 +1,15 @@
+"""pyminideprecator exceptions."""
+
+
+class DeprecatedError(Exception):
+    """
+    Exception raised when deprecated functions is accessed beyond its removal version.
+
+    This error indicates that the code has reached or exceeded the version where
+    the deprecated functionality is scheduled for removal, and attempts to use it
+    should be treated as errors rather than warnings.
+
+    Attributes:
+        message: Explanation of the deprecation error
+
+    """
