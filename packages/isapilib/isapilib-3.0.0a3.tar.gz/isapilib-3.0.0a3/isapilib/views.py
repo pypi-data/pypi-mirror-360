131251ceb125431aad10395d7e337dcfda67c2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:28 +0000

    Add package isapilib/3.0.0a3/isapilib-3.0.0a3.tar.gz

---
+++ packages/isapilib/isapilib-3.0.0a3.tar.gz/isapilib-3.0.0a3/isapilib/views.py
@@ -0,0 +1,51 @@
+from django.conf import settings
+from django.http import HttpResponse
+
+
+def index(request):
+    interfaz_name = getattr(settings, 'INTERFAZ_NAME')
+    url_doc = getattr(settings, 'URL_DOCUMENTATION', '')
+    if interfaz_name: interfaz_name = f'({interfaz_name})'
+
+    return HttpResponse(f'''
+        <style>
+            * {{
+                margin: 0;
+                padding: 0;
+                font-family: 'Roboto', sans-serif;
+                color: #dfe2e6;
+            }}
+
+            body {{
+                background-image: radial-gradient(circle, #004b75, #001c26);
+                display: flex;
+            }}
+
+            div {{
+                margin: auto;
+                display: flex;
+                font-size: 10vw;
+                user-select: none;
+                text-align: center;
+                flex-direction: column;
+            }}
+
+            a {{
+                opacity: 0.8;
+                font-size: 1vw;
+                text-decoration: none;
+                transition: all 0.3s ease-in-out;
+            }}
+
+            a:hover {{
+                transform: scale(1.3);
+            }}
+        </style>
+
+        <div>
+            <p>Intelisis {interfaz_name}</p>
+            {
+    f'<a href="{url_doc}">Documentation</a>' if url_doc != '' else ''
+    }
+        </div>
+    ''')
