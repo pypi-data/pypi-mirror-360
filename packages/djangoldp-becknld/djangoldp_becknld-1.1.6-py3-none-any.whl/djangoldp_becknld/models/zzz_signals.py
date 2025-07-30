64e9b5ba5666e53a2d7af12d231cd376990338b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:40 +0000

    Add package djangoldp-becknld/1.1.6/djangoldp_becknld-1.1.6-py3-none-any.whl

---
+++ packages/djangoldp-becknld/djangoldp_becknld-1.1.6-py3-none-any.whl/djangoldp_becknld/models/zzz_signals.py
@@ -0,0 +1,41 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from djangoldp_becknld.consts import BAP_URI, BPP_URI, IS_BAP, IS_BPP
+from djangoldp_becknld.models.transaction import Transaction
+
+
+def _get_transaction_uri(server_uri, transaction_id):
+    if not server_uri:
+        return None
+
+    if server_uri and server_uri[-1] != "/":
+        server_uri += "/"
+
+    return server_uri + "transactions/" + transaction_id + "/"
+
+
+@receiver(post_save, sender=Transaction)
+def handle_transaction_creation(sender, instance, created, **kwargs):
+    if created:
+        if IS_BAP:
+            if not instance.bap_uri:
+                # Reverse case only useful for gateways
+                instance.bap_uri = instance.urlid
+            if not instance.bpp_uri:
+                instance.bpp_uri = _get_transaction_uri(
+                    BPP_URI, instance.transaction_id
+                )
+
+        if IS_BPP:
+            if not instance.bpp_uri:
+                instance.bpp_uri = instance.urlid
+            if not instance.bap_uri:
+                instance.bpp_uri = _get_transaction_uri(
+                    BAP_URI, instance.transaction_id
+                )
+
+        instance.save()
+
+        # if IS_BAP and not IS_BPP:
+        # TODO: Create a copy in BPP with same transaction_id
