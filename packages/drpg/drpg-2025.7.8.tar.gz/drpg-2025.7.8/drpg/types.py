139aad52b8e451b7f2c6baa20ffceabce3782709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:56 +0000

    Add package drpg/2025.7.8/drpg-2025.7.8.tar.gz

---
+++ packages/drpg/drpg-2025.7.8.tar.gz/drpg-2025.7.8/drpg/types.py
@@ -0,0 +1,36 @@
+from typing import TypedDict
+
+
+class TokenResponse(TypedDict):
+    token: str
+    refreshToken: str
+    refreshTokenTTL: int
+
+
+class PrepareDownloadUrlResponse(TypedDict):
+    url: str
+    status: str
+
+
+class Product(TypedDict):
+    productId: str
+    publisher: "Publisher"
+    name: str
+    orderProductId: int
+    fileLastModified: str  # ISO format
+    files: list["DownloadItem"]
+
+
+class Publisher(TypedDict):
+    name: str
+
+
+class DownloadItem(TypedDict):
+    index: int
+    filename: str
+    checksums: list["Checksum"]
+
+
+class Checksum(TypedDict):
+    checksum: str
+    checksumDate: str  # ISO format
