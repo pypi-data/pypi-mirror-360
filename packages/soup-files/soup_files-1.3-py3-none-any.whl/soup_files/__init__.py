1441603c0660a9d2ef77eab9e3fd3eb91a92ae86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:23 +0000

    Add package soup-files/1.3/soup_files-1.3-py3-none-any.whl

---
+++ packages/soup-files/soup_files-1.3-py3-none-any.whl/soup_files/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from .version import __version__
+from .progress import ProgressBarAdapter, ProgressBarSimple, ABCProgressBar
+from .files import (
+    File,
+    Directory,
+    JsonData,
+    JsonConvert,
+    InputFiles,
+    LibraryDocs,
+    UserFileSystem,
+    UserAppDir,
+    KERNEL_TYPE,
+)
+
