bbaa825e9b92c1e7cb5724ea4349c4d962daf7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:46 +0000

    Add package plain.sessions/0.23.0/plain_sessions-0.23.0.tar.gz

---
+++ packages/plain.sessions/plain_sessions-0.23.0.tar.gz/plain_sessions-0.23.0/plain/sessions/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+from plain import models
+from plain.models import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Session",
+            fields=[
+                (
+                    "session_key",
+                    models.CharField(
+                        max_length=40,
+                        primary_key=True,
+                    ),
+                ),
+                ("session_data", models.TextField()),
+                (
+                    "expire_date",
+                    models.DateTimeField(),
+                ),
+            ],
+        ),
+    ]
