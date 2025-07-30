f7e294859dffbae5c3fc86be2183648defb2e900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:20 +0000

    Add package joulescope/1.3.1/joulescope-1.3.1-cp310-cp310-win_amd64.whl

---
+++ packages/joulescope/joulescope-1.3.1-cp310-cp310-win_amd64.whl/joulescope/version.py
@@ -0,0 +1,13 @@
+__version__ = "1.3.1"
+
+__title__ = "joulescope"
+__description__ = 'Joulescope™ host driver and utilities'
+__url__ = 'https://joulescope.readthedocs.io'
+__author__ = 'Jetperch LLC'
+__author_email__ = 'joulescope-dev@jetperch.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018-2025 Jetperch LLC'
+
+
+__all__ = ['__version__', '__title__', '__description__', '__url__',
+           '__author__', '__author_email__', '__license__', '__copyright__']
