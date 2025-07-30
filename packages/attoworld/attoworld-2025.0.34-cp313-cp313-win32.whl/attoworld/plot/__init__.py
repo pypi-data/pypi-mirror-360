a604f084b507d5446cd4a164a7542dab822cd219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:11 +0000

    Add package attoworld/2025.0.34/attoworld-2025.0.34-cp313-cp313-win32.whl

---
+++ packages/attoworld/attoworld-2025.0.34-cp313-cp313-win32.whl/attoworld/plot/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module will contain functions for plotting, and for formatting plots, with the goal of
+more easily obtaining a consistent style across plots made by different co-authors.
+"""
+
+from .plot import showmo, set_style, label_letter, Char
+
+__all__ = ["showmo", "set_style", "label_letter", "Char"]
