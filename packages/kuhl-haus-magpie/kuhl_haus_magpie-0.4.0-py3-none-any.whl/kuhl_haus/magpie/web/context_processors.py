4f8b87df99f20582210201e642c8f2603cbdd87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:14 +0000

    Add package kuhl-haus-magpie/0.4.0/kuhl_haus_magpie-0.4.0-py3-none-any.whl

---
+++ packages/kuhl-haus-magpie/kuhl_haus_magpie-0.4.0-py3-none-any.whl/kuhl_haus/magpie/web/context_processors.py
@@ -0,0 +1,34 @@
+
+def version_info(request):
+    """
+    Add version information to the template context
+    """
+    from os import environ
+    from django.core.cache import cache
+
+    result = cache.get('version_info')
+    if result is None:
+        result = {
+            'version_info': {
+                'image_version': environ.get('IMAGE_VERSION'),
+                'container_image': environ.get('CONTAINER_IMAGE'),
+            }
+        }
+        cache.set('version_info', result, 300)
+    return result
+
+
+def domain_info(request):
+    from os import environ
+    from django.core.cache import cache
+
+    result = cache.get('domain_info')
+    if result is None:
+        result = {
+            'MAGPIE_DOMAIN': environ.get('MAGPIE_DOMAIN'),
+            'FLOWER_DOMAIN': environ.get('FLOWER_DOMAIN'),
+            'LANGFLOW_DOMAIN': environ.get('LANGFLOW_DOMAIN'),
+        }
+        cache.set('domain_info', result, 300)
+    return result
+
