53a9d31296ad4a10b3adf5308b8c3e56d25a68c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:04 +0000

    Add package llamate/0.1.7/llamate-0.1.7.tar.gz

---
+++ packages/llamate/llamate-0.1.7.tar.gz/llamate-0.1.7/llamate/embedder.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+import numpy as np
+
+class OpenAIEmbedder:
+    def __init__(self, model="text-embedding-3-large"):
+        self.client = OpenAI()
+        self.model = model
+
+    def embed(self, text: str):
+        response = self.client.embeddings.create(
+            model=self.model,
+            input=text
+        )
+        return np.array(response.data[0].embedding).astype("float32")
