3537746019b13d4b72d5095649061ca38a7c941b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:16 +0000

    Add package autoomics/0.1.8/autoomics-0.1.8.tar.gz

---
+++ packages/autoomics/autoomics-0.1.8.tar.gz/autoomics-0.1.8/autoomics/modules/__init__.py
@@ -0,0 +1,4 @@
+# autoomics/modules/__init__.py
+
+from .detect_data import detect_data_type
+from .report_generator import add_scrna_section
---
+++ packages/autoomics/autoomics-0.1.8.tar.gz/autoomics-0.1.8/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+
+class CustomInstallCommand(install):
+    def run(self):
+        # Appel à la commande d'installation classique
+        install.run(self)
+
+        # Appel de ton script après l'installation
+        subprocess.call(['bash', 'setup.sh'])
+
+setup(
+    name='autoomics',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').readlines(),
+    author='Jordan BABADOUDOU',
+    author_email='jordan.babadoudou@umontreal.ca',
+    description='Un outil pour faire du multi-omique analyse (bulk seq, rnaseq, chip seq, atac seq)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TonNom/mon_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'autoomics = autoomics.main:main',
+        ],
+    },
+    cmdclass={'install': CustomInstallCommand},
+)
