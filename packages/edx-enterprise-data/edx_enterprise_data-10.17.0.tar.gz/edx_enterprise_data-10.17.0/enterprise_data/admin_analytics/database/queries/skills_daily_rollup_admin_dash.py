6409b3fab2922e595524cc210af5324ab9c38cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:25 +0000

    Add package edx-enterprise-data/10.17.0/edx_enterprise_data-10.17.0.tar.gz

---
+++ packages/edx-enterprise-data/edx_enterprise_data-10.17.0.tar.gz/edx_enterprise_data-10.17.0/enterprise_data/admin_analytics/database/queries/skills_daily_rollup_admin_dash.py
@@ -0,0 +1,149 @@
+"""
+Module containing queries for the skills_daily_rollup_admin_dash table.
+"""
+from ..query_filters import QueryFilters
+
+
+class SkillsDailyRollupAdminDashQueries:
+    """
+    Queries related to the skills_daily_rollup_admin_dash table.
+    """
+    @staticmethod
+    def get_top_skills():
+        """
+        Get the query to fetch the top skills for an enterprise customer.
+        """
+        return """
+            SELECT
+                skill_name,
+                skill_type,
+                SUM(enrolls) AS enrolls,
+                SUM(completions) AS completions
+            FROM
+                skills_daily_rollup_admin_dash
+            WHERE
+                enterprise_customer_uuid=%(enterprise_customer_uuid)s AND
+                date BETWEEN %(start_date)s AND %(end_date)s
+            GROUP BY
+                skill_name, skill_type
+            ORDER BY
+                enrolls DESC, completions DESC;
+        """
+
+    @staticmethod
+    def get_top_skills_by_enrollment():
+        """
+        Get the query to fetch the top skills by enrollment for an enterprise customer.
+        """
+        return """
+            WITH TopSkills AS (
+                -- Get top 10 skills by total enrollments
+                SELECT
+                    skill_name,
+                    SUM(enrolls) AS total_enrollment_count
+                FROM
+                    skills_daily_rollup_admin_dash
+                WHERE
+                    enterprise_customer_uuid=%(enterprise_customer_uuid)s
+                    AND date BETWEEN %(start_date)s AND %(end_date)s
+                GROUP BY
+                    skill_name
+                ORDER BY
+                    total_enrollment_count DESC
+                LIMIT 10
+            )
+            SELECT
+                sd.skill_name,
+                CASE
+                    WHEN sd.primary_subject_name IN (
+                        'business-management', 'computer-science',
+                        'data-analysis-statistics', 'engineering', 'communication'
+                    ) THEN sd.primary_subject_name
+                    ELSE 'other'
+                END AS subject_name,
+                SUM(sd.enrolls) AS count
+            FROM
+                skills_daily_rollup_admin_dash AS sd
+            JOIN
+                TopSkills AS ts ON sd.skill_name = ts.skill_name
+            WHERE
+                sd.enterprise_customer_uuid=%(enterprise_customer_uuid)s
+                AND date BETWEEN %(start_date)s AND %(end_date)s
+            GROUP BY
+                sd.skill_name, subject_name
+            ORDER BY
+                total_enrollment_count DESC;
+        """
+
+    @staticmethod
+    def get_top_skills_by_completion():
+        """
+        Get the query to fetch the top skills by completion for an enterprise customer.
+        """
+        return """
+            WITH TopSkills AS (
+                -- Get top 10 skills by total completions
+                SELECT
+                    skill_name,
+                    SUM(completions) AS total_completion_count
+                FROM
+                    skills_daily_rollup_admin_dash
+                WHERE
+                    enterprise_customer_uuid=%(enterprise_customer_uuid)s
+                    AND date BETWEEN %(start_date)s AND %(end_date)s
+                GROUP BY
+                    skill_name
+                ORDER BY
+                    total_completion_count DESC
+                LIMIT 10
+            )
+            SELECT
+                sd.skill_name,
+                CASE
+                    WHEN sd.primary_subject_name IN (
+                        'business-management', 'computer-science',
+                        'data-analysis-statistics', 'engineering', 'communication'
+                    ) THEN sd.primary_subject_name
+                    ELSE 'other'
+                END AS subject_name,
+                SUM(sd.completions) AS count
+            FROM
+                skills_daily_rollup_admin_dash AS sd
+            JOIN
+                TopSkills AS ts ON sd.skill_name = ts.skill_name
+            WHERE
+                sd.enterprise_customer_uuid=%(enterprise_customer_uuid)s
+                AND date BETWEEN %(start_date)s AND %(end_date)s
+            GROUP BY
+                sd.skill_name, subject_name
+            ORDER BY
+                total_completion_count DESC;
+        """
+
+    @staticmethod
+    def get_skills_by_learning_hours(query_filters: QueryFilters, record_count: int = 25):
+        """
+        Get the query to fetch skills by learning hours for an enterprise customer.
+        """
+        return f"""
+            WITH filtered_data AS (
+                SELECT
+                    skill_name,
+                    total_learning_time_hours
+                FROM
+                    skills_daily_rollup_admin_dash
+                WHERE
+                    {query_filters.to_sql()}
+            )
+
+            SELECT
+                skill_name,
+                SUM(total_learning_time_hours) AS learning_hours
+            FROM
+                filtered_data
+            GROUP BY
+                skill_name
+            ORDER BY
+                learning_hours DESC
+            LIMIT {record_count};
+        """
