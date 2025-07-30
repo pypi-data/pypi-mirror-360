25b2f1e65f290751654d28631adc749d3a6ae2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:20 +0000

    Add package libtimecontrol/0.1.2/libtimecontrol-0.1.2-py3-none-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/libtimecontrol/libtimecontrol-0.1.2-py3-none-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/libtimecontrol/microbenchmark/cffi_api/build.py
@@ -0,0 +1,20 @@
+from cffi import FFI
+
+ffibuilder = FFI()
+
+ffibuilder.cdef(
+    """
+    void set_speedup(float speedup, int32_t channel);
+"""
+)
+
+ffibuilder.set_source(
+    "_time_control",
+    "void set_speedup(float speedup, int32_t channel);",
+    libraries=["time_controller"],
+    library_dirs=["lib"],
+    include_dirs=[],
+)
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
