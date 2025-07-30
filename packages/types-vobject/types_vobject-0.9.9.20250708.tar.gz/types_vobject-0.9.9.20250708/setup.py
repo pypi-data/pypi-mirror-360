b25774c5e943f5afb1d6bfdd3fb7aac78b1b08b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:13 +0000

    Add package types-vobject/0.9.9.20250708/types_vobject-0.9.9.20250708.tar.gz

---
+++ packages/types-vobject/types_vobject-0.9.9.20250708.tar.gz/types_vobject-0.9.9.20250708/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'vobject-stubs': ['__init__.pyi', 'base.pyi', 'behavior.pyi', 'change_tz.pyi', 'hcalendar.pyi', 'icalendar.pyi', 'ics_diff.pyi', 'vcard.pyi', 'win32tz.pyi', 'METADATA.toml', 'py.typed']})
