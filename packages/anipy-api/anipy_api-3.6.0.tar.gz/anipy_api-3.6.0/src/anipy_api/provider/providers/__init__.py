7b029a66fb265023017eafe4fe15c68c315b0a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:18 +0000

    Add package anipy-api/3.6.0/anipy_api-3.6.0.tar.gz

---
+++ packages/anipy-api/anipy_api-3.6.0.tar.gz/anipy_api-3.6.0/src/anipy_api/provider/providers/__init__.py
@@ -0,0 +1,4 @@
+from anipy_api.provider.providers.animekai_provider import AnimekaiProvider
+from anipy_api.provider.providers.allanime_provider import AllAnimeProvider
+
+__all__ = ["AnimekaiProvider", "AllAnimeProvider"]
