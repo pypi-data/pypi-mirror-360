9a5b9b2ab2a744ee651dc257872ea7c21edff672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:10 +0000

    Add package mockstack/0.10.0/mockstack-0.10.0.tar.gz

---
+++ packages/mockstack/mockstack-0.10.0.tar.gz/mockstack-0.10.0/mockstack/constants.py
@@ -0,0 +1,27 @@
+"""Constants for mockstack."""
+
+from enum import StrEnum
+
+
+ENV_PREFIX = "mockstack__"
+ENV_FILE = ".env"
+ENV_NESTED_DELIMITER = "__"
+
+# Template files are identified by a file:// URL prefix.
+PROXYRULES_FILE_TEMPLATE_PREFIX = "file:///"
+
+SENSITIVE_HEADERS = ["authorization", "cookie", "set-cookie"]
+
+
+class ProxyRulesRedirectVia(StrEnum):
+    """The type of redirect to use for the proxy rules strategy.
+
+    - HTTP_* type redirects are handled by using a Http redirect response.
+    - REVERSE_PROXY type redirects are handled by using a reverse proxy to the target URL
+        which is opaque to the client.
+
+    """
+
+    HTTP_TEMPORARY_REDIRECT = "http_307_temporary"
+    HTTP_PERMANENT_REDIRECT = "http_301_permanent"
+    REVERSE_PROXY = "reverse_proxy"
