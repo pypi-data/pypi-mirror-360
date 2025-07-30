d00125df1eac49bd1c72b3f6d8156994fcabd847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:14 +0000

    Add package flet-audio-recorder/0.2.0.dev55/flet_audio_recorder-0.2.0.dev55.tar.gz

---
+++ packages/flet-audio-recorder/flet_audio_recorder-0.2.0.dev55.tar.gz/flet_audio_recorder-0.2.0.dev55/src/flet_audio_recorder/__init__.py
@@ -0,0 +1,12 @@
+from .audio_recorder import AudioRecorder
+from .types import (
+    AndroidAudioSource,
+    AndroidRecorderConfiguration,
+    AudioEncoder,
+    AudioRecorderConfiguration,
+    AudioRecorderState,
+    AudioRecorderStateChangeEvent,
+    InputDevice,
+    IosAudioCategoryOption,
+    IosRecorderConfiguration,
+)
