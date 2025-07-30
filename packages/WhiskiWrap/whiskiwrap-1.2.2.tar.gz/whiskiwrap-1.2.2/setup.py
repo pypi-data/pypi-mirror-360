4527ba38735dfb3434a6144983450af9fd0c610b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:35 +0000

    Add package WhiskiWrap/1.2.2/whiskiwrap-1.2.2.tar.gz

---
+++ packages/WhiskiWrap/whiskiwrap-1.2.2.tar.gz/whiskiwrap-1.2.2/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup
+
+setup(
+   name='WhiskiWrap',
+   version='1.2.2',
+   author='Chris Rodgers',
+   author_email='',
+   maintainer=', '.join(['cxrodgers','aiporre','vncntprvst']),
+   packages=['WhiskiWrap','wwutils'],
+   # scripts=['bin/script1','bin/script2'],
+   url='http://pypi.python.org/pypi/WhiskiWrap/',
+   license='LICENSE.txt',
+   description='Whisk package wrapper created by cxrodgers',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   install_requires=[
+      'tables>=3.5.1',
+      'pandas',
+      'MediaInfo',
+      'future',
+      'tifffile',
+      'imagecodecs',
+      'statsmodels',
+      'ffmpeg-python>=0.2.0',
+      'whisk-janelia[ffmpeg]',
+      'zarr',
+      'pyarrow',
+      'matplotlib',
+      'easygui',
+      ],
+   python_requires='>=3.10',
+   include_package_data=True,
+)
