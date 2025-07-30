8c8613df0d2cd16a387cf3f30d587811281697c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:49 +0000

    Add package kaqing/0.2/kaqing-0.2.tar.gz

---
+++ packages/kaqing/kaqing-0.2.tar.gz/kaqing-0.2/walker/config.py
@@ -0,0 +1,65 @@
+from typing import TypeVar, cast
+import yaml
+
+from walker.utils import get_deep_keys, log2
+
+T = TypeVar('T')
+
+class Config:
+    # the singleton pattern
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'): cls.instance = super(Config, cls).__new__(cls)
+
+        return cls.instance
+
+    def __init__(self, path: str = None):
+        if path:
+            try:
+                with open(path) as f:
+                    self.params = cast(dict[str, any], yaml.safe_load(f))
+            except:
+                pass
+
+    def action_node_samples(self, action: str, default: T):
+        return self.get(f'{action}.samples', default)
+
+    def action_workers(self, action: str, default: T):
+        return self.get(f'{action}.workers', default)
+
+    def keys(self) -> list[str]:
+        return get_deep_keys(self.params)
+
+    def get(self, key: str, default: T) -> T:
+        # params['nodetool']['status']['max-nodes']
+        d = self.params
+        for p in key.split("."):
+            if p in d:
+                d = d[p]
+            else:
+                return default
+
+        return d
+
+    def set(self, key: str, v: str):
+        d = Config().params
+        ps = key.split('.')
+        for p in ps[:len(ps) - 1]:
+            if p in d:
+                d = d[p]
+            else:
+                log2(f'incorrect path: {key}')
+                return None
+
+        try:
+            v = int(v)
+        except:
+            pass
+
+        p = ps[len(ps) - 1]
+        if p in d:
+            d[p] = v
+        else:
+            log2(f'incorrect path: {key}')
+            return None
+
+        return v
