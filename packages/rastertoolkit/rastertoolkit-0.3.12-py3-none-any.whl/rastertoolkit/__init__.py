c497913709bd62b859e50ac5354d0b7c139fa6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:54 +0000

    Add package rastertoolkit/0.3.12/rastertoolkit-0.3.12-py3-none-any.whl

---
+++ packages/rastertoolkit/rastertoolkit-0.3.12-py3-none-any.whl/rastertoolkit/__init__.py
@@ -0,0 +1,12 @@
+from .version import __version__, __versiondate__, __license__  # noqa : F401
+from rastertoolkit.raster import raster_clip, raster_clip_weighted  # noqa : F401
+from rastertoolkit.shape import shape_subdivide  # noqa : F401
+
+
+# module level doc-string
+__doc__ = """
+**rastertoolkit** package provides features for extracting data from raster files using shapes/geometries as selectors.  
+"""
+
+# Use __all__ to let type checkers know what is part of the public API.
+_all_ = ["raster_clip", "raster_clip_weighted", "shape_subdivide"]
---
+++ packages/rastertoolkit/rastertoolkit-0.3.12-py3-none-any.whl/rastertoolkit/version.py
@@ -0,0 +1,9 @@
+"""
+Version and license information.
+"""
+
+__all__ = ["__version__", "__versiondate__", "__license__"]
+
+__version__ = "0.3.12"
+__versiondate__ = "2025-07-06"
+__license__ = f"rastertoolkit {__version__} ({__versiondate__}) — © 2022-2025 by IDM"
