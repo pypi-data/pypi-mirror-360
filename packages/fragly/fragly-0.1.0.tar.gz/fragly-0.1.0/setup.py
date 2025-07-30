db2b5b275274372696d750d17c98c80dad4bc7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:21 +0000

    Add package fragly/0.1.0/fragly-0.1.0.tar.gz

---
+++ packages/fragly/fragly-0.1.0.tar.gz/fragly-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fragly",
+    version="0.1.0",
+    description="Async Python client for your FraglyFragment API service",
+    author="Andrey",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.9.0",
+    ],
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
