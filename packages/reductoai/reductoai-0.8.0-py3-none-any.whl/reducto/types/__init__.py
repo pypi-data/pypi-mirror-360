7636f5fb0c69f87da557d92771e16911b405eb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:06 +0000

    Add package reductoai/0.8.0/reductoai-0.8.0-py3-none-any.whl

---
+++ packages/reductoai/reductoai-0.8.0-py3-none-any.whl/reducto/types/__init__.py
@@ -0,0 +1,31 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import (
+    Upload as Upload,
+    PageRange as PageRange,
+    ParseUsage as ParseUsage,
+    BoundingBox as BoundingBox,
+    ParseResponse as ParseResponse,
+    SplitCategory as SplitCategory,
+    SplitResponse as SplitResponse,
+    ExtractResponse as ExtractResponse,
+    WebhookConfigNew as WebhookConfigNew,
+    ArrayExtractConfig as ArrayExtractConfig,
+    BaseProcessingOptions as BaseProcessingOptions,
+    AdvancedProcessingOptions as AdvancedProcessingOptions,
+    ExperimentalProcessingOptions as ExperimentalProcessingOptions,
+)
+from .job_get_response import JobGetResponse as JobGetResponse
+from .parse_run_params import ParseRunParams as ParseRunParams
+from .split_run_params import SplitRunParams as SplitRunParams
+from .extract_run_params import ExtractRunParams as ExtractRunParams
+from .client_upload_params import ClientUploadParams as ClientUploadParams
+from .parse_run_job_params import ParseRunJobParams as ParseRunJobParams
+from .split_run_job_params import SplitRunJobParams as SplitRunJobParams
+from .webhook_run_response import WebhookRunResponse as WebhookRunResponse
+from .extract_run_job_params import ExtractRunJobParams as ExtractRunJobParams
+from .parse_run_job_response import ParseRunJobResponse as ParseRunJobResponse
+from .split_run_job_response import SplitRunJobResponse as SplitRunJobResponse
+from .extract_run_job_response import ExtractRunJobResponse as ExtractRunJobResponse
