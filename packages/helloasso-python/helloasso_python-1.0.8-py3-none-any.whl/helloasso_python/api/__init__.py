237713025b324583287a47332c9066193841314e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:10 +0000

    Add package helloasso-python/1.0.8/helloasso_python-1.0.8-py3-none-any.whl

---
+++ packages/helloasso-python/helloasso_python-1.0.8-py3-none-any.whl/helloasso_python/api/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+# import apis into api package
+from helloasso_python.api.annuaire_api import AnnuaireApi
+from helloasso_python.api.checkout_api import CheckoutApi
+from helloasso_python.api.commandes_api import CommandesApi
+from helloasso_python.api.formulaires_api import FormulairesApi
+from helloasso_python.api.liste_de_valeurs_api import ListeDeValeursApi
+from helloasso_python.api.organisation_api import OrganisationApi
+from helloasso_python.api.paiements_api import PaiementsApi
+from helloasso_python.api.partenaires_api import PartenairesApi
+from helloasso_python.api.reus_fiscaux_api import ReusFiscauxApi
+from helloasso_python.api.tags_api import TagsApi
+from helloasso_python.api.utilisateurs_api import UtilisateursApi
+
