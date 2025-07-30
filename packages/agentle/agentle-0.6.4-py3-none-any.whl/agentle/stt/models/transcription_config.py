d78550c9e6ebd9b933d5e8bc0bc625d2b1c3ccb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:45 +0000

    Add package agentle/0.6.4/agentle-0.6.4-py3-none-any.whl

---
+++ packages/agentle/agentle-0.6.4-py3-none-any.whl/agentle/stt/models/transcription_config.py
@@ -0,0 +1,26 @@
+from rsb.models.base_model import BaseModel
+from rsb.models.field import Field
+
+
+class TranscriptionConfig(BaseModel):
+    """Configuration for transcription."""
+
+    timeout: float = Field(
+        default=50.0,
+        description="The timeout for the transcription in seconds.",
+    )
+
+    language: str = Field(
+        default="en",
+        description="The language of the transcription.",
+    )
+
+    temperature: float = Field(
+        default=0.0,
+        description="The temperature for the transcription.",
+    )
+
+    context_prompt: str = Field(
+        default="",
+        description="A prompt to provide context for the transcription.",
+    )
