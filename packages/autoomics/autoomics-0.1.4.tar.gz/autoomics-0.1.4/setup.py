65e98c4c12471f398f03289878d062de38ad15db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:08 +0000

    Add package autoomics/0.1.4/autoomics-0.1.4.tar.gz

---
+++ packages/autoomics/autoomics-0.1.4.tar.gz/autoomics-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autoomics',  # Nom de ton package
+    version='0.1.4',     # Version du package
+    packages=find_packages(),
+    install_requires=open('requirements.txt').readlines(),
+    author='Jordan BABADOUDOU',
+    author_email='jordan.babadoudou@umontreal.ca',
+    description='Un outil pour faire du multi-omique analyse (bulk seq, rnaseq, chip seq, atac seq)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TonNom/mon_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'autoomics = autoomics.main:main',  # Cela appelle la fonction main de main.py
+        ],
+    },
+
+)
