d086245eb35e46e4448e3119005e9fcfd4c5cd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:10 +0000

    Add package griptape/1.7.5/griptape-1.7.5.tar.gz

---
+++ packages/griptape/griptape-1.7.5.tar.gz/griptape-1.7.5/griptape/drivers/web_scraper/proxy_web_scraper_driver.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import requests
+from attrs import Factory, define, field
+
+from griptape.artifacts import TextArtifact
+from griptape.drivers.web_scraper import BaseWebScraperDriver
+
+
+@define
+class ProxyWebScraperDriver(BaseWebScraperDriver):
+    proxies: dict = field(kw_only=True, metadata={"serializable": False})
+    params: dict = field(default=Factory(dict), kw_only=True, metadata={"serializable": True})
+
+    def fetch_url(self, url: str) -> str:
+        response = requests.get(url, proxies=self.proxies, **self.params)
+
+        return response.text
+
+    def extract_page(self, page: str) -> TextArtifact:
+        return TextArtifact(page)
