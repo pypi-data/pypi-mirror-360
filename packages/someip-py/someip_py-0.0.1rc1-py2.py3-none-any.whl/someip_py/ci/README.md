54152b5b2e751a925b734c35cfe50d4d2fe5f539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:24 +0000

    Add package someip-py/0.0.1rc1/someip_py-0.0.1rc1-py2.py3-none-any.whl

---
+++ packages/someip-py/someip_py-0.0.1rc1-py2.py3-none-any.whl/someip_py/ci/README.md
@@ -0,0 +1,3 @@
+# SOME/IP-PY
+
+someip-py is Scalable Service-Oriented MiddlewarE over IP python Implment.
---
+++ packages/someip-py/someip_py-0.0.1rc1-py2.py3-none-any.whl/someip_py/cli.py
@@ -0,0 +1,48 @@
+import json
+
+import click
+
+from someip_py import SOMEIPService, __version__
+
+
+@click.version_option(version=__version__)
+@click.group(
+    context_settings=dict(help_option_names=["-h", "--help"]),
+)
+def main():
+    pass
+
+
+@main.command(name="decode")
+@click.option("-p", "--platform", help="the project platform", default="V2x")
+@click.option("-i", "--idl", help="someip service matrix path", default=None)
+@click.option("-s", "--service_id", help="service id", required=True)
+@click.option("-m", "--method_id", help="method id", required=True)
+@click.option("-t", "--message_type", type=int, help="method id", default=2)
+@click.option("-P", "--payload", help="method id", required=True)
+def decode(platform, idl, service_id, method_id, message_type, payload):
+    """decode some/ip message"""
+    s = SOMEIPService(platform=platform, arxml_path=idl)
+    click.echo(
+        click.style(
+            json.dumps(
+                s.decode_payload(
+                    service_id, method_id, message_type=message_type, payload=payload
+                ),
+                indent=4,
+            ),
+            fg="cyan",
+            bold=True,
+        )
+    )
+
+
+@main.command(name="parse")
+@click.option("-p", "--platform", help="the project platform", default="V2x")
+@click.option("-i", "--idl", help="someip service matrix path", default=None)
+@click.option("-f", "--file", help="pcap file path", required=True)
+@click.option("-o", "--output", help="output dir", default=None)
+def parse(platform, idl, file, output):
+    """parse pcap"""
+    s = SOMEIPService(platform=platform, arxml_path=idl)
+    s.parse_pcap(file, output=output)
