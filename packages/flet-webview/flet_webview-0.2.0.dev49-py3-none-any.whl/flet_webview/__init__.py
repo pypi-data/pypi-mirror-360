7375f10dee9273567c7643ce9ef5160252d47d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:19 +0000

    Add package flet-webview/0.2.0.dev49/flet_webview-0.2.0.dev49-py3-none-any.whl

---
+++ packages/flet-webview/flet_webview-0.2.0.dev49-py3-none-any.whl/flet_webview/__init__.py
@@ -0,0 +1,8 @@
+from .types import (
+    LogLevelSeverity,
+    RequestMethod,
+    WebViewConsoleMessageEvent,
+    WebViewJavaScriptEvent,
+    WebViewScrollEvent,
+)
+from .webview import WebView
