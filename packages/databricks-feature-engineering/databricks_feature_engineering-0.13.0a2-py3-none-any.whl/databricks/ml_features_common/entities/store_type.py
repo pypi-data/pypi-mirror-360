289de1d1c8b468fa0427aac95666e5bf675e9287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:07 +0000

    Add package databricks-feature-engineering/0.13.0a2/databricks_feature_engineering-0.13.0a2-py3-none-any.whl

---
+++ packages/databricks-feature-engineering/databricks_feature_engineering-0.13.0a2-py3-none-any.whl/databricks/ml_features_common/entities/store_type.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from databricks.ml_features_common.entities._proto_enum_entity import _ProtoEnumEntity
+from databricks.ml_features_common.protos.feature_store_serving_pb2 import (
+    StoreType as ProtoStoreType,
+)
+
+
+class StoreType(_ProtoEnumEntity):
+    """Online store types."""
+
+    AURORA_MYSQL = ProtoStoreType.Value("AURORA_MYSQL")
+    SQL_SERVER = ProtoStoreType.Value("SQL_SERVER")
+    MYSQL = ProtoStoreType.Value("MYSQL")
+    DYNAMODB = ProtoStoreType.Value("DYNAMODB")
+    COSMOSDB = ProtoStoreType.Value("COSMOSDB")
+    BRICKSTORE = ProtoStoreType.Value("BRICKSTORE")
+
+    @classmethod
+    def _enum_type(cls) -> Any:
+        return ProtoStoreType
