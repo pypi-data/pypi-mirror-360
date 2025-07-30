23383074c459fdccd6eb1f03a32b91ee0b266be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:16 +0000

    Add package xhsmcpqhzy/0.1.6/xhsmcpqhzy-0.1.6-py3-none-any.whl

---
+++ packages/xhsmcpqhzy/xhsmcpqhzy-0.1.6-py3-none-any.whl/xhsmcpqhzy/__init__.py
@@ -0,0 +1,14 @@
+import argparse
+from .xhsmcpqhzy import mcp
+
+def main():
+    """MCP Wiki kel: Read Wikipedia articles and convert them to Markdown."""
+    parser = argparse.ArgumentParser(
+        description="Gives you the ability to read Wikipedia articles and convert them to Markdown."
+    )
+    parser.parse_args()
+    mcp.run()
+
+if __name__ == "__main__":
+    main()
+
