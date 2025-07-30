0ac340bda7fc49b343e966be1b130911ac77be3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:20 +0000

    Add package ResistorEnDeCode/1.2.1/resistorendecode-1.2.1.tar.gz

---
+++ packages/ResistorEnDeCode/resistorendecode-1.2.1.tar.gz/resistorendecode-1.2.1/ResistorEnDeCode/src/main.py
@@ -0,0 +1,23 @@
+##!/bin/python3
+import os
+
+import sys
+import PySide6.QtWidgets as Qw
+import PySide6.QtCore as Qc
+from resistance_calc import ResistanceCalc
+
+
+def main():
+    Qw.QApplication.setAttribute(Qc.Qt.AA_EnableHighDpiScaling, True)
+    Qw.QApplication.setAttribute(Qc.Qt.AA_UseHighDpiPixmaps, True)
+
+    app = Qw.QApplication(sys.argv)
+
+    window = ResistanceCalc()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
