27062300cf93d9db5cd4e52dae92b65b32441812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:20 +0000

    Add package flet-ads/0.2.0.dev37/flet_ads-0.2.0.dev37-py3-none-any.whl

---
+++ packages/flet-ads/flet_ads-0.2.0.dev37-py3-none-any.whl/flet_ads/__init__.py
@@ -0,0 +1,10 @@
+from .banner_ad import BannerAd
+from .base_ad import BaseAd
+from .interstitial_ad import InterstitialAd
+from .types import (  # NativeAdTemplateStyle,; NativeAdTemplateTextStyle,; NativeAdTemplateType,; NativeTemplateFontStyle,
+    AdRequest,
+    PaidAdEvent,
+    PrecisionType,
+)
+
+# from .native_ad import NativeAd
