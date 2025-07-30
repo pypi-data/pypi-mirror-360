848b3b93b371ed21b0ede91ede102792718ac13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:11:18 +0000

    Add package fastapi-di-chain/0.0.3/fastapi_di_chain-0.0.3-py3-none-any.whl

---
+++ packages/fastapi-di-chain/fastapi_di_chain-0.0.3-py3-none-any.whl/fastapi_di_chain/utils.py
@@ -0,0 +1,26 @@
+import inspect
+from collections.abc import Callable
+from typing import Any
+
+
+def is_coroutine_callable(call: Callable[..., Any]) -> bool:
+    if inspect.isroutine(call):
+        return inspect.iscoroutinefunction(call)
+    if inspect.isclass(call):
+        return False
+    dunder_call = getattr(call, "__call__", None)  # noqa: B004
+    return inspect.iscoroutinefunction(dunder_call)
+
+
+def is_async_gen_callable(call: Callable[..., Any]) -> bool:
+    if inspect.isasyncgenfunction(call):
+        return True
+    dunder_call = getattr(call, "__call__", None)  # noqa: B004
+    return inspect.isasyncgenfunction(dunder_call)
+
+
+def is_gen_callable(call: Callable[..., Any]) -> bool:
+    if inspect.isgeneratorfunction(call):
+        return True
+    dunder_call = getattr(call, "__call__", None)  # noqa: B004
+    return inspect.isgeneratorfunction(dunder_call)
