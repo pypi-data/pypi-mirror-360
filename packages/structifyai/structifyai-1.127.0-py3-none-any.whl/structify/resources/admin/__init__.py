b81b27f2f412dae13e477b75c3a3889358467aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:35 +0000

    Add package structifyai/1.127.0/structifyai-1.127.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.127.0-py3-none-any.whl/structify/resources/admin/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .admin import (
+    AdminResource,
+    AsyncAdminResource,
+    AdminResourceWithRawResponse,
+    AsyncAdminResourceWithRawResponse,
+    AdminResourceWithStreamingResponse,
+    AsyncAdminResourceWithStreamingResponse,
+)
+from .users import (
+    UsersResource,
+    AsyncUsersResource,
+    UsersResourceWithRawResponse,
+    AsyncUsersResourceWithRawResponse,
+    UsersResourceWithStreamingResponse,
+    AsyncUsersResourceWithStreamingResponse,
+)
+from .human_llm import (
+    HumanLlmResource,
+    AsyncHumanLlmResource,
+    HumanLlmResourceWithRawResponse,
+    AsyncHumanLlmResourceWithRawResponse,
+    HumanLlmResourceWithStreamingResponse,
+    AsyncHumanLlmResourceWithStreamingResponse,
+)
+from .next_action import (
+    NextActionResource,
+    AsyncNextActionResource,
+    NextActionResourceWithRawResponse,
+    AsyncNextActionResourceWithRawResponse,
+    NextActionResourceWithStreamingResponse,
+    AsyncNextActionResourceWithStreamingResponse,
+)
+from .training_datasets import (
+    TrainingDatasetsResource,
+    AsyncTrainingDatasetsResource,
+    TrainingDatasetsResourceWithRawResponse,
+    AsyncTrainingDatasetsResourceWithRawResponse,
+    TrainingDatasetsResourceWithStreamingResponse,
+    AsyncTrainingDatasetsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "HumanLlmResource",
+    "AsyncHumanLlmResource",
+    "HumanLlmResourceWithRawResponse",
+    "AsyncHumanLlmResourceWithRawResponse",
+    "HumanLlmResourceWithStreamingResponse",
+    "AsyncHumanLlmResourceWithStreamingResponse",
+    "NextActionResource",
+    "AsyncNextActionResource",
+    "NextActionResourceWithRawResponse",
+    "AsyncNextActionResourceWithRawResponse",
+    "NextActionResourceWithStreamingResponse",
+    "AsyncNextActionResourceWithStreamingResponse",
+    "UsersResource",
+    "AsyncUsersResource",
+    "UsersResourceWithRawResponse",
+    "AsyncUsersResourceWithRawResponse",
+    "UsersResourceWithStreamingResponse",
+    "AsyncUsersResourceWithStreamingResponse",
+    "TrainingDatasetsResource",
+    "AsyncTrainingDatasetsResource",
+    "TrainingDatasetsResourceWithRawResponse",
+    "AsyncTrainingDatasetsResourceWithRawResponse",
+    "TrainingDatasetsResourceWithStreamingResponse",
+    "AsyncTrainingDatasetsResourceWithStreamingResponse",
+    "AdminResource",
+    "AsyncAdminResource",
+    "AdminResourceWithRawResponse",
+    "AsyncAdminResourceWithRawResponse",
+    "AdminResourceWithStreamingResponse",
+    "AsyncAdminResourceWithStreamingResponse",
+]
---
+++ packages/structifyai/structifyai-1.127.0-py3-none-any.whl/structify/types/chat_session_with_messages.py
@@ -0,0 +1,38 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+from datetime import datetime
+
+from .._models import BaseModel
+
+__all__ = ["ChatSessionWithMessages", "Message"]
+
+
+class Message(BaseModel):
+    id: str
+
+    chat_session_id: str
+
+    content: str
+
+    created_at: datetime
+
+    role: str
+
+    timestamp: datetime
+
+
+class ChatSessionWithMessages(BaseModel):
+    id: str
+
+    created_at: datetime
+
+    git_repo_id: str
+
+    messages: List[Message]
+
+    project_id: str
+
+    updated_at: datetime
+
+    user_id: str
