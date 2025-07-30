08f5004793b64990e21c09e8e6ae10b995494f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:10 +0000

    Add package timestamped-for-f1-historical-api/0.0.3/timestamped_for_f1_historical_api-0.0.3-py3-none-any.whl

---
+++ packages/timestamped-for-f1-historical-api/timestamped_for_f1_historical_api-0.0.3-py3-none-any.whl/timestamped_for_f1_historical_api/api/v1/race_control/services.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from timestamped_for_f1_historical_api.api.v1.race_control.models import RaceControl
+from timestamped_for_f1_historical_api.api.v1.event.models import Event
+from timestamped_for_f1_historical_api.core.db import AsyncSession, select
+
+
+async def get(db_session: AsyncSession, event_id: int) -> RaceControl | None:
+    """
+    Returns a pit with the given event id and date, or None if the pit does not exist.
+    """
+    
+    return (
+        await db_session.execute(
+            select(RaceControl)
+            .select_from(RaceControl)
+            .join(Event, RaceControl.event_id == Event.id)
+            .filter(RaceControl.event_id == event_id)
+        )
+    ).scalars().one_or_none()
