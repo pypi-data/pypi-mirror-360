9b7f42bb0768a33f518d339c7c2183583c1c7212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:21 +0000

    Add package reasoner-pydantic/5.1.1/reasoner_pydantic-5.1.1-py3-none-any.whl

---
+++ packages/reasoner-pydantic/reasoner_pydantic-5.1.1-py3-none-any.whl/reasoner_pydantic/base_model.py
@@ -0,0 +1,40 @@
+import json
+
+from pydantic import BaseModel as PydanticBaseModel
+
+from pydantic.class_validators import root_validator
+from .utils import make_hashable
+
+
+class BaseModel(PydanticBaseModel):
+    """
+    Custom base model for all classes
+
+    This provides hash and equality methods.
+    """
+
+    def __hash__(self) -> int:
+        """Hash function based on Pydantic implementation"""
+        return hash((self.__class__, tuple(self.__dict__.values())))
+
+    def __eq__(self, other) -> bool:
+        """Equality function that calls hash function"""
+        return self.__hash__() == other.__hash__()
+
+    def update(self, other):
+        """Update fields on this object with fields from other object"""
+        raise NotImplementedError(
+            f"Model {self.__class__.__name__} has no update method"
+        )
+
+    def get_field(self, field):
+        getattr(self, field, None)
+
+    # After running validation on all known properties, make sure everything else is hashable
+    @root_validator(allow_reuse=True, pre=False)
+    def make_hashable_root(cls, values):
+        # The root validator must take and return a dict
+        return {k: make_hashable(v) for k, v in values.items()}
+
+    def to_dict(self) -> dict:
+        return json.loads(self.json())
