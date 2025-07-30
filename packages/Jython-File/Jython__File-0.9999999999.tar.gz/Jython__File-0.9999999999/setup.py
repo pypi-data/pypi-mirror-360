9c7ec1a2aba7d73c6497e7491a5080b7967641ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:20 +0000

    Add package Jython-File/0.9999999999/Jython__File-0.9999999999.tar.gz

---
+++ packages/Jython-File/Jython__File-0.9999999999.tar.gz/Jython__File-0.9999999999/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Jython__File',
+    version='0.9999999999',
+    packages=find_packages(),
+    author='D',
+    author_email='nasr2python@gmail.com',
+    description='What',
+    include_package_data=True,
+    package_data={
+        'java_code': ['*.so'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
