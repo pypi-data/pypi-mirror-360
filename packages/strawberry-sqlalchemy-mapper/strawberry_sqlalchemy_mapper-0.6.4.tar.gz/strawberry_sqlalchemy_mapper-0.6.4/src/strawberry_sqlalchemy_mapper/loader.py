85ed9fed37530a400fff5a4ce4efa51ebc88525f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:04 +0000

    Add package strawberry-sqlalchemy-mapper/0.6.4/strawberry_sqlalchemy_mapper-0.6.4.tar.gz

---
+++ packages/strawberry-sqlalchemy-mapper/strawberry_sqlalchemy_mapper-0.6.4.tar.gz/strawberry_sqlalchemy_mapper-0.6.4/src/strawberry_sqlalchemy_mapper/loader.py
@@ -0,0 +1,93 @@
+import logging
+from collections import defaultdict
+from typing import (
+    Any,
+    AsyncContextManager,
+    Callable,
+    Dict,
+    List,
+    Mapping,
+    Optional,
+    Tuple,
+    Union,
+)
+
+from sqlalchemy import select, tuple_
+from sqlalchemy.engine.base import Connection
+from sqlalchemy.ext.asyncio import AsyncConnection, AsyncSession
+from sqlalchemy.orm import RelationshipProperty, Session
+from strawberry.dataloader import DataLoader
+
+
+class StrawberrySQLAlchemyLoader:
+    """
+    Creates DataLoader instances on-the-fly for SQLAlchemy relationships
+    """
+
+    _loaders: Dict[RelationshipProperty, DataLoader]
+
+    def __init__(
+        self,
+        bind: Union[Session, Connection, None] = None,
+        async_bind_factory: Optional[
+            Union[
+                Callable[[], AsyncContextManager[AsyncSession]],
+                Callable[[], AsyncContextManager[AsyncConnection]],
+            ]
+        ] = None,
+    ) -> None:
+        self._loaders = {}
+        self._bind = bind
+        self._async_bind_factory = async_bind_factory
+        self._logger = logging.getLogger("strawberry_sqlalchemy_mapper")
+        if bind is None and async_bind_factory is None:
+            self._logger.warning(
+                "One of bind or async_bind_factory must be set for loader to function properly."
+            )
+
+    async def _scalars_all(self, *args, **kwargs):
+        if self._async_bind_factory:
+            async with self._async_bind_factory() as bind:
+                return (await bind.scalars(*args, **kwargs)).all()
+        else:
+            assert self._bind is not None
+            return self._bind.scalars(*args, **kwargs).all()
+
+    def loader_for(self, relationship: RelationshipProperty) -> DataLoader:
+        """
+        Retrieve or create a DataLoader for the given relationship
+        """
+        try:
+            return self._loaders[relationship]
+        except KeyError:
+            related_model = relationship.entity.entity
+
+            async def load_fn(keys: List[Tuple]) -> List[Any]:
+                query = select(related_model).filter(
+                    tuple_(*[remote for _, remote in relationship.local_remote_pairs or []]).in_(
+                        keys
+                    )
+                )
+                if relationship.order_by:
+                    query = query.order_by(*relationship.order_by)
+                rows = await self._scalars_all(query)
+
+                def group_by_remote_key(row: Any) -> Tuple:
+                    return tuple(
+                        [
+                            getattr(row, remote.key)
+                            for _, remote in relationship.local_remote_pairs or []
+                            if remote.key
+                        ]
+                    )
+
+                grouped_keys: Mapping[Tuple, List[Any]] = defaultdict(list)
+                for row in rows:
+                    grouped_keys[group_by_remote_key(row)].append(row)
+                if relationship.uselist:
+                    return [grouped_keys[key] for key in keys]
+                else:
+                    return [grouped_keys[key][0] if grouped_keys[key] else None for key in keys]
+
+            self._loaders[relationship] = DataLoader(load_fn=load_fn)
+            return self._loaders[relationship]
