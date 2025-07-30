4ba65d8e26e01e33b90bd5da299b5552e4998514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:26 +0000

    Add package TexIV/0.1.6/texiv-0.1.6-py3-none-any.whl

---
+++ packages/TexIV/texiv-0.1.6-py3-none-any.whl/texiv/stata/__init__.py
@@ -0,0 +1,32 @@
+import asyncio
+from typing import List
+
+from ..core import TexIV
+
+
+class StataTexIV:
+    @staticmethod
+    def texiv(Data,
+              varname: str,
+              kws: str,
+              is_async: bool = True):
+        texiv = TexIV(is_async=is_async)
+        contents: List[str] = Data.get(varname)
+        if is_async:
+            freqs, counts, rates = asyncio.run(
+                texiv.async_texiv_stata(contents, kws)
+            )
+        else:
+            freqs, counts, rates = texiv.texiv_stata(contents, kws)
+
+        true_count_varname = f"{varname}_freq"
+        total_count_varname = f"{varname}_count"
+        rate_varname = f"{varname}_rate"
+
+        Data.addVarInt(true_count_varname)
+        Data.addVarInt(total_count_varname)
+        Data.addVarFloat(rate_varname)
+
+        Data.store(true_count_varname, None, freqs)
+        Data.store(total_count_varname, None, counts)
+        Data.store(rate_varname, None, rates)
