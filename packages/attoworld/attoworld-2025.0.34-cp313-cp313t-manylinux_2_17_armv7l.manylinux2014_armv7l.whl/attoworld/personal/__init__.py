ed629ce7fecca411654e0f5bb8a6a38858a6a58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:32 +0000

    Add package attoworld/2025.0.34/attoworld-2025.0.34-cp313-cp313t-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/attoworld/attoworld-2025.0.34-cp313-cp313t-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/attoworld/personal/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module will contain personal modules from our coworkers, allowing for easier sharing
+of data processing files that utilize functions that might not otherwise be of general use.
+"""
+
+from . import nick
+from . import vlad
+from . import marco
+
+__all__ = ["nick", "vlad", "marco"]
